0b9699ca211695679e7d79ac569258f841dae6e6
James Wilcox <wilcoxjay@gmail.com>
2013-12-12 12:50:34 -0800

    typo in logistic2.rkt

--- bench/logistic2.rkt
+++ bench/logistic2.rkt
@@ -5,7 +5,7 @@
 (casio-test (x y)
   "Akshay's logistic regression example 2"
 
-  (- (log (1+ (exp x))) (* x y))
+  (- (log (+ 1 (exp x))) (* x y))
 
   (if (<= x 0) ;;cases are flipped.
       (- (log (1+ (exp x))) (* x y))
