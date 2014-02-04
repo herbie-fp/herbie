8d3344b8c78d7dd478550ab9503bf1fc5a1b2947
Pavel Panchekha <me@pavpanchekha.com>
2014-02-03 18:29:55 -0800

    Rename my-match to pattern-match, clean it up
    + Don't use unquote for pattern variables (use raw symbols)
    + Fail by returning #f
    + Better documentation

--- casio/my-match.rkt
+++ casio/my-match.rkt
@@ -1,6 +1,16 @@
 #lang racket
 
-(define (my-match pattern expr)
+;; Our own pattern matcher.
+;
+; The racket (match) macro doesn't give us access to the bindings made
+; by the matcher, so we wrote our own.
+;
+; The syntax is simple:
+;   numbers are literals ; symbols are variables ; lists are expressions
+;
+; Bindings are stored as association lists
+
+(define (pattern-match pattern expr)
   ; pattern expr -> bindings
 
   (define (merge . bindings)
@@ -9,44 +19,38 @@
 
   (define (merge2 binding1 binding2)
     ; binding binding -> binding
-    (let loop ([acc binding1] [rest binding2])
-      (if (null? rest)
-          acc
-          (let* ([curr (car rest)]
-                 [lookup (assoc (car curr) acc)])
-            (if lookup
-                (if (equal? (cdr lookup) (cdr curr))
-                    (loop acc (cdr rest))
-                    (fail "my-match: Variable has two different bindings"
-                          (car curr) (cdr lookup) (cdr curr)))
-                (loop (cons curr acc) (cdr rest)))))))
+    (if (and binding1 binding2)
+        (let loop ([acc binding1] [rest binding2])
+          (if (null? rest)
+              acc
+              (let* ([curr (car rest)]
+                     [lookup (assoc (car curr) acc)])
+                (if lookup
+                    (if (equal? (cdr lookup) (cdr curr))
+                        (loop acc (cdr rest))
+                        (fail "pattern-match: Variable has two different bindings"
+                              (car curr) (cdr lookup) (cdr curr)))
+                    (loop (cons curr acc) (cdr rest))))))
+        #f))
 
-  (define (fail . irr)
-    (print irr)
-    (/ 0 0))
-
-  ; patterns:
-  ; ,a -> variable "a"
-  ; numbers -> literals
-  ; (op _ _ _) -> a list with _ being recursively patterns
+  (define (fail . irr) #f)
 
   (cond
    [(number? pattern)
     (if (and (number? expr) (= pattern expr))
         '()
-        (fail "my-match: Literals do not match"
+        (fail "pattern-match: Literals do not match"
               pattern expr))]
-   [(and (list? pattern) (eq? (car pattern) 'unquote))
-    (let ([var (cadr pattern)])
-      (list (cons var expr)))]
+   [(symbol? pattern)
+    (list (cons pattern expr))]
    ; TODO : test for allowed operators
-   [(and (list? pattern) (> (length pattern) 1))
+   [(list? pattern)
     (if (and (list? expr) (eq? (car expr) (car pattern))
              (= (length expr) (length pattern)))
         (apply merge
          (for/list ([pat (cdr pattern)] [subterm (cdr expr)])
-           (my-match pat subterm)))
-        (fail "my-match: Not a list, or wrong length, or wrong operator."
+           (pattern-match pat subterm)))
+        (fail "pattern-match: Not a list, or wrong length, or wrong operator."
               "Don't ask me, I don't know!"
               pattern expr))]
-   [#t (fail "my-match: Confused by pattern term" pattern)]))
+   [#t (fail "pattern-match: Confused by pattern term" pattern)]))
