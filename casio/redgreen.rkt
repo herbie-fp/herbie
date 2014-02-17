d50a8123f44822129fca1d9d4fb734d7e7240a3c
Pavel Panchekha <me@pavpanchekha.com>
2014-02-16 17:06:08 -0800

    Merged in both the brute force and the analyze-subexpressions work.

--- casio/redgreen.rkt
+++ casio/redgreen.rkt
@@ -1,19 +1,20 @@
 #lang racket
 
 (require casio/points)
+(require casio/alternative)
 
 (provide green? remove-red green-threshold)
 
-(define green-threshold (make-parameter 50))
+(define green-threshold (make-parameter 0))
 
 (define (error-sum alt)
-  (apply + (change-errors alt)))
+  (apply + (alt-errors alt)))
 
-(define (green-tipped? alt)
-  (or (alt-prev alt) ; The initial alternative is green-tipped by convention
+(define (green? alt)
+  (and (alt-prev alt) ; The initial alternative is not green-tipped by convention
        (< (green-threshold)
           (- (error-sum alt)
-             (error-sum (alt-prev alt)))))
+             (error-sum (alt-prev alt))))))
 
 ;; Eventually this should return an alternative with red changes undone.
 (define (remove-red alternative)
