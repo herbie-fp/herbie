0131bdb1ca7878db08e1ff00fa1a480d77b24462
Alex Sanchez-Stern <alex.sanchezstern@hotmail.com>
2014-02-20 15:18:21 -0800

    Fixed a small bug and refactored
    Also added some comments to make the code easier to understand.

--- casio/redgreen.rkt
+++ casio/redgreen.rkt
@@ -44,12 +44,19 @@
       (remove-until-green (swim-upstream altn))
       altn))
 
+;;Simple location match utility function. If 'a' is a continutation of 'b',
+;;such as in a='(cdr cdr car cdr car) b='(cdr cdr car), returns the tail of
+;;'a' after 'b', '(cdr car). Visa-versa for 'b' as a continuation of 'a'. If
+;;'a' and 'b' diverge at some point before the end, returns false.
+(define (match-loc a b)
+  (cond [(null? a) b]
+	[(null? b) a]
+	[(eq? (car a) (car b)) (match-loc (cdr a) (cdr b))]
+	[#t #f]))
+
 (define (translated-up start cur-change prev-change)
-  (define (match-loc a b)
-    (cond [(null? a) b]
-	  [(null? b) a]
-	  [(eq? (car a) (car b)) (match-loc (cdr a) (cdr b))]
-	  [#t #f]))
+  ;;Translates locations for rules that have one to one variable bindings,
+  ;;as in each variable only appears once in the input and once in the output.
   (define (simple-translate-loc post-rel-loc translations)
     (if (null? translations)
 	#f
@@ -59,37 +66,35 @@
 	      (simple-translate-loc post-rel-loc (cdr translations))))))
 	
   (if (orthogonal? cur-change prev-change)
-      cur-change
+      cur-change ;If the changes are orthogonal, we don't need to transform the change to move it up.
       (let ([translations (rule-location-translations (change-rule (prev-change)))])
 	(if (apply andmap (map (lambda (x)
 				 (and (= 1 (length (car x)))
 				      (= 1 (length (cadr x)))))
-			       translations))
+			       translations)) ;Test if the rule has one to one variable bindings (See simple-translate-loc)
 	    (let ([new-rel-loc (simple-translate-loc (match-loc (change-location prev-change)
 								(change-location cur-change))
 						     translations)])
-	      (if new-rel-loc
-		  (change (change-rule cur-change)
-			  (append (change-location prev-change) new-rel-loc)
-			  (pattern-match (rule-input (change-rule cur-change))
+	      (if new-rel-loc ;simple-translate-loc could still fail if one change is not within the bindings of the other.
+		  (change (change-rule cur-change) ;rule of our change hasn't changed.
+			  (append (change-location prev-change) new-rel-loc) ;If new-rel-loc isn't null, we need to add it to the
+			                                                     ;end of our location.
+			  (pattern-match (rule-input (change-rule cur-change)) ;;Get new bindings in case they've changed.
 					 (location-get (change-location cur-change)
 						       (alt-program start))))
 		  #f))
-	    #f))))
+	    #f)))) ;Support for this case could be added in the future, it's just a little more complex and won't always work.
 	    
 (define (translated-down start cur-change next-change)
   (if (orthogonal? cur-change next-change)
       cur-change
       #f))
 
+;;Returns whether or not two changes are orthogonal.
+;;Orthogonal changes can be rearranged without changing
+;;any of the characteristics of the change object.
 (define (orthogonal? change-a change-b)
-  (define (loc-match? loca locb)
-    (if (or (null? loca) (null? locb))
-	#t
-	(if (eq? (car loca) (car locb))
-	    (loc-match? (cdr loca) (cdr locb))
-	    #f)))
-  (not (loc-match? (change-location change-a) (change-location change-b))))
+  (not (match-loc (change-location change-a) (change-location change-b))))
 
 (define (rule-location-translations rule)
   (define (var-locs pattern loc)
@@ -102,7 +107,7 @@
 							  '(car))))
 				    (cdr pattern)))]
      [(number? pattern) '()]
-     [(symbol? pattern) (list (cons pattern loc))]
+     [(symbol? pattern) (list (cons pattern (list loc)))]
      [#t (error "Improper rule: " rule)]))
   (let ([in-locs (var-locs (rule-input rule) '())]
 	[out-locs (var-locs (rule-output rule) '())])
@@ -117,7 +122,7 @@
 	(a-append (cdr joe) (cons (cons (caar joe)
 					(let ([match (assoc (caar joe) bob)])
 					  (if match
-					      (list (cdr match) (cdar joe))
+					      (append (cdr match) (cdar joe))
 					      (cdar joe))))
 				  bob))))
   (if (< 2 (length args))
