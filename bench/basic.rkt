e3e86c85d724c8815f5aefa5fe13dddcf25fe2a3
Pavel Panchekha <me@pavpanchekha.com>
2014-02-26 23:36:35 -0800

    Added a set of very basic tests for reassociations and polynomial manipulation.

---
+++ bench/basic.rkt
@@ -0,0 +1,33 @@
+#lang racket
+(require casio/test)
+
+; Some tests on Casio's ability to reassociate and cancel
+(casio-test (x y)
+  "Canceling after commutation"
+  (- (+ x y) (+ y x))
+  0)
+
+(casio-test (x)
+  "Canceling after reassociation"
+  (- (+ 1 x) x)
+  1)
+
+(casio-test (x)
+  "Canceling after reassociation and commutation"
+  (- (+ x 1) x)
+  1)
+
+(casio-test (x y z)
+  "Canceling after commutation and reassociation"
+  (- (+ (+ x y) z) (+ x (+ y z)))
+  0)
+
+(casio-test (a)
+  "Reducing (a+1)^2 - 1"
+  (- (square (+ a 1)) 1)
+  (* a (+ a 2)))
+
+(casio-test (a b)
+  "Reducing a^2 - b^2"
+  (- (square a) (square b))
+  (* (+ a b) (- a b)))
