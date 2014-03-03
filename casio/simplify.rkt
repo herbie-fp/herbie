32ccbe7c420529b9434fbdac6199c501ad22b154
Alex Sanchez-Stern <alex.sanchezstern@hotmail.com>
2014-03-02 21:58:17 -0800

    Added step 1 of simplification
    simply removes instances of a function wrapped around it's inverse. I
    maintained a seperate list of functions and their inverses. In the
    future, this could also be accomplished by marking some of the rules as
    "simplifying rules", rules like (log (exp x)) -> x, but that would
    involve adding another field to the struct. This could also possibly
    help with tighter rule integration in the future, if we chose to pursue
    it. Anyway, this way works fine for now.

--- casio/simplify.rkt
+++ casio/simplify.rkt
@@ -4,6 +4,7 @@
 (require casio/alternative)
 (require casio/programs)
 (require casio/points)
+(require casio/common)
 
 (provide simplify)
 
@@ -43,7 +44,7 @@
       (apply-changes altn simplifying-changes))))
 
 (define (simplify-expression expr)
-  expr) ;TODO
+  (rm-fns-&-invs expr))
 
 (define (get-contained-vars expr)
   (define (get-duplicated-vars expr)
@@ -52,3 +53,29 @@
 	  [(list? expr) (map append (map get-contained-vars
 					 (cdr expr)))]))
   (remove-duplicates (get-duplicated-vars expr)))
+
+(define func-inverses
+  '((exp . log)
+    (square . sqrt)))
+
+(define (rm-fns-&-invs prog)
+  (define (rm-fn-&-inv expr)
+    (when (*debug*) (println "Attempting to remove functions with their inverses from: " expr))
+    (define (reverse-alist l)
+      (map (lambda (pair) (cons (cdr pair) (car pair)))
+	   l))
+    (if (pair? expr)
+	(let* ([all-invs (append func-inverses (reverse-alist func-inverses))]
+	       [inv (let ([item (assoc (car expr) all-invs)]) (when item (cdr item)))])
+	  (if (and inv (pair? (cadr expr)) (eq? (caadr expr) inv))
+	      (cadadr expr)
+	      expr))
+	expr))
+  (for-lists prog rm-fn-&-inv))
+
+(define (for-lists top-expr f)
+  (let ([expr* (f top-expr)])
+    (if (list? expr*)
+	(cons (car expr*) (map (lambda (x) (for-lists x f))
+			       (cdr expr*)))
+	expr*)))
