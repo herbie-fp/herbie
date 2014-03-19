b3c0cc893c02ee1e0bb4e27f82ca717a99bf51b6
Alex Sanchez-Stern <alex.sanchezstern@hotmail.com>
2014-03-19 00:32:15 -0700

    Added Test Suites.

---
+++ casio/testImprove.rkt
@@ -0,0 +1,28 @@
+#lang racket
+
+(require casio/main)
+(require racket/engine)
+
+(provide test-improve)
+
+(define (average-trials f num-trials)
+  (let loop ([remaining-trials num-trials] [acc '()])
+    (if (> remaining-trials 0)
+	(loop (- remaining-trials 1) (cons (f) acc))
+	(/ (apply + acc) num-trials))))
+
+(define (test-improve prog max-iters num-trials)
+  (average-trials (lambda () (let-values ([(end start) (improve prog max-iters)])
+			       (timeout (lambda () (improvement start end))
+					3000
+					0)))
+		  num-trials))
+
+(define (timeout f time-limit default-value)
+  (let* ([value #f]
+	 [result (engine-run time-limit
+			     (engine (lambda (_)
+				       (set! value (f)))))])
+    (if result
+	value
+	default-value)))
