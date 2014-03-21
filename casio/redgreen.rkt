82a60c57c7192d7b8fcd9883056ed4b82e547efb
Pavel Panchekha <me@pavpanchekha.com>
2014-03-20 19:46:18 -0700

    Fix sign bugs everywhere
    Conflicts:
    	casio/points.rkt
    	casio/redgreen.rkt

--- casio/redgreen.rkt
+++ casio/redgreen.rkt
@@ -6,9 +6,13 @@
 (require casio/programs)
 (require casio/common)
 
-(provide green? remove-red green-threshold errors-diff-score)
+(provide green? remove-red green-threshold errors-diff-score much-better?)
 
 (define green-threshold (make-parameter 0))
+
+(define (much-better? alt1 alt2)
+  (< (errors-diff-score (alt-errors alt1) (alt-errors alt2))
+     (green-threshold)))
 	     
 (define (green? altn)
   (and (alt-prev altn) ; The initial alternative is not green-tipped by convention
@@ -20,7 +24,7 @@
     (let*-values ([(reals infs) (partition (lambda (n) (rational? n)) d)]
 		  [(positive-infs negative-infs) (partition (lambda (n) (> 0 n)) infs)])
       (+ (apply + reals)
-	 (* 100 (- (length positive-infs) (length negative-infs)))))))
+	 (* 100 (- (length negative-infs) (length positive-infs)))))))
 
 ;; Terminology clarification: the "stream" in this metaphor flows from the original program to our passed alternative.
 ;; "Upstream" and "up" both mean backwards in the change history, "downstream" and "down" both mean forward in the
