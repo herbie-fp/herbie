d6e4c3a19af50e8787188337c0788d8cc7d33daf
Pavel Panchekha <me@pavpanchekha.com>
2014-03-21 01:00:49 -0700

    New, crazy, load-bench function.
    Start a new Racket shell in the `test.rkt` module. Run `(load-bench
    "hamming.rkt")`. The result you get is a list of "test" objects. These
    can now be run (use `test-improvement`) or analyzed programmatically.

--- casio/test.rkt
+++ casio/test.rkt
@@ -2,10 +2,14 @@
 
 (require casio/alternative)
 (require casio/programs)
+(require casio/points)
 (require casio/main)
-(require rackunit)
 
-(define *num-iterations* (make-parameter 10))
+(provide (struct-out test) *tests* *num-iterations*
+         test-improvement test-succeeds?
+         casio-test casio-bench)
+
+(define *num-iterations* (make-parameter 5))
 
 (define (unfold-let expr)
   (match expr
@@ -24,7 +28,9 @@
 (define (expand-associativity expr)
   (match expr
     [(list (? (curryr member '(+ - * /)) op) a ..2 b)
-     (list op (expand-associativity (cons op a)) (expand-associativity b))]
+     (list op
+           (expand-associativity (cons op a))
+           (expand-associativity b))]
     [(list op a ...)
      (cons op (map expand-associativity a))]
     [_
@@ -41,31 +47,60 @@
 (define (compile-program prog)
   (expand-associativity (unfold-let prog)))
 
-(define-binary-check (check-member (lambda (x y) (member y x)) elt lst))
+(struct test (name vars input output))
+(define *tests* (make-parameter '()))
+(define *test-cache* (make-hash))
 
 (define-syntax (casio-test stx)
   (syntax-case stx ()
     [(_ vars name input output)
-     #`(let* ([prog `(lambda vars ,(compile-program 'input))]
-              [altn (improve prog (*num-iterations*))])
-	 (if (member 'output (program-body (alt-program altn)))
-	     #t
-	     (begin
-	       (printf "failure on ~a\ninput: ~a\ndesired output: ~a\nactual output: ~a\n"
-		       'name 'input 'output altn)
-	       #f)))]))
-
-(define (bench-results name inerr outerr)
-  (printf "~a orders: ~s\n"
-          (/ (round (* (- (/ (log (/ outerr (max inerr 1e-16)))
-                             (log 10))) 10)) 10)
-          name))
+     #`(*tests* (cons (test name 'vars (compile-program 'input)
+                            (compile-program 'output)) (*tests*)))]))
 
 (define-syntax (casio-bench stx)
   (syntax-case stx ()
     [(_ vars name input)
-     #`(let* ([prog `(lambda vars ,(unfold-let 'input))]
-              [altn (improve prog (*num-iterations*))])
-	 'ok #;(bench-results name prog altn))]))
+     #`(*tests* (cons (test name 'vars (compile-program 'input) #f)
+                      (*tests*)))]))
 
-(provide casio-test casio-bench)
+(define (make-prog test)
+  `(λ ,(test-vars test) ,(test-input test)))
+
+(define (test-improvement test)
+  (let*-values ([(end stt) (improve (make-prog test) (*num-iterations*))]
+                [(diff) (errors-diff-score (alt-errors stt) (alt-errors end))])
+    (/ diff (length (alt-errors end)))))
+
+(define (test-succeeds? test)
+  (if (test-output test)
+      (let*-values ([(end stt)
+                     (improve (make-prog test) (*num-iterations*))])
+        (equal? (test-output test) (program-body (alt-program end))))
+      (error "Not a real test case (no output given)" test)))
+
+(define (load-bench . path)
+  (define bench-dir (string->path "../bench/"))
+  (when (not (directory-exists? bench-dir))
+    (error "Benchmark directory not found" (simplify-path bench-dir)))
+
+  (define subdirs (map string->path-element path))
+  (define p (apply build-path bench-dir subdirs))
+
+  (cond
+   [(directory-exists? p)
+    (let* ([fs (filter file-exists? (directory-list p #:build? #t))])
+      (parameterize ([*tests* '()])
+        (for ([f fs]) (dynamic-require f 0))
+        (if (null? (*tests*))
+            (begin (hash-ref *test-cache* p '()))
+            (begin (hash-set! *test-cache* p (*tests*))
+                   (*tests*)))))]
+   [(file-exists? p)
+    (parameterize ([*tests* '()])
+      (dynamic-require p 0)
+      (if (null? (*tests*))
+          (begin (hash-ref *test-cache* p '()))
+          (begin (hash-set! *test-cache* p (*tests*))
+                 (*tests*))))]
+   [else
+    (error "Didn't find a directory or file at" (simplify-path p))]))
