38992d7325e152a59e64cefbbef76b2ee5487724
Alex Sanchez-Stern <alex.sanchezstern@hotmail.com>
2014-03-24 01:35:46 -0700

    Can Now Combine Two Alts
    Ultimately the goal is that you should be able to pass in all of your
    alts, and an improvement function, and combine-alts will find the best
    two to combine into a conditional alt. We don't have that yet. We do,
    however, have combine-two-alts, so if you already know what alts you
    want to combine, you just pass the index of the variable to branch
    on (if your program is something like (lambda (x y) ...), and you want
    to branch on x, that's an index of 0, 1 for y, etc.), and the two
    alternatives, as well as an optional improvement function (which you'll
    probably always use), and it'll combine the alts for you. Your
    improvement function will have *points* dynamically scoped so that it
    equals the points on which the alt you're currently improving is in
    effect, so you can just do a normal improve and as long as you don't
    override *points*, it'll improve on the right points.

--- casio/combine-alts.rkt
+++ casio/combine-alts.rkt
@@ -3,73 +3,65 @@
 (require casio/programs)
 (require casio/rules)
 (require casio/points)
+(require casio/common)
 
 (provide (all-defined-out))
 
-(define (combine-alts alt0 alt1 dividor division-var)
-  (let* ([split-initial-prog `(lambda ,(program-variables (alt-program alt0))
-			       (if (< ,division-var ,dividor)
-				   (program-body (alt-initial alt0))
-				   (program-body (alt-initial alt1))))]
-	 [split-initial-alt (make-alt split-initial-prog)])
-    (build-alt split-initial-alt
-	       (append (map (lambda (chng)
-			      (change (change-rule chng)
-				      (append '(cdr cdr car cdr cdr car) (change-location chng))
-				      (change-bindings chng)))
-			    (alt-changes alt0))
-		       (map (lambda (chng)
-			      (change (change-rule chng)
-				      (append '(cdr cdr car cdr cdr cdr car) (change-location chng))
-				      (change-bindings chng)))
-			    (alt-changes alt1))))))
-    
-(define (get-splitpoint alt0 alt1)
-  (let* ([err-diff (errors-compare (alt-errors alt0) (alt-errors alt1))]
-	 [split-index (splitindex-from-difflist err-diff)])
-    (bfind-splitpoint (alt-program alt0) (alt-program alt1) (list-ref *points* split-index) (list-ref *points* (+ 1 split-index)))))
+(define (combine-two-alts var-index alt0 alt1 #:pre-combo-func [f (lambda (x _) x)])
+  (let* ([vars (program-variables (alt-program alt0))]
+	 [split-var (list-ref vars var-index)]
+	 [condition (get-condition (sort (get-splitpoints alt0 alt1 var-index) <)
+				   split-var)])
+    (let-values ([(points0 points1) (partition (compose (eval `(lambda (,split-var) ,condition))
+							(curry (flip-args list-ref) var-index))
+					       (*points*))])
+    `(lambda ,vars
+       (if ,condition
+	   ,(program-body (alt-program (parameterize [(*points* points0)] (f alt0)))
+	   ,(program-body (alt-program (parameterize [(*points* points1)] (f alt1))))))))))
 
-(define (bfind-splitpoint prog1 prog2 start end)
-  (if (= start end)
-      start
-      (let* ([midpoint (/ (+ start end) 2)]
-	     [exact (make-exacts prog1 (list midpoint))])
-	(if (> (abs (- ((eval-prog prog1 mode:fl) midpoint) exact))
-	       (abs (- ((eval-prog prog2 mode:fl) midpoint) exact)))
-	    (bfind-splitpoint prog1 prog2 midpoint end)
-	    (bfind-splitpoint prog1 prog2 start midpoint)))))
+(define (get-condition splitpoints var)
+  (if (nan? (car splitpoints))
+      (list 'not (get-condition (cdr splitpoints) var))
+      (let ([conditions (cons `(> ,(car splitpoints) ,var)
+			      (let loop ([rest (cdr splitpoints)] [conds '()])
+				(cond [(null? rest) conds]
+				      [(null? (cdr rest)) (cons `(< ,(car rest) ,var) conds)]
+				      [#t (loop (cddr rest) (cons `(and (< ,(car rest) ,var)
+									(> ,(cadr rest) ,var))
+								   conds))])))])
+	(if (< 1 (length conditions))
+	    (cons 'or conditions)
+	    (car conditions)))))
 
-(define (splitindex-from-difflist difflist)
-  (let loop ([cur-index 0] [more-count 0] [diff-rest difflist])
-    (cond [(null? diff-rest)
-	   cur-index]
-	  [(eq? (car diff-rest) '=)
-	   (loop cur-index more-count (cdr diff-rest))]
-	  [(and (eq? (car diff-rest) '<) (> 1 more-count))
-	   (loop (- (length difflist) (length diff-rest)) more-count (cdr diff-rest))]
-	  [(eq? (car diff-rest) '<)
-	   (loop cur-index (- more-count 1) (cdr diff-rest))]
-	  [(eq? (car diff-rest) '>)
-	   (loop cur-index (+ more-count 1) (cdr diff-rest))])))
+;; Given a list in point order (small-positive to large-positive, then small-negative to large-negative),
+;; Reorder it into ascending order (large-negative to small-negative, small-positive to large-positive).
+(define (ascending-order var-index l)
+  (let* ([num-positives (length (filter (compose positive? (curry (flip-args list-ref) var-index)) (*points*)))]
+	 [positives (take l num-positives)]
+	 [negatives (drop l num-positives)])
+    (append (reverse negatives) positives)))
 
-(define (get-splitpoints alt1 alt2 #:max-splitpoints [max-splits +inf.0])
-  (let* ([difflist (errors-compare (alt-errors alt1) (alt-errors alt2))]
-	 [sindices (get-splitindices difflist)])
+(define (get-splitpoints alt1 alt2 arg-index #:max-splitpoints [max-splits 4])
+  (let* ([difflist (errors-compare (ascending-order arg-index (alt-errors alt1))
+				   (ascending-order arg-index (alt-errors alt2)))]
+	 [sindices (get-splitindices difflist #:max-splitpoints max-splits)]
+	 [ascending-points (ascending-order arg-index (*points*))])
     (map (lambda (i)
 	   (cond [(= 0 i) +nan.0]
-		 [(eq? '= (car (list-ref difflist i)))
-		  (list-ref *points* i)]
-		 [(eq? '= (car (list-ref difflist (- i 1))))
-		  (list-ref *points* (- i 1))]
-		 [#t (binary-search-floats (compose (curry eq? (car (list-ref difflist i))) ;;Is it the same sign as the first point?
+		 [(eq? '= (list-ref difflist i))
+		  (list-ref (list-ref ascending-points i) arg-index)]
+		 [(eq? '= (list-ref difflist (- i 1)))
+		  (list-ref (list-ref ascending-points (- i 1)) arg-index)]
+		 [#t (binary-search-floats (compose (curry eq? (list-ref difflist i)) ;;Is it the same sign as the first point?
 						    (lambda (p) ;; Get the sign of the given point
-						      (let ([points (list p)])
+						      (let ([points (list (list p))])
 							(errors-compare (let ([prog (alt-program alt1)])
 									  (errors prog points (make-exacts prog points)))
 									(let ([prog (alt-program alt2)])
 									  (errors prog points (make-exacts prog points)))))))
-					   (list-ref *points* i)
-					   (list-ref *points* (- i 1)))]))
+					   (list-ref (list-ref ascending-points i) arg-index)
+					   (list-ref (list-ref ascending-points (- i 1)) arg-index))]))
 	 sindices)))
 					   
 									      
@@ -77,17 +69,23 @@
 ;; Given two points, the first of which is pred, and the second is not,
 ;; finds the point where pred becomes false, by calling split to binary
 ;; search the space until (split a b) returns a or b.
-(define (binary-search pred split p1 p2)
-  (if (eq? p1 p2) p1
+(define (binary-search split pred p1 p2)
+  (if (= p1 p2) p1
       (let ([midpoint (split p1 p2)])
-	(if (pred midpoint)
-	    (binary-search pred split midpoint p2)
-	    (binary-search pred split p1 midpoint)))))
+	(cond [(not midpoint) p1]
+	      [(or (= p1 midpoint) (= p2 midpoint)) midpoint]
+	      [(pred midpoint) (binary-search split pred midpoint p2)]
+	      [#t (binary-search split pred p1 midpoint)]))))
+
+(define (flip-args f) (lambda (x y) (f y x)))
+(define epsilon .005)
+(define (close-enough a b) (> epsilon (abs (- a b))))
 
 ;; Given two floating point numbers, the first of which is pred,
 ;; and the second is not, find where pred becomes false.
-(define (binary-search-floats pred p1 p2)
-  (binary-search pred (lambda (f1 f2) (/ (+ f1 f2) 2)) p1 p2))
+(define binary-search-floats (curry binary-search (lambda (a b) (if (close-enough a b) #f
+								    (/ (+ a b) 2)))))
+(define binary-search-ints (curry binary-search (compose floor (compose (curry (flip-args /) 2) +))))
 
 ;; Gets the indices to split a region into. By default the only requirement of these regions is that they be the most accurate
 ;; regions where no region is less than three points in size, but you can pass in a minimum region size (default three), a
@@ -97,13 +95,13 @@
   (let loop ([regions (swallow-regions (compose (curry > min-size) car)
 				       (diff-list-to-regions difflist))]
 	     [new-min-size (+ 1 min-size)])
-    (if (and (< (length (filter (compose (compose not (curry eq? '=)) cdr) regions)) (+ 1 max-splits)) (fit? regions))
+    (if (and (<= (length (filter (compose (compose not (curry eq? '=)) cdr) regions)) (+ 1 max-splits)) (fit? regions))
 	(regions-to-splitindices (swallow-regions (compose (curry eq? '=) cdr) regions))
-	(loop (swallow-regions (curry > new-min-size) regions) (+ 1 new-min-size)))))
+	(loop (swallow-regions (compose (curry > new-min-size) car) regions) (+ 1 new-min-size)))))
 
 (define (regions-to-splitindices regions)
   (let ([with-zero (reverse (cdr (foldl (lambda (reg acc) (cons (+ (car reg) (car acc)) acc)) '(0) regions)))])
-    (if (eq? '< (caar regions))
+    (if (eq? '< (cdar regions))
 	with-zero
 	(cdr with-zero)))) ;;without zero
 	 
