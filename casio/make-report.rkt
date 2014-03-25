51145ec50c52e2d86b064c9759576c298dfdd30f
Alex Sanchez-Stern <alex.sanchezstern@hotmail.com>
2014-03-25 03:17:13 -0700

    Version 1.0 of Making Reports

--- casio/make-report.rkt
+++ casio/make-report.rkt
@@ -10,9 +10,6 @@
 (require casio/alternative)
 (require racket/date)
 
-(define commit (with-output-to-string (λ() (system "git rev-parse HEAD"))))
-(define branch (with-output-to-string (λ() (system "git rev-parse --abbrev-ref HEAD"))))
-
 (define (get-table-data)
   (cons '("Test Name" "Errors Before Improvement" "Errors After Imrovement" "Total Improvement")
 	(map (lambda (test) (let-values ([(end start) (improve (make-prog test) (*num-iterations*))])
@@ -23,20 +20,35 @@
 	     (filter (lambda (test) (= 1 (length (test-vars test))))
 		     (load-all)))))
 
-(write-file "test.html"
-	    (text "<!DOCTYPE html>") (newline)
-	    (html (newline)
-		  (body (newline)
-			(bold (let ([cur-date (current-date)])
-				(text (date-year cur-date) " "
-				      (date-month cur-date) " "
-				      (date-day cur-date) " "
-				      (date-hour cur-date) ":"
-				      (date-minute cur-date) ":"
-				      (date-second cur-date)))
-			      (newline)
-			      (text " Commit: " commit " on " branch))
-			(newline)
-			(make-table (get-table-data))
-			(newline))
-		  (newline)))
+(define (make-report)
+  (let ([cur-date (current-date)]
+	[commit (with-output-to-string (lambda () (system "git rev-parse HEAD")))]
+	[branch (with-output-to-string (lambda () (system "git rev-parse --abbrev-ref HEAD")))]
+	[results (get-table-data)])
+    (write-file "report.html"
+		(text "<!DOCTYPE html>") (newline)
+		(html (newline)
+		      (body (newline)
+			    (b (text (date-year cur-date) " "
+				     (date-month cur-date) " "
+				     (date-day cur-date) " "
+				     (date-hour cur-date) ":"
+				     (date-minute cur-date) ":"
+				     (date-second cur-date)))
+			    (newline)
+			    (text " Commit: " commit " on " branch))
+		      (newline)
+		      (make-table results)
+		      (newline))
+		(newline))))
+
+(define (make-dummy-report)
+  (write-file "test.html"
+	      (text "<!DOCTYPE html>") (newline)
+	      (html (newline)
+		    (body (newline)
+			  (make-table '((1 2 3) (4 5 6) (7 8 9)))
+			  (newline))
+		    (newline))))
+
+(make-report)
