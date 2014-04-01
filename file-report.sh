1b5d720eb9abbca65e02695d048060268a7f5cd4
Alex Sanchez-Stern <alex.sanchezstern@hotmail.com>
2014-04-01 14:42:02 -0700

    Upload Reports To Server Instead of Github

--- file-report.sh
+++ file-report.sh
@@ -1,22 +1,19 @@
 #!/usr/bin/env bash
 
-WIKI="../casio.wiki"
-RDIR="reports"
+RHOST="totalcrazyhack.net:/var/www/casio/"
 
-mkdir -p $WIKI/$RDIR
 T=$(date +"%y%m%d%H%M%S")
 B=$(git rev-parse --abbrev-ref HEAD)
 C=$(git rev-parse HEAD | sed 's/\(..........\).*/\1/')
-RPATH="$RDIR/$T-$(hostname)-$B-$C.md"
-cp report.md $WIKI/$RPATH
+RDIR="reports/$T-$(hostname)-$B-$C"
+mkdir "$RDIR"
+cp report.md "$RDIR/"
+pandoc -f markdown -t html -o report.html report.md
 
 read -p "Publish? (y/N) " yn
 case $yn in
     y)
-	cd $WIKI && \
-	git add $RPATH && \
-	git commit -m "Added Report" && \
-	git push
+	rsync --verbose --recursive "$RDIR" "$RHOST/$RDIR"
 	;;
     *)
 	echo "Report copied, but not published."
