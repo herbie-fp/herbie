fabf1b97b6cd8bc43917c2d58f2a816d5178ee8b
Alex Sanchez-Stern <alex.sanchezstern@hotmail.com>
2014-04-01 20:32:52 -0700

    Added Report Index

---
+++ casio/make-index.rkt
@@ -0,0 +1,39 @@
+#lang racket
+
+(require casio/tools-common)
+(require casio/markdown-tools)
+(require racket/date)
+
+(define (make-index-page foldernames)
+  (let ([sorted-files (sort (map (lambda (filename) (list (parse-datestring filename) (substring filename 12 (length filename)) filename))
+				 foldernames)
+			    (lambda (f1 f2)
+			      (< (string->number (substring (caddr f1 0 12))) (string->number (substring (caddr f2 0 12))))))])
+    (write-file "index.md"
+		(for/list ([file sorted-files])
+		  (index-row (car file) (cadr file) (caddr file))
+		  (newline)))))
+
+(define (parse-datestring filename)
+  (let ([year-string (substring filename 0 2)]
+	[month-string (substring filename 2 4)]
+	[day-string (substring filename 4 6)]
+	[hour-string (substring filename 6 8)]
+	[minute-string (substring filename 8 10)]
+	[second-string (substring filename 10 12)])
+    (let ([year (+ 2000 (string->number year-string))]
+	  [month (string->number month-string)]
+	  [day (string->number day-string)]
+	  [hour (string->number hour-string)]
+	  [minute (string->number minute-string)]
+	  [second (string->number second-string)])
+      (date second minute hour day month year 0 0 #f 0))))
+
+(define (index-row date host-branch-commit filename)
+  (text "[" (date->string) " on " host-branch-commit "](" filename "/report.md)"))
+
+(make-index-page
+ (command-line
+  #:program "make-index"
+  #:args foldernames
+  foldernames))
--- file-report.sh
+++ file-report.sh
@@ -6,15 +6,19 @@ T=$(date +"%y%m%d%H%M%S")
 B=$(git rev-parse --abbrev-ref HEAD)
 C=$(git rev-parse HEAD | sed 's/\(..........\).*/\1/')
 RDIR="reports/$T-$(hostname)-$B-$C"
+IDIR="reports"
 mkdir "$RDIR"
 cp report.md "$RDIR/report.md"
-pandoc -f markdown -t html -o report.html report.md
+cp index.md "$IDIR/index.md"
+pandoc -f markdown -t html -o "$RDIR/report.html" "$RDIR/report.md"
+pandoc -f markdown -t html -o "$IDIR/index.html" "$IDIR/index.md"
 
 read -p "Publish? (y/N) " yn
 case $yn in
     y)
 	read -p "Username: " user
 	rsync --verbose --recursive "$RDIR" "$user@$RHOST$RDIR"
+	rsync --verbose --recursive "$IDIR" "$user@$RHOST$IDER"
 	;;
     *)
 	echo "Report copied, but not published."
