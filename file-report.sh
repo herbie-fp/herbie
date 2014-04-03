caefbb294875826cc1ba1cf6a701077225c85489
Alex Sanchez-Stern <alex.sanchezstern@hotmail.com>
2014-04-03 10:54:24 -0700

    Sync Style File to Server, And Normalized Path Variable Style

--- file-report.sh
+++ file-report.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
 
-RHOST="totalcrazyhack.net:/var/www/casio/"
+RHOST="totalcrazyhack.net:/var/www/casio"
 TOPDIR=$(pwd)
 
 T=$(date +"%y%m%d%H%M%S")
 B=$(git rev-parse --abbrev-ref HEAD)
 C=$(git rev-parse HEAD | sed 's/\(..........\).*/\1/')
-RFOLDER="reports/"
-RDIR="$RFOLDER$T-$(hostname)-$B-$C"
+RFOLDER="reports"
+RDIR="$RFOLDER/$T-$(hostname)-$B-$C"
 mkdir "$RDIR"
 cp report.md "$RDIR/report.md"
+cp "$RFOLDER/reportStyle.css" "$RDIR/reportStyle.css"
 cd "$RFOLDER"
 REPORTS=$(find * -maxdepth 0 -type d)
 cd "$TOPDIR"
@@ -22,8 +23,8 @@ read -p "Publish? (y/N) " yn
 case $yn in
     y)
 	read -p "Username: " user
-	rsync --verbose --recursive "$RDIR" "$user@$RHOST$RFOLDER"
-	rsync --verbose --recursive "$RFOLDER/index.html" "$user@$RHOST$RFOLDER"
+	rsync --verbose --recursive "$RDIR" "$user@$RHOST/$RFOLDER"
+	rsync --verbose --recursive "$RFOLDER/index.html" "$user@$RHOST/$RFOLDER"
 	;;
     *)
 	echo "Report copied, but not published."
