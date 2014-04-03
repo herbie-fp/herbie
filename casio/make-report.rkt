46f3561c9df65ef7d03498ee5484df9b3583e270
Alex Sanchez-Stern <alex.sanchezstern@hotmail.com>
2014-04-03 11:36:00 -0700

    Fixed Green Highlighting Not Working Properly

--- casio/make-report.rkt
+++ casio/make-report.rkt
@@ -71,7 +71,7 @@
       (eq? 'Yes (list-ref row 4))))
 
 (define (good? row)
-  (and (number? row)
+  (and (number? (list-ref row 1))
        (< 5 (list-ref row 1))))
 
 (define (make-report bench-dir)
