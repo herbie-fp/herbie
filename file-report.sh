fce26056f7d217553c8f59a300a7ed831efb959a
Alex Sanchez-Stern <alex.sanchezstern@hotmail.com>
2014-04-16 13:09:21 -0700

    Fixed Build Process And Reduced Extra Files

--- file-report.sh
+++ file-report.sh
@@ -10,7 +10,8 @@ C=$(git rev-parse HEAD | sed 's/\(..........\).*/\1/')
 RFOLDER="reports"
 RDIR="$RFOLDER/$T-$(hostname)-$B-$C"
 mkdir "$RDIR"
-cp report.md "$RDIR/report.md"
+mv report.md "$RDIR/report.md"
+mv graphs "$RDIR/"
 cp "$RFOLDER/reportStyle.css" "$RDIR/reportStyle.css"
 pandoc --css "reportStyle.css" -f markdown -t html -o "$RDIR/report.html" "$RDIR/report.md"
 
@@ -21,7 +22,7 @@ case $yn in
 	rsync --verbose --recursive "$RDIR" "$user@$RHOST:$RHOSTDIR/$RFOLDER"
 	REPORTS=$(ssh $user@$RHOST "cd $RHOSTDIR/$RFOLDER; find * -maxdepth 0 -type d")
 	racket casio/make-index.rkt $REPORTS
-	cp index.md "$RFOLDER/index.md"
+	mv index.md "$RFOLDER/index.md"
 	pandoc -f markdown -t html -o "$RFOLDER/index.html" "$RFOLDER/index.md"
 	rsync --verbose --recursive "$RFOLDER/index.html" "$user@$RHOST:$RHOSTDIR/$RFOLDER"
 	;;
