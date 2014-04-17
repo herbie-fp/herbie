589201a434e687849f320e2cfc62d44790cbfb5e
Alex Sanchez-Stern <alex.sanchezstern@hotmail.com>
2014-04-17 12:54:30 -0700

    Fixes

--- reports/make-graph.rkt
+++ reports/make-graph.rkt
@@ -12,18 +12,24 @@
 ;; with starting alt 'start' and ending alt 'end',
 ;; and writes it to a file at filename. dir should
 ;; be a string.
-(define (make-graph start end points exacts dir path-to-graphscript)
+(define (make-graph start end points exacts dir path-to-graphscript path-to-graphstyle)
   (let ([dest (string-append dir "graph.js")])
     (when (file-exists? dest) (delete-file dest))
     (copy-file path-to-graphscript dest))
+  (let ([dest (string-append dir "graph.css")])
+    (when (file-exists? dest) (delete-file dest))
+    (copy-file path-to-graphstyle dest))
   (make-graph-json start end (string-append dir "data.json") points exacts)
   (let-values ([(points exacts) (prepare-points (alt-program start))])
     (let ([page-path (string-append dir "graph.html")])
       (write-file page-path
 		  (html (newline)
 			(head (newline)
+			      (link #:args '((rel . "stylesheet") (type . "text/css") (href . "graph.css") (media . "screen")))
+			      (newline)
 			      (script #:args '((src . "http://d3js.org/d3.v3.min.js") (charset . "utf-8")))
 			      (newline))
+			(newline)
 			(body (newline)
 			      (script #:args '((src . "graph.js") (charset . "utf-8")))
 			      (newline))
