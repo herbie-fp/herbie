32ea586d76f3527db72610fbc1cfd4ffa93ef5d9
Alex Sanchez-Stern <alex.sanchezstern@hotmail.com>
2014-04-19 18:26:19 -0700

    Reorganized and Added Key

--- reports/graph.js
+++ reports/graph.js
@@ -1,17 +1,3 @@
-var filename = "data.json"
-
-var w = 1200,
-h = 400,
-margin = 100
-
-var vis = d3.select("body")
-    .append("svg:svg")
-    .attr("width", w)
-    .attr("height", h)
-
-var g = vis.append("svg:g")
-    .attr("transform", "translate(0, 400)");
-
 function takeEvery(data, n){
     var result = [];
     for(var i = 0; i < data.length; i+= n){
@@ -28,7 +14,7 @@ function takeOnly(data, n){
     return result;
 }
 
-function drawLines(lines, colors, g){
+function drawLines(lines, colors, names, g){
     var negLinePoints = lines.map( function(p){return p[0]});
     var posLinePoints = lines.map( function(p){return p[1]});
     var allNegLinePoints = [];
@@ -42,6 +28,8 @@ function drawLines(lines, colors, g){
     var minNegX = d3.min(allNegLinePoints, function(p){return p[0];});
     var maxY = d3.max(allNegLinePoints.concat(allPosLinePoints), function(p){return p[1]});
     var minY = 1; // Encoding domain knowledge.
+
+    // Scales for translating points.
     
     var y = d3.scale.log().clamp(true)
 	.domain([minY, maxY])
@@ -53,6 +41,8 @@ function drawLines(lines, colors, g){
 	.domain([minNegX, maxNegX])
 	.range([0 + margin, (w/2)]);
 
+    // Line functions for drawing paths.
+    
     var line = d3.svg.line()
 	.x(function(d) { return x(d[0]); })
 	.y(function(d) { return -1 * y(d[1]); });
@@ -60,6 +50,7 @@ function drawLines(lines, colors, g){
 	.x(function(d) { return negX(d[0]); })
 	.y(function(d) { return -1 * y(d[1]); });
 
+    // Draw the positive and negative paths for each given dataset.
     var index;
     for(index = 0; index < posLinePoints.length; ++index){
 	g.append("svg:path").attr("d", line(posLinePoints[index]))
@@ -70,6 +61,8 @@ function drawLines(lines, colors, g){
 	    .attr("stroke", colors[index]);
     }
 
+    // Draw the axis
+    
     g.append("svg:line")
     	.attr("x1", negX(minNegX))
     	.attr("y1", -1 * y(minY))
@@ -81,9 +74,33 @@ function drawLines(lines, colors, g){
     	attr("x2", x(0)).
     	attr("y2", -1 * h)
 
+    // Draw the key
+    var keyPosX = 50;
+    var keyPosY = -h + 20;
+    var verticleSpace = 20;
+    var horizontalSpace = 10;
+    for(var index = 0; index < lines.length; ++index){
+	g.append("svg:circle")
+	    .attr("cx", keyPosX)
+	    .attr("cy", keyPosY + (verticleSpace * index))
+	    .attr("r", (verticleSpace / 2) - 2)
+	    .attr("stroke", colors[index])
+	    .attr("fill", colors[index]);
+
+	g.append("svg:text")
+	    .attr("x", keyPosX + horizontalSpace)
+	    .attr("y", keyPosY + (verticleSpace * index) + 5)
+	    .attr("fill", "black")
+	    .text(names[index]);
+    }
+
+    // Get the intervals at which to draw labels and tick marks.
+    
     var xticks = takeOnly(x.ticks(), 8);
     var negXticks = takeOnly(negX.ticks(), 8);
     var yticks = takeOnly(y.ticks(), 8);
+
+    // Draw the labels
 
     g.selectAll(".xLabel").
     	data(xticks).
@@ -107,12 +124,14 @@ function drawLines(lines, colors, g){
     	data(yticks).
     	enter().append("svg:text").
     	attr("class", "yLabel").
-    	text(d3.format("e")).
+    	text(d3.format(".0e")).
     	attr("x", 0 + (margin /2)).
     	attr("y", function(d) {return -1 * y(d) - 5 }).
     	attr("text-anchor", "right").
     	attr("dy", 4)
 
+    // Draw the tick marks
+    
     g.selectAll(".xTicks").
     	data(xticks).
     	enter().append("svg:line").
@@ -141,6 +160,27 @@ function drawLines(lines, colors, g){
     	attr("x2", x(0))
 
 }
+var filename = "data.json"
+
+var w = 1200,
+h = 400,
+margin = 100
+
+var vise = d3.select("body")
+    .append("svg:svg")
+    .attr("width", w)
+    .attr("height", h)
+
+var ge = vise.append("svg:g")
+    .attr("transform", "translate(0, 400)");
+
+var visf = d3.select("body")
+    .append("svg:svg")
+    .attr("width", w)
+    .attr("height", h);
+
+var gf = visf.append("svg:g")
+    .attr("transform", "translate(0, 450)");
 
 d3.json(filename, function(error,dat){
     if (error) return console.warn(error);
@@ -148,7 +188,10 @@ d3.json(filename, function(error,dat){
     filedata = dat;
     var preerrors = filedata[0];
     var posterrors = filedata[1];
+    var improvement = filedata[2];
+    var exacts = filedata[3];
     
-    drawLines([preerrors, posterrors], ["yellow", "blue"], g);
+    drawLines([preerrors, posterrors, improvement], ["yellow", "blue", "green"], ["preerrors", "posterrors", "improvement"], ge);
+    drawLines([exacts], ["green"], ["exacts"], gf);
 });
 
