e2e1baa8f6e7a306276455b83d9f32315231dcf0
Alex Sanchez-Stern <alex.sanchezstern@hotmail.com>
2014-04-24 11:05:02 -0700

    Restructured Macro Libraries

--- reports/html-tools.rkt
+++ reports/html-tools.rkt
@@ -1,53 +1,8 @@
 #lang racket
 
-(provide (all-defined-out))
+(require reports/xml-common)
 
-(require reports/tools-common)
-
-;; args should be in the form of an alist
-(define-syntax (tag stx)
-  (syntax-case stx ()
-    [(_ label #:args args)
-     #'(begin (text "<" label)
-	      (for ([arg args])
-		(text " " (car arg) "=\"" (cdr arg) "\""))
-	      (text ">")
-	      (text "</" label ">"))]
-    [(_ label #:args args . rest)
-     #'(begin (text "<" label)
-	      (for ([arg args])
-		(text " " (car arg) "=\"" (cdr arg)"\""))
-	      (text ">")
-	      ((lambda () . rest))
-	      (text "</" label ">"))]
-    [(_ label . rest)
-     #'(tag label #:args '() . rest)]))
-
-(define-syntax (unitag stx)
-  (syntax-case stx ()
-    [(_ label #:args args)
-     #'(begin (text "<" label)
-	      (for ([arg args])
-		(text " " (car arg) "=\"" (cdr arg) "\""))
-	      (text "/>"))]
-    [(_ label)
-     #'(unitag label #:args '())]))
-
-(define-syntax (make-tag stx)
-  (syntax-case stx ()
-    [(_ tagname)
-     #'(define-syntax (tagname stx)
-	 (syntax-case stx ()
-	   [(_ #:args args . rest) #'(tag 'tagname #:args args . rest)]
-	   [(_ . rest) #'(tag 'tagname . rest)]))]))
-
-(define-syntax (make-unitag stx)
-  (syntax-case stx ()
-    [(_ tagname)
-     #'(define-syntax (tagname stx)
-	 (syntax-case stx ()
-	   [(_ #:args args) #'(unitag 'tagname #:args args)]
-	   [(_) #'(unitag 'tagname)]))]))
+(provide (all-defined-out) (all-from-out reports/xml-common))
 
 (define (make-table datum)
   (table #:args '((border . 1) (style . 'width:300px)) (newline)
