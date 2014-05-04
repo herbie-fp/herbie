40f5165f5b530dc275155f42b876e437f29b4f0b
Alex Sanchez-Stern <alex.sanchezstern@hotmail.com>
2014-05-04 15:27:13 -0700

    Initial Commit of print-alt-info
    Prints in a convenient format what changes were made to arrive at the
    given alternative.

---
+++ casio/process-info.rkt
@@ -0,0 +1,18 @@
+#lang racket
+
+(require casio/main)
+(require casio/alternative)
+(require casio/common)
+(require casio/rules)
+
+(provide print-alt-info)
+
+(define (print-alt-info altn)
+  (if (not (alt-prev altn))
+      (println "Started with: " (alt-program altn))
+      (begin (print-alt-info (alt-prev altn))
+	     (let ([chng (alt-change altn)])
+	       (println "Applied rule " (change-rule chng)
+			" at " (change-location chng)
+			", and got " (alt-program altn))))))
+		      
