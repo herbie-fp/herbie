416dd2a5ca1f1e7e47b9f3abb41132be9fe71d73
Alex Sanchez-Stern <alex.sanchezstern@hotmail.com>
2014-05-14 19:21:20 -0700

    First Draft Of New Main Loop

--- casio/main.rkt
+++ casio/main.rkt
@@ -11,41 +11,24 @@
 (require casio/rules)
 (require casio/combine-alts)
 
-(define (rewrite-local-error altn loc)
-  (alt-rewrite-rm altn #:root loc))
-
-(define (rewrite-brute-force altn)
-  (alt-rewrite-tree altn))
-
-(define (try-analyze altn)
-  (let ([locs (analyze-local-error altn)])
-    (append
-     (apply append
-	    (for/list ([loc locs])
-	      (rewrite-local-error altn loc)))
-     (apply append
-	    (for/list ([loc locs])
-	      (let-values ([(parent other) (location-parent loc)])
-		(if (and
-		     parent
-		     (= (length (location-get parent (alt-program altn))) 3))
-		    (alt-rewrite-expression altn #:root other)
-		    '())))))))
+(define (zaching-changes altn locs)
+  (map list (apply append
+		   (for/list ([loc locs])
+		     (let-values ([(parent other) (location-parent loc)])
+		       (if (and
+			    parent
+			    (= (length (location-get parent (alt-program altn))) 3))
+			   (rewrite-expression (location-get other (alt-program altn)) #:root other)
+			   '()))))))
 
 (define (analyze-and-rm altn)
   (let ([locs (map car (analyze-local-error altn))])
-    (apply append
-	   (for/list ([loc locs])
-	     (let ([subtree (location-get loc (alt-program altn))])
-	       (map reverse (rewrite-expression-head subtree #:root loc)))))))
-
-(define (try-simplify altn #:conservative [conservative #t])
-  (simplify altn #:fitness-func (if conservative
-				    (lambda (chng)
-				      (much-better? (alt-apply altn chng)
-						    altn))
-				    (lambda (chng)
-				      (not (much-better? altn (alt-apply altn chng)))))))
+    (append
+     (apply append
+	    (for/list ([loc locs])
+	      (let ([subtree (location-get loc (alt-program altn))])
+		(map reverse (rewrite-expression-head subtree #:root loc)))))
+     (zaching-changes altn locs))))
 
 (define (improve prog max-iters)
   (debug-reset)
@@ -80,6 +63,8 @@
 			       (alt-program altn)
 			       #f)))
 	      alts))
+    (define (register-alt altn)
+      (hash-set! seen-programs (alt-program altn) #t))
     ;; Consider one alt from our list of alts to consider, and return
     ;; new versions of alts, maybes, and olds appropriately.
     (define (step alts maybes olds green-threshold)
@@ -88,10 +73,7 @@
       (let* ([next (best-alt alts)]
 	     [new-alts (get-children next)])
 	(let-values ([(greens non-greens) (split-greens-nongreens green-threshold new-alts)])
-	  (let ([greens-filtered (filter-seen greens)]
-		[non-greens-filtered (filter-seen non-greens)])
-	    ;; Register that we've seen these programs.
-	    (for ([altn (append greens-filtered non-greens-filtered)]) (hash-set! seen-programs (alt-program altn) #t))
+	  (let ([greens-filtered (filter-seen greens)])
 	    (values (append greens (map alt-cycles++ (remove next alts)))
 		    (append non-greens maybes)
 		    (cons next olds))))))
@@ -102,8 +84,13 @@
       (let* ([change-lists (analyze-and-rm altn)]
 	     [analyze-improved-alts (for/list ([chng-lst change-lists])
 				      (apply-changes altn chng-lst))])
-	(for/list ([unsimplified analyze-improved-alts])
-	  (simplify-alt unsimplified))))
+	(let* ([unsimplified-alts (filter-seen analyze-improved-alts)]
+	       [simplified-alts (filter-seen (map simplify-alt unsimplified-alts))])
+	  (for/list ([ualt unsimplified-alts])
+	    (register-alt ualt))
+	  (for/list ([salt simplified-alts])
+	    (register-alt salt))
+	  simplified-alts)))
     ;; Simplify an alternative
     (define (simplify-alt altn)
       (let ([simplifying-changes (simplify altn)])
@@ -124,7 +111,7 @@
 	      (or best-combo (best-alt alts))))))
     ;; Determine the alternative most likely to get us closer to our goal.
     (define (best-alt alts)
-      (argmax (λ (altn) (- (errors-score (alt-errors altn)))) alts))
+      (argmin (λ (altn) (avg-bits-errors (alt-errors altn))) alts))
     ;; Main loop 2.0
     (let loop ([alts (list (simplify-alt start-altn))] [maybes '()] [olds '()] [green-threshold max-threshold])
       (if (null? alts)
@@ -137,70 +124,6 @@
 	  (let-values ([(alts* maybes* olds*) (step alts maybes olds green-threshold)])
 	    (loop alts* maybes* olds* green-threshold))))))
 
-;; This should only be called in a scope where *points* and *exacts* are
-;; dynamically defined.
-(define (improve-with-points-old altn max-iters)
-  (define (final-result alts olds trace)
-    (let* ([sorted (sort (reverse (append alts olds trace))
-			 much-better?)]
-	   [result (try-simplify (car sorted) #:conservative #f)])
-      (debug "Done:" result #:from 'improve)
-      result))
-  (let loop ([alts (list (try-simplify altn))] [olds (list)] [trace (list)]
-	     [iter max-iters])
-    ;; Invariant: (no-duplicates? alts)
-    ;; Invariant: (no-duplicates? olds)
-    (cond
-     [(= iter 0)
-      (debug "Run out of iterations, trying combinations" #:from 'improve #:depth 2)
-      (let ([plausible-combinors (plausible-alts (append alts olds trace))])
-	(debug "Found the plausible-combinors: " plausible-combinors #:from 'improve #:depth 3)
-	(if (> 2 (length plausible-combinors))
-	    (begin (debug "Not enough plausible-combinors for combination" #:from 'improve #:depth 3)
-		   (final-result alts olds trace))
-	    (let ([best-combo (best-combination plausible-combinors
-						#:pre-combo-func (curry (flip-args improve-with-points) max-iters))])
-	      (or best-combo (final-result alts olds trace)))))]
-     [(and (null? alts) (not (null? olds)))
-					; We've exhausted all "intelligent" things to do
-      (debug "Resorting to brute force"
-	     #:from 'improve #:depth 2)
-      (let* ([old (car olds)]
-	     [old* (cdr olds)]
-	     [alts* (rewrite-brute-force old)]
-	     [greens
-	      (map remove-red #;identity (filter (curryr much-better? old) alts*))])
-	(cond
-	 [(null? greens)
-	  (debug "Produced" (length alts*) "alternatives, none green"
-		 #:from 'improve #:tag 'info #:depth 3)
-	  (loop alts* (map alt-cycles++ old*) (map alt-cycles++ (cons old trace)) (- iter 1))]
-	 [else
-	  (debug "Discovered" (length greens) "green changes"
-		 #:from 'improve #:tag 'info #:depth 3)
-	  (loop greens (list) (map alt-cycles++ (append olds alts alts* trace))
-		(- iter 1))]))]
-     [(and (null? alts) (null? olds))
-      (error "(improve) cannot proceed: no olds or alts")]
-     [else
-      (debug "Step:" (car alts) #:from 'improve #:depth 2)
-      (let* ([altn (car alts)]
-	     [alts* (cdr alts)]
-	     [next (map try-simplify (try-analyze altn))]
-	     [greens
-	      (map remove-red #;identity (filter (curryr much-better? altn) next))])
-	(cond
-	 [(null? greens)
-	  (let ([next-alts (append alts* next)]
-		[next-olds (cons altn olds)])
-	    (debug "Produced" (length next) "alternatives, none green"
-		   #:from 'improve #:tag 'info #:depth 3)
-	    (loop next-alts (map alt-cycles++ next-olds) (map alt-cycles++ trace)(- iter 1)))]
-	 [else 
-	  (debug "Discovered" (length greens) "green changes"
-		 #:from 'improve #:tag 'info #:depth 3)
-	  (loop (sort greens alternative<?) (list)
-		(map alt-cycles++ (append alts olds next)) (- iter 1))]))])))
 
 ;; For usage at the REPL, we define a few helper functions.
 ;;
