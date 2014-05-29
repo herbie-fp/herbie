986c0ffdcd5e21ffe5e08e19ae5be71f34812096
Alex Sanchez-Stern <alex.sanchezstern@hotmail.com>
2014-05-29 12:56:15 -0700

    Moved Debug Message Down a bit

--- casio/simplify.rkt
+++ casio/simplify.rkt
@@ -997,7 +997,7 @@
 	    (loop (cdr rest-chngs) expr*))))))
 >>>>>>> variant B
 (define (simplify-expression expr)
-  (debug "Simplifying expression: " expr #:from 'simplify #:depth 2)
+  (debug "Simplifying expression: " expr #:from 'simplify #:depth 3)
   (let* ([canon-changes (canonicalize expr)]
 	 [expr* (changes-apply canon-changes expr)]
 	 [resolve-changes (resolve expr*)])
