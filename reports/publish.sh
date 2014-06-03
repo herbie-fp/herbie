98ed13f66585b72374758f2dedc29de54ba678f8
Pavel Panchekha <me@pavpanchekha.com>
2014-06-03 00:43:11 -0700

    Remove user prompt from publish.sh; please use a .ssh/config

--- reports/publish.sh
+++ reports/publish.sh
@@ -11,10 +11,9 @@ C=$(git rev-parse HEAD | sed 's/\(..........\).*/\1/')
 RFOLDER="reports"
 RDIR="$T-$(hostname)-$B-$C"
 
-read -p "Username: " user
-rsync --verbose --recursive graphs/ "$user@$RHOST:$RHOSTDIR/$RFOLDER/$RDIR"
-ssh "$user@$RHOST" chmod a+rx "$RHOSTDIR/$RFOLDER/$RDIR" -R
-REPORTS=$(ssh "$user@$RHOST" "cd $RHOSTDIR/$RFOLDER; find * -maxdepth 0 -type d")
+rsync --verbose --recursive graphs/ "$RHOST:$RHOSTDIR/$RFOLDER/$RDIR"
+ssh "$RHOST" chmod a+rx "$RHOSTDIR/$RFOLDER/$RDIR" -R
+REPORTS=$(ssh "$RHOST" "cd $RHOSTDIR/$RFOLDER; find * -maxdepth 0 -type d")
 racket reports/make-index.rkt $REPORTS
-rsync --verbose --recursive "index.html" "$user@$RHOST:$RHOSTDIR/$RFOLDER"
+rsync --verbose --recursive "index.html" "$RHOST:$RHOSTDIR/$RFOLDER"
 rm index.html
