b8ce6382ca851fc4620d68a5539271022724c725
Alex Sanchez-Stern <alex.sanchezstern@hotmail.com>
2014-06-05 12:20:00 -0700

    More Informative Error Messages

--- casio/combine-alts.rkt
+++ casio/combine-alts.rkt
@@ -176,7 +176,8 @@
     ;; Parameterize the function call with *points* as the given points,
     ;; and *exacts* as the exacts made from those points.
     (when (or (null? points) (null? exacts))
-      (error "Condition " (option-condition opt) " encompasses all of it's points in one of it's branches."))
+      (error "Condition " (option-condition opt) " encompasses all of it's points in one of it's branches."
+	     "Pointpred is " (*point-pred*)))
     (parameterize [(*points* points)
 		   (*exacts* exacts)
 		   (*point-pred* (let ([vars (program-variables (alt-program (option-altn1 opt)))])
