d39ea3217aabe4c2cfa3c88da8689a0c0236bdab
Pavel Panchekha <me@pavpanchekha.com>
2014-06-07 16:02:00 -0700

    Report fixes

--- reports/make-graph.rkt
+++ reports/make-graph.rkt
@@ -54,10 +54,10 @@
 
 (define (output-history altn #:stop-at [stop-at #f])
   (cond
-   [(not (alt-change altn))
-    (printf "<li>Started with <code>~a</code></li>\n" (alt-program altn))]
    [(and stop-at (equal? (alt-program stop-at) (alt-program altn)))
     (void)]
+   [(not (alt-change altn))
+    (printf "<li>Started with <code>~a</code></li>\n" (alt-program altn))]
    [(eq? (rule-name (change-rule (alt-change altn))) 'regimes)
     (let* ([vars (change-bindings (alt-change altn))]
            [lft1 (second (assoc 'lft vars))]
