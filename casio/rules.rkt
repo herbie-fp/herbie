a8f7fd94a67b53e0007ac1331013de037bf81ebd
Pavel Panchekha <me@pavpanchekha.com>
2014-06-09 12:37:19 -0700

    More rules, from FastMath; and, simplified a benchmark a bit.

--- casio/rules.rkt
+++ casio/rules.rkt
@@ -62,6 +62,8 @@
 (define-rule   +-inverses        (- a a)               0)
 (define-rule   sub-neg           (- a b)               (+ a (- b)))
 (define-rule   unsub-neg         (+ a (- b))           (- a b))
+(define-rule   neg-sub0          (- b)                 (- 0 b))
+(define-rule   sub0-neg          (- 0 b)               (- b))
 (define-rule   remove-double-neg (- (- a))             a)
 (define-rule   *-lft-identity    (* 1 a)               a)
 (define-rule   *-rgt-identity    (* a 1)               a)
@@ -70,6 +72,10 @@
 (define-rule   un-div-inv        (* a (/ b))           (/ a b))
 (define-rule   remove-double-div (/ (/ a))             a)
 (define-rule   div1              (/ 1)                 1)
+(define-rule   div0              (/ 0 a)               0)
+(define-rule   mul0              (* 0 a)               0)
+(define-rule   mul-1-neg         (* -1 a)              (- a))
+(define-rule   neg-mul-1         (- a)                 (* -1 a))
 
 ; Dealing with fractions
 (define-rule   div-sub     (/ (- a b) c)        (- (/ a c) (/ b c))
@@ -122,10 +128,22 @@
 (define-rule   div-exp      (/ (exp a) (exp b))  (exp (- a b))
   #:simplify ((1)))
 (define-rule   exp-prod     (exp (* a b))        (expt (exp a) b))
-(define-rule   expt-exp     (expt (exp a) b)     (exp (* a b))
+
+; Powers
+(define-rule   expt-exp        (expt (exp a) b)            (exp (* a b))
   #:simplify ((1)))
-(define-rule   expt-to-exp  (expt a b)           (exp (* (log a) b)))
-(define-rule   exp-to-expt  (exp (* (log a) b))  (expt a b))
+(define-rule   expt-to-exp     (expt a b)                  (exp (* (log a) b)))
+(define-rule   exp-to-expt     (exp (* (log a) b))         (expt a b))
+(define-rule   expt-prod-up    (* (expt a b) (expt a c))   (expt a (+ b c)))
+(define-rule   expt-prod-down  (* (expt b a) (expt c a))   (expt (* b c) a))
+(define-rule   expt1           a                           (expt a 1))
+(define-rule   unexpt1         (expt a 1)                  a)
+(define-rule   unexpt0         (expt a 0)                  1)
+(define-rule   unexpt2         (expt a 2)                  (sqr a))
+(define-rule   expt2           (sqr a)                     (expt a 2))
+(define-rule   unexpt1/2       (expt a 1/2)                (sqrt a))
+(define-rule   expt1/2         (sqrt a)                    (expt a 1/2))
+(define-rule   expt-plus       (* (expt a b) a)            (expt a (+ b 1)))
 
 ; Logarithms
 (define-rule   sum-log      (+ (log a) (log b))  (log (* a b))
@@ -138,6 +156,7 @@
   #:simplify ((1)))
 (define-rule   log-rec      (log (/ a))          (- (log a))
   #:simplify ((1)))
+(define-rule   log-pow      (log (expt a b))     (* b (log a)))
 
 ; Multiplying by x / x
 (define-rule   flip-+     (+ a b)  (/ (- (sqr a) (sqr b)) (- a b))
