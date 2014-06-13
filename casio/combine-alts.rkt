1e54a0896b74734e7c10299086db90cae03abac7
Alex Sanchez-Stern <alex.sanchezstern@hotmail.com>
2014-06-13 13:43:08 -0700

    Changed core algorithm api
    Now instead of taking a variable number of err-lst arguments, it takes a
    list of err-lsts. I didn't want to have to change it, but there was no
    way to do an apply with keyword arguments, so it was the only way.

--- casio/combine-alts.rkt
+++ casio/combine-alts.rkt
@@ -338,7 +338,8 @@
 ;; Given error-lsts, returns a list of sp objects representing where the optimal splitpoints are.
 ;; Takes two optional parameters: max-splits, the maximum number of splitpoints to return, and
 ;; min-weight, the minimum total error in a region (?).
-(define (err-lsts->split-indices #:max-splits [max-splits 5] #:min-region-size [min-region-size 5] #:min-weight [min-weight 5]. err-lsts)
+(define (err-lsts->split-indices #:max-splits [max-splits 5] #:min-region-size [min-region-size *min-region-size*]
+				 #:min-weight [min-weight 5] err-lsts)
   ;; We have num-candidates candidates, each of whom has error lists of length num-points.
   ;; We keep track of the partial sums of the error lists so that we can easily find the cost of regions.
   (let ([num-candidates (length err-lsts)]
