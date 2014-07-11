31b09463d9deb1f161fdeaa5e0d1152b2fb040ba
Alex Sanchez-Stern <alex.sanchezstern@hotmail.com>
2014-07-10 17:03:13 -0700

    Refactored Debug Printing

--- casio/common.rkt
+++ casio/common.rkt
@@ -36,30 +36,36 @@
 			[#t (*debug*)])])
     (*debug* (cons (cons from depth) existing))))
 
+(define (should-print-debug? from depth)
+  (or (eq? (*debug*) #t) ;; If debug is true, print no matter what
+      (and (*debug*) ;; If debug is false, never print
+	   (let ([max-depth (if (and from (dict-has-key? (*debug*) from))
+				;; If we were given a #:from, and we have it in the dictionary,
+				;; look up it's max depth
+				(dict-ref (*debug*) from)
+				;; Otherwise, just use whatevers default.
+				(dict-ref (*debug*) #t))])
+	     ;; If the max depth is true, turn everything on.
+	     ;; If the max depth isn't positve, turn everything off.
+	     ;; Otherwise, if our dept is less than the max-depth,
+	     ;; return true.
+	     (or (eq? max-depth #t)
+		 (and (>= max-depth depth)
+		      (> max-depth 0)))))))
+
 (define (debug #:from from #:tag [tag #f] #:depth [depth 1] . args)
-  (when (or (eq? (*debug*) #t) ;; If debug is true, print no matter what
-	    (and (*debug*) ;; If debug is false, never print
-		 (let ([max-depth (if (and from (dict-has-key? (*debug*) from))
-				      ;; If we were given a #:from, and we have it in the dictionary,
-				      ;; look up it's max depth
-				      (dict-ref (*debug*) from)
-				      ;; Otherwise, just use whatevers default.
-				      (dict-ref (*debug*) #t))])
-		   ;; If the max depth is true, turn everything on.
-		   ;; If the max depth isn't positve, turn everything off.
-		   ;; Otherwise, if our dept is less than the max-depth,
-		   ;; return true.
-		   (or (eq? max-depth #t)
-		       (and (>= max-depth depth)
-			    (> max-depth 0))))))
-    (set! *log* (cons (list* from tag args) *log*))
-    (display (hash-ref *tags* tag "; "))
-    (write from)
-    (display ": ")
-    (for/list ([arg args])
-      (display " ")
-      ((if (string? arg) display write) arg))
-    (newline)))
+  (when (should-print-debug? from depth)
+    (debug-print from tag args (current-output-port)))
+  (set! *log* (cons (list* from tag args) *log*))
+
+(define (debug-print from tag args port)
+  (display (hash-ref *tags* tag "; ") port)
+  (write from port)
+  (display ": " port)
+  (for/list ([arg args])
+    (display " " port)
+    ((if (string? arg) display write) arg port))
+  (newline port))
 
 (define (debug-reset)
   (set! *log* '()))
