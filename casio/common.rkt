ee94fa0973c1a15ad9fd47d0ef154c41356eaa22
Alex Sanchez-Stern <alex.sanchezstern@hotmail.com>
2014-07-10 17:03:25 -0700

    Debug Messages Now Go To Log

--- casio/common.rkt
+++ casio/common.rkt
@@ -20,6 +20,9 @@
       (last possible-returns))))
 
 (define *debug* (make-parameter #f))
+(when (not (directory-exists? "../logs"))
+  (make-directory "../logs"))
+(define *log-path* (make-parameter (string-append "../logs/" (number->string (current-seconds)) ".log")))
 (define *log* '())
 
 (define *tags*
@@ -57,6 +60,8 @@
   (when (should-print-debug? from depth)
     (debug-print from tag args (current-output-port)))
   (set! *log* (cons (list* from tag args) *log*))
+  (call-with-output-file (*log-path*) #:exists 'append
+			 (curry debug-print from tag args)))
 
 (define (debug-print from tag args port)
   (display (hash-ref *tags* tag "; ") port)
