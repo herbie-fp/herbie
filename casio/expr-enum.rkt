453f9840e78238d9567358ad0a3d299adbcfb373
Alex Sanchez-Stern <alex.sanchezstern@hotmail.com>
2014-07-17 12:13:46 -0700

    Added Basic Expression Enumeration

---
+++ casio/expr-enum.rkt
@@ -0,0 +1,22 @@
+#lang racket
+
+(require casio/common)
+
+(struct oprtr (symbol n-args))
+
+(define *constants* `(1 0 pi ,(exp 1) -1))
+(define *operators* (list (oprtr '+ 2) (oprtr '- 2) (oprtr '- 1)
+			  (oprtr '* 2) (oprtr '/ 2) (oprtr '/ 1)
+			  (oprtr 'sqr 1) (oprtr 'sqrt 1) (oprtr 'exp 1)
+			  (oprtr 'log 1) (oprtr 'sin 1) (oprtr 'cos 1)
+			  (oprtr 'tan 1) (oprtr 'cotan 1)))
+
+(define (gen-expr n vars)
+  (if (= n 0)
+      (append vars *constants*)
+      (apply append
+	     (for/list ([op *operators*])
+	       (map (curry cons (oprtr-symbol op))
+		    (apply list-product
+			   (make-list (oprtr-n-args op)
+				      (gen-expr (sub1 n) vars))))))))
