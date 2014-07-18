a18258e678dff6298583c09cbb7f973386d7c477
Pavel Panchekha <me@pavpanchekha.com>
2014-07-18 13:17:24 -0700

    Reorganized benchmarks a bit, removed quadratic formula (redundant)

--- bench/logistic.rkt
+++ bench/logistic.rkt
@@ -1,15 +1,7 @@
 #lang racket
 (require casio/test)
 
-(casio-test (x y)
-  "Logistic regression 1"
-
-  (- (/ 1 (+ 1 (exp (- x)))) y)
-
-  (if (> x 0)
-      (- (/ 1 (+ 1 (exp (- x)))) y)
-      (- (- 1 y) (/ 1 (+ 1 (exp x))))))
-
+; Suggested by Akshay
 
 (casio-test (x y)
   "Logistic regression 2"
@@ -19,3 +11,20 @@
   (if (<= x 0) ;;cases are flipped.
       (- (log (+ 1 (exp x))) (* x y))
       (- (log (+ 1 (exp (- x)))) (* (- x) (- 1 y)))))
+
+; Suggested by Arvind
+
+(casio-test (a b)
+  "Exp of sum of logs"
+  (exp (+ (log a) (log b)))
+  (* a b))
+
+(casio-test (a b)
+  "Quotient of sum of exps"
+  (/ (exp a) (+ (exp a) (exp b)))
+  (/ 1 (+ 1 (exp (- b a)))))
+
+(casio-test (a1 a2 b1 b2)
+  "Quotient of products"
+  (/ (* a1 a2) (* b1 b2))
+  (* (/ a1 b1) (/ a2 b2)))
--- bench/muldiv.rkt
+++
@@ -1,6 +0,0 @@
-#lang racket
-(require casio/test)
-
-(casio-bench (a b c)
-  "A la Freetype's FT_MulDiv"
-  (/ (+ a (/ c 2)) b))
