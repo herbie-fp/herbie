5156387675d0f8a91cc095e38716c60e6c90ee4a
Pavel Panchekha <me@pavpanchekha.com>
2014-07-21 14:30:43 -0700

    Added some more physics papers

---
+++ bench/physics/dissociation-protenated-water.rkt
@@ -0,0 +1,5 @@
+#lang racket
+(require casio/test)
+
+; Collision-induced dissociation of protonated water clusters
+; F. Berthias, V. Buridon, H. Abdoul-Carime, B. Farizon, M. Farizon, P. M. Dinh, P.-G. Reinhard, E. Suraud, T. D. Mark
---
+++ bench/physics/universal-linear-optics.rkt
@@ -0,0 +1,18 @@
+#lang racket
+(require casio/test)
+
+; Generation of universal linear optics by any beam splitter
+; A. Bouland and S. Aaronson
+; Phys. Rev. A 89, 062316 (2014), http://link.aps.org/doi/10.1103/PhysRevA.89.062316
+
+(casio-bench (a b)
+  "Bouland and Aaronson, Equation (24)"
+  (- (+ (sqr (+ (sqr a) (sqr b))) (* 4 (+ (* (sqr a) (- 1 a)) (* (sqr b) (+ 3 a))))) 1))
+
+(casio-bench (a b)
+  "Bouland and Aaronson, Equation (25)"
+  (- (+ (sqr (+ (sqr a) (sqr b))) (* 4 (+ (* (sqr a) (+ 1 a)) (* (sqr b) (- 1 (* 3 a)))))) 1))
+
+(casio-bench (a b)
+  "Bouland and Aaronson, Equation (26)"
+  (- (+ (sqr (+ (sqr a) (sqr b))) (* 4 (sqr b))) 1))
