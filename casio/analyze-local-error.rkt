cd44a861cc0a6f0c68a4be8099ac4f6b2e80fe96
Pavel Panchekha <me@pavpanchekha.com>
2014-07-21 19:06:30 -0700

    Better way of doing things: a ->bf and ->flonum that understand 'pi and 'e

--- casio/analyze-local-error.rkt
+++ casio/analyze-local-error.rkt
@@ -35,21 +35,14 @@
 
    #:constant
    (λ (c loc)
-      (let* ([exact (repeat (bf c))] [approx (repeat (real->double-flonum c))] [error (repeat 0)])
+      (let* ([exact (repeat (->bf c))] [approx (repeat (->flonum c))] [error (repeat 1)])
         (annotation c exact approx error error loc)))
 
    #:variable
    (λ (v loc)
-      (cond
-       [(member v (program-variables prog))
-        (let* ([var (for/list ([vmap varmap]) (cdr (assoc v vmap)))]
-               [exact (map bf var)] [approx (map real->double-flonum var)]
-               [error (map (compose add1 flonums-between) (map ->flonum exact) (map ->flonum approx))])
-          (annotation v exact approx error error loc))]
-       [(hash-has-key? constants v)
-        (let* ([exact ((list-ref (hash-ref constants v) mode:bf))]
-               [approx ((list-ref (hash-ref constants v) mode:fl))])
-            (annotation v (repeat exact) (repeat approx) (repeat 0) (repeat 0) loc))]))
+      (let* ([var (for/list ([vmap varmap]) (cdr (assoc v vmap)))]
+             [exact (map ->bf var)] [approx (map ->flonum var)])
+        (annotation v exact approx (repeat 1) (repeat 1) loc)))
 
    #:primitive
    (λ (expr loc)
