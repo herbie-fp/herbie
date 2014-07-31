dd60ec3df371f6d0c82572c0ea41a531d28aa389
Alex Sanchez-Stern <alex.sanchezstern@hotmail.com>
2014-07-30 18:11:02 -0700

    Fixed ssh group bug

--- reports/publish.sh
+++ reports/publish.sh
@@ -12,9 +12,9 @@ RFOLDER="reports"
 RDIR="$T-$(hostname)-$B-$C"
 
 rsync --verbose --recursive graphs/ "$RHOST:$RHOSTDIR/$RFOLDER/$RDIR"
-ssh "$RHOST" chmod a+rx "$RHOSTDIR/$RFOLDER/$RDIR" -R ; chgrp -R casio "$RHOSTDIR/$RFOLDER/$RDIR"
+ssh "$RHOST" chmod a+rx "$RHOSTDIR/$RFOLDER/$RDIR" -R
 REPORTS=$(ssh "$RHOST" "cd $RHOSTDIR/$RFOLDER; find * -maxdepth 0 -type d")
 racket reports/make-index.rkt $REPORTS
 rsync --verbose --recursive "index.html" "$RHOST:$RHOSTDIR/$RFOLDER"
-ssh "$RHOST" chgrp -R casio "$RHOSTDIR/$RFOLDER/index.html"
+ssh "$RHOST" chgrp -R casio "$RHOSTDIR/$RFOLDER/"
 rm index.html
