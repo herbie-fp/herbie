2fdeaf012f93867aaf1d10b947dea0ee329f667c
Pavel Panchekha <me@pavpanchekha.com>
2014-08-01 02:06:20 -0700

    Added batch-compiler

--- casio/compile.rkt
+++ casio/compile.rkt
@@ -2,6 +2,10 @@
 
 (require casio/common)
 (require casio/programs)
+(require net/uri-codec)
+
+(define (fix-name name)
+  (string-replace (uri-encode (~a name)) #rx"[^a-zA-Z0-9]" "_"))
 
 (define (apply-converter conv args)
   (cond
@@ -15,7 +19,7 @@
   [-        '(#f "-~a" "~a - ~a")]
   [*        "~a * ~a"]
   [/        '(#f "1.0/~a" "~a / ~a")]
-  [abs      "abs(~a)"]
+  [abs      "fabs(~a)"]
   [sqrt     "sqrt(~a)"]
   [sqr      (λ (x) (format "~a * ~a" x x))]
   [exp      "exp(~a)"]
@@ -42,7 +46,7 @@
   [mod      "fmod2(~a, ~a)"])
 
 (define-table constants->c
-  [pi    "PI"]
+  [pi    "atan2(1.0, 0.0)"]
   [e     "exp(1.0)"])
 
 (define (comparison? l)
@@ -52,14 +56,13 @@
   (and (list? l) (eq? (car l) 'if)))
 
 (define (compile->c prog [type "double"] [fname "f"])
-  (define fmod-used? #f)
   (define vars (program-variables prog))
   (define body (compile (program-body prog)))
 
   (define (value->c expr)
     (cond
-     [(member expr vars) expr]
-     [(member expr constants) (apply-converter (hash-ref constants->c expr))]
+     [(member expr vars) (fix-name expr)]
+     [(member expr constants) (apply-converter (car (hash-ref constants->c expr)) '())]
      [(symbol? expr) expr]
      [else (->flonum expr)]))
 
@@ -67,14 +70,12 @@
     (if (list? expr)
         (let* ([rec (list-ref (hash-ref operators->c (car expr)) 0)]
                [args (map value->c (cdr expr))])
-          (when (eq? (car expr) 'mod)
-            (set! fmod-used? #t))
           (apply-converter rec args))
         (value->c expr)))
 
   (write-string
    (printf "float ~a(~a) {\n" fname
-           (string-join (for/list ([var vars]) (format "float ~a" var)) ", "))
+           (string-join (for/list ([var vars]) (format "float ~a" (fix-name var))) ", "))
 
    (for/list ([assignment (cadr body)])
      (if (comparison? (cadr assignment))
@@ -84,18 +85,12 @@
                  (app->c (cadr assignment)))))
 
    (printf "        return ~a;\n" (value->c (caddr body)))
-   (printf "}\n")
-
-   (when fmod-used?
-     (printf "\nlong double fmod2(long double n, long double d) {\n")
-     (printf "        double r = fmodl(n, d);\n")
-     (printf "        return r < 0 ? r + d : r;\n")
-     (printf "}\n"))))
+   (printf "}\n\n")))
 
 (define-table operators->mpfr
   [+        "mpfr_add(~a, ~a, ~a, MPFR_RNDN)"]
   [-        '(#f #f "mpfr_neg(~a, ~a, MPFR_RNDN)" "mpfr_sub(~a, ~a, ~a, MPFR_RNDN)")]
-  [*        "mpfr_mul(~a, ~a, MPFR_RNDN)"]
+  [*        "mpfr_mul(~a, ~a, ~a, MPFR_RNDN)"]
   [/        '(#f #f "mpfr_ui_div(~a, 1, ~a, MPFR_RNDN)" "mpfr_div(~a, ~a, ~a, MPFR_RNDN)")]
   [abs      "mpfr_abs(~a, ~a, MPFR_RNDN)"]
   [sqrt     "mpfr_sqrt(~a, ~a, MPFR_RNDN)"]
@@ -124,36 +119,26 @@
   [mod      "mpfr_fmod2(~a, ~a, ~a)"])
 
 (define-table constants->mpfr
-  [pi    "mpfr_const_pi(~a)"]
+  [pi    "mpfr_const_pi(~a, MPFR_RNDN)"]
   [e     "ERROR(\"E unsupported\")"])
 
 (define (compile->mpfr prog [bits 128] [fname "f"])
-  (define fmod-used? #f)
   (define vars (program-variables prog))
   (define body (compile (program-body prog)))
-
-  (define (value->mpfr out expr)
-    (cond
-     [(member expr (program-variables prog)) expr]
-     [(member expr constants) ((hash-ref constants->mpfr expr) out)]
-     [(symbol? expr) expr]
-     [else (error "Unknown syntax for MPFR" expr)]))
 
   (define (app->mpfr out expr)
     (cond
      [(list? expr)
       (let* ([rec (list-ref (hash-ref operators->mpfr (car expr)) 0)]
-             [args (map (curry value->mpfr out) (cdr expr))])
-        (when (eq? (car expr) 'mod)
-          (set! fmod-used? #t))
+             [args (cdr expr)])
         (apply-converter rec (cons out args)))]
      [(number? expr)
       (let ([register (gensym "r")])
         (format "mpfr_init_set_str(~a, \"~a\", 10, MPFR_RNDN)" out expr))]
      [(member expr (program-variables prog))
-      (format "mpfr_set_flt(~a, ~a, MPFR_RNDN)" out expr)]
+      (format "mpfr_set_flt(~a, ~a, MPFR_RNDN)" out (fix-name expr))]
      [(member expr constants)
-      (apply-converter (hash-ref constants->mpfr expr) (list out))]
+      (apply-converter (car (hash-ref constants->mpfr expr)) (list out))]
      [(symbol? expr)
       (format "mpfr_set(~a, ~a, MPFR_RNDN)" out expr)]))
 
@@ -168,31 +153,53 @@
    (printf "}\n\n")
 
    (printf "float ~a(~a) {\n" fname
-           (string-join (for/list ([var vars]) (format "float ~a" var)) ", "))
+           (string-join (for/list ([var vars]) (format "float ~a" (fix-name var))) ", "))
 
    (for ([assignment (cadr body)])
      (printf "        ~a;\n" (app->mpfr (car assignment) (cadr assignment))))
 
 
   (printf "        return mpfr_get_flt(~a, MPFR_RNDN);\n" (caddr body))
-  (printf "}\n")
+  (printf "}\n\n")))
 
-  (when fmod-used?
-    (printf "\nvoid mpfr_fmod2(mpfr_t r, mpfr_t n, mpfr_t d) {\n")
-    (printf "        mpfr_fmod(r, n, d, MPFR_RNDN);\n")
-    (printf "        if (mpfr_cmp_ui(r, 0) < 0) mpfr_add(r, r, d, MPFR_RNDN);\n")
-    (printf "}\n"))))
-
-(define (compile->all iprog oprog bits)
+(define (compile->all name iprog oprog bits)
   (printf "#include <tgmath.h>\n")
   (printf "#include <gmp.h>\n")
   (printf "#include <mpfr.h>\n\n")
+  (printf "#include <stdbool.h>\n\n")
+  (printf "/* ~a */\n\n" name)
   (display (compile->c iprog "float" "f_if"))
   (display (compile->c iprog "double" "f_id"))
   (display (compile->c iprog "long double" "f_il"))
+  (printf "long double fmod2(long double n, long double d) {\n")
+  (printf "        double r = fmodl(n, d);\n")
+  (printf "        return r < 0 ? r + d : r;\n")
+  (printf "}\n\n")
   (newline)
   (display (compile->c oprog "float" "f_of"))
   (display (compile->c oprog "double" "f_od"))
   (display (compile->c oprog "long double" "f_ol"))
+  (printf "void mpfr_fmod2(mpfr_t r, mpfr_t n, mpfr_t d) {\n")
+  (printf "        mpfr_fmod(r, n, d, MPFR_RNDN);\n")
+  (printf "        if (mpfr_cmp_ui(r, 0) < 0) mpfr_add(r, r, d, MPFR_RNDN);\n")
+  (printf "}\n\n")
   (newline)
   (display (compile->mpfr iprog bits "f_im")))
+
+(define (do-all results-file)
+  (let ([fd (open-input-file results-file)])
+    (let loop ([idx 0])
+      (let ([line (read fd)])
+        (if (eof-object? line)
+            (void)
+            (let* ([name (first line)]
+                   [iprog (second line)]
+                   [oprog (third line)]
+                   [tprog (fourth line)]
+                   [bits (fifth line)])
+              (when oprog
+                (printf "Outputing ~a to tc~a.c (~a)\n" name idx bits)
+                (write-file (format "../compile/tc~a.c" idx)
+                 (compile->all name `(λ ,(program-variables oprog) ,iprog)
+                               oprog bits)))
+              (loop (+ 1 idx))))))))
