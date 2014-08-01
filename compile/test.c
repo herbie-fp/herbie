10114aa6ced6b574c0ac867d8e3dd3eee7459ca2
Pavel Panchekha <me@pavpanchekha.com>
2014-08-01 12:25:30 -0700

    Support multiple arguments

--- compile/test.c
+++ compile/test.c
@@ -7,6 +7,10 @@
 #include <gmp.h>
 #include <mpfr.h>
 
+#ifndef NARGS
+#define NARGS 1
+#endif
+
 void setup_mpfr();
 
 float f_if(float);
@@ -36,8 +40,8 @@ float rand_float() {
 
 float *get_random(int nums) {
         int i;
-        float *arr = malloc(sizeof(float) * nums);
-        for (i = 0; i < nums; i++) {
+        float *arr = malloc(sizeof(float) * nums * NARGS);
+        for (i = 0; i < nums * NARGS; i++) {
                 arr[i] = rand_float();
         }
         return arr;
@@ -45,7 +49,7 @@ float *get_random(int nums) {
 
 #define SETUP()                                 \
         clock_t start, end, zero;               \
-        int i;                                  \
+        int i, j;                               \
         unsigned long long int max = 0;         \
         double total = 0;                       \
         float *rands, *out, *correct;           \
@@ -54,27 +58,77 @@ float *get_random(int nums) {
 #define CALIBRATE(iter)                                         \
         start = clock();                                        \
         for (i = 0; i < iter; i++) {                            \
-                out[i] = 1 / rands[i];                          \
+                for (j = 0; j < NARGS; j++) {                   \
+                        out[i] += 1 / rands[NARGS*i + j];       \
+                }                                               \
         }                                                       \
         end = clock();                                          \
         zero = end - start;                                     \
         printf("cal: time %lu\n", zero / 100);
 
-#define TEST(type, iter)                                        \
-        start = clock();                                        \
-        for (i = 0; i < iter; i++) {                            \
-                out[i] = f_##type (rands[i]);              \
-        }                                                       \
-        end = clock();                                          \
-        printf("%s: time %lu\n", #type, (end - start - zero) / 100);
-
-#define CHECK(type, iter)                                         \
-        max = total = 0;                                          \
+#if NARGS == 1
+#define TEST(type, iter)                                                \
+        start = clock();                                                \
         for (i = 0; i < iter; i++) {                                    \
-                unsigned long long int error = ulp(out[i], correct[i]); \
-                if (error > max) max = error; \
-                total += log(error + 1.0) / iter / log(2);    \
-        } \
+                out[i] = f_##type (rands[i]);                           \
+        }                                                               \
+        end = clock();                                                  \
+        printf("%s: time %lu\n", #type, (end - start - zero) / 100);
+#elif nargs == 2
+#define test(type, iter)                                                \
+        start = clock();                                                \
+        for (i = 0; i < iter; i++) {                                    \
+                out[i] = f_##type (rands[2*i], rands[2*i + 1]);         \
+        }                                                               \
+        end = clock();                                                  \
+        printf("%s: time %lu\n", #type, (end - start - zero) / 100);
+#elif nargs == 3
+#define test(type, iter)                                                \
+        start = clock();                                                \
+        for (i = 0; i < iter; i++) {                                    \
+                out[i] = f_##type (rands[3*i], rands[3*i + 1], rands[3*i + 2]); \
+        }                                                               \
+        end = clock();                                                  \
+        printf("%s: time %lu\n", #type, (end - start - zero) / 100);
+#elif nargs == 4
+#define test(type, iter)                                                \
+        start = clock();                                                \
+        for (i = 0; i < iter; i++) {                                    \
+                out[i] = f_##type (rands[4*i], rands[4*i + 1], rands[4*i + 2], rands[4*i + 3]); \
+        }                                                               \
+        end = clock();                                                  \
+        printf("%s: time %lu\n", #type, (end - start - zero) / 100);
+#elif nargs == 5
+#define test(type, iter)                                                \
+        start = clock();                                                \
+        for (i = 0; i < iter; i++) {                                    \
+                out[i] = f_##type (rands[5*i], rands[5*i + 1], rands[5*i + 2], \
+                                   rands[5*i + 3], rands[5*i + 4]);     \
+        }                                                               \
+        end = clock();                                                  \
+        printf("%s: time %lu\n", #type, (end - start - zero) / 100);
+#elif nargs == 6
+#define test(type, iter)                                                \
+        start = clock();                                                \
+        for (i = 0; i < iter; i++) {                                    \
+                out[i] = f_##type (rands[6*i], rands[6*i + 1], rands[6*i + 2], \
+                                   rands[6*i + 3], rands[6*i + 4], rands[6*i + 5]); \
+        }                                                               \
+        end = clock();                                                  \
+        printf("%s: time %lu\n", #type, (end - start - zero) / 100);
+#else
+#define test(type, iter) abort();
+#endif
+
+#define CHECK(type, iter)                                               \
+        max = total = 0;                                                \
+        for (i = 0; i < iter; i++) {                                    \
+                if (correct[i] == correct[i]) {                         \
+                        unsigned long long int error = ulp(out[i], correct[i]); \
+                        if (error > max) max = error;                   \
+                        total += log(error + 1.0) / iter / log(2);      \
+                }                                                       \
+        }                                                               \
         printf("%s: max %f avg %f\n", #type, log(max + 1.0) / log(2), total);
 
 #define SAMPLE(iter) \
