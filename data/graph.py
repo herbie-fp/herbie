46541297ae9826ecb49643e179499acfa58910f5
James Wilcox <wilcoxjay@gmail.com>
2014-08-06 21:37:39 -0700

    add mostly copy-pasted code to draw time cdf

--- data/graph.py
+++ data/graph.py
@@ -63,8 +63,46 @@ def draw_axes(h_capt=None, v_capt=None, h_ticks=[], v_ticks=[]):
 def draw_point(p):
     print "\\draw[fill=black] (%f,%f) circle (%s);" % (p[0], p[1], PT_WEIGHT)
 
-def draw_cdf(data):
-    "given a list of numbers, print tikz for a cdf"
+def draw_rect(ll,ur):
+    print "\\draw (%f,%f) rectangle (%f,%f);" % (ll[0], ll[1], ur[0], ur[1])
+
+def draw_histo(data, n_buckets=10):
+    data.sort()
+    n = len(data)
+    lo = data[0]
+    hi = data[-1]
+    rng = hi - lo
+
+    histo = [[] for i in range(n_buckets)]
+
+    for x in data:
+        i = int(n_buckets * (float(x-lo) / rng))
+        if i == n_buckets:
+            i -= 1
+        histo[i].append(x)
+
+    top = max(map(len, histo))
+
+    def to_plot_space(pt):
+        scale = 1.0
+        x = PLOT_X * (float(pt[0]) / (scale * n_buckets))
+        y = PLOT_Y * (float(pt[1]) / (scale * top))
+        return (x,y)
+
+    corners = [(0,0)]
+
+    for (i,b) in enumerate(histo):
+        corners.append((i,len(b)))
+
+    begin_picture()
+    draw_axes()
+    for i in range(1,len(corners)):
+        prev = corners[i-1]
+        cur = corners[i]
+        draw_rect((prev[0], 0), cur)
+    end_picture()
+
+def draw_mpfr_bits_cdf(data):
     data.sort()
     n = len(data)
     lo = data[0]
@@ -81,7 +119,6 @@ def draw_cdf(data):
     for (i,d) in enumerate(data):
         x = d
         y = i+1
-        (xp,yp) = to_plot_space((x, y))
         pts.append((x,y))
 
     pts.append((hi, pts[-1][1]))
@@ -90,8 +127,8 @@ def draw_cdf(data):
         cur = pts[i]
 
         mid = (cur[0], prev[1])
-        draw_line(to_plot_space(prev), to_plot_space(mid), opts="thick")
-        draw_line(to_plot_space(mid), to_plot_space(cur), opts="thick")
+        draw_line(to_plot_space(prev), to_plot_space(mid)) #, opts="thick")
+        draw_line(to_plot_space(mid), to_plot_space(cur)) #, opts="thick")
 
     h_ticks = []
     for i in range(0,551,50):
@@ -111,8 +148,58 @@ def draw_cdf(data):
 
     end_picture()
 
+def draw_time_cdf(data):
+    data.sort()
+
+    n = len(data)
+    hi = data[-1] * 1.15
+
+    def to_plot_space(pt):
+        x = PLOT_X * (float(pt[0]) / hi)
+        y = PLOT_Y * (float(pt[1]) / n)
+        return (x,y)
+
+    begin_picture()
+
+    pts = [(0,0)]
+    for (i,d) in enumerate(data):
+        x = d
+        y = i+1
+        pts.append((x,y))
+    pts.append((hi, pts[-1][1]))
+
+    for i in range(1, len(pts)):
+        prev = pts[i-1]
+        cur = pts[i]
+
+        mid = (cur[0], prev[1])
+        draw_line(to_plot_space(prev), to_plot_space(mid))
+        draw_line(to_plot_space(mid), to_plot_space(cur))
+
+
+    h_ticks = []
+    for i in range(0,351,50):
+        xp = to_plot_space((i,0))[0]
+        h_ticks.append((xp, i))
+
+
+    v_ticks = [(0,"0\\%")]
+    for i in range(1,5):
+        v_ticks.append((to_plot_space((0,(float(n)/4)*i))[1], "%d\\%%" % ((100 / 4)*i)))
+
+    draw_axes(h_capt="Time to run Casio (s)",
+              v_capt="\\% of benchmarks",
+              h_ticks=h_ticks,
+              v_ticks=v_ticks)
+
+    end_picture()
+
 mpfr_data = [336,336,208,208,304,528,528,336,208,80,208,336,208,336,336,208,320,208,336,208,528,528,208,336,336,320,80,208]
+
+time_data = [13.3,5.3,300,174,44.5,44,102,48.2,22.3,300,192,32.4,282,14.9,30.7,126,40.2,25.1,28.3,11.6,41.8,78,144,144,96,53.3,41.3,66,29.5]
 if __name__ == '__main__':
     begin_doc()
-    draw_cdf(mpfr_data)
+    #draw_mpfr_bits_cdf(mpfr_data)
+    #draw_histo(mpfr_data)
+    draw_time_cdf(time_data)
     end_doc()
