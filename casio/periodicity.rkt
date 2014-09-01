1612aa8a906f9a5809b648c1797d4559cebc3283
Pavel Panchekha <me@pavpanchekha.com>
2014-09-01 01:21:04 -0700

    Use symbolic pi in periodicity

--- casio/periodicity.rkt
+++ casio/periodicity.rkt
@@ -214,7 +214,7 @@
 (define (symbol-mod v periods)
   (if (assoc v periods)
       (let ([coeff (cdr (assoc v periods))])
-        `(mod ,v ,(if (= 1/2 coeff) pi `(* ,(* 2 coeff) ,pi))))
+        `(mod ,v ,(if (= 1/2 coeff) pi `(* ,(* 2 coeff) pi))))
       v))
 
 (define (coerce-conditions prog periods)
