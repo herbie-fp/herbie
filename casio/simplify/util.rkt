fc5a2985d51c6308ba028f612d7296d4d2d1da22
Alex Sanchez-Stern <alex.sanchezstern@hotmail.com>
2014-09-04 14:15:36 -0700

    Special Cased Equality Checks For Asserts

--- casio/simplify/util.rkt
+++ casio/simplify/util.rkt
@@ -10,7 +10,19 @@
 (provide (all-defined-out))
 
 (define-syntax assert
-  (syntax-rules ()
+  (syntax-rules (=)
+    [(assert (= a b) #:loc location)
+     (when (not (= a b))
+       (error location "~a returned false!~n~a is ~a, but ~a is ~a"
+	      (list '= 'a 'b)
+	      'a a
+	      'b b))]
+    [(assert (= a b))
+     (when (not (= a b))
+       (error 'assert "~a returned false!~n~a is ~a, but ~a is ~a"
+	      (list '= 'a 'b)
+	      'a a
+	      'b b))]
     [(assert pred #:loc location)
      (when (not pred)
        (error location "~a returned false!" 'pred))]
