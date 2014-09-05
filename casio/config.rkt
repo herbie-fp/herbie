2e517461a2f5bc483cc1c894e2cb8b8533a62450
Alex Sanchez-Stern <alex.sanchezstern@hotmail.com>
2014-09-05 16:05:58 -0700

    Readded Double Precision Flag

--- casio/config.rkt
+++ casio/config.rkt
@@ -12,6 +12,7 @@
   (make-parameter
    #hash([generate . (simplify rm)]
          [reduce   . (regimes zach taylor)]
+	 [evaluate . (#;double-precision)]
          [regimes  . ()]
          [simplify . ()]
          [setup    . (simplify periodicity)]
