75ee277f90de837929f45ebdf3d78ebc7c2dfc50
Alex Sanchez-Stern <alex.sanchezstern@hotmail.com>
2014-09-11 11:26:49 -0700

    Fixed Debug Not Printing

--- reports/thread-pool.rkt
+++ reports/thread-pool.rkt
@@ -42,8 +42,10 @@
     (with-handlers ([(const #t) (λ (e) (list 'error e (bf-precision)))])
       (setup (test-program test) (test-samplers test)
              (λ (alt)
-                  (parameterize ([*debug-port* (open-output-file (file "debug.txt") #:exists 'replace)])
-                    (list 'good alt (improve alt (*num-iterations*)) (*points*) (*exacts*)))))))
+	       (parameterize ([*debug-port* (open-output-file (file "debug.txt") #:exists 'replace)]
+			      [*debug* #t])
+		 (begin0 (list 'good alt (improve alt (*num-iterations*)) (*points*) (*exacts*))
+		   (close-output-port (*debug-port*))))))))
 
   (let* ([start-time (current-inexact-milliseconds)] [eng (engine run-casio)])
     (engine-run *timeout* eng)
