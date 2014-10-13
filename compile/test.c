07870e56fcac3ec7e5391de86b66a2857bd649bc
Pavel Panchekha <me@pavpanchekha.com>
2014-10-13 10:26:59 -0700

    Changes to figures and data, still broken

--- compile/test.c
+++ compile/test.c
@@ -44,13 +44,13 @@ unsigned int ulpf(float x, float y) {
         if (y == 0) y = fabsf(y); // -0 == 0
 
         if (x != x && y != y) return 0;
-        if (x != x) return INT_MIN;
-        if (y != y) return INT_MIN;
+        if (x != x) return INT_MIN; // Maximum error
+        if (y != y) return INT_MIN; // Maximum error
 
-        int xx = *((unsigned int*) &x);
+        unsigned int xx = *((unsigned int*) &x);
         xx = xx < 0 ? INT_MIN - xx : xx;
 
-        int yy = *((unsigned int*)&y);
+        unsigned int yy = *((unsigned int*) &y);
         yy = yy < 0 ? INT_MIN - yy : yy;
 
         return xx >= yy ? xx - yy : yy - xx;
@@ -61,13 +61,13 @@ unsigned long long ulpd(double x, double y) {
         if (y == 0) y = fabs(y); // -0 == 0
 
         if (x != x && y != y) return 0;
-        if (x != x) return LLONG_MIN;
-        if (y != y) return LLONG_MIN;
+        if (x != x) return LLONG_MIN; // Maximum error
+        if (y != y) return LLONG_MIN; // Maximum error
 
         unsigned long long xx = *((unsigned long long*) &x);
         xx = xx < 0 ? LLONG_MIN - xx : xx;
 
-        unsigned long long yy = *((unsigned long long*)&y);
+        unsigned long long yy = *((unsigned long long*) &y);
         yy = yy < 0 ? LLONG_MIN - yy : yy;
 
         return xx >= yy ? xx - yy : yy - xx;
@@ -85,7 +85,14 @@ double rand_double() {
         return *(double*)&c;
 }
 
-double *get_random(int nums) {
+float rand_float() {
+        unsigned int c0 = rand()&0xffff;
+        unsigned int c1 = rand()&0xffff;
+        unsigned int c = ((c1<<16) | c0);
+        return *(float*)&c;
+}
+
+double *get_random_doubles(int nums) {
         int i;
         double *arr = malloc(sizeof(double) * nums * NARGS);
         for (i = 0; i < nums * NARGS; i++) {
@@ -96,54 +103,54 @@ double *get_random(int nums) {
 
 #define SETUP()                                                         \
         struct timespec start, end;                                     \
-        double rtime, zero;                                              \
+        double rtime, zero;                                             \
         int i, maxi;                                                    \
         unsigned long long int max = 0;                                 \
         double total = 0;                                               \
-        double *rands;                                                   \
+        float *randsf;                                                  \
+        double *randsd;                                                 \
+        double *randsm;                                                 \
         double *out, *correct;                                          \
         setup_mpfr_f_im();
 
 #define CALIBRATE(iter)                                         \
         clock_gettime(CLOCK_MONOTONIC, &start);                 \
         for (i = 0; i < iter; i++) {                            \
-                out[i] = 1 / rands[NARGS*i];                    \
+                out[i] = 1 / randsd[NARGS*i];                    \
         }                                                       \
         clock_gettime(CLOCK_MONOTONIC, &end);              \
         zero = (end.tv_sec - start.tv_sec) * 1.0e9 + (end.tv_nsec - start.tv_nsec);
 
-#define TEST(type, iter)                                                \
+#define TEST(io, type, iter)                                             \
         clock_gettime(CLOCK_MONOTONIC, &start);                         \
         for (i = 0; i < iter; i++) {                                    \
-                out[i] = EVAL(f_##type);                                \
+                out[i] = EVAL(rands##type, f_##io##type);                     \
         }                                                               \
         clock_gettime(CLOCK_MONOTONIC, &end);                           \
         rtime = (end.tv_sec - start.tv_sec) * 1.0e9 + (end.tv_nsec - start.tv_nsec);
 
 #if NARGS == 1
-#define EVAL(f) f(rands[i])
+#define EVAL(rands, f) f(rands[i])
 #elif NARGS == 2
-#define EVAL(f) f(rands[2*i], rands[2*i + 1])
+#define EVAL(rands, f) f(rands[2*i], rands[2*i + 1])
 #elif NARGS == 3
-#define EVAL(f) f(rands[3*i], rands[3*i + 1], rands[3*i + 2])
+#define EVAL(rands, f) f(rands[3*i], rands[3*i + 1], rands[3*i + 2])
 #elif NARGS == 4
-#define EVAL(f) f(rands[4*i], rands[4*i + 1], rands[4*i + 2], rands[4*i + 3])
+#define EVAL(rands, f) f(rands[4*i], rands[4*i + 1], rands[4*i + 2], rands[4*i + 3])
 #elif NARGS == 5
-#define EVAL(f) f(rands[5*i], rands[5*i + 1], rands[5*i + 2], rands[5*i + 3], rands[5*i + 4])
+#define EVAL(rands, f) f(rands[5*i], rands[5*i + 1], rands[5*i + 2], rands[5*i + 3], rands[5*i + 4])
 #elif NARGS == 6
-#define EVAL(f) f(rands[6*i], rands[6*i + 1], rands[6*i + 2], rands[6*i + 3], rands[6*i + 4], rands[6*i + 5])
+#define EVAL(rands, f) f(rands[6*i], rands[6*i + 1], rands[6*i + 2], rands[6*i + 3], rands[6*i + 4], rands[6*i + 5])
 #else
-#define EVAL(f) abort()
+#define EVAL(rands, f) abort()
 #endif
 
 #define CHECK(io, type, iter)                                            \
         maxi = -1;                                                      \
         max = total = 0;                                                \
         for (i = 0; i < iter; i++) {                                    \
-                /*printf("in[i] = %g; out[i] = %g; cor[i] = %g", rands[i], out[i], correct[i]);*/ \
                 if (1 / correct[i] != 0 && correct[i] == correct[i]) {  \
                         unsigned long long int error = ulp##type(out[i], correct[i]); \
-                        /*printf(" error = %g\n", log(error + 1.0) / log(2));*/ \
                         if (error > max) { maxi = i; max = error; }     \
                         total += log(error + 1.0) / log(2);             \
                 }                                                       \
@@ -153,7 +160,12 @@ double *get_random(int nums) {
 
 #define SAMPLE(iter)                                                    \
         srand(time(NULL));                                              \
-        rands = get_random(iter);                                       \
+        randsd = get_random_doubles(iter);                              \
+        randsm = randsd;                                                \
+        randsf = malloc(sizeof(float) * iter);                          \
+        for (i = 0; i < iter; i++) {                                    \
+                randsf[i] = (float) randsd[i];                          \
+        }                                                               \
         out = malloc(sizeof(double) * iter);
 
 #define SAVE(iter)                                                      \
@@ -180,19 +192,19 @@ int main(int argc, char** argv) {
         SAMPLE(iter);
         CALIBRATE(iter);
 
-        TEST(im, iter);
+        TEST(i, m, iter);
         SAVE(iter);
 
-        TEST(if, iter);
+        TEST(i, f, iter);
         CHECK(i, f, iter);
 
-        TEST(id, iter);
+        TEST(i, d, iter);
         CHECK(i, d, iter);
 
-        TEST(of, iter);
+        TEST(o, f, iter);
         CHECK(o, f, iter);
 
-        TEST(od, iter);
+        TEST(o, d, iter);
         CHECK(o, d, iter);
 
         return 0;
