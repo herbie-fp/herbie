ab9d8a8540212cc7f581e2e033c779ab6f098964
Pavel Panchekha <me@pavpanchekha.com>
2014-11-04 14:32:45 -0800

    First pass at a test of the exact evaluation pipeline

---
+++ test/exacts-test.rkt
@@ -0,0 +1,27 @@
+#lang racket
+
+(require rackunit)
+(require casio/config)
+(require casio/programs)
+(require casio/points)
+(require casio/test)
+(require math/bigfloat)
+(require casio/load-tests)
+
+(define tests (load-tests "../bench/hamming"))
+
+(define exacts-test
+  (test-suite
+   "Test that exact evaluation matches 65536-bit floating point"
+   (for ([test tests])
+     (bf-precision (*precision-step*))
+     (printf ">> ~a" (test-program test))
+     (define ctx (prepare-points (test-program test) (test-samplers test)))
+     (printf " (at ~a bits)\n" (bf-precision))
+     (bf-precision 65536)
+     (define f (eval-prog (test-program test) mode:bf))
+     (for ([(pt ex) (in-pcontext ctx)])
+       (with-check-info
+        (['point pt] ['program (test-program test)])
+        (when (not (= ex (f pt)))
+          (check-equal? ex (f pt))))))))
