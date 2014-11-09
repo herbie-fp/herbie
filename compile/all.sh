b9c90f9b167136d29df5b963589fecf8a38614be
Pavel Panchekha <me@pavpanchekha.com>
2014-11-08 23:27:38 -0800

    Added shortnames to the build system

--- compile/all.sh
+++ compile/all.sh
@@ -2,7 +2,9 @@
 
 line () { tail $1 -n+$2 | head -n1; }
 
-for i in $1*.out; do line $i 1 | cut -c 4-; done > $1.names.csv
+for i in $1*.out; do
+    line $i 1 | cut -c 4- | paste -d, - shortnames.csv
+done > $1.names.csv
 for i in $1*.out; do line $i 3; done > $1.if.csv
 for i in $1*.out; do line $i 4; done > $1.id.csv
 for i in $1*.out; do line $i 5; done > $1.of.csv
