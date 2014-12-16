dcfcd437df9aea083056fe89320e743162be0521
Alex Sanchez-Stern <alex.sanchezstern@hotmail.com>
2014-12-16 14:07:12 -0800

    Seperately Print Results To File For Num-Points Test

--- test/num-points-test.sh
+++ test/num-points-test.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 # RUN this from herbie root!
+echo "" > test/num-points-test-results.csv
 for power in `seq 2 11`; do
     points=`awk 'BEGIN{print 2^'$power'}'`
     racket reports/make-report.rkt -s $points bench/hamming/
     racket compile/results-to-csv.rkt graphs/results.herbie.dat graphs/results.herbie.csv
     awk -F , 'BEGIN{totalPointsRecovered=0; totalTime=0}\
 { totalPointsRecovered += $5; totalTime += $6} \
-END {print '$points',",",(log(totalPointsRecovered)/log(2)), ",", (totalTime / 1000)}' graphs/results.herbie.csv
-done > test/num-points-test-results.csv
+END {print '$points',",",(log(totalPointsRecovered)/log(2)), ",", (totalTime / 1000)}' graphs/results.herbie.csv >> test/num-points-test-results.csv
+done
