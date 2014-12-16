d42353f1288a4ac97f7117d5f9f93513333b087e
Alex Sanchez-Stern <alex.sanchezstern@hotmail.com>
2014-12-15 21:20:43 -0800

    Fixed awk script

--- test/num-points-test.sh
+++ test/num-points-test.sh
@@ -4,7 +4,7 @@ for power in `seq 2 11`; do
     points=`awk 'BEGIN{print 2^'$power'}'`
     racket reports/make-report.rkt -s $points bench/hamming/
     racket compile/results-to-csv.rkt graphs/results.herbie.dat graphs/results.herbie.csv
-    awk 'BEGIN{print '$points',","} \
+    awk -F , 'BEGIN{totalPointsRecovered=0; totalTime=0}\
 { totalPointsRecovered += $5; totalTime += $6} \
-END {print totalPointsRecovered, ",", totalTime}' graphs/results.herbie.csv
+END {print '$points',","totalPointsRecovered, ",", totalTime}' graphs/results.herbie.csv
 done > test/num-points-test-results.csv
