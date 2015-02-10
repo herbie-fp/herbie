3d94042e0b9cf57697799f447741a4a195b1354d
Alex Sanchez-Stern <alex.sanchezstern@hotmail.com>
2015-02-10 12:22:20 -0800

    Allow clearing test cache for editing tests

---
+++ herbie/load-tests.rkt
@@ -0,0 +1,41 @@
+#lang racket
+
+(require "common.rkt")
+(require "programs.rkt")
+(require "test.rkt")
+(require "points.rkt")
+(require "alternative.rkt")
+
+(define *test-cache* (make-hash))
+
+(provide load-tests clear-cache!)
+
+(define (load-file p)
+  (parameterize ([*tests* '()])
+    (dynamic-require p 0)
+    (if (null? (*tests*))
+        (begin (hash-ref *test-cache* p '()))
+        (begin (hash-set! *test-cache* p (*tests*))
+               (*tests*)))))
+(define (clear-cache!)
+  (set! *test-cache* (make-hash)))
+
+(define (is-racket-file? f)
+  (and (equal? (filename-extension f) #"rkt") (file-exists? f)))
+
+(define (walk-tree p callback)
+  (cond
+   [(file-exists? p)
+    (callback p)]
+   [(directory-exists? p)
+    (for ([obj (directory-list p #:build? #t)])
+      (walk-tree obj callback))]))
+
+(define (load-tests [path "../bench/"])
+  (define (handle-file sow p)
+    (when (is-racket-file? p)
+      (sow (load-file p))))
+
+  (apply append
+         (reap [sow]
+               (walk-tree path (curry handle-file sow)))))
