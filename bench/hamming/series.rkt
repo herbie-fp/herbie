a57a0a418df9b5de2efa7a702bd9ea9fd54069ed
Pavel Panchekha <me@pavpanchekha.com>
2015-02-09 20:23:26 -0800

    Added bounds to the Hamming series expansion problems

--- bench/hamming/series.rkt
+++ bench/hamming/series.rkt
@@ -1,22 +1,24 @@
 #lang racket
 (require "../../herbie/test.rkt")
 
-(herbie-test (x)
+(herbie-test ([x (< -0.00017 default)])
   "NMSE example 3.7"
   (- (exp x) 1)
   (* x (+ 1 (/ x 2) (/ (sqr x) 6))))
 
-(herbie-test (N)
+(herbie-test ([N (> default 6.8e15)])
   "NMSE example 3.8"
   (- (* (+ N 1) (log (+ N 1))) (* N (log N)) 1)
   (- (log (+ N 1)) (- (/ 1 (* 2 N)) (- (/ 1 (* 3 (sqr N))) (/ 4 (expt N 3))))))
 
-(herbie-test (x)
+(herbie-test ([x (< -.026 default .026)])
   "NMSE example 3.9"
   (- (/ 1 x) (cotan x))
-  (* (/ x 3) (+ 1 (/ (sqr x) 15))))
+  (if (< (abs x) .026)
+      (* (/ x 3) (+ 1 (/ (sqr x) 15)))
+      (- (/ 1 x) (cotan x))))
 
-(herbie-test (x)
+(herbie-test ([x (< -1 default 1)])
   "NMSE example 3.10"
   (/ (log (- 1 x)) (log (+ 1 x)))
   (- (+ 1 x (/ (sqr x) 2) (* 5/12 (expt x 3)))))
@@ -25,7 +27,7 @@
   "NMSE problem 3.4.1"
   (/ (- 1 (cos x)) (sqr x)))
 
-(herbie-test (a b eps)
+(herbie-test (a b [eps (< -1 default 1)])
   "NMSE problem 3.4.2"
   (/ (* eps (- (exp (* (+ a b) eps)) 1))
      (* (- (exp (* a eps)) 1) (- (exp (* b eps)) 1)))
