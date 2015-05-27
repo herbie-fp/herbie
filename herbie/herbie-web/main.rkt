8e1d10c06b6014b8079cf0141d8949be4afb1f26
Alex Sanchez-Stern <alex.sanchezstern@hotmail.com>
2015-05-27 03:39:11 -0700

    High level method for selecting location

--- herbie/herbie-web/main.rkt
+++ herbie/herbie-web/main.rkt
@@ -5,7 +5,7 @@
 (require "../points.rkt")
 (require "../localize-error.rkt")
 
-(struct session-data (alts pcontext pcontext-extended))
+(struct sdat (alts pcontext pcontext-extended children locations chosen-alt-idx cur-combo best-axis))
 
 (define *graph-name* (make-parameter "graph.png"))
 
@@ -19,15 +19,46 @@
     (define pcontext-extended (parameterize ([*num-points* 1024]) (prepare-points prog samplers)))
     (parameterize ([*pcontext* context] [*analyze-context* context])
       (define alt (simplify-alt (make-alt prog)))
-      (define session-dat (session-data (list alt) pcontext pcontext-extended))
       (define locs (localize-error alt))
       (define axis (find-best-axis alt))
+      (define session-data (sdat (list alt) pcontext pcontext-extended '() locs 0 alt axis))
       (define response
 	(hash
-	 'formula (texify-formula prog locs)
-	 'error-graph (*graph-name*)
-	 'loc-ranges (make-ranges alt locs axis)))
+	 "formula" (texify-formula prog locs)
+	 "error-graph" (*graph-name*)
+	 "loc-ranges" (make-ranges alt locs axis)))
       (define content
 	(hash
+	 "start.json" response
 	 (*graph-name*) (graph-error alt axis)))
-      (values content response))))
+      (values content session-data))))
+
+(define (select-location data location-idx)
+  (let ([loc (list-ref (sdat-locations data) location-idx)]
+	[alt (sdat-chosen-alt data)])
+    (define children (expand-at-loc alt loc))
+    (define response
+      (hash
+       "selected_formula" (texify-formula prog (list loc))
+       "calts" (for/list ([child children] [idx (in-naturals)])
+		 (hash
+		  "steps" (make-steps child alt)
+		  "graph" (format "child~aerror.png" idx)))))
+    (define content
+      (apply make-hash
+	     (cons "select-location.json" response)
+	     (for/list ([child children]
+			[idx (in-range (length children))])
+	       (cons (format "child~aerror.png" idx)
+		     (graph-error child (sdat-best-axis data) cur-combo)))))
+    (define session-data (sdat (list alt)
+			       (sdat-pcontext data) (sdat-pcontext-extended data)
+			       '()
+			       (list loc)
+			       (sdat-chosen-alt-idx data)
+			       (sdat-cur-combo data)
+			       (sdat-best-axis data)))
+    (values content session-data)))
+
+(define (sdat-chosen-alt data)
+  (list-ref (sdat-alts data) (sdat-chosen-alt-idx data)))
