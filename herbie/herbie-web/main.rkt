4fce8e33adcbff5da89aa06549f13c6913bff46d
Alex Sanchez-Stern <alex.sanchezstern@hotmail.com>
2015-05-27 17:35:09 -0700

    Main code for herbie viz deep-backend

--- herbie/herbie-web/main.rkt
+++ herbie/herbie-web/main.rkt
@@ -1,13 +1,24 @@
 #lang racket
 
+;;======== Dependencies ===========
 (require "../common.rkt")
 (require "../main.rkt")
 (require "../points.rkt")
 (require "../localize-error.rkt")
+(require "../programs.rkt")
+(require "../alternative.rkt")
 
+(require "tools.rkt")
+
+;;========= Structures and Parameters ===========
 (struct sdat (alts pcontext pcontext-extended children locations chosen-alt-idx cur-combo best-axis))
+(define (sdat-chosen-alt data)
+  (list-ref (sdat-alts data) (sdat-chosen-alt-idx data)))
 
 (define *graph-name* (make-parameter "graph.png"))
+
+;;========= Top Level Interface ============
+(provide start-session select-location choose-children pick-next)
 
 ;; Starts a session with the herbie-web-viz. Returns a list of two
 ;; things: a table of objects to respond with mapped to their names,
@@ -17,7 +28,7 @@
     (define samplers (map (curryr cons sample-default) (program-variables prog)))
     (define pcontext (parameterize ([*num-points* 64]) (prepare-points prog samplers)))
     (define pcontext-extended (parameterize ([*num-points* 1024]) (prepare-points prog samplers)))
-    (parameterize ([*pcontext* context] [*analyze-context* context])
+    (parameterize ([*pcontext* pcontext] [*analyze-context* pcontext])
       (define alt (simplify-alt (make-alt prog)))
       (define locs (localize-error alt))
       (define axis (find-best-axis alt))
@@ -26,20 +37,26 @@
 	(hash
 	 "formula" (texify-formula prog locs)
 	 "error-graph" (*graph-name*)
-	 "loc-ranges" (make-ranges alt locs axis)))
+	 "loc-ranges" (make-ranges pcontext alt locs axis)))
       (define content
 	(hash
 	 "start.json" response
-	 (*graph-name*) (graph-error alt axis)))
+	 (*graph-name*) (graph-error pcontext-extended alt axis)))
       (values content session-data))))
 
+;; Starts phase two, candidate selection. Takes the session from the
+;; session, and a location to improve at, and returns two values: the
+;; content table to send back to the user as specified above, and the
+;; new session state.
 (define (select-location data location-idx)
   (let ([loc (list-ref (sdat-locations data) location-idx)]
-	[alt (sdat-chosen-alt data)])
+	[alt (sdat-chosen-alt data)]
+        [pcontext-extended (sdat-pcontext-extended data)]
+        [cur-combo (sdat-cur-combo data)])
     (define children (expand-at-loc alt loc))
     (define response
       (hash
-       "selected_formula" (texify-formula prog (list loc))
+       "selected_formula" (texify-formula (alt-program alt) (list loc))
        "calts" (for/list ([child children] [idx (in-naturals)])
 		 (hash
 		  "steps" (make-steps child alt)
@@ -50,15 +67,69 @@
 	     (for/list ([child children]
 			[idx (in-range (length children))])
 	       (cons (format "child~aerror.png" idx)
-		     (graph-error child (sdat-best-axis data) cur-combo)))))
+		     (graph-error pcontext-extended child (sdat-best-axis data) cur-combo)))))
     (define session-data (sdat (list alt)
-			       (sdat-pcontext data) (sdat-pcontext-extended data)
+			       (sdat-pcontext data) pcontext-extended
 			       '()
 			       (list loc)
 			       (sdat-chosen-alt-idx data)
-			       (sdat-cur-combo data)
+                               cur-combo
 			       (sdat-best-axis data)))
     (values content session-data)))
 
-(define (sdat-chosen-alt data)
-  (list-ref (sdat-alts data) (sdat-chosen-alt-idx data)))
+;; Starts phase three, choosing the next alt to work on. Takes in
+;; addition to session data a list of indicies into the list of
+;; children to keep. Returns as above.
+(define (choose-children data child-idxs)
+  (let* ([alts* (for/list ([idx child-idxs])
+                  (list-ref (sdat-children data) idx))]
+         [pcontext* (sdat-pcontext data)]
+         [pcontext-extended* (sdat-pcontext-extended data)]
+         [children* '()]
+         [locations* '()]
+         [chosen-alt-idx* '()]
+         [cur-combo* (make-combo alts*)]
+         [best-axis* (sdat-best-axis data)])
+    (define session-data
+      (sdat alts* pcontext* pcontext-extended* children* locations*
+            chosen-alt-idx* cur-combo* best-axis*))
+    (define response
+      (hash 
+       "combo_graph" (*graph-name*)
+       "candidates" (for/list ([alt alts*] [idx (in-naturals)])
+                      (hash
+                       "id" idx
+                       "formula" (texify-formula (alt-program alt))
+                       "graph" (format "cand~aerror.png")))))
+    (define content
+      (apply make-hash
+             (cons "choose-children.json" response)
+             (for/list ([alt alts*] [idx (in-naturals)])
+               (cons (format "cand~aerror.png" idx)
+                     (graph-error pcontext-extended* alt best-axis* cur-combo*)))))
+    (values content session-data)))
+
+;; Ends phase three and starts over, by picking the next candidate to
+;; work on. Takes a candidate index of the candidate to work on, and
+;; returns as above.
+(define (pick-next data cand-idx)
+  (let* ([alt (list-ref (sdat-alts data) cand-idx)]
+         [locs* (localize-error alt)]
+         [pcontext (sdat-pcontext data)]
+         [pcontext-extended (sdat-pcontext-extended data)]
+         [axis (sdat-best-axis data)]
+         [combo (sdat-cur-combo data)])
+    (define session-data
+      (sdat (sdat-alts data) pcontext pcontext-extended
+            '() locs* cand-idx
+            combo axis))
+    (define response
+      (hash
+       "formula" (texify-formula (alt-program alt))
+       "error_graph" (*graph-name*)
+       "loc_ranges" (make-ranges pcontext alt locs* axis)))
+    (define content
+      (hash
+       "pick-next.json" response
+       (*graph-name*) (graph-error (sdat-pcontext-extended data) alt axis combo)))
+    (values content session-data)))
--- herbie/herbie-web/tools.rkt
+++ herbie/herbie-web/tools.rkt
@@ -1,2 +1,68 @@
 #lang racket
 
+(require "../alternative.rkt")
+(require "../programs.rkt")
+(require "../matcher.rkt")
+(require "../infer-regimes.rkt")
+
+(provide find-best-axis texify-formula make-ranges graph-error
+         expand-at-loc make-steps make-combo)
+
+;; Find the axis that best portrays the error behavior
+(define (find-best-axis alt) (car (program-variables (alt-program alt))))
+;; Generate the tex for the given prog, with the given locations
+;; highlighted and given MathJax ID's
+(define (texify-formula prog [locs '()]) "x")
+;; Given a context and an alt and some locations, identify which
+;; ranges of error coorespond to which locations along the given axis,
+;; and generate list of hash table objects for them.
+(define (make-ranges context alt locs axis) '())
+;; Draw the graph of average error using the given points for the
+;; given alt, along the given axis. If combo is given draw it also on
+;; the same graph in a different color.
+(define (graph-error context alt axis [combo #f])
+  (define (sow-data sow data-points theme)
+    (sow (error-points data-points pnts #:axis axis #:color theme))
+    (sow (error-avg data-points pnts #:axis axis #:color theme)))
+  (let ([points (for/list ([(p e) (in-pcontext context)]) p)])
+    (parameterize ([*pcontext* context])
+      (reap [sow]
+            (sow (error-avg (alt-errors alt) points #:axis axis #:color *red-theme*))
+            (when combo
+              (sow (error-avg (alt-errors alt) points #:axis axis #:color *blue-theme*)))))))
+;; Generate at most three or four children for the given alt at the
+;; given location.
+(define (expand-at-loc alt loc)
+  (general-filter
+   (append (taylor-filter (taylor-alt alt loc))
+           (rewrite-filter (alt-rewrite alt #:root loc)))))
+;; Generate the list of steps hash objects representing the changes
+;; between the parent and the child.
+(define (make-steps child parent)
+  (let steps-left ([cur child] [steps '()])
+    (cond [(not cur) (error "The given parent is not a parent of the child!")]
+          [(equal? cur parent)
+           steps]
+          [#t (steps-left (alt-prev cur)
+                          (cons
+                           (hash
+                            "rule" (let ([rule (change-rule (alt-change cur))])
+                                     (if (equal? (rule-name rule) 'simplify)
+                                         "simplify"
+                                         (texify-formula (change-rule (alt-change cur)))))
+                            "prog" (texify-formula (alt-program cur)))
+                           steps))])))
+    
+;; Combine the given alternatives into the best combination.
+(define (make-combo alts)
+  (match-let ([`(,splitpoints ,involved-alts) (infer-splitpoints alts)])
+    (if (= (length involved-alts) 1)
+        (car involved-alts)
+        (combine-alts splitpoints involved-alts))))
+
+;; =============== Lower level helper functions =============
+
+;; Filter children
+(define (general-filter alts) alts)
+(define (taylor-filter alts) alts)
+(define (rewrite-filter alts) alts)
