cb6467928e7ca2de8a3c85c09b561641131dcffe
Alex Sanchez-Stern <alex.sanchezstern@hotmail.com>
2015-05-30 14:47:39 -0700

    bugfixes

--- herbie/herbie-web/tools.rkt
+++ herbie/herbie-web/tools.rkt
@@ -1,9 +1,17 @@
 #lang racket
 
+;; ================== Dependencies ===================
 (require "../alternative.rkt")
 (require "../programs.rkt")
 (require "../matcher.rkt")
 (require "../infer-regimes.rkt")
+(require "../plot.rkt")
+(require "../points.rkt")
+(require "../common.rkt")
+(require "../main.rkt")
+(require "../compile/tex.rkt")
+
+;; ================== Interface =======================
 
 (provide find-best-axis texify-formula make-ranges graph-error
          expand-at-loc make-steps make-combo)
@@ -21,21 +29,21 @@
 ;; given alt, along the given axis. If combo is given draw it also on
 ;; the same graph in a different color.
 (define (graph-error context alt axis [combo #f])
-  (define (sow-data sow data-points theme)
-    (sow (error-points data-points pnts #:axis axis #:color theme))
-    (sow (error-avg data-points pnts #:axis axis #:color theme)))
-  (let ([points (for/list ([(p e) (in-pcontext context)]) p)])
+  (let ([points (for/list ([(p e) (in-pcontext context)]) p)]
+        [vars (program-variables (alt-program alt))])
     (parameterize ([*pcontext* context])
       (reap [sow]
-            (sow (error-avg (alt-errors alt) points #:axis axis #:color *red-theme*))
+            (sow (error-avg (alt-errors alt) points #:axis axis
+                            #:vars vars #:color *red-theme*))
             (when combo
-              (sow (error-avg (alt-errors alt) points #:axis axis #:color *blue-theme*)))))))
+              (sow (error-avg (alt-errors alt) points #:axis axis
+                              #:vars vars #:color *blue-theme*)))))))
 ;; Generate at most three or four children for the given alt at the
 ;; given location.
 (define (expand-at-loc alt loc)
   (general-filter
    (append (taylor-filter (taylor-alt alt loc))
-           (rewrite-filter (alt-rewrite alt #:root loc)))))
+           (rewrite-filter (alt-rewrite-rm alt #:root loc)))))
 ;; Generate the list of steps hash objects representing the changes
 ;; between the parent and the child.
 (define (make-steps child parent)
@@ -43,22 +51,24 @@
     (cond [(not cur) (error "The given parent is not a parent of the child!")]
           [(equal? cur parent)
            steps]
-          [#t (steps-left (alt-prev cur)
-                          (cons
-                           (hash
-                            "rule" (let ([rule (change-rule (alt-change cur))])
-                                     (if (equal? (rule-name rule) 'simplify)
-                                         "simplify"
-                                         (texify-formula (change-rule (alt-change cur)))))
-                            "prog" (texify-formula (alt-program cur)))
-                           steps))])))
+          [#t (steps-left
+               (alt-prev cur)
+               (cons
+                (hash
+                 "rule" (let ([rule (change-rule (alt-change cur))])
+                          (if (equal? (rule-name rule) 'simplify)
+                              "simplify"
+                              (texify-formula (change-rule (alt-change cur)))))
+                 "prog" (texify-formula (alt-program cur)))
+                steps))])))
     
 ;; Combine the given alternatives into the best combination.
 (define (make-combo alts)
-  (match-let ([`(,splitpoints ,involved-alts) (infer-splitpoints alts)])
-    (if (= (length involved-alts) 1)
-        (car involved-alts)
-        (combine-alts splitpoints involved-alts))))
+  (parameterize ([*start-prog* (car alts)])
+    (match-let ([`(,splitpoints ,involved-alts) (infer-splitpoints alts)])
+      (if (= (length involved-alts) 1)
+          (car involved-alts)
+          (combine-alts splitpoints involved-alts)))))
 
 ;; =============== Lower level helper functions =============
 
