cddcbce6f37f95042bddc6031bab0827ea9c5c6d
Pavel Panchekha <me@pavpanchekha.com>
2015-09-01 22:45:03 -0700

    Use Racket-isms in inout.rkt

--- herbie/interface/inout.rkt
+++ herbie/interface/inout.rkt
@@ -1,24 +1,25 @@
 #lang racket
 
 (require "interact.rkt")
+(require "../points.rkt")
 (require "../alternative.rkt")
 (require "../test.rkt")
 (require "../config.rkt")
 
 (define (run)
   (define in-expr (read))
-  (display (format "improving ~a...~n" in-expr) (current-error-port))
-  (define out-prog
-    (alt-program
-     (match in-expr
-       [`(herbie-test . ,_)
-        (let ([tst (parse-test in-expr)])
-          (run-improve (test-program tst) (*num-iterations*)
-                       #:samplers (test-samplers tst)))]
-       [`(,(or 'λ 'lambda) ,vars ,body)
-        (run-improve in-expr (*num-iterations*))]
-       [_ (error "did not recognize input")])))
-  (write out-prog)
-  (newline))
+  (eprintf "improving ~a...\n" in-expr)
+  (define out-alt
+    (match in-expr
+      [`(herbie-test . ,_)
+       (let ([tst (parse-test in-expr)])
+         (set! in-expr (test-program tst))
+         (run-improve (test-program tst) (*num-iterations*)
+                      #:samplers (test-samplers tst)))]
+      [`(,(or 'λ 'lambda) ,vars ,body)
+       (run-improve in-expr (*num-iterations*))]
+      [_ (error "did not recognize input")]))
+  (printf "~a\n" (alt-program out-alt)))
 
-(run)
+(module+ main
+  (run))
