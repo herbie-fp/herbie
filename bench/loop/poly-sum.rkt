4a626d5d1c387441fcf0b1693720b36f77429851
Pavel Panchekha <me@pavpanchekha.com>
2015-09-22 09:14:31 -0700

    Fix the polynomial example
    - Keep x between 0.9 and 1.1; this x^n doesn't rapidly approach 0

--- bench/loop/poly-sum.rkt
+++ bench/loop/poly-sum.rkt
@@ -1,6 +1,6 @@
 
 ;; Inspired by a SPEC Benchmark
-(herbie-test ([coeffs (list 10 double .25)] [x double])
+(herbie-test ([coeffs (list 100 double .25)] [x (< 0.9 double 1.1)])
   "Calculate the value of a polynomial"
   (do-list ([sum 0.0 (+ sum (* coeff x-pow))]
             [x-pow 1 (* x-pow x)])
