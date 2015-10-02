ca5fac71210c3ba2278fcc77c04d6c01f458d60c
Pavel Panchekha <me@pavpanchekha.com>
2015-10-01 22:52:51 -0700

    Associate groups with each ruleset
    - Groups of rulesets can be toggled from the `rules` config group.
    - The current rules are arithmetic, polynomials, fractions, exponents,
      trigonometry, and numerics, where the last one is the special
      numerical functions like expm1, log1p, and hypot.

--- herbie/config.rkt
+++ herbie/config.rkt
@@ -15,7 +15,8 @@
    #hash([precision . (double)]
          [setup    . (simplify)]
          [generate . (rr taylor simplify)]
-         [reduce   . (regimes taylor simplify avg-error)])))
+         [reduce   . (regimes taylor simplify avg-error)]
+         [rules . (arithmetic polynomials fractions exponents trigonometry numerics)])))
 
 (define (toggle-flag! category flag)
   (*flags*
