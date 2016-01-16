edccc6688cd16a1acb15ea1df5159f0996b897e9
Pavel Panchekha <me@pavpanchekha.com>
2016-01-16 12:34:28 -0800

    More bug fix.

--- herbie/reports/make-index.rkt
+++ herbie/reports/make-index.rkt
@@ -61,7 +61,7 @@
         (~r x #:precision 2)]))
 
     (printf "<tr>")
-    (printf "<td title='~a on ~a'>~a</td>"
+    (printf "<td title='~a:~a on ~a'>~a</td>"
             (date-hour date) (~r (date-minute date) #:min-width 2 #:pad-string "0")
             (date->string date) (date->string/short date))
     (printf "<td title='~a'>~a</td>" commit branch)
