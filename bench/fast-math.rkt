3094aac7e79cbdcfef057253bdc012a55a3682b1
Pavel Panchekha <me@pavpanchekha.com>
2016-01-19 15:33:58 -0800

    Change to new format in the Fast-Math tests

--- bench/fast-math.rkt
+++ bench/fast-math.rkt
@@ -1,44 +1,44 @@
 
-; All from the FastMath library for haskell, from
-; https://github.com/liyang/fast-math/blob/master/test/
-; These test Herbie's ability to do basic arithmetic
+;; All from the FastMath library for haskell, from
+;; https://github.com/liyang/fast-math/blob/master/test/
+;; These test Herbie's ability to do basic arithmetic
 
-(herbie-test (d1 d2 d3)
-            "FastMath dist"
-            (+ (* d1 d2) (* d1 d3))
-            (* d1 (+ d2 d3)))
+(lambda (d1 d2 d3)
+  #:name "FastMath dist"
+  (+ (* d1 d2) (* d1 d3))
+  #:target (* d1 (+ d2 d3)))
 
-(herbie-test (d)
-            "FastMath test1"
-            (+ (* d 10) (* d 20))
-            (* d 30))
+(lambda (d)
+  #:name "FastMath test1"
+  (+ (* d 10) (* d 20))
+  #:target (* d 30))
 
-(herbie-test (d1 d2)
-            "FastMath test2"
-            (+ (* d1 10) (* d1 d2) (* d1 20))
-            (* d1 (+ 30 d2)))
+(lambda (d1 d2)
+  #:name "FastMath test2"
+  (+ (* d1 10) (* d1 d2) (* d1 20))
+  #:target (* d1 (+ 30 d2)))
 
-(herbie-test (d1 d2 d3)
-            "FastMath dist3"
-            (+ (* d1 d2) (* (+ d3 5) d1) (* d1 32))
-            (* d1 (+ 37 d3 d2)))
+(lambda (d1 d2 d3)
+  #:name "FastMath dist3"
+  (+ (* d1 d2) (* (+ d3 5) d1) (* d1 32))
+  #:target (* d1 (+ 37 d3 d2)))
 
-(herbie-test (d1 d2 d3 d4)
-            "FastMath dist4"
-            (- (+ (- (* d1 d2) (* d1 d3)) (* d4 d1)) (* d1 d1))
-            (* d1 (- (+ (- d2 d3) d4) d1)))
+(lambda (d1 d2 d3 d4)
+  #:name "FastMath dist4"
+  (- (+ (- (* d1 d2) (* d1 d3)) (* d4 d1)) (* d1 d1))
+  #:target (* d1 (- (+ (- d2 d3) d4) d1)))
 
-(herbie-test (d1 d2 d3)
-            "FastMath test3"
-            (+ (* d1 3) (* d1 d2) (* d1 d3))
-            (* d1 (+ 3 d2 d3)))
+(lambda (d1 d2 d3)
+  #:name "FastMath test3"
+  (+ (* d1 3) (* d1 d2) (* d1 d3))
+  #:target (* d1 (+ 3 d2 d3)))
 
-(herbie-test (d1)
-            "FastMath repmul"
-            (* d1 d1 d1 d1)
-            (expt d1 4))
+(lambda (d1)
+  #:name "FastMath repmul"
+  (* d1 d1 d1 d1)
+  #:target (expt d1 4))
 
-(herbie-test (d1)
-            "FastMath test5"
-            (* d1 (* d1 (* d1 d1) d1 d1 (* d1 d1) d1) d1)
-            (expt d1 10))
+(lambda (d1)
+  #:name "FastMath test5"
+  (* d1 (* d1 (* d1 d1) d1 d1 (* d1 d1) d1) d1)
+  #:target (expt d1 10))
