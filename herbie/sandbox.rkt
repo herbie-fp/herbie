7b4725e75395b0ba8dd67dadf7a3f0ea6f5a6ef4
Pavel Panchekha <me@pavpanchekha.com>
2016-01-26 16:35:20 -0800

    Forgot to include new file -.-

---
+++ herbie/sandbox.rkt
@@ -0,0 +1,85 @@
+#lang racket
+(require profile)
+(require math/bigfloat)
+(require racket/engine)
+
+(require "interface/interact.rkt")
+(require "common.rkt")
+(require "debug.rkt")
+(require "reports/datafile.rkt")
+(require "programs.rkt")
+(require "points.rkt")
+(require "test.rkt")
+(require "alternative.rkt")
+(require "main.rkt")
+
+(provide get-test-result *reeval-pts* *timeout*)
+
+(define *reeval-pts* (make-parameter 8000))
+(define *timeout* (make-parameter (* 1000 60 10)))
+
+(define (default-setup)
+  (set-debug-level! #t #t)
+  (set-debug-level! 'backup-simplify #f))
+
+(define (get-p&es context)
+  (call-with-values
+      (λ ()
+        (for/lists (pts exs)
+            ([(pt ex) (in-pcontext context)])
+          (values pt ex)))
+    list))
+
+(define (get-test-result test rdir
+                         #:setup! [setup! default-setup]
+                         #:seed [seed #f] #:profile [profile? #f])
+  (define (file name) (build-path rdir name))
+  (set-seed! seed)
+
+  (define (on-error e) `(error ,e ,(bf-precision)))
+
+  (define (compute-result test)
+    (call-with-output-file (file "debug.txt") #:exists 'replace
+      (λ (p)
+        (parameterize ([*debug-port* p])
+          (setup!)
+          (with-handlers ([(const #t) on-error])
+            (match-define (list alt context)
+                          (run-improve (test-program test)
+                                       (*num-iterations*)
+                                       #:get-context #t
+                                       #:samplers (test-samplers test)))
+            `(good ,(make-alt (test-program test)) ,alt ,context))))))
+
+  (define (in-engine _)
+    (if profile?
+        (with-output-to-file (file "profile.txt") #:exists 'replace
+          (λ () (profile (compute-result test))))
+        (compute-result test)))
+  
+  (let* ([start-time (current-inexact-milliseconds)] [eng (engine in-engine)])
+    (engine-run (*timeout*) eng)
+
+    (match (engine-result eng)
+      [`(good ,start ,end ,context)
+       (define newcontext
+         (parameterize ([*num-points* (*reeval-pts*)])
+           (prepare-points (alt-program start) (test-samplers test))))
+       (match-define (list newpoints newexacts) (get-p&es newcontext))
+       (match-define (list points exacts) (get-p&es context))
+       (test-result test rdir
+                    (- (current-inexact-milliseconds) start-time)
+                    (bf-precision)
+                    start end points exacts
+                    (errors (alt-program start) context)
+                    (errors (alt-program end) context)
+                    newpoints newexacts
+                    (errors (alt-program start) newcontext)
+                    (errors (alt-program end) newcontext)
+                    (if (test-output test)
+                        (errors (test-target test) newcontext)
+                        #f))]
+      [`(error ,e ,bits)
+       (test-failure test bits e (- (current-inexact-milliseconds) start-time) rdir)]
+      [#f
+       (test-timeout test (bf-precision) rdir)])))
