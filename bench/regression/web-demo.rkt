0388e27b64f581ef3e3552580ba81034a49a35ae
Zach Tatlock <ztatlock@cs.washington.edu>
2016-01-26 21:00:30 -0800

    more regression examples

--- bench/regression/web-demo.rkt
+++ bench/regression/web-demo.rkt
@@ -1,3 +1,23 @@
 (herbie-test (x y z)
-  "abs fraction"
+  "abs fraction 1"
   (abs (- (/ (+ x 4) y) (* (/ x y) z))))
+
+(herbie-test (a b)
+  "abs fraction 2"
+  (/ (abs (- a b)) 2))
+
+(herbie-test (f n)
+  "subtraction fraction"
+  (/ (- (+ f n)) (- f n)))
+
+(herbie-test (x)
+  "exp neg sub"
+  (exp (- (- 1 (* x x)))))
+
+(herbie-test (x)
+  "sqrt times"
+  (* (sqrt (- x 1)) (sqrt x)))
+
+(herbie-test (x)
+  "neg log"
+  (- (log (- (/ 1 x) 1))))
