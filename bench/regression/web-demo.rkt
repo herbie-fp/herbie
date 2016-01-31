f0f74f7b3d29c24ef8342fd310744ea4372ba1c6
Zach Tatlock <ztatlock@cs.washington.edu>
2016-01-31 15:15:10 -0800

    add sqrt sqr regression and update to modern test fmt

--- bench/regression/web-demo.rkt
+++ bench/regression/web-demo.rkt
@@ -1,27 +1,31 @@
-(herbie-test (x y z)
-  "abs fraction 1"
+(lambda (x y z)
+  #:name "abs fraction 1"
   (abs (- (/ (+ x 4) y) (* (/ x y) z))))
 
-(herbie-test (a b)
-  "abs fraction 2"
+(lambda (a b)
+  #:name "abs fraction 2"
   (/ (abs (- a b)) 2))
 
-(herbie-test (f n)
-  "subtraction fraction"
+(lambda (f n)
+  #:name "subtraction fraction"
   (/ (- (+ f n)) (- f n)))
 
-(herbie-test (x)
-  "exp neg sub"
+(lambda (x)
+  #:name "exp neg sub"
   (exp (- (- 1 (* x x)))))
 
-(herbie-test (x)
-  "sqrt times"
+(lambda (x)
+  #:name "sqrt times"
   (* (sqrt (- x 1)) (sqrt x)))
 
-(herbie-test (x)
-  "neg log"
+(lambda (x)
+  #:name "neg log"
   (- (log (- (/ 1 x) 1))))
 
-(herbie-test (B x)
-  "VandenBroeck and Keller, Equation (24)"
+(lambda (B x)
+  #:name "VandenBroeck and Keller, Equation (24)"
   (+ (- (* x (cotan B))) (/ 1 (sin B))))
+
+(lambda (x)
+  #:name "sqr sqrt"
+  (- (/ x x) (* (/ 1 x) (sqrt (* x x)))))
