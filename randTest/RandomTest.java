01fbf8c007ec1de4d3bb2253edbd5b87023da170
Jason Qiu <qiuchen0213@gmail.com>
2016-02-07 23:36:13 -0800

    Add command line argument and complete lists of operators

--- randTest/RandomTest.java
+++ randTest/RandomTest.java
@@ -3,27 +3,40 @@ public class RandomTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		OperatorTree opt = new OperatorTree(6);
-		opt.createEmpty();
-		opt.populate(opt.root);
-		printTest(opt);
+		int treeSize = Integer.parseInt(args[0]);
+		int numOfVars = Integer.parseInt(args[1]);
+		int numOfTest = Integer.parseInt(args[2]);
+		
+		OperatorTree opt = new OperatorTree(treeSize, numOfVars);
+		printTest(opt, numOfTest);
 //		opt.preOrder();
 //		OperatorTree.printNode(opt.root);
 		
 	}
 	
 	public static void printTest(OperatorTree opt) {
-		System.out.print("(herbie-test (");
-		String[] varArr = new String[OperatorTree.VARIABLES.size()];
-		OperatorTree.VARIABLES.toArray(varArr);
-		System.out.print(varArr[0]);
-		for (int i = 1; i < varArr.length; i++) {
-			System.out.print(" " + varArr[i]);
+		printTest(opt, 1);
+	}
+	
+	public static void printTest(OperatorTree opt, int n) {
+		for (int i  = 0; i < n; i++) {
+			//initialize tree
+			opt.createEmpty();
+			opt.populate(opt.root);
+			// print test
+			System.out.print("(herbie-test (");
+			String[] varArr = new String[OperatorTree.VARIABLES.size()];
+			OperatorTree.VARIABLES.toArray(varArr);
+			System.out.print(varArr[0]);
+			for (int j = 1; j < varArr.length; j++) {
+				System.out.print(" " + varArr[j]);
+			}
+			System.out.println(")");
+			System.out.println("  \"Random test " + (i+1) + "\"");
+			System.out.print("    " + opt.parse());
+			System.out.println(")");
+			System.out.println();
 		}
-		System.out.println(")");
-		System.out.println("  \"Random test\"");
-		System.out.println("    " + opt.parse());
-		System.out.println(" )");
 	}
 
 }
