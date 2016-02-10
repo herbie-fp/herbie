b22db183ed03588de5d27d25771dc76ceefcde9c
Zach Tatlock <ztatlock@cs.washington.edu>
2016-02-09 21:43:49 -0800

    rand test white space, braces, style

--- randTest/OperatorTree.java
+++ randTest/OperatorTree.java
@@ -115,7 +115,6 @@ public class OperatorTree {
     }
   }
 
-
   public void populate(Node n) {
     if (n != null) {
       if (n.left == null && n.right == null) {
@@ -183,85 +182,90 @@ public class OperatorTree {
 
   // All code below are just printing functions.
   public static void printNode(Node root) {
-        int maxLevel = maxLevel(root);
+    int maxLevel = maxLevel(root);
 
-        printNodeInternal(Collections.singletonList(root), 1, maxLevel);
+    printNodeInternal(Collections.singletonList(root), 1, maxLevel);
+  }
+
+  private static void printNodeInternal(List<Node> nodes, int level, int maxLevel) {
+    if (nodes.isEmpty() || isAllElementsNull(nodes)) {
+      return;
     }
 
-    private static void printNodeInternal(List<Node> nodes, int level, int maxLevel) {
-        if (nodes.isEmpty() || isAllElementsNull(nodes))
-            return;
+    int floor = maxLevel - level;
+    int endgeLines = (int) Math.pow(2, (Math.max(floor - 1, 0)));
+    int firstSpaces = (int) Math.pow(2, (floor)) - 1;
+    int betweenSpaces = (int) Math.pow(2, (floor + 1)) - 1;
 
-        int floor = maxLevel - level;
-        int endgeLines = (int) Math.pow(2, (Math.max(floor - 1, 0)));
-        int firstSpaces = (int) Math.pow(2, (floor)) - 1;
-        int betweenSpaces = (int) Math.pow(2, (floor + 1)) - 1;
+    printWhitespaces(firstSpaces);
 
-        printWhitespaces(firstSpaces);
+    List<Node> newNodes = new ArrayList<Node>();
+    for (Node node : nodes) {
+      if (node != null) {
+          System.out.print(node.data);
+          newNodes.add(node.left);
+          newNodes.add(node.right);
+      } else {
+          newNodes.add(null);
+          newNodes.add(null);
+          System.out.print(" ");
+      }
 
-        List<Node> newNodes = new ArrayList<Node>();
-        for (Node node : nodes) {
-            if (node != null) {
-                System.out.print(node.data);
-                newNodes.add(node.left);
-                newNodes.add(node.right);
-            } else {
-                newNodes.add(null);
-                newNodes.add(null);
-                System.out.print(" ");
-            }
+      printWhitespaces(betweenSpaces);
+    }
+    System.out.println("");
 
-            printWhitespaces(betweenSpaces);
-        }
-        System.out.println("");
-
-        for (int i = 1; i <= endgeLines; i++) {
-            for (int j = 0; j < nodes.size(); j++) {
-                printWhitespaces(firstSpaces - i);
-                if (nodes.get(j) == null) {
-                    printWhitespaces(endgeLines + endgeLines + i + 1);
-                    continue;
-                }
-
-                if (nodes.get(j).left != null)
-                    System.out.print("/");
-                else
-                    printWhitespaces(1);
-
-                printWhitespaces(i + i - 1);
-
-                if (nodes.get(j).right != null)
-                    System.out.print("\\");
-                else
-                    printWhitespaces(1);
-
-                printWhitespaces(endgeLines + endgeLines - i);
-            }
-
-            System.out.println("");
+    for (int i = 1; i <= endgeLines; i++) {
+      for (int j = 0; j < nodes.size(); j++) {
+        printWhitespaces(firstSpaces - i);
+        if (nodes.get(j) == null) {
+          printWhitespaces(endgeLines + endgeLines + i + 1);
+          continue;
         }
 
-        printNodeInternal(newNodes, level + 1, maxLevel);
-    }
-
-    private static void printWhitespaces(int count) {
-        for (int i = 0; i < count; i++)
-            System.out.print(" ");
-    }
-
-    private static int maxLevel(Node node) {
-        if (node == null)
-            return 0;
-
-        return Math.max(maxLevel(node.left), maxLevel(node.right)) + 1;
-    }
-
-    private static boolean isAllElementsNull(List list) {
-        for (Object object : list) {
-            if (object != null)
-                return false;
+        if (nodes.get(j).left != null) {
+          System.out.print("/");
+        } else {
+          printWhitespaces(1);
         }
 
-        return true;
+        printWhitespaces(i + i - 1);
+
+        if (nodes.get(j).right != null) {
+          System.out.print("\\");
+        } else {
+          printWhitespaces(1);
+        }
+
+        printWhitespaces(endgeLines + endgeLines - i);
+      }
+
+      System.out.println("");
     }
+
+    printNodeInternal(newNodes, level + 1, maxLevel);
+  }
+
+  private static void printWhitespaces(int count) {
+    for (int i = 0; i < count; i++)
+      System.out.print(" ");
+  }
+
+  private static int maxLevel(Node node) {
+    if (node == null) {
+      return 0;
+    }
+
+    return Math.max(maxLevel(node.left), maxLevel(node.right)) + 1;
+  }
+
+  private static boolean isAllElementsNull(List list) {
+    for (Object object : list) {
+      if (object != null) {
+        return false;
+      }
+    }
+
+    return true;
+  }
 }
