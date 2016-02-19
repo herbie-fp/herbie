0eff51265709be247e155cc012481484e4eb2da1
Zach Tatlock <ztatlock@cs.washington.edu>
2016-02-19 15:16:07 -0800

    Add "wiggle" to random test generator
    Can add a random number to size or number of variables in generated tests.

--- randTest/RandomTest.java
+++ randTest/RandomTest.java
@@ -1,10 +1,25 @@
+import java.util.Random;
+
 public class RandomTest {
   private static int size   = 1;
   private static int nVars  = 1;
   private static int nTests = 1;
 
+  private static int sizeWiggle  = 0;
+  private static int nVarsWiggle = 0;
+
   private static final String usage =
-    "Usage: java RandomTest --nops N --nvars N --ntests N";
+    String.join("\n"
+      , "Usage: java RandomTest OPTIONS"
+      , ""
+      , "where OPTIONS include:"
+      , "  --help              print this usage info and exit"
+      , "  --size N            size of expressions to generate (default 1)"
+      , "  --size-wiggle N     how much to wiggle to randomly add to size (default 0)"
+      , "  --nvars N           number of variables in generated expressions (default 1)"
+      , "  --nvars-wiggle N    how much wiggle to randomly add to number of vars (default 0)"
+      , "  --ntests N          number of tests to generate (default 1)"
+      );
 
   private static class BogusCL extends Exception {
     String msg;
@@ -20,11 +35,11 @@ public class RandomTest {
     }
     catch(ArrayIndexOutOfBoundsException e) {
       throw new BogusCL(
-        String.format("ERROR: expected integer argument at position %d", i));
+        String.format("expected integer argument at position %d", i));
     }
     catch(NumberFormatException e) {
       throw new BogusCL(
-        String.format("ERROR: could not parse '%s' as integer", args[i]));
+        String.format("could not parse '%s' as integer", args[i]));
     }
   }
 
@@ -40,20 +55,60 @@ public class RandomTest {
         case "--size":
           i++;
           size = parseArgInt(args, i);
+          if(size < 1) {
+            throw new BogusCL(
+              String.format("size must be positive, but got %d", size));
+          }
+          break;
+        case "-sw":
+        case "--size-wiggle":
+          i++;
+          sizeWiggle = parseArgInt(args, i);
+          if(sizeWiggle < 0) {
+            throw new BogusCL(
+              String.format("wiggle must be nonnegative, but got %d", sizeWiggle));
+          }
           break;
         case "-v":
         case "--nvars":
           i++;
           nVars = parseArgInt(args, i);
+          if(nVars < 0) {
+            throw new BogusCL(
+              String.format("number of vars must be nonnegative, but got %d", nVars));
+          }
+          if(nVars + nVarsWiggle > 26) {
+            throw new BogusCL(
+              String.format("number of vars plus wiggle must be <= 26, but got %d"
+                           , nVars + nVarsWiggle));
+          }
+          break;
+        case "-vw":
+        case "--nvars-wiggle":
+          i++;
+          nVarsWiggle = parseArgInt(args, i);
+          if(nVarsWiggle < 0) {
+            throw new BogusCL(
+              String.format("wiggle must be nonnegative, but got %d", nVarsWiggle));
+          }
+          if(nVars + nVarsWiggle > 26) {
+            throw new BogusCL(
+              String.format("number of vars plus wiggle must be <= 26, but got %d"
+                           , nVars + nVarsWiggle));
+          }
           break;
         case "-t":
         case "--ntests":
           i++;
           nTests = parseArgInt(args, i);
+          if(size < 1) {
+            throw new BogusCL(
+              String.format("number of tests must be positive, but got %d", nTests));
+          }
           break;
         default:
           throw new BogusCL(
-            String.format("ERROR: invalid argument '%s'", args[i]));
+            String.format("invalid argument '%s'", args[i]));
       }
       i++;
     }
@@ -64,14 +119,18 @@ public class RandomTest {
       parseArgs(args);
     }
     catch(BogusCL e) {
-      System.err.println(e.msg);
+      System.err.println("ERROR: " + e.msg);
       System.err.println();
       System.err.println(usage);
       System.exit(1);
     }
 
+    Random rnd = new Random();
+
     for(int i=0; i<nTests; i++) {
-      System.out.println(new OperatorTree(size, nVars));
+      int s = size + rnd.nextInt(sizeWiggle + 1);
+      int v = nVars + rnd.nextInt(nVarsWiggle + 1);
+      System.out.println(new OperatorTree(s, v));
       System.out.println();
     }
   }
