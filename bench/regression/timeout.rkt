4459023ec64f0a7c7b1a1d5ac6d405b7eb2d171d
Zach Tatlock <ztatlock@cs.washington.edu>
2016-02-23 15:50:05 -0800

    Add two additional timeout examples

--- bench/regression/timeout.rkt
+++ bench/regression/timeout.rkt
@@ -1,3 +1,11 @@
 (lambda (a b c)
   #:name "Random Jason Test Timeout 001"
   (+ c (asin (cosh c))))
+
+(lambda (a b c d)
+  #:name "Random Jason Test Timeout 002"
+  (mod (sinh c) (- c (sqr -2.9807307601812193E165))))
+
+(lambda (a b c)
+  #:name "Random Jason Test Timeout 003"
+  (sin (expt (sqrt (atan2 b b)) (- b a))))
