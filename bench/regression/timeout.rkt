b69c07429799b091b8531bfe31764b9cc31a1307
Zach Tatlock <ztatlock@cs.washington.edu>
2016-02-23 22:52:23 -0800

    Add more timeout tests

--- bench/regression/timeout.rkt
+++ bench/regression/timeout.rkt
@@ -1,11 +1,24 @@
 (lambda (a b c)
-  #:name "Random Jason Test Timeout 001"
+  #:name "Random Jason Timeout Test 001"
   (+ c (asin (cosh c))))
 
 (lambda (a b c d)
-  #:name "Random Jason Test Timeout 002"
+  #:name "Random Jason Timeout Test 002"
   (mod (sinh c) (- c (sqr -2.9807307601812193E165))))
 
 (lambda (a b c)
-  #:name "Random Jason Test Timeout 003"
+  #:name "Random Jason Timeout Test 003"
   (sin (expt (sqrt (atan2 b b)) (- b a))))
+
+(lambda (a b c d)
+  #:name "Random Jason Timeout Test 004"
+  (mod (cosh c) (log1p a)))
+
+(lambda (a)
+  #:name "Random Jason Timeout Test 005"
+  (cotan (expm1 (log1p (+ (asin 5.5642831668124635E247) (sqrt a))))))
+
+(lambda (a)
+  #:name "Random Jason Timeout Test 006"
+  (abs (mod (atan2 (expm1 (sin (expm1 a))) (atan a)) a)))
+
