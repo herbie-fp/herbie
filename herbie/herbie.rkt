76d0dadc25e691dd1503cc950f32c57e260e06c7
Pavel Panchekha <me@pavpanchekha.com>
2016-02-28 17:55:29 -0800

    herbie.rkt can be run

--- herbie/herbie.rkt
+++ herbie/herbie.rkt
@@ -41,18 +41,18 @@
        (regexp-match? #rx"\\.rkt$" (file-name-from-path fname))
        (file-exists? fname)))) ; Herbie input format 1 or 2
 
-(define (in-herbie-files . files)
-  (apply in-sequences (map file->input files)))
+(define (in-herbie-files files)
+  (if (null? files)
+      (sequence-map parse-test (in-port read (current-input-port)))
+      (all-herbie-tests files)))
 
-(define (file->input file)
-  (cond
-   [(not file)
-    (sequence-map parse-test (in-port read (current-input-port)))]
-   [(not (file-name-from-path file)) ; directory
-    (apply sequence-append (map file->input (filter herbie-input? (directory-list file #:build? #t))))]
-   [(regexp-match? #rx"\\.rkt$" (file-name-from-path file))
-    (call-with-input-file file
-      (λ (port) (map parse-test (sequence->list (in-port read port)))))]))
+(define (all-herbie-tests files)
+  (apply append
+   (for/list ([file files])
+     (if (directory-exists? file)
+         (all-herbie-tests (filter herbie-input? (directory-list file #:build? #t)))
+         (call-with-input-file file
+           (λ (port) (map parse-test (sequence->list (in-port read port)))))))))
 
 (define get-test-results #f)
 (define (get-get-test-results)
@@ -90,7 +90,7 @@
     [(or 'json 'console) ; TODO: Remove 'json the threads don't generate details
      (printf "Running Herbie...\nSeed: ~a\n" seed)
      (sequence-map (λ (test) (get-test-result test #:seed seed))
-                   (apply in-herbie-files files))]
+                   (in-herbie-files files))]
     [(or 'json 'report)
      ((get-get-test-results)
       #:threads (threads) #:seed seed #:profile (profile?)
@@ -99,7 +99,7 @@
 (define (run-herbie files)
   (define seed (get-seed))
   (define outputs
-    (for/list ([output (in-herbie-output (if (null? files) '("-") files) #:seed seed)]
+    (for/list ([output (in-herbie-output files #:seed seed)]
                [idx (in-naturals)] #:when output)
       (define success?
         (match output
