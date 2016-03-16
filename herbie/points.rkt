1ba2cbdf6dce135cfedf224da5339c64bd950ba2
Jason Qiu <qiuchen0213@gmail.com>
2016-03-16 15:53:56 -0700

    Fix a bug in interact where we didn't pass the samplers to setup-prog!

--- herbie/points.rkt
+++ herbie/points.rkt
@@ -129,8 +129,7 @@
                ; Then, we remove the points for which the answers
                ; are not representable
                [pts* (filter-points pts1 exs1)]
-               [exs* (filter-exacts pts1 exs1)]
-	       [pdebug (println "pts* length is " (length pts*) "\n" "exs* length is " (length exs*) "\n"  (eq? (length pts*) (length exs*)))])
+               [exs* (filter-exacts pts1 exs1)])
           (loop (append pts* pts) (append exs* exs))))))
 
 (define (prepare-points-period prog periods)
