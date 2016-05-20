40280cddff99cf01b03cc65ae9f6cbe6da2b9ef7
chenq9 <chenq9@uw.edu>
2016-05-20 10:53:48 -0700

    update conversion tool

--- src/formats/convert.rkt
+++ src/formats/convert.rkt
@@ -21,13 +21,25 @@
   (match-define (list (cons vars samp) ...) (map var&dist vars*))
   (match-define (list body args ...) (args&body args*))
 
-  (~a "(FPCore " vars
-      (let ([name (get '#:name args #f)])
-        (if name (~a " :name " name) ""))
-      (~a " :pre (and " (~a samp) ") ")
-      body ")"))
+  (define (translate-prop old new)
+    (let ([name (get old args #f)])
+      (if name (list new name) (list))))
 
+  `(FPCore ,vars
+      ,@(translate-prop '#:name ':name)
+      ,@(translate-prop '#:expected ':herbie-expected)
+      ,@(translate-prop '#:target ':target)
+      :herbie-samplers ,samp
+      ,body))
 
 (define (get kw target default)
   (let ([rec (assoc kw target)])
     (if rec (cdr rec) default)))
+
+(module+ main
+ (command-line
+   #:program "herbie-to-fpcore"
+   #:args (file)
+   (for ([test (in-port read (open-input-file file))])
+     (pretty-print (convert test)))))
+
