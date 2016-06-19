7979952c7ab90d75fc04e72dfcb0c5f7110e86c2
Pavel Panchekha <me@pavpanchekha.com>
2016-06-18 23:07:25 -0400

    Don't need these old Herbie input files anymore

--- bench/hamming/machine-decide.rkt
+++
@@ -1,8 +0,0 @@
-
-(lambda (a x)
-  #:name "NMSE section 3.5"
-  (- (exp (* a x)) 1)
-  #:target
-  (if (< (fabs (* a x)) 1/10)
-      (* (* a x) (+ 1 (+ (/ (* a x) 2) (/ (sqr (* a x)) 6))))
-      (- (exp (* a x)) 1)))
--- bench/hamming/overflow-underflow.rkt
+++
@@ -1,6 +0,0 @@
-
-(lambda (x)
-  #:name "NMSE section 3.11"
-  (/ (exp x) (- (exp x) 1))
-  #:target
-  (/ 1 (- 1 (exp (- x)))))
