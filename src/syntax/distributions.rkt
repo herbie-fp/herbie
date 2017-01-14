9728c6569ee98cbb4a6a338bc69644ad85560a9f
Jason Qiu <qiuchen0213@gmail.com>
2017-01-13 16:30:24 -0800

    Style fixes

--- src/syntax/distributions.rkt
+++ src/syntax/distributions.rkt
@@ -21,29 +21,34 @@
 (define (sample-int)
   (- (random-exp 32) (expt 2 31)))
 
-(define (sample-bounded lo hi [left-closed #t] [right-closed #t])
-  (cond [(> lo hi) #f]
-        [(and (equal? (exact->inexact hi) (exact->inexact lo)) (or (not left-closed) (not right-closed))) #f]
-        [(equal? (exact->inexact hi) (exact->inexact lo)) (exact->inexact lo)]
-        [else
-          (let* ([ordinal (- (flonum->ordinal (exact->inexact hi)) (flonum->ordinal (exact->inexact lo)))]
-                 [num-bits (ceiling (/ (log ordinal) (log 2)))]
-                 [random-num (random-exp (inexact->exact num-bits))])
-            (if (or (and (not left-closed) (equal? 0 random-num))
-                    (and (not right-closed) (equal? ordinal random-num))
-                    (> random-num ordinal))
-              (sample-bounded lo left-closed hi right-closed)
-              (ordinal->flonum (+ (flonum->ordinal (exact->inexact lo)) random-num))))]))
+(define (sample-bounded lo hi #:left-closed? [left-closed? #t] #:right-closed? [right-closed? #t])
+  (define lo* (exact->inexact lo))
+  (define hi* (exact->inexact hi))
+  (cond
+   [(> lo* hi*) #f]
+   [(= lo* hi*)
+    (if (and left-closed? right-closed?) lo* #f)]
+   [(< lo* hi*)
+    (define ordinal (- (flonum->ordinal hi*) (flonum->ordinal lo*)))
+    (define num-bits (ceiling (/ (log ordinal) (log 2))))
+    (define random-num (random-exp (inexact->exact num-bits)))
+    (if (or (and (not left-closed?) (equal? 0 random-num))
+            (and (not right-closed?) (equal? ordinal random-num))
+            (> random-num ordinal))
+      ;; Happens with p < .5 so will not loop forever
+      (sample-bounded lo hi #:left-closed? left-closed? #:right-closed? right-closed?)
+      (ordinal->flonum (+ (flonum->ordinal lo*) random-num)))]))
 
 (module+ test
   (check-true (<= 1.0 (sample-bounded 1 2) 2.0))
-  (let ([a (sample-bounded 1 2 #f)])
+  (let ([a (sample-bounded 1 2 #:left-closed? #f)])
     (check-true (< 1 a))
     (check-true (<= a 2)))
-  (check-false (sample-bounded 1 1.0 #f) "Empty interval due to left openness")
-  (check-false (sample-bounded 1 1.0 #t #f) "Empty interval due to right openness")
-  (check-false (sample-bounded 1 1.0 #f #f) "Empty interval due to both-openness")
-  (check-false (sample-bounded 2.0 1.0 "Interval bounds flipped")))
+  (check-false (sample-bounded 1 1.0 #:left-closed? #f) "Empty interval due to left openness")
+  (check-false (sample-bounded 1 1.0 #:right-closed? #f) "Empty interval due to right openness")
+  (check-false (sample-bounded 1 1.0 #:left-closed? #f #:right-closed? #f)
+               "Empty interval due to both-openness")
+  (check-false (sample-bounded 2.0 1.0) "Interval bounds flipped"))
 
 (define (eval-op op)
   (match op ['> >] ['< <] ['>= >=] ['<= <=]))
