6515ea61ad7d9b7c9433fdcbae4d4688c23d06bc
Pavel Panchekha <me@pavpanchekha.com>
2017-01-25 11:39:29 -0800

    Updated all of the FPCore files to look nice
    The indentation and line breaks are now nice, and I've deleted the
    old-form Herbie files.

--- bench/numerics/kahan.rkt
+++
@@ -1,7 +0,0 @@
-
-(herbie-test (x)
-  "Kahan's exp quotient"
-  (/ (- (exp x) 1) x)
-  (if (and (< x 1) (> x -1))
-      (/ (- (exp x) 1) (log (exp x)))
-      (/ (- (exp x) 1) x)))
--- bench/regression/non-smooth.rkt
+++
@@ -1,7 +0,0 @@
-(herbie-test (r a b)
-  "r*sin(b)/cos(a+b), A"
-  (/ (* r (sin b)) (cos (+ a b))))
-
-(herbie-test (r a b)
-  "r*sin(b)/cos(a+b), B"
-  (* r (/ (sin b) (cos (+ a b)))))
