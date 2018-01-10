e0d030c3c49de3f3080d0a064ee959645fdd0370
Pavel Panchekha <me@pavpanchekha.com>
2018-01-09 16:39:35 -0800

    Formatting changes

--- src/syntax/syntax.rkt
+++ src/syntax/syntax.rkt
@@ -104,35 +104,29 @@
     (dict-set! rows 'operator row)))
 
 (define-operator (+ real real) real
-  [cost 40]
-  [bf bf+]
-  [fl +]
+  [fl +] [bf bf+] [cost 40]
   [->c/double (curry format "~a + ~a")]
   [->c/mpfr (curry format "mpfr_add(~a, ~a, ~a, MPFR_RNDN)")]
   [->tex (curry format "~a + ~a")])
 
 ;; TODO: Support unary minus
 
-(define-operator (- real real) real
-  [cost 40]
-  [bf bf-]
-  [fl -]
+(define-operator (- real [real]) real
+  ;; Override the normal argument handling because - can be unary
+  [args '(1 2)] [type (hash 1 '((real) real) 2 '((real real) real))]
+  [fl -] [bf bf-] [cost 40]
   [->c/double (curry format "~a - ~a")]
   [->c/mpfr (curry format "mpfr_sub(~a, ~a, ~a, MPFR_RNDN)")]
   [->tex (curry format "~a - ~a")])
 
 (define-operator (* real real) real
-  [cost 40]
-  [bf bf*]
-  [fl *]
+  [fl *] [bf bf*] [cost 40]
   [->c/double (curry format "~a * ~a")]
   [->c/mpfr (curry format "mpfr_mul(~a, ~a, ~a, MPFR_RNDN)")]
   [->tex (curry format "~a \\cdot ~a")])
 
 (define-operator (/ real real) real
-  [cost 40]
-  [bf bf/]
-  [fl /]
+  [fl /] [bf bf/] [cost 40]
   [->c/double (curry format "~a / ~a")]
   [->c/mpfr (curry format "mpfr_div(~a, ~a, ~a, MPFR_RNDN)")]
   [->tex (curry format "\\frac{~a}{~a}")])
@@ -196,74 +190,73 @@
   [->tex (curry format "\\tanh^{-1} ~a")])
 
 (define-operator/libm (cbrt real) real
- (libm cbrt cbrtf) (bf bfcbrt) (cost 80)
- (->c/double (curry format "cbrt(~a)"))
- (->c/mpfr (curry format "mpfr_cbrt(~a, ~a, MPFR_RNDN)"))
- (->tex (curry format "\\sqrt[3]{~a}")))
+  [libm cbrt cbrtf] [bf bfcbrt] [cost 80]
+  [->c/double (curry format "cbrt(~a)")]
+  [->c/mpfr (curry format "mpfr_cbrt(~a, ~a, MPFR_RNDN)")]
+  [->tex (curry format "\\sqrt[3]{~a}")])
 
 (define-operator/libm (ceil real) real
- (libm ceil ceilf) (bf bfceiling) (cost 80)
- (->c/double (curry format "ceil(~a)"))
- (->c/mpfr (curry format "mpfr_ceil(~a, ~a)"))
- (->tex (curry format "\\left\\lceil~a\\right\\rceil")))
+  [libm ceil ceilf] [bf bfceiling] [cost 80]
+  [->c/double (curry format "ceil(~a)")]
+  [->c/mpfr (curry format "mpfr_ceil(~a, ~a)")]
+  [->tex (curry format "\\left\\lceil~a\\right\\rceil")])
 
 (define (bfcopysign x y)
   (bf* (bfabs x) (bf (expt -1 (bigfloat-signbit y)))))
 
 (define-operator/libm (copysign real real) real
- (libm copysign copysignf) (bf bfcopysign) (cost 80)
- (->c/double (curry format "copysign(~a, ~a)"))
- (->c/mpfr (curry format "mpfr_copysign(~a, ~a, ~a, MPFR_RNDN)"))
- (->tex (curry format "\\mathsf{copysign}\\left(~a, ~a\\right)")))
+  [libm copysign copysignf] [bf bfcopysign] [cost 80]
+  [->c/double (curry format "copysign(~a, ~a)")]
+  [->c/mpfr (curry format "mpfr_copysign(~a, ~a, ~a, MPFR_RNDN)")]
+  [->tex (curry format "\\mathsf{copysign}\\left(~a, ~a\\right)")])
 
 (define-operator/libm (cos real) real
- (libm cos cosf) (bf bfcos) (cost 60)
- (->c/double (curry format "cos(~a)"))
- (->c/mpfr (curry format "mpfr_cos(~a, ~a, MPFR_RNDN)"))
- (->tex (curry format "\\cos ~a")))
+  [libm cos cosf] [bf bfcos] [cost 60]
+  [->c/double (curry format "cos(~a)")]
+  [->c/mpfr (curry format "mpfr_cos(~a, ~a, MPFR_RNDN)")]
+  [->tex (curry format "\\cos ~a")])
 
 (define-operator/libm (cosh real) real
- (libm cosh coshf) (bf bfcosh) (cost 55)
- (->c/double (curry format "cosh(~a)"))
- (->c/mpfr (curry format "mpfr_cosh(~a, ~a, MPFR_RNDN)"))
- (->tex (curry format "\\cosh ~a")))
+  [libm cosh coshf] [bf bfcosh] [cost 55]
+  [->c/double (curry format "cosh(~a)")]
+  [->c/mpfr (curry format "mpfr_cosh(~a, ~a, MPFR_RNDN)")]
+  [->tex (curry format "\\cosh ~a")])
 
 (define-operator/libm (erf real) real
- (libm erf erff) (bf bferf) (cost 70)
- (->c/double (curry format "erf(~a)"))
- (->c/mpfr (curry format "mpfr_erf(~a, ~a, MPFR_RNDN)"))
- (->tex (curry format "\\mathsf{erf} ~a")))
+  [libm erf erff] [bf bferf] [cost 70]
+  [->c/double (curry format "erf(~a)")]
+  [->c/mpfr (curry format "mpfr_erf(~a, ~a, MPFR_RNDN)")]
+  [->tex (curry format "\\mathsf{erf} ~a")])
 
 (define-operator/libm (erfc real) real
- (libm erfc erfcf) (bf bferfc) (cost 70)
- (->c/double (curry format "erfc(~a)"))
- (->c/mpfr (curry format "mpfr_erfc(~a, ~a, MPFR_RNDN)"))
- (->tex (curry format "\\mathsf{erfc} ~a")))
+  [libm erfc erfcf] [bf bferfc] [cost 70]
+  [->c/double (curry format "erfc(~a)")]
+  [->c/mpfr (curry format "mpfr_erfc(~a, ~a, MPFR_RNDN)")]
+  [->tex (curry format "\\mathsf{erfc} ~a")])
 
 (define-operator/libm (exp real) real
- (libm exp expf) (bf bfexp) (cost 70)
- (->c/double (curry format "exp(~a)"))
- (->c/mpfr (curry format "mpfr_exp(~a, ~a, MPFR_RNDN)"))
- (->tex (curry format "e^{~a}")))
+  [libm exp expf] [bf bfexp] [cost 70]
+  [->c/double (curry format "exp(~a)")]
+  [->c/mpfr (curry format "mpfr_exp(~a, ~a, MPFR_RNDN)")]
+  [->tex (curry format "e^{~a}")])
 
 (define-operator/libm (exp2 real) real
- (libm exp2 exp2f) (bf bfexp2) (cost 70)
- (->c/double (curry format "exp2(~a)"))
- (->c/mpfr (curry format "mpfr_exp2(~a, ~a, MPFR_RNDN)"))
- (->tex (curry format "2^{~a}")))
+  [libm exp2 exp2f] [bf bfexp2] [cost 70]
+  [->c/double (curry format "exp2(~a)")]
+  [->c/mpfr (curry format "mpfr_exp2(~a, ~a, MPFR_RNDN)")]
+  [->tex (curry format "2^{~a}")])
 
 (define-operator/libm (expm1 real) real
- (libm expm1 expm1f) (bf bfexpm1) (cost 70)
- (->c/double (curry format "expm1(~a)"))
- (->c/mpfr (curry format "mpfr_expm1(~a, ~a, MPFR_RNDN)"))
- (->tex (curry format "(e^{~a} - 1)^*")))
+  [libm expm1 expm1f] [bf bfexpm1] [cost 70]
+  [->c/double (curry format "expm1(~a)")]
+  [->c/mpfr (curry format "mpfr_expm1(~a, ~a, MPFR_RNDN)")]
+  [->tex (curry format "(e^{~a} - 1)^*")])
 
 (define-operator/libm (fabs real) real
- (libm fabs fabsf) (bf bfabs) (cost 40)
- (->c/double (curry format "fabs(~a)"))
- (->c/mpfr (curry format "mpfr_abs(~a, ~a, MPFR_RNDN)"))
- (->tex (curry format "\\left|~a\\right|")))
-
+  [libm fabs fabsf] [bf bfabs] [cost 40]
+  [->c/double (curry format "fabs(~a)")]
+  [->c/mpfr (curry format "mpfr_abs(~a, ~a, MPFR_RNDN)")]
+  [->tex (curry format "\\left|~a\\right|")])
 
 (define (bffdim x y)
   (if (bf> x y)
@@ -271,181 +264,181 @@
     0.bf))
 
 (define-operator/libm (fdim real real) real
- (libm fdim fdimf) (bf bffdim) (cost 55)
- (->c/double (curry format "fdim(~a, ~a)"))
- (->c/mpfr (curry format "mpfr_dim(~a, ~a, ~a, MPFR_RNDN)"))
- (->tex (curry format "\\mathsf{fdim}\\left(~a, ~a\\right)")))
+  [libm fdim fdimf] [bf bffdim] [cost 55]
+  [->c/double (curry format "fdim(~a, ~a)")]
+  [->c/mpfr (curry format "mpfr_dim(~a, ~a, ~a, MPFR_RNDN)")]
+  [->tex (curry format "\\mathsf{fdim}\\left(~a, ~a\\right)")])
 
 (define-operator/libm (floor real) real
- (libm floor floorf) (bf bffloor) (cost 55)
- (->c/double (curry format "floor(~a)"))
- (->c/mpfr (curry format "mpfr_floor(~a, ~a)"))
- (->tex (curry format "\\left\\lfloor~a\\right\\rfloor")))
+  [libm floor floorf] [bf bffloor] [cost 55]
+  [->c/double (curry format "floor(~a)")]
+  [->c/mpfr (curry format "mpfr_floor(~a, ~a)")]
+  [->tex (curry format "\\left\\lfloor~a\\right\\rfloor")])
 
 (define (bffma x y z)
   (bf+ (bf* x y) z))
 
 (define-operator/libm (fma real real real) real
- (libm fma fmaf) (bf bffma) (cost 55)
- (->c/double (curry format "fma(~a, ~a, ~a)"))
- (->c/mpfr (curry format "mpfr_fma(~a, ~a, ~a, ~a, MPFR_RNDN)"))
- (->tex (curry format "(~a \\cdot ~a + ~a)_*")))
+  [libm fma fmaf] [bf bffma] [cost 55]
+  [->c/double (curry format "fma(~a, ~a, ~a)")]
+  [->c/mpfr (curry format "mpfr_fma(~a, ~a, ~a, ~a, MPFR_RNDN)")]
+  [->tex (curry format "(~a \\cdot ~a + ~a)_*")])
 
 (define-operator/libm (fmax real real) real
- (libm fmax fmaxf) (bf bfmax) (cost 55)
- (->c/double (curry format "fmax(~a, ~a)"))
- (->c/mpfr (curry format "mpfr_fmax(~a, ~a, ~a, MPFR_RNDN)"))
- (->tex (curry format "\\mathsf{fmax}\\left(~a, ~a\\right)")))
+  [libm fmax fmaxf] [bf bfmax] [cost 55]
+  [->c/double (curry format "fmax(~a, ~a)")]
+  [->c/mpfr (curry format "mpfr_fmax(~a, ~a, ~a, MPFR_RNDN)")]
+  [->tex (curry format "\\mathsf{fmax}\\left(~a, ~a\\right)")])
 
 (define-operator/libm (fmin real real) real
- (libm fmin fminf) (bf bfmin) (cost 55)
- (->c/double (curry format "fmin(~a, ~a)"))
- (->c/mpfr (curry format "mpfr_fmin(~a, ~a, ~a, MPFR_RNDN)"))
- (->tex (curry format "\\mathsf{fmin}\\left(~a, ~a\\right)")))
+  [libm fmin fminf] [bf bfmin] [cost 55]
+  [->c/double (curry format "fmin(~a, ~a)")]
+  [->c/mpfr (curry format "mpfr_fmin(~a, ~a, ~a, MPFR_RNDN)")]
+  [->tex (curry format "\\mathsf{fmin}\\left(~a, ~a\\right)")])
 
 (define (bffmod x mod)
   (bf- x (bf* mod (bffloor (bf/ x mod)))))
 
 (define-operator/libm (fmod real real) real
- (libm fmod fmodf) (bf bffmod) (cost 70)
- (->c/double (curry format "fmod(~a, ~a)"))
- (->c/mpfr (curry format "mpfr_fmod(~a, ~a, ~a, MPFR_RNDN)"))
- (->tex (curry format "~a \\bmod ~a")))
+  [libm fmod fmodf] [bf bffmod] [cost 70]
+  [->c/double (curry format "fmod(~a, ~a)")]
+  [->c/mpfr (curry format "mpfr_fmod(~a, ~a, ~a, MPFR_RNDN)")]
+  [->tex (curry format "~a \\bmod ~a")])
 
 (define-operator/libm (hypot real real) real
- (libm hypot hypotf) (bf bfhypot) (cost 55)
- (->c/double (curry format "hypot(~a, ~a)"))
- (->c/mpfr (curry format "mpfr_hypot(~a, ~a, ~a, MPFR_RNDN)"))
- (->tex (curry format "\\sqrt{~a^2 + ~a^2}^*")))
+  [libm hypot hypotf] [bf bfhypot] [cost 55]
+  [->c/double (curry format "hypot(~a, ~a)")]
+  [->c/mpfr (curry format "mpfr_hypot(~a, ~a, ~a, MPFR_RNDN)")]
+  [->tex (curry format "\\sqrt{~a^2 + ~a^2}^*")])
 
 (define-operator/libm (j0 real) real
- (libm j0 j0f) (bf bfbesj0) (cost 55)
- (->c/double (curry format "j0(~a)"))
- (->c/mpfr (curry format "mpfr_j0(~a, ~a, MPFR_RNDN)"))
- (->tex (curry format "\\mathsf{j0} ~a")))
+  [libm j0 j0f] [bf bfbesj0] [cost 55]
+  [->c/double (curry format "j0(~a)")]
+  [->c/mpfr (curry format "mpfr_j0(~a, ~a, MPFR_RNDN)")]
+  [->tex (curry format "\\mathsf{j0} ~a")])
 
 (define-operator/libm (j1 real) real
- (libm j1 j1f) (bf bfbesj1) (cost 55)
- (->c/double (curry format "j1(~a)"))
- (->c/mpfr (curry format "mpfr_j1(~a, ~a, MPFR_RNDN)"))
- (->tex (curry format "\\mathsf{j1} ~a")))
+  [libm j1 j1f] [bf bfbesj1] [cost 55]
+  [->c/double (curry format "j1(~a)")]
+  [->c/mpfr (curry format "mpfr_j1(~a, ~a, MPFR_RNDN)")]
+  [->tex (curry format "\\mathsf{j1} ~a")])
 
 (define-operator/libm (lgamma real) real
- (libm lgamma lgammaf) (bf bflog-gamma) (cost 55)
- (->c/double (curry format "lgamma(~a)"))
- (->c/mpfr (curry format "mpfr_lngamma(~a, ~a, MPFR_RNDN)"))
- (->tex (curry format "\\log_* \\left( \\mathsf{gamma} ~a \\right)")))
+  [libm lgamma lgammaf] [bf bflog-gamma] [cost 55]
+  [->c/double (curry format "lgamma(~a)")]
+  [->c/mpfr (curry format "mpfr_lngamma(~a, ~a, MPFR_RNDN)")]
+  [->tex (curry format "\\log_* \\left( \\mathsf{gamma} ~a \\right)")])
 
 (define-operator/libm (log real) real
- (libm log logf) (bf bflog) (cost 70)
- (->c/double (curry format "log(~a)"))
- (->c/mpfr (curry format "mpfr_log(~a, ~a, MPFR_RNDN)"))
- (->tex (curry format "\\log ~a")))
+  [libm log logf] [bf bflog] [cost 70]
+  [->c/double (curry format "log(~a)")]
+  [->c/mpfr (curry format "mpfr_log(~a, ~a, MPFR_RNDN)")]
+  [->tex (curry format "\\log ~a")])
 
 (define-operator/libm (log10 real) real
- (libm log10 log10f) (bf bflog10) (cost 70)
- (->c/double (curry format "log10(~a)"))
- (->c/mpfr (curry format "mpfr_log10(~a, ~a, MPFR_RNDN)"))
- (->tex (curry format "\\log_{10} ~a")))
+  [libm log10 log10f] [bf bflog10] [cost 70]
+  [->c/double (curry format "log10(~a)")]
+  [->c/mpfr (curry format "mpfr_log10(~a, ~a, MPFR_RNDN)")]
+  [->tex (curry format "\\log_{10} ~a")])
 
 (define-operator/libm (log1p real) real
- (libm log1p log1pf) (bf bflog1p) (cost 90)
- (->c/double (curry format "log1p(~a)"))
- (->c/mpfr (curry format "mpfr_log1p(~a, ~a, MPFR_RNDN)"))
- (->tex (curry format "\\log_* (1 + ~a)")))
+  [libm log1p log1pf] [bf bflog1p] [cost 90]
+  [->c/double (curry format "log1p(~a)")]
+  [->c/mpfr (curry format "mpfr_log1p(~a, ~a, MPFR_RNDN)")]
+  [->tex (curry format "\\log_* (1 + ~a)")])
 
 (define-operator/libm (log2 real) real
- (libm log2 log2f) (bf bflog2) (cost 70)
- (->c/double (curry format "log2(~a)"))
- (->c/mpfr (curry format "mpfr_log2(~a, ~a, MPFR_RNDN)"))
- (->tex (curry format "\\log_{2} ~a")))
+  [libm log2 log2f] [bf bflog2] [cost 70]
+  [->c/double (curry format "log2(~a)")]
+  [->c/mpfr (curry format "mpfr_log2(~a, ~a, MPFR_RNDN)")]
+  [->tex (curry format "\\log_{2} ~a")])
 
 (define (bflogb x)
   (bigfloat-exponent x))
 
 (define-operator/libm (logb real) real
- (libm logb logbf) (bf bflogb) (cost 70)
- (->c/double (curry format "logb(~a)"))
- (->c/mpfr (curry format "mpfr_set_si(~a, mpfr_get_exp(~a), MPFR_RNDN)"))
- (->tex (curry format "\\log^{*}_{b} ~a")))
+  [libm logb logbf] [bf bflogb] [cost 70]
+  [->c/double (curry format "logb(~a)")]
+  [->c/mpfr (curry format "mpfr_set_si(~a, mpfr_get_exp(~a), MPFR_RNDN)")]
+  [->tex (curry format "\\log^{*}_{b} ~a")])
 
 (define-operator/libm (pow real real) real
- (libm pow powf) (bf bfexpt) (cost 210)
- (->c/double (curry format "pow(~a, ~a)"))
- (->c/mpfr (curry format "mpfr_pow(~a, ~a, ~a, MPFR_RNDN)"))
- (->tex (curry format "{~a}^{~a}")))
+  [libm pow powf] [bf bfexpt] [cost 210]
+  [->c/double (curry format "pow(~a, ~a)")]
+  [->c/mpfr (curry format "mpfr_pow(~a, ~a, ~a, MPFR_RNDN)")]
+  [->tex (curry format "{~a}^{~a}")])
 
 (define-operator/libm (remainder real real) real
- (libm remainder remainderf) (bf bfremainder) (cost 70)
- (->c/double (curry format "remainder(~a, ~a)"))
- (->c/mpfr (curry format "mpfr_remainder(~a, ~a, ~a, MPFR_RNDN)"))
- (->tex (curry format "~a \\mathsf{rem} ~a")))
+  [libm remainder remainderf] [bf bfremainder] [cost 70]
+  [->c/double (curry format "remainder(~a, ~a)")]
+  [->c/mpfr (curry format "mpfr_remainder(~a, ~a, ~a, MPFR_RNDN)")]
+  [->tex (curry format "~a \\mathsf{rem} ~a")])
 
 (define-operator/libm (rint real) real
- (libm rint rintf) (bf bfrint) (cost 70)
- (->c/double (curry format "rint(~a)"))
- (->c/mpfr (curry format "mpfr_rint(~a, ~a, MPFR_RNDN)"))
- (->tex (curry format "\\mathsf{rint} ~a")))
+  [libm rint rintf] [bf bfrint] [cost 70]
+  [->c/double (curry format "rint(~a)")]
+  [->c/mpfr (curry format "mpfr_rint(~a, ~a, MPFR_RNDN)")]
+  [->tex (curry format "\\mathsf{rint} ~a")])
 
 (define-operator/libm (round real) real
- (libm round roundf) (bf bfround) (cost 70)
- (->c/double (curry format "round(~a)"))
- (->c/mpfr (curry format "mpfr_round(~a, ~a)"))
- (->tex (curry format "\\mathsf{round} ~a")))
+  [libm round roundf] [bf bfround] [cost 70]
+  [->c/double (curry format "round(~a)")]
+  [->c/mpfr (curry format "mpfr_round(~a, ~a)")]
+  [->tex (curry format "\\mathsf{round} ~a")])
 
 (define-operator/libm (sin real) real
- (libm sin sinf) (bf bfsin) (cost 60)
- (->c/double (curry format "sin(~a)"))
- (->c/mpfr (curry format "mpfr_sin(~a, ~a, MPFR_RNDN)"))
- (->tex (curry format "\\sin ~a")))
+  [libm sin sinf] [bf bfsin] [cost 60]
+  [->c/double (curry format "sin(~a)")]
+  [->c/mpfr (curry format "mpfr_sin(~a, ~a, MPFR_RNDN)")]
+  [->tex (curry format "\\sin ~a")])
 
 (define-operator/libm (sinh real) real
- (libm sinh sinhf) (bf bfsinh) (cost 55)
- (->c/double (curry format "sinh(~a)"))
- (->c/mpfr (curry format "mpfr_sinh(~a, ~a, MPFR_RNDN)"))
- (->tex (curry format "\\sinh ~a")))
+  [libm sinh sinhf] [bf bfsinh] [cost 55]
+  [->c/double (curry format "sinh(~a)")]
+  [->c/mpfr (curry format "mpfr_sinh(~a, ~a, MPFR_RNDN)")]
+  [->tex (curry format "\\sinh ~a")])
 
 (define-operator/libm (sqrt real) real
- (libm sqrt sqrtf) (bf bfsqrt) (cost 40)
- (->c/double (curry format "sqrt(~a)"))
- (->c/mpfr (curry format "mpfr_sqrt(~a, ~a, MPFR_RNDN)"))
- (->tex (curry format "\\sqrt{~a}")))
+  [libm sqrt sqrtf] [bf bfsqrt] [cost 40]
+  [->c/double (curry format "sqrt(~a)")]
+  [->c/mpfr (curry format "mpfr_sqrt(~a, ~a, MPFR_RNDN)")]
+  [->tex (curry format "\\sqrt{~a}")])
 
 (define-operator/libm (tan real) real
- (libm tan tanf) (bf bftan) (cost 95)
- (->c/double (curry format "tan(~a)"))
- (->c/mpfr (curry format "mpfr_tan(~a, ~a, MPFR_RNDN)"))
- (->tex (curry format "\\tan ~a")))
+  [libm tan tanf] [bf bftan] [cost 95]
+  [->c/double (curry format "tan(~a)")]
+  [->c/mpfr (curry format "mpfr_tan(~a, ~a, MPFR_RNDN)")]
+  [->tex (curry format "\\tan ~a")])
 
 (define-operator/libm (tanh real) real
- (libm tanh tanhf) (bf bftanh) (cost 55)
- (->c/double (curry format "tanh(~a)"))
- (->c/mpfr (curry format "mpfr_tanh(~a, ~a, MPFR_RNDN)"))
- (->tex (curry format "\\tanh ~a")))
+  [libm tanh tanhf] [bf bftanh] [cost 55]
+  [->c/double (curry format "tanh(~a)")]
+  [->c/mpfr (curry format "mpfr_tanh(~a, ~a, MPFR_RNDN)")]
+  [->tex (curry format "\\tanh ~a")])
 
 (define-operator/libm (tgamma real) real
- (libm tgamma tgammaf) (bf bfgamma) (cost 55)
- (->c/double (curry format "tgamma(~a)"))
- (->c/mpfr (curry format "mpfr_gamma(~a, ~a, MPFR_RNDN)"))
- (->tex (curry format "\\mathsf{gamma} ~a")))
+  [libm tgamma tgammaf] [bf bfgamma] [cost 55]
+  [->c/double (curry format "tgamma(~a)")]
+  [->c/mpfr (curry format "mpfr_gamma(~a, ~a, MPFR_RNDN)")]
+  [->tex (curry format "\\mathsf{gamma} ~a")])
 
 (define-operator/libm (trunc real) real
- (libm trunc truncf) (bf bftruncate) (cost 55)
- (->c/double (curry format "trunc(~a)"))
- (->c/mpfr (curry format "mpfr_trunc(~a, ~a)"))
- (->tex (curry format "\\mathsf{trunc} ~a")))
+  [libm trunc truncf] [bf bftruncate] [cost 55]
+  [->c/double (curry format "trunc(~a)")]
+  [->c/mpfr (curry format "mpfr_trunc(~a, ~a)")]
+  [->tex (curry format "\\mathsf{trunc} ~a")])
 
 (define-operator/libm (y0 real) real
- (libm y0 y0f) (bf bfbesy0) (cost 55)
- (->c/double (curry format "y0(~a)"))
- (->c/mpfr (curry format "mpfr_y0(~a, ~a, MPFR_RNDN)"))
- (->tex (curry format "\\mathsf{y0} ~a")))
+  [libm y0 y0f] [bf bfbesy0] [cost 55]
+  [->c/double (curry format "y0(~a)")]
+  [->c/mpfr (curry format "mpfr_y0(~a, ~a, MPFR_RNDN)")]
+  [->tex (curry format "\\mathsf{y0} ~a")])
 
 (define-operator/libm (y1 real) real
- (libm y1 y1f) (bf bfbesy1) (cost 55)
- (->c/double (curry format "y1(~a)"))
- (->c/mpfr (curry format "mpfr_y1(~a, ~a, MPFR_RNDN)"))
- (->tex (curry format "\\mathsf{y1} ~a")))
+  [libm y1 y1f] [bf bfbesy1] [cost 55]
+  [->c/double (curry format "y1(~a)")]
+  [->c/mpfr (curry format "mpfr_y1(~a, ~a, MPFR_RNDN)")]
+  [->tex (curry format "\\mathsf{y1} ~a")])
 
 
 ; Programs are just lambda expressions
