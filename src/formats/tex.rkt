d1f12d9cf1713a52a847cb17bc513821ae1e788d
Pavel Panchekha <me@pavpanchekha.com>
2018-01-10 16:39:07 -0800

    Eliminate all uses of texify-(const/expr)

--- src/formats/tex.rkt
+++ src/formats/tex.rkt
@@ -8,30 +8,20 @@
 (define mathjax-url
   "https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.0/MathJax.js?config=TeX-AMS-MML_HTMLorMML")
 
-(define-table texify-constants
-  [l       "\\ell"]
-  [PI      "\\pi"]
-  [E       "e"]
-  [eps     "\\varepsilon"]
-  [epsilon "\\varepsilon"]
-  [alpha   "\\alpha"]
-  [beta    "\\beta"]
-  [gamma   "\\gamma"]
-  [phi     "\\phi"]
-  [phi1    "\\phi_1"]
-  [phi2    "\\phi_2"]
-  [lambda  "\\lambda"]
-  [lambda1 "\\lambda_1"]
-  [lambda2 "\\lambda_2"])
-
-; Note that normal string converters ignore idx and
-; procedure converters take idx as first arg.
-(define (apply-converter conv args [idx #f])
-  (cond
-    [(string? conv)     (apply format conv args)]
-    [(list? conv)       (apply-converter (list-ref conv (length args)) args idx)]
-    [(procedure? conv)  (apply conv (if idx (cons idx args) args))]
-    [else               (error "Unknown syntax entry" conv)]))
+(define/match (texify-variable var)
+  [('l)       "\\ell"]
+  [('eps)     "\\varepsilon"]
+  [('epsilon) "\\varepsilon"]
+  [('alpha)   "\\alpha"]
+  [('beta)    "\\beta"]
+  [('gamma)   "\\gamma"]
+  [('phi)     "\\phi"]
+  [('phi1)    "\\phi_1"]
+  [('phi2)    "\\phi_2"]
+  [('lambda)  "\\lambda"]
+  [('lambda1) "\\lambda_1"]
+  [('lambda2) "\\lambda_2"]
+  [(_) (symbol->string var)])
 
 ; "enclose" is a MathJax extension which may
 ; not work with standard TeX processors.
@@ -43,12 +33,6 @@
 
 (define (untag str)
   (format "\\color{black}{~a}" str))
-
-(define ((tag-inner-untag str) idx . args)
-  (tag (apply format str (map untag args)) idx))
-
-(define ((tag-infix op) idx arg1 arg2)
-  (format "~a ~a ~a" arg1 (tag op idx) arg2))
 
 ; self-paren-level : #t --> paren me
 ;                    #f --> do not paren me
@@ -76,6 +60,127 @@
     [(or '== '< '> '<= '>= '!=)
      (values #f #t)]
     [_ (values 'fn #f)]))
+
+(define ((highlight-template op) idx args)
+  (define to-tag-infix
+    #hash((+ . "+") (- . "-") (* . "\\cdot") (fmod . "\\bmod") (remainder . "\\mathsf{rem}")
+          (< . "\\lt") (> . "\\gt") (== . "=") (!= . "\\ne") (<= . "\\le") (>= . "\\ge")
+          (and . "\\land") (or . "\\lor")))
+  (cond
+   [(and (equal? (length args) 2) (hash-has-key? to-tag-infix op))
+    (match-define (list a b) args)
+    (format "~a ~a ~a" a (tag (hash-ref to-tag-infix op) idx) b)]
+   [(equal? op 'if)
+    (match-define (list a b c) args)
+    (format "~a ~a ~a : ~a" a (tag "?" idx) b c)]
+   [(equal? op 'sqr)
+    (match-define (list a) args)
+    (format "{~a}^{~a}" a (tag "2" idx))]
+   [(equal? op 'cube)
+    (match-define (list a) args)
+    (format "{~a}^{~a}" a (tag "3" idx))]
+   [else
+    (tag (apply (operator-info op '->tex) (map untag args)) idx)]))
+
+(define (collect-branches expr loc)
+  (match expr
+    [`(if ,cond ,ift ,iff)
+     (cons (list cond ift loc)
+           (collect-branches iff (cons 3 loc)))]
+    [else
+     (list (list #t expr loc))]))
+
+;; The highlight ops are an alist of locations to indexes that marks
+;; those locations as highlighted with the given location
+;; index. highlight-ops and loc/colors are not meant to be used
+;; simultaniously.
+(define (texify-prog prog
+                     #:loc [color-loc #f]
+                     #:color [color "red"]
+                     #:highlight-ops [highlight-locs '()])
+  "Compile the body of a program to math mode TeX."
+  (let texify ([expr (program-body prog)] [parens #t] [loc '(2)])
+    (format
+      (if (and color-loc (equal? (reverse color-loc) loc))
+        (format "\\color{~a}{~~a}" color)
+        "~a")
+      (match expr
+        [(? exact-integer?)
+         (number->string expr)]
+        [(? exact-rational?)
+         (format "\\frac{~a}{~a}" (numerator expr) (denominator expr))]
+        [(? real?)
+         (match (string-split (number->string expr) "e")
+           [(list num) num]
+           [(list significand exp)
+            (define num
+              (if (equal? significand "1")
+                  (format "10^{~a}" exp)
+                  (format "~a \\cdot 10^{~a}" significand exp)))
+            (if (precedence< parens #f) num (format "\\left( ~a \\right)" num))])]
+        [(? constant?)
+         (constant-info expr '->tex)]
+        [(? symbol?) (texify-variable expr)]
+        [`(if ,cond ,ift ,iff)
+         (define NL "\\\\\n")
+         (define IND "\\;\\;\\;\\;")
+         (with-output-to-string
+           (λ ()
+             (printf "\\begin{array}{l}\n")
+             (for ([branch (collect-branches expr loc)])
+               (match branch
+                 [(list #t bexpr bloc)
+                  (printf "\\mathbf{else}:~a~a~a~a\n"
+                          NL IND (texify bexpr #t (cons 2 bloc)) NL)]
+                 [(list bcond bexpr bloc)
+                  (printf "\\mathbf{if}\\;~a:~a~a~a~a\n"
+                          (texify bcond #t (cons 1 bloc))
+                          NL IND (texify bexpr #t (cons 2 bloc)) NL)]))
+             (printf "\\end{array}")))]
+        [`(,f ,args ...)
+         (define-values (self-paren-level arg-paren-level) (precedence-levels f))
+         (let ([texed-args
+                (for/list ([arg args] [id (in-naturals 1)])
+                  (texify arg arg-paren-level (cons id loc)))]
+               [hl-loc
+                (assoc (reverse loc) highlight-locs)])
+           (format
+                                        ; omit parens if parent contex has lower precedence
+            (if (precedence< parens self-paren-level)
+                "~a"
+                "\\left(~a\\right)")
+            (if hl-loc
+                ((highlight-template f) (cdr hl-loc) texed-args)
+                (apply (operator-info f '->tex) texed-args))))]))))
+
+; TODO probably a better way to write this wrapper using
+;      make-keyword-procedure and keyword-apply
+(define (texify-expr expr
+                     #:loc [color-loc #f]
+                     #:color [color "red"]
+                     #:highlight-ops [highlight-locs '()])
+  (texify-prog (expr->prog expr)
+               #:loc color-loc
+               #:color color
+               #:highlight-ops highlight-locs))
+
+(define (exact-rational? r)
+  (and (rational? r) (exact? r)))
+
+; Note that normal string converters ignore idx and
+; procedure converters take idx as first arg.
+(define (apply-converter conv args [idx #f])
+  (cond
+    [(string? conv)     (apply format conv args)]
+    [(list? conv)       (apply-converter (list-ref conv (length args)) args idx)]
+    [(procedure? conv)  (apply conv (if idx (cons idx args) args))]
+    [else               (error "Unknown syntax entry" conv)]))
+
+(define ((tag-inner-untag str) idx . args)
+  (tag (apply format str (map untag args)) idx))
+
+(define ((tag-infix op) idx arg1 arg2)
+  (format "~a ~a ~a" arg1 (tag op idx) arg2))
 
 (define-table texify-operators
   [+    '(#f "+~a" "~a + ~a")
@@ -271,100 +376,22 @@
           (tag-infix "\\lor")
           '+ '+])
 
-(define (collect-branches expr loc)
-  (match expr
-    [`(if ,cond ,ift ,iff)
-     (cons (list cond ift loc)
-           (collect-branches iff (cons 3 loc)))]
-    [else
-     (list (list #t expr loc))]))
-
-;; The highlight ops are an alist of locations to indexes that marks
-;; those locations as highlighted with the given location
-;; index. highlight-ops and loc/colors are not meant to be used
-;; simultaniously.
-(define (texify-prog prog
-                     #:loc [color-loc #f]
-                     #:color [color "red"]
-                     #:highlight-ops [highlight-locs '()])
-  "Compile the body of a program to math mode TeX."
-  (let texify ([expr (program-body prog)] [parens #t] [loc '(2)])
-    (format
-      (if (and color-loc (equal? (reverse color-loc) loc))
-        (format "\\color{~a}{~~a}" color)
-        "~a")
-      (match expr
-        [(? exact-integer?)
-         (number->string expr)]
-        [(? exact-rational?)
-         (format "\\frac{~a}{~a}" (numerator expr) (denominator expr))]
-        [(? real?)
-         (match (string-split (number->string expr) "e")
-           [(list num) num]
-           [(list significand exp)
-            (define num
-              (if (equal? significand "1")
-                  (format "10^{~a}" exp)
-                  (format "~a \\cdot 10^{~a}" significand exp)))
-            (if (precedence< parens #f) num (format "\\left( ~a \\right)" num))])]
-        [(? symbol?)
-         (if (hash-has-key? texify-constants expr)
-           (car (hash-ref texify-constants expr))
-           (symbol->string expr))]
-        [`(if ,cond ,ift ,iff)
-         (define NL "\\\\\n")
-         (define IND "\\;\\;\\;\\;")
-         (with-output-to-string
-           (λ ()
-             (printf "\\begin{array}{l}\n")
-             (for ([branch (collect-branches expr loc)])
-               (match branch
-                 [(list #t bexpr bloc)
-                  (printf "\\mathbf{else}:~a~a~a~a\n"
-                          NL IND (texify bexpr #t (cons 2 bloc)) NL)]
-                 [(list bcond bexpr bloc)
-                  (printf "\\mathbf{if}\\;~a:~a~a~a~a\n"
-                          (texify bcond #t (cons 1 bloc))
-                          NL IND (texify bexpr #t (cons 2 bloc)) NL)]))
-             (printf "\\end{array}")))]
-        [`(,f ,args ...)
-         (match-define (list _ highlight-template _ _) (hash-ref texify-operators f))
-         (define-values (self-paren-level arg-paren-level) (precedence-levels f))
-         (let ([texed-args
-                (for/list ([arg args] [id (in-naturals 1)])
-                  (texify arg arg-paren-level (cons id loc)))]
-               [hl-loc
-                (assoc (reverse loc) highlight-locs)])
-           (format
-                                        ; omit parens if parent contex has lower precedence
-            (if (precedence< parens self-paren-level)
-                "~a"
-                "\\left(~a\\right)")
-            (if hl-loc
-                (apply-converter highlight-template texed-args (cdr hl-loc))
-                (apply (operator-info f '->tex) texed-args))))]))))
-
-; TODO probably a better way to write this wrapper using
-;      make-keyword-procedure and keyword-apply
-(define (texify-expr expr
-                     #:loc [color-loc #f]
-                     #:color [color "red"]
-                     #:highlight-ops [highlight-locs '()])
-  (texify-prog (expr->prog expr)
-               #:loc color-loc
-               #:color color
-               #:highlight-ops highlight-locs))
-
-(define (exact-rational? r)
-  (and (rational? r) (exact? r)))
 
 (module+ test
   (require rackunit)
   (for ([f (in-dict-keys texify-operators)])
-    (match-define (list template_ highlight-template self-paren-level_ arg-paren-level_) (hash-ref texify-operators f))
+    (match-define (list template_ highlight-template_ self-paren-level_ arg-paren-level_) (hash-ref texify-operators f))
     (define-values (self-paren-level arg-paren-level) (precedence-levels f))
     (check-equal? self-paren-level self-paren-level_)
     (check-equal? arg-paren-level arg-paren-level_)
     (define template (operator-info f '->tex))
-    (when (and (string? template_) (number? (procedure-arity template)))
-      (check-equal? template_ (apply template (build-list (procedure-arity template) (const "~a")))))))
+    (define num-args
+      (let ([hash (operator-info f 'args)])
+        (if (= (length hash) 1)
+            (match (car hash) ['* 2] [x x])
+            2)))
+    (define args (build-list num-args (const "~a")))
+
+    (when (string? template_)
+      (check-equal? template_ (apply template args)))
+    (check-equal? (apply-converter highlight-template_ args "idx") ((highlight-template f) "idx" args))))
