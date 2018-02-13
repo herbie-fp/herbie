f11696191c532da546283b0fc7119cf5197ad541
Pavel Panchekha <me@pavpanchekha.com>
2018-02-12 16:04:02 -0800

    Added JSON caching to make-index

--- infra/make-index.rkt
+++ infra/make-index.rkt
@@ -1,6 +1,6 @@
 #lang racket
 (require racket/runtime-path)
-(require (only-in xml write-xexpr))
+(require (only-in xml write-xexpr) json)
 (define-runtime-path report-json-path "../graphs/reports/")
 
 
@@ -19,6 +19,7 @@
 (define name->timestamp (compose string->number first parse-folder-name))
 
 (define (read-report-info folder)
+  (eprintf "Reading ~a\n" folder)
   (let ([info-file (build-path report-json-path folder "results.json")])
     (if (file-exists? info-file)
         (read-datafile info-file)
@@ -45,6 +46,8 @@
       (cons 'fuel:2 flags)
       flags))
 
+(define *cache* (make-parameter (make-hasheq)))
+
 (define (compute-row folder)
   (define info (read-report-info folder))
   (match-define (report-info date commit branch seed flags points iterations bit-width note tests) info)
@@ -62,16 +65,21 @@
     (for/sum ([row (or tests '())])
       (if (not (equal? (table-row-status row) "ex-start")) 1 0)))
 
-  (hash 'date date
-        'folder folder
+  (hash 'date-full (format "~a:~a on ~a" (date-hour date) (~r (date-minute date) #:min-width 2 #:pad-string "0") (date->string date))
+        'date-short (date->string/short date)
+        'date-unix (date->seconds date)
+        'folder (path->string folder)
         'commit commit
         'branch branch
-        'options (get-options info)
+        'options (map ~a (get-options info))
         'note note
         'tests-passed total-passed
         'tests-available total-available
         'bits-improved (- total-start total-end)
         'bits-available total-start))
+
+(define (read-row folder)
+  (dict-ref! (*cache*) (string->symbol (path->string folder)) (λ () (compute-row folder))))
 
 (define (round* x)
   (cond
@@ -89,73 +97,77 @@
          `(tr
            ;; TODO: Best to output a datetime field in RFC3338 format,
            ;; but Racket doesn't make that easy.
-           (td ((title ,(format "~a:~a on ~a" (date-hour (field 'date)) (~r (date-minute (field 'date)) #:min-width 2 #:pad-string "0") (date->string (field 'date)))))
-               (time ((data-unix ,(format "~a" (date->seconds (field 'date)))))
-                     ,(date->string/short (field 'date))))
+           (td ((title ,(field 'date-full)))
+               (time ((data-unix ,(~a (field 'date-unix))))
+                     ,(field 'date-short)))
            (td ((title ,(field 'commit))) ,(field 'branch))
-           (td ((title ,(string-join (map ~a (field 'options)) " "))
+           (td ((title ,(string-join (field 'options) " "))
                 (class ,(if (field 'note) "note" ""))) ,(or (field 'note) "⭐"))
            (td ,(if (> (field 'tests-available) 0) (format "~a/~a" (field 'tests-passed) (field 'tests-available)) ""))
            (td ,(if (field 'bits-improved) (format "~a/~a" (round* (field 'bits-improved)) (round* (field 'bits-available))) ""))
            (td (a ((href ,(format "./~a/report.html" (field 'folder)))) "»")))))))
 
 (define (make-index-page)
+  (when (file-exists? (build-path report-json-path "index.cache"))
+    (*cache* (hash-copy (call-with-input-file (build-path report-json-path "index.cache") read-json))))
+
   (define dirs (directory-list report-json-path))
-  (let* ([folders
-          (map (λ (dir) (compute-row dir))
-               (remove-duplicates
-                (sort (filter name->timestamp dirs) > #:key name->timestamp)
-                #:key name->timestamp))])
+  (define folders
+    (map (λ (dir) (read-row dir))
+         (remove-duplicates
+          (sort (filter name->timestamp dirs) > #:key name->timestamp)
+          #:key name->timestamp)))
 
-    (define branch-infos*
-      (sort
-       (group-by (curryr dict-ref 'branch) folders)
-       > #:key (λ (x) (date->seconds (dict-ref (first x) 'date)))))
+  (define branch-infos*
+    (sort
+     (group-by (curryr dict-ref 'branch) folders)
+     > #:key (λ (x) (dict-ref (first x) 'date-unix))))
 
-    (define-values (master-info* other-infos)
-      (partition (λ (x) (equal? (dict-ref (first x) 'branch) "master"))
-                 branch-infos*))
+  (define-values (master-info* other-infos)
+    (partition (λ (x) (equal? (dict-ref (first x) 'branch) "master"))
+               branch-infos*))
+  (define master-info (if (null? master-info*) '() (first master-info*)))
 
-    (define master-info (if (null? master-info*) '() (first master-info*)))
+  (write-file "index.html"
+    (printf "<!doctype html>\n")
+    (write-xexpr
+     `(html
+       (head
+        (meta ((charset "utf-8")))
+        (title "Herbie Reports")
+        (link ((rel "stylesheet") (href "index.css")))
+        (script ((src "http://d3js.org/d3.v3.min.js") (charset "utf-8")))
+        (script ((src "regression-chart.js")))
+        (script ((src "report.js"))))
+       (body
+        ((onload "index()"))
+        (div
+         ((id "large"))
+         (div "Reports: " (span ((class "number")) ,(~a (length folders))))
+         (div "Branches: " (span ((class "number")) ,(~a (length branch-infos*))))
+         ,(if master-info
+              `(div "On Master: " (span ((class "number")) ,(~a (length master-info))))
+              ""))
+        (ul ((id "toc"))
+            ,@(for/list ([rows (if master-info (cons master-info other-infos) other-infos)])
+                (define branch (dict-ref (first rows) 'branch))
+                `(li (a ((href ,(format "#reports-~a" branch))) ,branch))))
+        (figure
+         (ul ((id "classes")))
+         (svg ((id "graph") (width "800")))
+         (ul ((id "suites")))
+         (script "window.addEventListener('load', function(){draw_results(d3.select('#graph'))})"))
+        (table
+         ((id "reports"))
+         ,@(if master-info
+               (print-rows master-info #:name "master")
+               '())
+         ,@(apply
+            append
+            (for/list ([rows other-infos])
+              (print-rows rows #:name (dict-ref (first rows) 'branch)))))))))
 
-    (write-file "index.html"
-      (printf "<!doctype html>\n")
-      (write-xexpr
-       `(html
-         (head
-          (meta ((charset "utf-8")))
-          (title "Herbie Reports")
-          (link ((rel "stylesheet") (href "index.css")))
-          (script ((src "http://d3js.org/d3.v3.min.js") (charset "utf-8")))
-          (script ((src "regression-chart.js")))
-          (script ((src "report.js"))))
-         (body
-          ((onload "index()"))
-          (div
-           ((id "large"))
-           (div "Reports: " (span ((class "number")) ,(~a (length folders))))
-           (div "Branches: " (span ((class "number")) ,(~a (length branch-infos*))))
-           ,(if master-info
-                `(div "On Master: " (span ((class "number")) ,(~a (length master-info))))
-                ""))
-          (ul ((id "toc"))
-              ,@(for/list ([rows (if master-info (cons master-info other-infos) other-infos)])
-                  (define branch (dict-ref (first rows) 'branch))
-                  `(li (a ((href ,(format "#reports-~a" branch))) ,branch))))
-          (figure
-           (ul ((id "classes")))
-           (svg ((id "graph") (width "800")))
-           (ul ((id "suites")))
-           (script "window.addEventListener('load', function(){draw_results(d3.select('#graph'))})"))
-          (table
-           ((id "reports"))
-           ,@(if master-info
-                 (print-rows master-info #:name "master")
-                 '())
-           ,@(apply
-              append
-              (for/list ([rows other-infos])
-                (print-rows rows #:name (dict-ref (first rows) 'branch)))))))))))
+  (call-with-output-file (build-path report-json-path "index.cache") #:exists 'replace (curry write-json (*cache*))))
 
 (module+ main
   (make-index-page))
