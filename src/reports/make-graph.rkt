84cc2076e49509e3f1359c0972e7dab521d93aa6
DavidThien <dthien@cs.washington.edu>
2018-06-04 01:21:42 -0700

    Added script to actually run the inputs provided when the user clicks the Submit button.

--- src/reports/make-graph.rkt
+++ src/reports/make-graph.rkt
@@ -105,15 +105,45 @@
   (match (alt-program alt)
     [(list _ args expr) (list 'FPCore args ':name 'alt expr)]))
 
+(define (make-js-file start-fpcore end-fpcore)
+  (define start-js (compile-program start-fpcore #:name "start"))
+  (define end-js (compile-program end-fpcore #:name "end"))
+  (define submit-inputs
+    (string-append
+      "function submit_inputs(vars) {\n"
+      "    var originalOutputElem = document.getElementsByName('original-output')[0];\n"
+      "    var herbieOutputElem = document.getElementsByName('herbie-output')[0];\n"
+      "    var inputs = document.getElementsByClassName('input-submit');\n"
+      "    var originalInputVals = [];\n"
+      "    var herbieInputVals = [];\n"
+      "    for (var i = 0; i < inputs.length; i++) {\n"
+      "        var val = parseFloat(inputs[i].value);\n"
+      "        if (isNaN(val)) {\n"
+      "            originalOutputElem.innerHTML = 'Please enter valid numbers for all inputs';\n"
+      "            herbieOutputElem.innerHTML = 'Please enter valid numbers for all inputs';\n"
+      "            return;\n"
+      "        } else {\n"
+      "            originalInputVals.push(val);\n"
+      "            if (vars.indexOf(inputs[i].getAttribute('name')) > -1) {\n"
+      "                herbieInputVals.push(val);\n"
+      "            } else {\n"
+      "                alert(inputs[i].getAttribute('name'));\n"
+      "                alert(vars);\n"
+      "            }\n"
+      "        }\n"
+      "    }\n"
+      "    originalOutputElem.innerHTML = start.apply(null, originalInputVals);\n"
+      "    herbieOutputElem.innerHTML = end.apply(null, herbieInputVals);\n"
+      "}\n"))
+  (display-to-file (string-append start-js end-js submit-inputs)
+                 (build-path web-resource-path "interactive.js")
+                 #:exists 'replace))
+
 (define/contract (render-interactive start-prog end-prog)
   (-> timeline? timeline? xexpr?) ;; TODO This isn't the best type
   (define start-fpcore (alt2fpcore start-prog))
   (define end-fpcore (alt2fpcore end-prog))
-  (define start-js (compile-program start-fpcore #:name "start"))
-  (define end-js (compile-program end-fpcore #:name "end"))
-  (display-to-file (string-append headers start-js end-js)
-                 (build-path web-resource-path "interactive.js")
-                 #:exists 'replace)
+  (make-js-file start-fpcore end-fpcore)
   `(section ([id "try-results"])
     (h1 "Try it out")
     (form
@@ -124,8 +154,12 @@
        ,@(for/list ([var-name (second start-fpcore)])
            `(div
              (p ,(~a var-name))
-             (input ([type "text"] [name ,(string-append "var-" (~a var-name))]))))
-       (div (input ([type "submit"] [value "Submit Inputs"]))))
+             (input ([type "text"] [name ,(~a var-name)] [class "input-submit"]))))
+       (div (button ([type "button"]
+                     [onClick ,(string-append "submit_inputs(["
+                                              (string-join (map (curry format "'~a'") (second start-fpcore)) ", ")
+                                              "]);")])
+                    "Submit Inputs")))
       (li
        (div
         (p "Original Output: ")
@@ -188,7 +222,9 @@
        (title "Result for " ,(~a (test-name test)))
        (link ([rel "stylesheet"] [type "text/css"] [href "../graph.css"]))
        (script ([src ,mathjax-url]))
-       (script ([src "../report.js"])))
+       (script ([src "../report.js"]))
+       (script ([src "../interactive.js"]))
+       (script ([src "https://unpkg.com/mathjs@4.4.2/dist/math.min.js"])))
       (body ([onload "graph()"])
 
        (section ([id "large"])
@@ -231,7 +267,7 @@
                  (figcaption (p "Bits error versus " (var ,(~a var)))))]
               [else ""]))))
 
-       ,(render-interactive start-alt end-alt) ;; TODO Pass in the actual program
+       ,(render-interactive start-alt end-alt)
 
        ,(if (test-output test)
             `(section ([id "comparison"])
