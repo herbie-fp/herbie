7d7e99592ab4af53090a62f31bf328250d068bb0
DavidThien <dthien@cs.washington.edu>
2018-09-09 16:57:22 -0700

    Add back all defined operations from softposit-rkt because lazy-require doesn't work without it.

--- src/syntax/softposit.rkt
+++ src/syntax/softposit.rkt
@@ -1,6 +1,5 @@
 #lang racket
 
-(provide (all-defined-out) (all-from-out 'softposit-rkt))
 
 (require math/bigfloat racket/lazy-require)
 (lazy-require [softposit-rkt (_posit8 _posit16 _posit32 _posit64 _posit128
@@ -37,7 +36,42 @@
          posit32->double float->posit32 double->posit32
          double->quire8 double->quire16 double->quire32
          quire8->double quire16->double quire32->double)])
-;; This file makes racket bindings for all the posit functions
+
+(provide (all-defined-out)
+         _posit8 _posit16 _posit32 _posit64 _posit128
+         posit8? posit16? posit32? posit64? posit128?
+         _quire8 _quire16 _quire32
+         quire8? quire16? quire32?
+         uint32->posit8 uint32->posit16 uint32->posit32
+         uint64->posit8 uint64->posit16 uint64->posit32
+         int32->posit8 int32->posit16 int32->posit32
+         int64->posit8 int64->posit16 int64->posit32
+         posit8->uint32 posit8->uint64 posit8->int32 posit8->int64
+         posit8->posit16 posit8->posit32
+         posit8-round-to-int posit8-add posit8-sub posit8-mul
+         posit8-mulAdd posit8-div posit8-sqrt
+         posit8-eq? posit8-le? posit8-lt?
+         create-quire8 create-quire16 create-quire32
+         quire8-fdp-add quire8-fdp-sub quire8->posit8
+         posit8->double double->posit8
+         posit16->uint32 posit16->uint64 posit16->int32 posit16->int64
+         posit16->posit8 posit16->posit32
+         posit16-round-to-int posit16-add posit16-sub posit16-mul
+         posit16-mulAdd posit16-div posit16-sqrt
+         posit16-eq? posit16-le? posit16-lt?
+         quire16-fdp-add quire16-fdp-sub quire16->posit16
+         quire16-twos-complement
+         posit16->double float->posit16 double->posit16
+         posit32->uint32 posit32->uint64 posit32->int32 posit32->int64
+         posit32->posit8 posit32->posit16
+         posit32-round-to-int posit32-add posit32-sub posit32-mul
+         posit32-mulAdd posit32-div posit32-sqrt
+         posit32-eq? posit32-le? posit32-lt?
+         quire32-fdp-add quire32-fdp-sub quire32->posit32
+         quire32-twos-complement
+         posit32->double float->posit32 double->posit32
+         double->quire8 double->quire16 double->quire32
+         quire8->double quire16->double quire32->double)
 
 (struct big-posit8 (v))
 (struct big-posit16 (v))
