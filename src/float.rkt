4dbe32f950a5a644d0e89d35826c43aab4dd481e
DavidThien <dthien@cs.washington.edu>
2019-03-29 23:47:29 -0700

    Use the new representation structs for ulp-difference

--- src/float.rkt
+++ src/float.rkt
@@ -4,6 +4,8 @@
 (require "config.rkt")
 (require "common.rkt")
 (require "syntax/softposit.rkt")
+(require "interface.rkt")
+;; TODO remove references to interface.rkt for single-flonum->ordinal
 
 (provide midpoint-float ulp-difference *bit-width* ulps->bits bit-difference sample-float sample-double)
 
@@ -11,26 +13,20 @@
   (- (single-flonum->ordinal y) (single-flonum->ordinal x)))
 
 (define (ulp-difference x y)
-  (match* (x y)
+  (define ->ordinal (representation-repr->ordinal (match* (x y)
     [((? real?) (? real?))
-     (if (flag-set? 'precision 'double) (flonums-between x y) (single-flonums-between x y))]
+     (if (flag-set? 'precision 'double) binary64 binary32)]
     [((? complex?) (? complex?))
      (+ (ulp-difference (real-part x) (real-part y))
         (ulp-difference (imag-part x) (imag-part y)))]
-    [((? boolean?) (? boolean?))
-     (if (equal? x y) 0 64)]
-    [((? posit8?) (? posit8?))
-     (p8s-between x y)]
-    [((? posit16?) (? posit16?))
-     (p16s-between x y)]
-    [((? posit32?) (? posit32?))
-     (p32s-between x y)]
-    [((? quire8?) (? quire8?))
-     (p8s-between (quire8->posit8 x) (quire8->posit8 y)) ]
-    [((? quire16?) (? quire16?))
-     (p16s-between (quire16->posit16 x) (quire16->posit16 y))]
-    [((? quire32?) (? quire32?))
-     (p32s-between (quire32->posit32 x) (quire32->posit32 y))]))
+    [((? boolean?) (? boolean?)) bool ]
+    [((? posit8?) (? posit8?)) posit8]
+    [((? posit16?) (? posit16?)) posit16]
+    [((? posit32?) (? posit32?)) posit32]
+    [((? quire8?) (? posit8?)) posit8]
+    [((? quire16?) (? posit16?)) posit16]
+    [((? quire32?) (? posit32?)) posit32])))
+  (- (->ordinal y) (->ordinal x)))
 
 (define (midpoint-float p1 p2)
   (cond 
