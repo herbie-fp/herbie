5cfe2ede2af785a6f6bb8731fcf814b6087e573c
DavidThien <dthien@cs.washington.edu>
2019-04-02 10:21:29 -0700

    Fix import path for float.rkt in rules.rkt and remove duplicate import in test module

--- src/syntax/rules.rkt
+++ src/syntax/rules.rkt
@@ -5,7 +5,7 @@
 (require "../common.rkt")
 (require "syntax.rkt")
 (require "softposit.rkt")
-(require "float.rkt")
+(require "../float.rkt")
 
 (provide (struct-out rule) *rules* *simplify-rules* *fp-safe-simplify-rules*)
 
@@ -788,7 +788,7 @@
 
 (module+ test
   (require rackunit math/bigfloat)
-  (require "../programs.rkt" "../float.rkt")
+  (require "../programs.rkt")
   (define num-test-points 2000)
 
   (define *conditions*
