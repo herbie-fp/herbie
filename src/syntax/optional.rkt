61e0ad23c03d8d32b42332198370212993ca8da7
Pavel Panchekha <me@pavpanchekha.com>
2019-06-17 09:23:13 -0700

    Herbie plugin system, using the herbie-plugin key in info files

--- src/syntax/optional.rkt
+++ src/syntax/optional.rkt
@@ -1,11 +1,24 @@
 #lang racket
-(require racket/lazy-require racket/runtime-path)
+(require racket/lazy-require racket/runtime-path setup/getinfo)
 
 (define (module-exists? module)
   (with-handlers ([exn:fail:filesystem:missing-module? (const false)])
     (dynamic-require module #f)
     true))
 
+(define (load-herbie-plugins)
+  (for ([dir (find-relevant-directories '(herbie-plugin))])
+    (define info
+      (with-handlers ([exn:fail:filesystem? (const false)])
+        (get-info/full dir)))
+    (define value (info 'herbie-plugin (const false)))
+    (when (and value (module-exists? value))
+      (dynamic-require value #f))))
+
+;; Load all the plugins
+(load-herbie-plugins)
+
+;; Legacy code to load softposit file (should be moved to softposit-rkt package)
 (define-runtime-path posits-module "softposit.rkt")
 (when (module-exists? 'softposit-rkt)
   (dynamic-require (make-resolved-module-path posits-module) #f))
