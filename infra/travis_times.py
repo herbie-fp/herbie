9dee4d403e142213b41fce008f7c459672c17504
Pavel Panchekha <me@pavpanchekha.com>
2019-06-18 16:02:14 -0700

    Better Travis plot

--- infra/travis_times.py
+++ infra/travis_times.py
@@ -13,22 +13,29 @@ def travis_req(after_number=None):
     req.add_header("Accept", "application/vnd.travis-ci.2.1+json")
     resp = urllib.request.urlopen(req)
     data = json.load(resp)
-    for build in data["builds"]:
-        yield build
+    return data["builds"], data["commits"]
 
 def get_travis():
+    commits = {}
     last = None
     while True:
-        for x in travis_req(after_number=last):
+        data, coms = travis_req(after_number=last)
+        for x in coms:
+            commits[x["id"]] = x
+        for x in data:
             last = x["number"]
+            if "commit_id" in x:
+                x["commit"] = commits[x["commit_id"]]
             yield x
 
 def times():
     for build in get_travis():
-        if "started_at" in build and build["started_at"] < "2018-06-15":
-            break
-        if build["event_type"] == "push" and build["duration"] and build["state"] == "passed":
-            yield build["started_at"], build["duration"]
+        if build["event_type"] != "push": continue
+        if not build["commit"]["committed_at"]: continue
+        if build["commit"]["committed_at"] < "2018-06-15": break
+        if build["commit"]["branch"] == "master": continue
+        if build["state"] != "passed": continue
+        yield build["started_at"], build["duration"]
 
 if __name__ == "__main__":
     for d, t in times():
