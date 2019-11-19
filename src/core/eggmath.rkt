e457d0b70ca2f27cf43ffea361ceb04c0e0b308d
oliver <oflatt@gmail.com>
2019-11-19 16:34:27 -0700

    refactor ffi bindings into own file (getting ready for packaging)

--- src/core/eggmath.rkt
+++ src/core/eggmath.rkt
@@ -4,57 +4,14 @@
          ffi/unsafe/define)
 (require racket/runtime-path)
 
-(require "../debug.rkt" "../timeline.rkt" "../common.rkt")
+(require "../debug.rkt" "../common.rkt")
 (require "../syntax/rules.rkt")
 (require "../syntax/herbie-to-egg-pattern.rkt")
+(require "./eggmath-bindings.rkt")
+
 (module+ test (require rackunit))
 
 (provide egraph-run egraph-add-exprs egraph-run-iter egraph-get-simplest egg-expr->expr egg-add-exn? make-ffi-rules egraph-get-cost egraph-get-size)
-
-
-(define-runtime-path libeggmath-path
-  (build-path 'up 'up "egg-herbie" "target" "release"
-              (case (system-type)
-                [(windows) "egg_math"]
-                [else "libegg_math"])))
-
-
-(define-ffi-definer define-eggmath (ffi-lib libeggmath-path))
-
-(define _egraph-pointer (_cpointer 'egraph))
-
-(define-cstruct _EGraphAddResult
-  ([id _uint]
-   [successp _bool]))
-
-(define-cstruct _FFIRule
-  ([name _string/utf-8]
-   [left _string/utf-8]
-   [right _string/utf-8]))
-
-
-;;  -> a pointer to an egraph
-(define-eggmath egraph_create (_fun -> _egraph-pointer))
-
-(define-eggmath egraph_destroy (_fun _egraph-pointer -> _void))
-
-;; egraph pointer, s-expr string -> node number
-(define-eggmath egraph_add_expr (_fun _egraph-pointer _string/utf-8 -> _EGraphAddResult-pointer))
-
-(define-eggmath egraph_addresult_destroy (_fun _EGraphAddResult-pointer -> _void))
-
-
-;; egraph pointer, a node limit, a pointer to an array of ffirule, and the size of that array
-(define-eggmath egraph_run_iter (_fun _egraph-pointer
-                                      _uint
-                                      (ffi-rules : (_list i _FFIRule-pointer))
-                                      (_uint = (length ffi-rules)) -> _void))
-
-;; node number -> s-expr string
-(define-eggmath egraph_get_simplest (_fun _egraph-pointer _uint -> _string/utf-8))
-
-(define-eggmath egraph_get_cost (_fun _egraph-pointer _uint -> _uint))
-(define-eggmath egraph_get_size (_fun _egraph-pointer -> _uint))
 
 
 (struct egraph-data (egraph-pointer egg->herbie-dict herbie->egg-dict))
