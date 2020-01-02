828f8d2973763d77a0aec894ce34481ae260e630
oliver <oflatt@gmail.com>
2020-01-02 08:29:30 -0700

    move eggmath to egg-herbie:

--- src/info.rkt
+++ src/info.rkt
@@ -29,9 +29,9 @@
     "profile-lib"
     "rackunit-lib"
     "web-server-lib"
-    ("egg-herbie-windows" #:platform "win32\\x86_64" #:version "1.3")
-    ("egg-herbie-osx" #:platform "x86_64-macosx" #:version "1.3")
-    ("egg-herbie-linux" #:platform "x86_64-linux" #:version "1.3")))
+    ("egg-herbie-windows" #:platform "win32\\x86_64" #:version "1.4")
+    ("egg-herbie-osx" #:platform "x86_64-macosx" #:version "1.4")
+    ("egg-herbie-linux" #:platform "x86_64-linux" #:version "1.4")))
 
 (define build-deps
   '("rackunit-lib"))
