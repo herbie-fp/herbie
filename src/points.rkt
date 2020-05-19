4a598d8a1b8d2332ac6491f55d7ca6effb836f4b
Pavel Panchekha <me@pavpanchekha.com>
2020-05-19 17:40:55 -0600

    Oops

--- src/points.rkt
+++ src/points.rkt
@@ -15,7 +15,7 @@
 (define (sample-multi-bounded ranges repr)
   (define ->ordinal (representation-repr->ordinal repr))
   (define <-ordinal (representation-ordinal->repr repr))
-  (define <-exact (compose (representation-bigfloat->repr repr) bf))
+  (define <-exact (compose (representation-bf->repr repr) bf))
 
   (define ordinal-ranges
     (for/list ([range ranges])
