878c5141a640763c8a3c1f91882d3973eeddd67f
Pavel Panchekha <me@pavpanchekha.com>
2020-05-26 14:22:51 -0600

    if doesn't always mean if

--- src/programs.rkt
+++ src/programs.rkt
@@ -131,7 +131,7 @@
         [(? constant?) (list (constant-info prog mode))]
         [(? variable?) prog]
         [`(if ,c ,t ,f)
-         (list 'if
+         (list (operator-info 'if mode)
                (inductor c (get-representation 'bool))
                (inductor t repr)
                (inductor f repr))]
