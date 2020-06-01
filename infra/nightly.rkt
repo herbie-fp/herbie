3c1f064cf587fcc7081ab91630625660341cb3cb
Pavel Panchekha <me@pavpanchekha.com>
2020-06-01 10:53:14 -0600

    Make timeline HTML for nightlies

--- infra/nightly.rkt
+++ infra/nightly.rkt
@@ -2,7 +2,7 @@
 (require json)
 (require "../src/timeline.rkt" "../src/profile.rkt")
 
-(define (merge-timelines out dirs)
+(define (merge-timelines . dirs)
   (define tls
     (filter
      (conjoin (negate eof-object?) identity)
@@ -10,20 +10,23 @@
        (with-handlers ([exn? (const #f)])
          (define tl (call-with-input-file (build-path dir "timeline.json") read-json))
          (timeline-relink dir tl)))))
-  (write-json (apply timeline-merge tls) out))
+  (define info (read-datafile (build-path (first dir) "results.json")))
+  (define joint-tl (apply timeline-merge tls))
+  (call-with-output-file "timeline.json" #:exists 'replace (curry write-json joint-tl))
+  (call-with-output-file "timeline.html" #:exists 'replace (curryr make-summary-html info joint-tl)))
 
-(define (merge-profiles out dirs)
-  (define tls
+(define (merge-profiles dirs)
+  (define pfs
     (filter
      (conjoin (negate eof-object?) identity)
      (for/list ([dir (in-list dirs)])
        (with-handlers ([exn? (const #f)])
          (call-with-input-file (build-path dir "profile.json") read-json)))))
-  (write-json (profile->json (apply profile-merge (map json->profile tls))) out))
+  (define joint-pf (apply profile-merge (map json->profile pfs)))
+  (call-with-output-file "profiles.json" #:exists 'replace (curry write-json (profile->json joint-pf))))
 
 (module+ main
   (command-line
    #:args dirs
-   (call-with-output-file "timeline.json" #:exists 'replace (curryr merge-timelines dirs))
-   (call-with-output-file "profiles.json" #:exists 'replace (curryr merge-profiles dirs))
-   ))
+   (apply merge-timelines dirs)
+   (apply merge-profiles dirs)))
