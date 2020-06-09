960427635db49f354523f011b85edb6f516545ea
oliver <oflatt@gmail.com>
2020-06-09 16:50:51 -0600

    slightly lower find depth

--- src/config.rkt
+++ src/config.rkt
@@ -50,7 +50,7 @@
 (define *num-iterations* (make-parameter 4))
 
 ;; The maximum depth for splitting the space when searching for valid areas of points.
-(define *max-find-range-depth* (make-parameter 15))
+(define *max-find-range-depth* (make-parameter 14))
 
 ;; The maximum number of consecutive skipped points for sampling valid points
 (define *max-skipped-points* (make-parameter 100))
