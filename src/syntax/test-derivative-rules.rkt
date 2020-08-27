7868c83c681e654a5fb04d02bca1869aa186b181
oliver <oflatt@gmail.com>
2020-08-27 16:03:25 -0600

    fix up pow test

--- src/syntax/test-derivative-rules.rkt
+++ src/syntax/test-derivative-rules.rkt
@@ -12,12 +12,13 @@
           `(((d a a) . 1)
             ((d (+.f64 1 (*.f64 2 x)) x) . 2)
             ((d (+.f64 1 (*.f64 y x)) x) . y)
-            ((d (pow.f64 x 3) x) . (* 3 (pow x 2)))
+            ((d (pow.f64 x 3) x) . (*.f64 x (*.f64 x 3)))
             ((subst (+.f64 a a) a 4) . 8)
             ((subst c a 4) . c)
             ((subst (subst (/.f64 c d) c 4) d 5) . 4/5)))
   
-  #;(pretty-print (*differentiation-rules*))
+  #;(for ([rule (*differentiation-rules*)])
+         (printf "~a ~a ~a \n" (rule-name rule) (rule-input rule) (rule-output rule)))
 
   (for ([pair derivatives])
        (check-equal? (differentiate-expr (car pair)) (cdr pair))))
