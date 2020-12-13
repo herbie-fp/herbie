1708ec01073bd2511b4f3dc77000724c3a7cbc54
Pavel Panchekha <me@pavpanchekha.com>
2020-12-12 21:41:51 -0700

    Oops

--- src/symmetry.rkt
+++ src/symmetry.rkt
@@ -12,7 +12,7 @@
              #:when (equal? swap* orig))
     swap))
 
-(define (connected-components expr swaps)
+(define (connected-components expr)
   (define vars (program-variables expr))
   (define body (program-body expr))
 
