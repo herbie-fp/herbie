ce83966c3834d444e8843ba470c9cca72abdc91c
Brett Saiki <bksaiki@gmail.com>
2021-06-03 12:57:12 -0700

    Unit tests for function inlining

--- src/syntax/function.rkt
+++ src/syntax/function.rkt
@@ -2,6 +2,8 @@
 
 (provide *functions* inline-functions)
 
+(module+ test (require rackunit))
+
 ;; name -> (vars prec body)
 (define *functions* (make-parameter (make-hash)))
 
@@ -25,3 +27,21 @@
       (cons op (map loop args))]
      [_ expr])))
     
+
+(module+ test
+  ;; Test classic quadp and quadm examples
+  (define discr-func-info (list (list 'a 'b 'c) 'binary64 `(sqrt (- (* b b) (* 4 a c)))))
+  (hash-set! (*functions*) 'discr discr-func-info)
+  (define quadp `(/ (+ (- y) (discr x y z)) (* 2 x)))
+  (define quadm `(/ (- (- y) (discr x y z)) (* 2 x)))
+  (check-equal? (inline-functions quadp) '(/ (+ (- y) (sqrt (- (* y y) (* 4 x z)))) (* 2 x)))
+  (check-equal? (inline-functions quadm) '(/ (- (- y) (sqrt (- (* y y) (* 4 x z)))) (* 2 x)))
+
+  ;; x^5 = x^3 * x^2
+  (define sqr-info (list (list 'x) 'binary64 '(* x x)))
+  (define cube-info (list (list 'x) 'binary64 '(* x x x)))
+  (hash-set! (*functions*) 'sqr sqr-info)
+  (hash-set! (*functions*) 'cube cube-info)
+  (define fifth '(* (cube a) (sqr a)))
+  (check-equal? (inline-functions fifth) '(* (* a a a) (* a a))))
+
