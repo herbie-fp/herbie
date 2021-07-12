275286eab86b1dc7c9c4c11aa7c58886a5654ba2
Brett Saiki <bksaiki@gmail.com>
2021-07-12 10:20:29 -0700

    re-add libm operator impls

--- src/binary32.rkt
+++ src/binary32.rkt
@@ -5,11 +5,15 @@
 (require math/bigfloat math/flonum)
 (require (submod "syntax/syntax.rkt" internals)
          (submod "interface.rkt" internals)
-         "common.rkt" "float32.rkt")
+         "syntax/syntax.rkt" "common.rkt"
+         "errors.rkt" "float32.rkt")
 
 (eprintf "Loading binary32 support...\n")
 
-;; Only load everything before if single flonum supported?
+; for define-libm-operator (must be top-level)
+(require ffi/unsafe)
+
+;; Only load everything below if single flonum supported?
 ;; BC or CS (>= 8.0)
 (when (single-flonum-supported?)
 
@@ -47,12 +51,94 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;; operators ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+(define-syntax (define-libm-operator stx)
+  (syntax-case stx (real libm)
+    [(_ (op real ...) [libm id] [key value] ...)
+     (let* ([num-args (length (cdr (syntax-e (cadr (syntax-e stx)))))]
+            [sym2-append (λ (x y) (string->symbol (string-append (symbol->string x) (symbol->string y))))]
+            [name (sym2-append (syntax-e (car (syntax-e (cadr (syntax-e stx))))) '.f32)])
+       #`(begin
+           (define (fallback . args)
+             (warn 'fallback #:url "faq.html#native-ops"
+                   "native `~a` not supported on your system, using fallback; ~a"
+                   'op
+                   "use --disable precision:fallback to disable fallbacks")
+             (apply (operator-info 'op 'nonffi) args))
+           (define fl-proc (get-ffi-obj 'id #f (_fun #,@(build-list num-args (λ (_) #'_float)) -> _float)
+                                        (lambda () (*unknown-ops* (cons 'name (*unknown-ops*))) fallback)))
+           (define-operator-impl (op name #,@(build-list num-args (λ (_) #'binary32))) binary32
+             [fl fl-proc])))]))
+
+(define-syntax-rule (define-1ary-libm-operator op name)
+  (define-libm-operator (op real) [libm name]))
+
+(define-syntax-rule (define-2ary-libm-operator op name)
+  (define-libm-operator (op real real) [libm name]))
+
+(define-syntax-rule (define-1ary-libm-operators [op name] ...)
+  (begin (define-1ary-libm-operator op name) ...))
+
+(define-syntax-rule (define-2ary-libm-operators [op name] ...)
+  (begin (define-2ary-libm-operator op name) ...))
+
+
 (define-operator-impl (neg neg.f32 binary32) binary32 [fl fl32-])
 (define-operator-impl (+ +.f32 binary32 binary32) binary32 [fl fl32+])
 (define-operator-impl (- -.f32 binary32 binary32) binary32 [fl fl32-])
 (define-operator-impl (* *.f32 binary32 binary32) binary32 [fl fl32*])
 (define-operator-impl (/ /.f32 binary32 binary32) binary32 [fl fl32/])
 
+(define-1ary-libm-operators
+ [acos acosf]
+ [acosh acoshf]
+ [asin asinf]
+ [asinh asinhf]
+ [atan atanf]
+ [atanh atanhf]
+ [cbrt cbrtf]
+ [ceil ceilf]
+ [cos cosf]
+ [cosh coshf]
+ [erf erff]
+ [erfc erfcf]
+ [exp expf]
+ [exp2 exp2f]
+ [expm1 expm1f]
+ [fabs fabsf]
+ [floor floorf]
+ [j0 j0f]
+ [j1 j1f]
+ [lgamma lgammaf]
+ [log logf]
+ [log10 log10f]
+ [log1p log1pf]
+ [log2 log2f]
+ [logb logbf]
+ [rint rintf]
+ [round roundf]
+ [sin sinf]
+ [sinh sinhf]
+ [sqrt sqrtf]
+ [tan tanf]
+ [tanh tanhf]
+ [tgamma tgammaf]
+ [trunc truncf]
+ [y0 y0f]
+ [y1 y1f])
+
+(define-2ary-libm-operators
+ [atan2 atan2f]
+ [copysign copysignf]
+ [fdim fdimf]
+ [fmax fmaxf]
+ [fmin fminf]
+ [fmod fmodf]
+ [hypot hypotf]
+ [pow powf]
+ [remainder remainderf])
+
+(define-libm-operator (fma real real real)
+ [libm fmaf])
 
 (define-operator-impl (== ==.f32 binary32 binary32) bool
   [itype 'binary32] [otype 'bool] ; Override number of arguments
@@ -77,7 +163,6 @@
 (define-operator-impl (>= >=.f32 binary32 binary32) bool
   [itype 'binary32] [otype 'bool] ; Override number of arguments
   [fl (comparator >=)])
-
 
 (define-operator-impl (cast binary64->binary32 binary64) binary32
   [fl (curryr ->float32)])
--- src/binary64.rkt
+++ src/binary64.rkt
@@ -5,7 +5,8 @@
 (require math/bigfloat math/flonum)
 (require (submod "syntax/syntax.rkt" internals)
          (submod "interface.rkt" internals)
-         "common.rkt")
+         "syntax/syntax.rkt" "common.rkt"
+         "errors.rkt")
 
 (eprintf "Loading binary64 support...\n")
 
@@ -43,12 +44,95 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;; operators ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+(require ffi/unsafe)
+(define-syntax (define-libm-operator stx)
+  (syntax-case stx (real libm)
+    [(_ (op real ...) [libm id] [key value] ...)
+     (let* ([num-args (length (cdr (syntax-e (cadr (syntax-e stx)))))]
+            [sym2-append (λ (x y) (string->symbol (string-append (symbol->string x) (symbol->string y))))]
+            [name (sym2-append (syntax-e (car (syntax-e (cadr (syntax-e stx))))) '.f64)])
+       #`(begin
+           (define (fallback . args)
+             (warn 'fallback #:url "faq.html#native-ops"
+                   "native `~a` not supported on your system, using fallback; ~a"
+                   'op
+                   "use --disable precision:fallback to disable fallbacks")
+             (apply (operator-info 'op 'nonffi) args))
+           (define fl-proc (get-ffi-obj 'id #f (_fun #,@(build-list num-args (λ (_) #'_double)) -> _double)
+                                         (lambda () (*unknown-ops* (cons 'name (*unknown-ops*))) fallback)))
+           (define-operator-impl (op name #,@(build-list num-args (λ (_) #'binary64))) binary64
+             [fl fl-proc])))]))
+
+(define-syntax-rule (define-1ary-libm-operator op name)
+  (define-libm-operator (op real) [libm name]))
+
+(define-syntax-rule (define-2ary-libm-operator op name)
+  (define-libm-operator (op real real) [libm name]))
+
+(define-syntax-rule (define-1ary-libm-operators [op name] ...)
+  (begin (define-1ary-libm-operator op name) ...))
+
+(define-syntax-rule (define-2ary-libm-operators [op name] ...)
+  (begin (define-2ary-libm-operator op name) ...))
+
+
 (define-operator-impl (neg neg.f64 binary64) binary64 [fl -])
 (define-operator-impl (+ +.f64 binary64 binary64) binary64 [fl +])
 (define-operator-impl (- -.f64 binary64 binary64) binary64 [fl -])
 (define-operator-impl (* *.f64 binary64 binary64) binary64 [fl *])
 (define-operator-impl (/ /.f64 binary64 binary64) binary64 [fl /])
 
+(define-1ary-libm-operators
+ [acos acos]
+ [acosh acosh]
+ [asin asin]
+ [asinh asinh]
+ [atan atan]
+ [atanh atanh]
+ [cbrt cbrt]
+ [ceil ceil]
+ [cos cos]
+ [cosh cosh]
+ [erf erf]
+ [erfc erfc]
+ [exp exp]
+ [exp2 exp2]
+ [expm1 expm1]
+ [fabs fabs]
+ [floor floor]
+ [j0 j0]
+ [j1 j1]
+ [lgamma lgamma]
+ [log log]
+ [log10 log10]
+ [log1p log1p]
+ [log2 log2]
+ [logb logb]
+ [rint rint]
+ [round round]
+ [sin sin]
+ [sinh sinh]
+ [sqrt sqrt]
+ [tan tan]
+ [tanh tanh]
+ [tgamma tgamma]
+ [trunc trunc]
+ [y0 y0]
+ [y1 y1])
+
+(define-2ary-libm-operators
+ [atan2 atan2]
+ [copysign copysign]
+ [fdim fdim]
+ [fmax fmax]
+ [fmin fmin]
+ [fmod fmod]
+ [hypot hypot]
+ [pow pow]
+ [remainder remainder])
+
+(define-libm-operator (fma real real real)
+ [libm fma])
 
 (define-operator-impl (== ==.f64 binary64 binary64) bool
   [itype 'binary64] [otype 'bool] ; Override number of arguments
