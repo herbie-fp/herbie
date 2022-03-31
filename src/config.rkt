9376098a737205f4c6bb59f13aa2b9bbc161ace2
Brett Saiki <bksaiki@gmail.com>
2022-03-31 13:36:53 -0700

    precision:fallback flag is deprecated

--- src/config.rkt
+++ src/config.rkt
@@ -11,7 +11,7 @@
         [rules . (arithmetic polynomials fractions exponents trigonometry hyperbolic numerics special bools branches)]))
 
 (define default-flags
-  #hash([precision . (fallback)]
+  #hash([precision . ()]
         [setup . (simplify search)]
         [generate . (rr taylor simplify)]
         [reduce . (regimes avg-error binary-search branch-expressions)]
@@ -21,7 +21,11 @@
   (match* (category flag)
     [('precision 'double)
      (eprintf "The precision:double option has been removed.\n")
-     (eprintf "  Please use :precision binary32 and :precision binary64 instead.\n")
+     (eprintf "  The double-precision representation is specified with :precision binary64.\n")
+     (eprintf "See <https://herbie.uwplse.org/doc/~a/input.html> for more.\n" *herbie-version*)]
+    [('precision 'fallback)
+     (eprintf "The precision:fallback option has been removed.\n")
+     (eprintf "  The fallback representation is specified with :precision racket.\n")
      (eprintf "See <https://herbie.uwplse.org/doc/~a/input.html> for more.\n" *herbie-version*)]
     [(_ _)
      (void)]))
