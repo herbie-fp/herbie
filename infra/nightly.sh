e1f85c7b7335e7dfb36498916bb3589127f5b5f5
Brett Saiki <bksaiki@gmail.com>
2022-05-16 21:34:36 -0700

    separate run script from nightly

--- infra/nightly.sh
+++ infra/nightly.sh
@@ -1,59 +1,39 @@
 #!/bin/bash
 
+# exit immediately upon first error
+set -e -x
+
 # lowered number of cores from 6 to 4 to avoid pagetable error
 # caused by heavy use of FFI by eggmath.rkt
 CORES=4
-
-set -e -x
 SEED=$(date "+%Y%j")
 
-function output_error {
-    NAME="$2"
-    DATE=`date +%s`
-    COMMIT=`git rev-parse HEAD`
-    BRANCH=`git rev-parse --abbrev-ref HEAD`
-    HOSTNAME=`hostname`
-    cat >"$1" <<EOF
-{ "date": $DATE, "commit": "$COMMIT", "branch": "$BRANCH",
-  "hostname": "$HOSTNAME", "seed": "$SEED", "flags": [],
-  "points": -1, "iterations": -1, "bit_width": -1,
-  "note": "$NAME", "crash": true, "tests": [] }
-EOF
-}
-
-function run {
-  bench="$1"; shift
-  name="$1"; shift
-  
-  echo "Running $name test with flags $@"
-  rm -rf "reports/$name"
-  racket "src/herbie.rkt" report \
-      --note "$name" \
-      --profile \
-      --debug \
-      --seed "$SEED" \
-      --threads "$CORES" \
-      "$@" \
-      "$bench" "reports/$name" \
-      || output_error "reports/$name/results.json" "$name"
-}
-
-DIR="$1"
-shift
-
-report=$(git rev-parse --abbrev-ref HEAD)-$(date "+%Y-%m-%d")
-mkdir -p "$DIR"
-rm -rf "$DIR"/* || echo "No existing reports to delete"
-dirs=""
-for bench in bench/*; do
-  name=$(basename "$bench" .fpcore)
-  run "$bench" "$name" "$@"
-  if [ "$?" -eq 0 ]; then
-      dirs="$dirs $name";
-  fi
+# determine physical directory of this script
+src="${BASH_SOURCE[0]}"
+while [ -L "$src" ]; do
+  dir="$(cd -P "$(dirname "$src")" && pwd)"
+  src="$(readlink "$src")"
+  [[ $src != /* ]] && src="$dir/$src"
 done
+INFRA_DIR="$(cd -P "$(dirname "$src")" && pwd)"
 
+if [ -z "$1" ]; then
+  echo "Usage: $0 <output_dir>"
+else
+  DIR="$1"; shift
+  FLAGS="$@"
+fi
 
-racket infra/nightly.rkt "$DIR" $dirs
-bash infra/publish.sh upload "$DIR"
-bash infra/publish.sh index
+# run
+RECURSE=1 LOG=1 \
+  bash "$INFRA_DIR"/run.sh \
+    "$INFRA_DIR"/../bench "$DIR" \
+    --profile \
+    --debug \
+    --seed "$SEED" \
+    --threads "$CORES" \
+    $FLAGS
+
+# upload
+bash $INFRA_DIR/publish.sh upload "$DIR"
+bash $INFRA_DIR/publish.sh index
