aa37086ba8c1e09c8d7a3e68909e8f63211cbf94
Brett Saiki <bksaiki@gmail.com>
2022-06-05 11:33:58 -0700

    better errors

--- src/syntax/syntax-check.rkt
+++ src/syntax/syntax-check.rkt
@@ -43,7 +43,7 @@
       (error! stx "Invalid `if` expression with ~a arguments (expects 3)" (length args))
       (unless (null? args) (loop (last args) vars))]
      [#`(! #,props ... #,body)
-      (check-properties* props '() body)
+      (check-properties* props '() body deprecated-ops)
       (loop body vars)]
      [#`(,(? (curry set-member? '(+ - * / and or = != < > <= >=))) #,args ...)
       ;; These expand by associativity so we don't check the number of arguments
@@ -75,7 +75,7 @@
   (unless (equal? (substring name 0 1) ":")
     (error! prop "Invalid property name ~a" prop)))
 
-(define (check-properties* props vars error!)
+(define (check-properties* props vars error! deprecated-ops)
   (define prop-dict
     (let loop ([props props] [out '()])
       (match props
@@ -113,20 +113,10 @@
         (error! cite "Invalid :cite ~a; must be a list" cite)))   
 
   (when (dict-has-key? prop-dict ':pre)
-    (define deprecated-ops (mutable-set))
-    (check-expression* (dict-ref prop-dict ':pre) vars error! deprecated-ops)
-    (for ([op (in-set deprecated-ops)])
-      (warn 'deprecated #:url "faq.html#native-ops"
-            "property `pre`: operator `~a` is deprecated and will be removed in the next release."
-            op)))
+    (check-expression* (dict-ref prop-dict ':pre) vars error! deprecated-ops))
 
   (when (dict-has-key? prop-dict ':herbie-target)
-    (define deprecated-ops (mutable-set))
-    (check-expression* (dict-ref prop-dict ':herbie-target) vars error! deprecated-ops)
-    (for ([op (in-set deprecated-ops)])
-      (warn 'deprecated #:url "faq.html#native-ops"
-            "property `herbie-target`: operator `~a` is deprecated and will be removed in the next release."
-            op)))
+    (check-expression* (dict-ref prop-dict ':herbie-target) vars error! deprecated-ops))
     
   (when (dict-has-key? prop-dict ':herbie-conversions)
     (define conversion-stx (dict-ref prop-dict ':herbie-conversions))
@@ -153,13 +143,14 @@
       (error! stx "Argument ~a is not a variable name" var))
    (when (check-duplicate-identifier vars*)
       (error! stx "Duplicate argument name ~a" (check-duplicate-identifier vars*))))
-  (check-properties* props (immutable-bound-id-set vars*) error!)
   (define deprecated-ops (mutable-set))
+  (check-properties* props (immutable-bound-id-set vars*) error! deprecated-ops)
   (check-expression* body (immutable-bound-id-set vars*) error! deprecated-ops)
   (for ([op (in-set deprecated-ops)])
-    (warn 'deprecated #:url "faq.html#native-ops"
-          "operator `~a` is deprecated and will be removed in the next release."
-          op)))
+    (define message
+      (format (syntax->error-format-string stx)
+        (format "operator `~a` is deprecated and will be removed in the next release." op)))
+    (warn 'deprecated #:url "faq.html#native-ops" message)))
 
 (define (check-fpcore* stx error!)
   (match stx
