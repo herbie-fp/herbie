5f7bb8465c5ed99c2c37d8e68bf65e06182bdb06
Edward Misback <misback@cs.washington.edu>
2022-06-20 16:02:01 -0700

    Remove tooltips.

--- src/web/resources/report.js
+++ src/web/resources/report.js
@@ -124,169 +124,169 @@ const ClientGraph = new Component('#graphs', {
             return get_points_memo()
         })()
         
-        const addTooltips = (() => {
-            /* addTooltips code adapted from https://observablehq.com/@mkfreeman/plot-tooltip */
-            const hover = (tip, pos, text) => {
-                const side_padding = 10;
-                const vertical_padding = 5;
-                const vertical_offset = 15;
+        // const addTooltips = (() => {
+        //     /* addTooltips code adapted from https://observablehq.com/@mkfreeman/plot-tooltip */
+        //     const hover = (tip, pos, text) => {
+        //         const side_padding = 10;
+        //         const vertical_padding = 5;
+        //         const vertical_offset = 15;
 
-                // Empty it out
-                tip.selectAll("*").remove();
+        //         // Empty it out
+        //         tip.selectAll("*").remove();
 
-                // Append the text
-                tip
-                .style("text-anchor", "middle")
-                .style("pointer-events", "none")
-                .attr("transform", `translate(${pos[0]}, ${pos[1] + 7})`)
-                .selectAll("text")
-                .data(text)
-                .join("text")
-                .style("dominant-baseline", "ideographic")
-                .text((d) => d)
-                .attr("y", (d, i) => (i - (text.length - 1)) * 15 - vertical_offset)
-                .style("font-weight", (d, i) => (i === 0 ? "bold" : "normal"));
+        //         // Append the text
+        //         tip
+        //         .style("text-anchor", "middle")
+        //         .style("pointer-events", "none")
+        //         .attr("transform", `translate(${pos[0]}, ${pos[1] + 7})`)
+        //         .selectAll("text")
+        //         .data(text)
+        //         .join("text")
+        //         .style("dominant-baseline", "ideographic")
+        //         .text((d) => d)
+        //         .attr("y", (d, i) => (i - (text.length - 1)) * 15 - vertical_offset)
+        //         .style("font-weight", (d, i) => (i === 0 ? "bold" : "normal"));
 
-                const bbox = tip.node().getBBox();
+        //         const bbox = tip.node().getBBox();
 
-                // Add a rectangle (as background)
-                tip
-                .append("rect")
-                .attr("y", bbox.y - vertical_padding)
-                .attr("x", bbox.x - side_padding)
-                .attr("width", bbox.width + side_padding * 2)
-                .attr("height", bbox.height + vertical_padding * 2)
-                .style("fill", "white")
-                .style("stroke", "#d3d3d3")
-                .lower();
-            }
-            // To generate a unique ID for each chart so that they styles only apply to that chart
-            const id_generator = () => {
-                var S4 = function () {
-                    return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
-                };
-                return "a" + S4() + S4();
-                }
-            const addTooltips = (chart, hover_styles = { fill: "blue", opacity: 0.5 }) => {
-                let styles = hover_styles;
-                const line_styles = {
-                stroke: "blue",
-                "stroke-width": 3
-                };
-                // Workaround if it's in a figure
-                const type = d3.select(chart).node().tagName;
-                let wrapper =
-                type === "FIGURE" ? d3.select(chart).select("svg") : d3.select(chart);
+        //         // Add a rectangle (as background)
+        //         tip
+        //         .append("rect")
+        //         .attr("y", bbox.y - vertical_padding)
+        //         .attr("x", bbox.x - side_padding)
+        //         .attr("width", bbox.width + side_padding * 2)
+        //         .attr("height", bbox.height + vertical_padding * 2)
+        //         .style("fill", "white")
+        //         .style("stroke", "#d3d3d3")
+        //         .lower();
+        //     }
+        //     // To generate a unique ID for each chart so that they styles only apply to that chart
+        //     const id_generator = () => {
+        //         var S4 = function () {
+        //             return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
+        //         };
+        //         return "a" + S4() + S4();
+        //         }
+        //     const addTooltips = (chart, hover_styles = { fill: "blue", opacity: 0.5 }) => {
+        //         let styles = hover_styles;
+        //         const line_styles = {
+        //         stroke: "blue",
+        //         "stroke-width": 3
+        //         };
+        //         // Workaround if it's in a figure
+        //         const type = d3.select(chart).node().tagName;
+        //         let wrapper =
+        //         type === "FIGURE" ? d3.select(chart).select("svg") : d3.select(chart);
 
-                // Workaround if there's a legend....
-                const numSvgs = d3.select(chart).selectAll("svg").size();
-                if (numSvgs === 2)
-                wrapper = d3
-                    .select(chart)
-                    .selectAll("svg")
-                    .filter((d, i) => i === 1);
-                wrapper.style("overflow", "visible"); // to avoid clipping at the edges
+        //         // Workaround if there's a legend....
+        //         const numSvgs = d3.select(chart).selectAll("svg").size();
+        //         if (numSvgs === 2)
+        //         wrapper = d3
+        //             .select(chart)
+        //             .selectAll("svg")
+        //             .filter((d, i) => i === 1);
+        //         wrapper.style("overflow", "visible"); // to avoid clipping at the edges
 
-                // Set pointer events to visibleStroke if the fill is none (e.g., if its a line)
-                wrapper.selectAll("path").each(function (data, index, nodes) {
-                // For line charts, set the pointer events to be visible stroke
-                if (
-                    d3.select(this).attr("fill") === null ||
-                    d3.select(this).attr("fill") === "none"
-                ) {
-                    d3.select(this).style("pointer-events", "visibleStroke");
-                    styles = hover_styles.fill == 'blue' && hover_styles.opacity == .5 //_.isEqual(hover_styles, { fill: "blue", opacity: 0.5 })
-                    ? line_styles
-                    : hover_styles;
-                }
-                });
+        //         // Set pointer events to visibleStroke if the fill is none (e.g., if its a line)
+        //         wrapper.selectAll("path").each(function (data, index, nodes) {
+        //         // For line charts, set the pointer events to be visible stroke
+        //         if (
+        //             d3.select(this).attr("fill") === null ||
+        //             d3.select(this).attr("fill") === "none"
+        //         ) {
+        //             d3.select(this).style("pointer-events", "visibleStroke");
+        //             styles = hover_styles.fill == 'blue' && hover_styles.opacity == .5 //_.isEqual(hover_styles, { fill: "blue", opacity: 0.5 })
+        //             ? line_styles
+        //             : hover_styles;
+        //         }
+        //         });
 
-                const tip = wrapper
-                .selectAll(".hover-tip")
-                .data([""])
-                .join("g")
-                .attr("class", "hover")
-                .style("pointer-events", "none")
-                .style("text-anchor", "middle");
+        //         const tip = wrapper
+        //         .selectAll(".hover-tip")
+        //         .data([""])
+        //         .join("g")
+        //         .attr("class", "hover")
+        //         .style("pointer-events", "none")
+        //         .style("text-anchor", "middle");
 
-                // Add a unique id to the chart for styling
-                const id = id_generator();
+        //         // Add a unique id to the chart for styling
+        //         const id = id_generator();
 
-                // Add the event listeners
-                d3.select(chart)
-                .classed(id, true) // using a class selector so that it doesn't overwrite the ID
-                .selectAll("title")
-                .each(function () {
-                    // Get the text out of the title, set it as an attribute on the parent, and remove it
-                    const title = d3.select(this); // title element that we want to remove
-                    const parent = d3.select(this.parentNode); // visual mark on the screen
-                    const t = title.text();
-                    if (t) {
-                    parent.attr("__title", t).classed("has-title", true);
-                    title.remove();
-                    }
-                    // Mouse events
-                    parent
-                    .on("mousemove", function (event) {
-                        const text = d3.select(this).attr("__title");
-                        const pointer = d3.pointer(event, wrapper.node());
-                        if (text) tip.call(hover, pointer, text.split("\n"));
-                        else tip.selectAll("*").remove();
+        //         // Add the event listeners
+        //         d3.select(chart)
+        //         .classed(id, true) // using a class selector so that it doesn't overwrite the ID
+        //         .selectAll("title")
+        //         .each(function () {
+        //             // Get the text out of the title, set it as an attribute on the parent, and remove it
+        //             const title = d3.select(this); // title element that we want to remove
+        //             const parent = d3.select(this.parentNode); // visual mark on the screen
+        //             const t = title.text();
+        //             if (t) {
+        //             parent.attr("__title", t).classed("has-title", true);
+        //             title.remove();
+        //             }
+        //             // Mouse events
+        //             parent
+        //             .on("mousemove", function (event) {
+        //                 const text = d3.select(this).attr("__title");
+        //                 const pointer = d3.pointer(event, wrapper.node());
+        //                 if (text) tip.call(hover, pointer, text.split("\n"));
+        //                 else tip.selectAll("*").remove();
 
-                        // Raise it
-                        d3.select(this).raise();
-                        // Keep within the parent horizontally
-                        const tipSize = tip.node().getBBox();
-                        if (pointer[0] + tipSize.x < 0)
-                        tip.attr(
-                            "transform",
-                            `translate(${tipSize.width / 2}, ${pointer[1] + 7})`
-                        );
-                        else if (pointer[0] + tipSize.width / 2 > wrapper.attr("width"))
-                        tip.attr(
-                            "transform",
-                            `translate(${wrapper.attr("width") - tipSize.width / 2}, ${
-                            pointer[1] + 7
-                            })`
-                        );
-                    })
-                    .on("mouseout", function (event) {
-                        tip.selectAll("*").remove();
-                        // Lower it!
-                        d3.select(this).lower();
-                    });
-                });
+        //                 // Raise it
+        //                 d3.select(this).raise();
+        //                 // Keep within the parent horizontally
+        //                 const tipSize = tip.node().getBBox();
+        //                 if (pointer[0] + tipSize.x < 0)
+        //                 tip.attr(
+        //                     "transform",
+        //                     `translate(${tipSize.width / 2}, ${pointer[1] + 7})`
+        //                 );
+        //                 else if (pointer[0] + tipSize.width / 2 > wrapper.attr("width"))
+        //                 tip.attr(
+        //                     "transform",
+        //                     `translate(${wrapper.attr("width") - tipSize.width / 2}, ${
+        //                     pointer[1] + 7
+        //                     })`
+        //                 );
+        //             })
+        //             .on("mouseout", function (event) {
+        //                 tip.selectAll("*").remove();
+        //                 // Lower it!
+        //                 d3.select(this).lower();
+        //             });
+        //         });
 
-                // Remove the tip if you tap on the wrapper (for mobile)
-                wrapper.on("touchstart", () => tip.selectAll("*").remove());
-                // Add styles
-                const style_string = Object.keys(styles)
-                .map((d) => {
-                    return `${d}:${styles[d]};`;
-                })
-                    .join("");
+        //         // Remove the tip if you tap on the wrapper (for mobile)
+        //         wrapper.on("touchstart", () => tip.selectAll("*").remove());
+        //         // Add styles
+        //         const style_string = Object.keys(styles)
+        //         .map((d) => {
+        //             return `${d}:${styles[d]};`;
+        //         })
+        //             .join("");
                 
-                function html(string) {
-                    const t = document.createElement('template');
-                    t.innerHTML = string;
-                    return t.content;
-                }
+        //         function html(string) {
+        //             const t = document.createElement('template');
+        //             t.innerHTML = string;
+        //             return t.content;
+        //         }
 
-                // Define the styles
-                const style = html(`<style>
-                    .${id} .has-title {
-                    cursor: pointer; 
-                    pointer-events: all;
-                    }
-                    .${id} .has-title:hover {
-                    ${style_string}
-                }
-                </style>`);
-                chart.appendChild(style);
-                return chart;
-            }
-            return addTooltips
-        })()
+        //         // Define the styles
+        //         const style = html(`<style>
+        //             .${id} .has-title {
+        //             cursor: pointer; 
+        //             pointer-events: all;
+        //             }
+        //             .${id} .has-title:hover {
+        //             ${style_string}
+        //         }
+        //         </style>`);
+        //         chart.appendChild(style);
+        //         return chart;
+        //     }
+        //     return addTooltips
+        // })()
         const plot = async (varName, function_names) => {
             const functions = [
                 { name: 'start', line: { stroke: '#aa3333ff' }, area: { fill: "#c001"}, dot: { stroke: '#ff000007'} },
@@ -378,25 +378,25 @@ const ClientGraph = new Component('#graphs', {
                     }),
                 ]
             }
-            const out = addTooltips(Plot.plot({
-            width: '800',
-            height: '400',                
-                x: {
-                    tickFormat: d => tick_strings[tick_ordinals.indexOf(d)],
-                    ticks: tick_ordinals, label: `value of ${varName}`,
-                    labelAnchor: 'center', /*labelOffset: [200, 20], tickRotate: 70, */
-                    domain,
-                    grid: true
-                },
-                y: {
-                    label: "Bits of error", domain: [0, bits],
-                    ticks: new Array(bits / 4 + 1).fill(0).map((_, i) => i * 4),
-                    tickFormat: d => d % 8 != 0 ? '' : d
-                },
-                marks: await Promise.all([...await extra_axes_and_ticks(),
-                    ...functions.map(async config =>
-                                    await line_and_dot_graphs(config)).flat()])
-        }))
+            const out = Plot.plot({
+                width: '800',
+                height: '400',                
+                    x: {
+                        tickFormat: d => tick_strings[tick_ordinals.indexOf(d)],
+                        ticks: tick_ordinals, label: `value of ${varName}`,
+                        labelAnchor: 'center', /*labelOffset: [200, 20], tickRotate: 70, */
+                        domain,
+                        grid: true
+                    },
+                    y: {
+                        label: "Bits of error", domain: [0, bits],
+                        ticks: new Array(bits / 4 + 1).fill(0).map((_, i) => i * 4),
+                        tickFormat: d => d % 8 != 0 ? '' : d
+                    },
+                    marks: await Promise.all([...await extra_axes_and_ticks(),
+                        ...functions.map(async config =>
+                                        await line_and_dot_graphs(config)).flat()])
+            })
             out.setAttribute('viewBox', '0 0 800 430')
             return out
         }
