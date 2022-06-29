7cd3d1ecdf76e055a13ec38160f92a0cd085bb3b
Pavel Panchekha <me@pavpanchekha.com>
2022-06-28 19:12:26 -0600

    Woops, bad merge

--- src/alternative.rkt
+++ src/alternative.rkt
@@ -1,6 +1,6 @@
 #lang racket
 
-(require "cost.rkt" "interface.rkt")
+(require "cost.rkt")
 (provide (struct-out change) (struct-out alt) make-alt alt?
          alt-program alt-add-event *start-prog* *all-alts*
          alt-cost alt-equal?)
