5829a7d3130842aadf515e55aa7f55bba238296a
oflatt <oflatt@gmail.com>
2022-10-21 16:35:21 +0200

    better script

--- evalegglog.sh
+++ evalegglog.sh
@@ -3,8 +3,10 @@
 # exit immediately upon first error
 set -e -x
 
+# build egg-herbie to make sure updated
+cargo build --release --manifest-path=egg-herbie/Cargo.toml
 
-racket src/herbie.rkt report bench/hamming egglogreport
+racket src/herbie.rkt report egglogreport
 
 racket src/herbie.rkt report --egglog-disabled bench/hamming vanillareport
 
