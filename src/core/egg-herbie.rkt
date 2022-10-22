ed17c599c8e77330dde1f9f5a03279398fd75f4d
Pavel Panchekha <me@pavpanchekha.com>
2022-10-22 12:42:59 -0600

    Moving stuff around to organize it

--- src/core/egg-herbie.rkt
+++ src/core/egg-herbie.rkt
@@ -25,16 +25,97 @@
     [(? number?)
      datum]))
 
+;; Converts a string expression from egg into a Racket S-expr
+(define (egg-expr->expr expr eg-data)
+  (define parsed (read (open-input-string expr)))
+  (egg-parsed->expr parsed (egraph-data-egg->herbie-dict eg-data)))
+
+;; Like `egg-expr->expr` but expected the string to
+;; parse into a list of S-exprs
+(define (egg-exprs->exprs exprs eg-data)
+  (for/list ([egg-expr (in-port read (open-input-string exprs))])
+    (egg-parsed->expr egg-expr (egraph-data-egg->herbie-dict eg-data))))
+
+(define (egg-parsed->expr parsed rename-dict)
+  (match parsed
+    [`(Rewrite=> ,rule ,expr)
+     `(Rewrite=> ,rule ,(egg-parsed->expr expr rename-dict))]
+    [`(Rewrite<= ,rule ,expr)
+     `(Rewrite<= ,rule ,(egg-parsed->expr expr rename-dict))]
+    [(list op 'real rest-parsed ...)
+     (cons op (map (curryr egg-parsed->expr rename-dict) rest-parsed))]
+    [(list op prec rest-parsed ...)
+     (define new-op (string->symbol (format "~s.~s" op prec)))
+     (cons new-op (map (curryr egg-parsed->expr rename-dict) rest-parsed))]
+    [(? number? num)
+     num]
+    [_
+     (hash-ref rename-dict parsed)]))
+
+;; returns a pair of the string representing an egg expr, and updates the hash tables in the egraph
+(define (expr->egg-expr expr egg-data)
+  (define egg->herbie-dict (egraph-data-egg->herbie-dict egg-data))
+  (define herbie->egg-dict (egraph-data-herbie->egg-dict egg-data))
+  (expr->egg-expr-helper expr egg->herbie-dict herbie->egg-dict))
+
+(define (expr->egg-expr-helper expr egg->herbie-dict herbie->egg-dict)
+  (cond
+    [(list? expr)
+     (match-define (list op prec) (extract-operator (first expr)))
+     `(,op ,prec ,@(map (curryr expr->egg-expr-helper egg->herbie-dict herbie->egg-dict) (rest expr)))]
+    [(and (number? expr) (exact? expr) (real? expr))
+     expr]
+    [(hash-has-key? herbie->egg-dict expr)
+     (hash-ref herbie->egg-dict expr)]
+    [else
+     (define replacement (string->symbol (format "h~a" (hash-count herbie->egg-dict))))
+     (hash-set! herbie->egg-dict expr replacement)
+     (hash-set! egg->herbie-dict replacement expr)
+     replacement]))
+
 (module+ test
   (check-equal? (pattern->egg `(+ a b)) '(+ real ?a ?b))
   (check-equal? (pattern->egg `(/ c (- 2 a))) '(/ real ?c (- real 2 ?a)))
   (check-equal? (pattern->egg `(cos.f64 (PI.f64))) '(cos f64 (PI f64)))
-  (check-equal? (pattern->egg `(if (TRUE) x y)) '(if real (TRUE real) ?x ?y)))
+  (check-equal? (pattern->egg `(if (TRUE) x y)) '(if real (TRUE real) ?x ?y))
+
+  (define test-exprs
+    (list (cons '(+ y x) "(+ real h0 h1)")
+          (cons '(+ x y) "(+ real h1 h0)")
+          (cons '(- 2 (+ x y)) "(- real 2 (+ real h1 h0))")
+          (cons '(- z (+ (+ y 2) x)) "(- real h2 (+ real (+ real h0 2) h1))")
+          (cons '(*.f64 x y) "(* f64 h1 h0)")
+          (cons '(+.f32 (*.f32 x y) 2) "(+ f32 (* f32 h1 h0) 2)")
+          (cons '(cos.f64 (PI.f64)) "(cos f64 (PI f64))")
+          (cons '(if (TRUE) x y) "(if real (TRUE real) h1 h0)")))
+
+  (with-egraph
+   (lambda (egg-graph)
+     (for ([(in expected-out) (in-dict test-exprs)])
+       (let* ([out (~a (expr->egg-expr in egg-graph))]
+              [computed-in (egg-expr->expr out egg-graph)])
+         (check-equal? out expected-out)
+         (check-equal? computed-in in)))))
+
+  (define extended-expr-list
+    (list
+     '(/ (- (exp x) (exp (- x))) 2)
+     '(/ (+ (- b) (sqrt (- (* b b) (* (* 3 a) c)))) (* 3 a))
+     '(/ (+ (- b) (sqrt (- (* b b) (* (* 3 a) c)))) (* 3 a)) ;; duplicated to make sure it would still work
+     '(* r 30)
+     '(* 23/54 r)
+     '(+ 3/2 1.4)))
+
+  (with-egraph
+   (lambda (egg-graph)
+     (for ([expr extended-expr-list])
+       (check-equal? 
+        (egg-expr->expr (~a (expr->egg-expr expr egg-graph)) egg-graph)
+        expr)))))
 
 ;; the first hash table maps all symbols and non-integer values to new names for egg
 ;; the second hash is the reverse of the first
 (struct egraph-data (egraph-pointer egg->herbie-dict herbie->egg-dict))
-(struct iteration-data (num-nodes num-eclasses time))
 
 (define (egraph-get-simplest egraph-data node-id iteration)
   (define ptr (egraph_get_simplest (egraph-data-egraph-pointer egraph-data) node-id iteration))
@@ -43,17 +124,20 @@
   (egg-expr->expr str egraph-data))
 
 (define (egraph-get-variants egraph-data node-id orig-expr)
-  (define expr-str (expr->egg-expr orig-expr egraph-data))
+  (define expr-str (~a (expr->egg-expr orig-expr egraph-data)))
   (define ptr (egraph_get_variants (egraph-data-egraph-pointer egraph-data) node-id expr-str))
   (define str (cast ptr _pointer _string/utf-8))
   (destroy_string ptr)
   (egg-exprs->exprs str egraph-data))
 
+(define (egraph-is-unsound-detected egraph-data)
+  (egraph_is_unsound_detected (egraph-data-egraph-pointer egraph-data)))
+
 (define (egraph-get-cost egraph-data node-id iteration)
   (egraph_get_cost (egraph-data-egraph-pointer egraph-data) node-id iteration))
 
-(define (egraph-is-unsound-detected egraph-data)
-  (egraph_is_unsound_detected (egraph-data-egraph-pointer egraph-data)))
+(define (egraph-get-times-applied egraph-data rule)
+  (egraph_get_times_applied (egraph-data-egraph-pointer egraph-data) (FFIRule-name rule)))
 
 (define (egraph-stop-reason egraph-data)
   (match (egraph_get_stop_reason (egraph-data-egraph-pointer egraph-data))
@@ -71,8 +155,6 @@
   (ptr-set! ptr _byte n 0)
   ptr)
 
-(define (egraph-get-times-applied egraph-data rule)
-  (egraph_get_times_applied (egraph-data-egraph-pointer egraph-data) (FFIRule-name rule)))
 
 (define (make-ffi-rules rules)
   (for/list ([rule (in-list rules)])
@@ -87,6 +169,37 @@
     (free (FFIRule-left rule))
     (free (FFIRule-right rule))
     (free rule)))
+
+
+;; calls the function on a new egraph, and cleans up
+(define (with-egraph egraph-function)
+  (define egraph (egraph-data (egraph_create) (make-hash) (make-hash)))
+  (define res (egraph-function egraph))
+  (egraph_destroy (egraph-data-egraph-pointer egraph))
+  res)
+
+(struct egg-add-exn exn:fail ())
+
+(define (egraph-get-proof egraph-data expr goal)
+  (define egg-expr (~a (expr->egg-expr expr egraph-data)))
+  (define egg-goal (~a (expr->egg-expr goal egraph-data)))
+  (define pointer (egraph_get_proof (egraph-data-egraph-pointer egraph-data) egg-expr egg-goal))
+  (define res (cast pointer _pointer _string/utf-8))
+  (destroy_string pointer)
+  (for/list ([line (in-list (string-split res "\n"))])
+    (egg-expr->expr line egraph-data)))
+
+;; result function is a function that takes the ids of the nodes
+(define (egraph-add-expr eg-data expr)
+  (define egg-expr (~a (expr->egg-expr expr eg-data)))
+  (define result (egraph_add_expr (egraph-data-egraph-pointer eg-data) egg-expr))
+  (when (= result 0)
+    (raise (egg-add-exn
+            "Failed to add expr to egraph"
+            (current-continuation-marks))))
+  (- result 1))
+
+(struct iteration-data (num-nodes num-eclasses time))
 
 (define (convert-iteration-data egraphiters size)
   (cond
@@ -106,130 +219,6 @@
   (define res (convert-iteration-data egraphiters res-len))
   (destroy_egraphiters res-len egraphiters)
   res)
-
-
-;; calls the function on a new egraph, and cleans up
-(define (with-egraph egraph-function)
-  (define egraph (egraph-data (egraph_create) (make-hash) (make-hash)))
-  (define res (egraph-function egraph))
-  (egraph_destroy (egraph-data-egraph-pointer egraph))
-  res)
-
-;; Converts a string expression from egg into a Racket S-expr
-(define (egg-expr->expr expr eg-data)
-  (define parsed (read (open-input-string expr)))
-  (egg-parsed->expr parsed (egraph-data-egg->herbie-dict eg-data)))
-
-;; Like `egg-expr->expr` but expected the string to
-;; parse into a list of S-exprs
-(define (egg-exprs->exprs exprs eg-data)
-  (for/list ([egg-expr (in-port read (open-input-string exprs))])
-    (egg-parsed->expr egg-expr (egraph-data-egg->herbie-dict eg-data))))
-
-(define (egg-parsed->expr parsed rename-dict)
-  (match parsed
-    [`(Rewrite=> ,rule ,expr)
-      `(Rewrite=> ,rule ,(egg-parsed->expr expr rename-dict))]
-    [`(Rewrite<= ,rule ,expr)
-      `(Rewrite<= ,rule ,(egg-parsed->expr expr rename-dict))]
-    [(list op prec rest-parsed ...)
-     (define new-op
-       (if (equal? prec 'real)
-           op
-           (string->symbol (format "~s.~s" op prec))))
-     (cons new-op (map (curryr egg-parsed->expr rename-dict) rest-parsed))]
-    [(? number? num)
-     num]
-    [_
-     (hash-ref rename-dict parsed)]))
-
-;; returns a pair of the string representing an egg expr, and updates the hash tables in the egraph
-(define (expr->egg-expr expr egg-data)
-  (define egg->herbie-dict (egraph-data-egg->herbie-dict egg-data))
-  (define herbie->egg-dict (egraph-data-herbie->egg-dict egg-data))
-  (expr->egg-expr-helper expr egg->herbie-dict herbie->egg-dict))
-
-(define (expr->egg-expr-helper expr egg->herbie-dict herbie->egg-dict)
-  (cond
-    [(list? expr)
-     (match-define (list op prec) (extract-operator (first expr)))
-     (format "(~a ~a ~a)" op prec
-             (string-join
-              (map (lambda (e) (expr->egg-expr-helper e egg->herbie-dict herbie->egg-dict))
-                   (rest expr))
-              " "))]
-    [(and (number? expr) (exact? expr) (real? expr))
-     (number->string expr)]
-    [(hash-has-key? herbie->egg-dict expr)
-     (symbol->string (hash-ref herbie->egg-dict expr))]
-    [else
-     (define new-key (format "h~a" (number->string (hash-count herbie->egg-dict))))
-     (define new-key-symbol (string->symbol new-key))
-         
-     (hash-set! herbie->egg-dict
-                expr
-                new-key-symbol)
-     (hash-set! egg->herbie-dict
-                new-key-symbol
-                expr)
-      new-key]))
-
-(struct egg-add-exn exn:fail ())
-
-(define (egraph-get-proof egraph-data expr goal)
-  (define egg-expr (expr->egg-expr expr egraph-data))
-  (define egg-goal (expr->egg-expr goal egraph-data))
-  (define pointer (egraph_get_proof (egraph-data-egraph-pointer egraph-data) egg-expr egg-goal))
-  (define res (cast pointer _pointer _string/utf-8))
-  (destroy_string pointer)
-  (for/list ([line (in-list (string-split res "\n"))])
-    (egg-expr->expr line egraph-data)))
-
-;; result function is a function that takes the ids of the nodes
-(define (egraph-add-expr eg-data expr)
-  (define egg-expr (expr->egg-expr expr eg-data))
-  (define result (egraph_add_expr (egraph-data-egraph-pointer eg-data) egg-expr))
-  (when (= result 0)
-    (raise (egg-add-exn
-            "Failed to add expr to egraph"
-            (current-continuation-marks))))
-  (- result 1))
-
-(module+ test
-
-  (define test-exprs
-    (list (cons '(+ y x) "(+ real h0 h1)")
-          (cons '(+ x y) "(+ real h1 h0)")
-          (cons '(- 2 (+ x y)) "(- real 2 (+ real h1 h0))")
-          (cons '(- z (+ (+ y 2) x)) "(- real h2 (+ real (+ real h0 2) h1))")
-          (cons '(*.f64 x y) "(* f64 h1 h0)")
-          (cons '(+.f32 (*.f32 x y) 2) "(+ f32 (* f32 h1 h0) 2)")
-          (cons '(cos.f64 (PI.f64)) "(cos f64 (PI f64 ))")
-          (cons '(if (TRUE) x y) "(if real (TRUE real ) h1 h0)")))
-
-  (with-egraph
-   (lambda (egg-graph)
-     (for ([(in expected-out) (in-dict test-exprs)])
-       (let* ([out (expr->egg-expr in egg-graph)]
-              [computed-in (egg-expr->expr out egg-graph)])
-         (check-equal? out expected-out)
-         (check-equal? computed-in in)))))
-
-  (define extended-expr-list
-    (list
-     '(/ (- (exp x) (exp (- x))) 2)
-     '(/ (+ (- b) (sqrt (- (* b b) (* (* 3 a) c)))) (* 3 a))
-     '(/ (+ (- b) (sqrt (- (* b b) (* (* 3 a) c)))) (* 3 a)) ;; duplicated to make sure it would still work
-     '(* r 30)
-     '(* 23/54 r)
-     '(+ 3/2 1.4)))
-
-  (with-egraph
-   (lambda (egg-graph)
-     (for ([expr extended-expr-list])
-       (check-equal? 
-        (egg-expr->expr (expr->egg-expr expr egg-graph) egg-graph)
-        expr)))))
 
 (define ffi-rules-cache #f)
 
