9cebf5e6120bed8401edc65bf0f0392f7d9d85af
Brett Saiki <bksaiki@gmail.com>
2022-11-01 12:55:20 -0700

    add CI job for checking determinism

---
+++ infra/diff.rkt
@@ -0,0 +1,28 @@
+#lang racket
+
+(require "../src/datafile.rkt")
+
+(define (field-equal? access t1 t2)
+  (equal? (access t1) (access t2)))
+
+;; Only interested in output
+;; Only compare against runs with the same configuration
+(define (datafile-tests-equal? df1 df2)
+  (define tests1 (report-info-tests df1))
+  (define tests2 (report-info-tests df2))
+  (and (= (length tests1) (length tests2))
+       (andmap (λ (t1 t2)
+                 (and (field-equal? table-row-name t1 t2)
+                      (field-equal? table-row-identifier t1 t2)
+                      (field-equal? table-row-output t1 t2)
+                      (field-equal? table-row-cost-accuracy t1 t2)))
+               tests1 tests2)))
+
+(module+ main
+  (command-line
+   #:args (outdir1 outdir2)
+   (define df1 (read-datafile (build-path outdir1 "results.json")))
+   (define df2 (read-datafile (build-path outdir2 "results.json")))
+   (unless (datafile-tests-equal? df1 df2)
+     (printf "datafiles do not have the same output expressions\n")
+     (exit 1))))
