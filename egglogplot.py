30092591a5326c7bcbd528563564c50a8f623e14
oflatt <oflatt@gmail.com>
2022-11-07 16:06:30 -0800

    macros

--- egglogplot.py
+++ egglogplot.py
@@ -7,19 +7,25 @@ matplotlib.rcParams['pdf.fonttype'] = 42
 matplotlib.rcParams['ps.fonttype'] = 42
 matplotlib.rcParams['savefig.bbox'] = 'tight'
 
-assert (len(sys.argv) == 4)
+assert (len(sys.argv) == 5)
 results_file_vanilla = sys.argv[1]
 results_file_egglog = sys.argv[2]
 output_plot_error = sys.argv[3]
+macro_file = sys.argv[4]
 
 vanilla_data = json.load(open(results_file_vanilla))["tests"]
 egglog_data = json.load(open(results_file_egglog))["tests"]
 
 
-tests = set(map(lambda row: row["name"], vanilla_data))
-assert (len(tests) == len(vanilla_data))
+tests_unfiltered = set(map(lambda row: row["name"], vanilla_data))
+assert (len(tests_unfiltered) == len(vanilla_data))
+
 vanilla_tests = dict(map(lambda row: (row["name"], row), vanilla_data))
 egglog_tests = dict(map(lambda row: (row["name"], row), egglog_data))
+
+
+tests = set(filter(lambda test: vanilla_tests[test]["start"] > 0.5, tests_unfiltered))
+
 
 def test_error_diff(test):
   return egglog_tests[test]["end"] - vanilla_tests[test]["end"]
@@ -27,11 +33,8 @@ def test_error_diff(test):
 def plot_error():
   tests_sorted = list(tests)
   tests_sorted.sort(key = lambda test: test_error_diff(test))
-  for test in tests_sorted:
-    print(egglog_tests[test]["name"])
-    print(egglog_tests[test]["end"])
-    print(vanilla_tests[test]["end"])
-    print(test_error_diff(test))
+  #for test in tests_sorted:
+   # print(egglog_tests[test]["name"])
   
   xs = range(len(tests_sorted))
   ys = list(map(lambda test: test_error_diff(test), tests_sorted))
@@ -45,4 +48,17 @@ plot_error()
 
 #plt.savefig('egglogreport/error.pdf')
 
+macro_port = open(macro_file, "w")
 
+def save_macro(name, val):
+  macro_port.write("\\newcommand{\\%s}{%s}\n" % (name, val))
+
+def generate_macros():
+  num_tests = len(tests)
+  save_macro("numtests", num_tests)
+  num_better_error = len(list(filter(lambda test: test_error_diff(test) < 0, tests)))
+  save_macro("numbettererror", num_better_error)
+  num_worse_error = len(list(filter(lambda test: test_error_diff(test) > 0, tests)))
+  save_macro("numworseerror", num_worse_error)
+  
+generate_macros()
--- egglogreport.sh
+++ egglogreport.sh
@@ -7,4 +7,4 @@ if [ -d "egglogdata" ]; then rm -Rf egglogdata; fi
 
 mkdir egglogdata
 
-python3 egglogplot.py "vanillareport/results.json" "egglogreport/results.json" "egglogdata/error.pdf"
+python3 egglogplot.py "vanillareport/results.json" "egglogreport/results.json" "egglogdata/error.pdf" "egglogdata/macros.txt"
