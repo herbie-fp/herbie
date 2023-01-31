9ab3f05f242e70ed3ac56259fafee3318ebe8789
oliver <oflatt@gmail.com>
2023-01-31 10:54:04 -0800

    another small fix

--- evalscripts/evalegglog.sh
+++ evalscripts/evalegglog.sh
@@ -11,11 +11,11 @@ TIMEOUT=500
 
 ls
 make egglog
-racket src/herbie.rkt report --threads "$CORES" --no-pareto --seed "$SEED" --timeout "$TIMEOUT"  "$BENCHMARKS" egglogreport
+racket -y src/herbie.rkt report --threads "$CORES" --no-pareto --seed "$SEED" --timeout "$TIMEOUT"  "$BENCHMARKS" egglogreport
 
 git checkout main
 cargo build --release --manifest-path=egg-herbie/Cargo.toml
-racket src/herbie.rkt report --threads "$CORES" --timeout "$TIMEOUT" --seed "$SEED" --no-pareto "$BENCHMARKS" vanillareport
+racket -y src/herbie.rkt report --threads "$CORES" --timeout "$TIMEOUT" --seed "$SEED" --no-pareto "$BENCHMARKS" vanillareport
 git checkout oflatt-egglog-ctx
 
 
