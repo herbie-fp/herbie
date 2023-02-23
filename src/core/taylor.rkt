b6e17b2521cc6701f62f1701dba5aefd40506f56
Pavel Panchekha <me@pavpanchekha.com>
2023-02-23 16:51:40 -0700

    Fix taylor expansions of arc-hyperbolic-trig functions

--- src/core/taylor.rkt
+++ src/core/taylor.rkt
@@ -158,16 +158,7 @@
     [`(tan ,arg)
      (taylor var `(/ (sin ,arg) (cos ,arg)))]
     [`(log ,arg)
-     (let* ([arg* (normalize-series (taylor var arg))]
-            [rest (taylor-log (cdr arg*))])
-       (if (zero? (car arg*))
-           rest
-           (cons 0
-                 (λ (n)
-                    (if (= n 0)
-                        (simplify `(+ (* (neg ,(car arg*)) (log ,var))
-                                      ,((cdr rest) 0)))
-                        ((cdr rest) n))))))]
+     (taylor-log var (taylor var arg))]
     [`(pow ,base ,(? exact-integer? power))
      (taylor-pow (normalize-series (taylor var base)) power)]
     [`(pow ,base 1/2)
@@ -197,14 +188,14 @@
      (taylor-quotient x- x+)]
     [`(asinh ,x)
      (define tx (taylor var x))
-     (taylor-log (taylor-add tx (taylor-sqrt (taylor-add (taylor-mult tx tx) (taylor-exact 1)))))]
+     (taylor-log var (taylor-add tx (taylor-sqrt (taylor-add (taylor-mult tx tx) (taylor-exact 1)))))]
     [`(acosh ,x)
      (define tx (taylor var x))
-     (taylor-log (taylor-add tx (taylor-sqrt (taylor-add (taylor-mult tx tx) (taylor-exact -1)))))]
+     (taylor-log var (taylor-add tx (taylor-sqrt (taylor-add (taylor-mult tx tx) (taylor-exact -1)))))]
     [`(atanh ,x)
      (define tx (taylor var x))
      (taylor-mult (taylor-exact 1/2)
-                  (taylor-log (taylor-quotient (taylor-add (taylor-exact 1) tx)
+                  (taylor-log var (taylor-quotient (taylor-add (taylor-exact 1) tx)
                                                (taylor-add (taylor-exact 1) (taylor-negate tx)))))]
     [_
      (taylor-exact expr)]))
@@ -482,26 +473,31 @@
              (λ ()
                 (logstep (logcompute (- i 1))))))
 
-(define (taylor-log coeffs)
-  "coeffs is assumed to start with a nonzero term"
-  (let ([hash (make-hash)])
-    (hash-set! hash 0 (simplify `(log ,(coeffs 0))))
-    (cons 0
-          (λ (n)
-            (hash-ref! hash n
-                       (λ ()
-                         (let* ([tmpl (logcompute n)])
-                           (simplify
-                            `(/
-                              (+ ,@(for/list ([term tmpl])
-                                     (match term
-                                       [`(,coeff ,k ,ps ...)
-                                        `(* ,coeff (/ (* ,@(for/list ([i (in-naturals 1)] [p ps])
-                                                             (if (= p 0)
-                                                                 1
-                                                                 `(pow (* ,(factorial i) ,(coeffs i)) ,p))))
-                                                      (pow ,(coeffs 0) ,(- k))))])))
-                              ,(factorial n))))))))))
+(define (taylor-log var arg)
+  (match-define (cons shift coeffs) (normalize-series arg))
+  (define hash (make-hash))
+  (hash-set! hash 0 (simplify `(log ,(coeffs 0))))
+
+  (define (series n)
+    (hash-ref! hash n
+               (λ ()
+                 (let* ([tmpl (logcompute n)])
+                   (simplify
+                    `(/
+                      (+ ,@(for/list ([term tmpl])
+                             (match term
+                               [`(,coeff ,k ,ps ...)
+                                `(* ,coeff (/ (* ,@(for/list ([i (in-naturals 1)] [p ps])
+                                                     (if (= p 0)
+                                                         1
+                                                         `(pow (* ,(factorial i) ,(coeffs i)) ,p))))
+                                              (pow ,(coeffs 0) ,(- k))))])))
+                      ,(factorial n)))))))
+
+  (cons 0 (λ (n)
+            (if (and (= n 0) (not (zero? shift)))
+                (simplify `(+ (* (neg ,shift) (log ,var)) ,(series 0)))
+                (series n)))))
 
 (module+ test
   (require rackunit "../syntax/types.rkt" "../load-plugin.rkt")
