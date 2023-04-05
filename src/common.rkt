4f48ab8f5bfae3e8673e770aa4c99a1d1d192846
Varun Pradeep <varun10p@cs.washington.edu>
2023-04-04 17:00:16 -0700

    Renaming Page Content.

--- src/common.rkt
+++ src/common.rkt
@@ -9,7 +9,7 @@
          argmins argmaxs index-of set-disjoint?
          get-seed set-seed!
          quasisyntax dict sym-append
-         format-time format-bits format-error format-cost web-resource
+         format-time format-bits format-accuracy format-cost web-resource
          (all-from-out "config.rkt"))
 
 ;; Various syntactic forms of convenience used in Herbie
@@ -170,7 +170,7 @@
    [(and (> r 0) sign) (format "+~a~a" (/ (round (* r 10)) 10) unit)]
    [else (format "~a~a" (/ (round (* r 10)) 10) unit)]))
 
-(define (format-error r repr #:sign [sign #f] #:unit [unit #f])
+(define (format-accuracy r repr #:sign [sign #f] #:unit [unit #f])
   (cond 
     [(not r) ""]
     [else
