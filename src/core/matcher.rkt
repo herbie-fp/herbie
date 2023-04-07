1b2e9883f9a4c79f4542963cf0a51d024a8ace66
Varun Pradeep <varun10p@cs.washington.edu>
2023-04-06 17:43:35 -0700

    Quick fix.

--- src/core/matcher.rkt
+++ src/core/matcher.rkt
@@ -75,7 +75,7 @@
           (when result
             (define canon-name (hash-ref canon-names (rule-name rule)))
             (hash-update! rule-apps canon-name (curry + 1) 1)
-            (sow (list (car result) root-loc))))))) ; TODO : HWHAT TO DO HERE
+            (sow (list (list (car result) root-loc)))))))) ; TODO : HWHAT TO DO HERE
   ;; rule statistics
   (for ([(name count) (in-hash rule-apps)])
     (when (> count 0) (timeline-push! 'rules (~a name) count)))
