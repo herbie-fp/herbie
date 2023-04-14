d0899d949fd0a2ec0bc37e38532ac1bb1cbbbb67
adityaakhileshwaran <aakhiles@cs.washinton.edu>
2023-04-13 21:18:43 -0700

    Fixed rewrite-once bug

--- src/soundiness.rkt
+++ src/soundiness.rkt
@@ -38,42 +38,52 @@
                           num-decrease (length prev)))))
   proof-diffs)
   
-(define (generate-rewrite-once-proof rule loc prog)
-  (list "Rewrite=> " 
-                  (rule-name rule) ; TODO : How to get name? Is rule a struct
-                  (location-get loc prog))) ;; expr
+(define (generate-rewrite-once-proof rule loc prog prev)
+  (list (program-body (alt-program prev)) ;; Start Expression
+        (list 'Rewrite=> 
+              (rule-name rule) ;; name
+              (program-body prog)))) ;; expr
 
 (define (add-soundiness-to pcontext ctx altn)
   (match altn
     ;; This is alt coming from rr
     [(alt prog `(rr, loc, input #f #f) `(,prev))
-      (define proof 
-            (if (rr-input? input) ; Check if input is an rr-input struct (B-E-R) or rule struct (R-O)
-                (get-rr-proof (rr-input-rules input) ; TODO : Don't summon rules like this
-                              (rr-input-input-exprs input) (rr-input-iter-limit input)
-                              (location-get loc (alt-program prev))
-                              (location-get loc prog))
-                (generate-rewrite-once-proof input loc prog))) ;; (R-O) case
       (cond
-       [proof
-        ;; Proofs are actually on subexpressions,
-        ;; we need to construct the proof for the full expression
-        (define proof*
-          (for/list ([step proof])
-            (let ([step* (canonicalize-rewrite step)])
-              (program-body (location-do loc prog (λ _ step*))))))
-        (define errors
-          (let ([vars (program-variables prog)])
-            (get-proof-errors proof* pcontext ctx vars)))
-        (alt prog `(rr, loc, input, proof* ,errors) `(,prev))]
+       [(rr-input? input) ;; Check if input is an rr-input struct (B-E-R)
+        (define proof 
+          (get-rr-proof (rr-input-rules input)
+                        (rr-input-input-exprs input) (rr-input-iter-limit input)
+                        (location-get loc (alt-program prev)) (location-get loc prog)))
+        (cond
+          [proof
+            (define proof*
+              (for/list ([step proof])
+                (let ([step* (canonicalize-rewrite step)])
+                  (program-body (location-do loc prog (λ _ step*))))))
+            (define errors
+              (let ([vars (program-variables prog)])
+                (get-proof-errors proof* pcontext ctx vars)))
+            (alt prog `(rr, loc, input, proof* ,errors) `(,prev))]
+          [else
+            (alt prog `(rr ,loc, input #f #f) `(,prev))])]
+        
+        [(rule? input) ;; (R-O) case
+          (define proof-ro
+            (generate-rewrite-once-proof input loc prog prev))
+          (define errors-ro
+            (let ([vars (program-variables prog)])
+              (get-proof-errors proof-ro pcontext ctx vars)))
+          (alt prog `(rr, loc, input, proof-ro, errors-ro) `(,prev))]
+
        [else
         (alt prog `(rr ,loc, input #f #f) `(,prev))])]
-    ;; This is alt coming from simplify
+        
+      ;; This is alt coming from simplify
     [(alt prog `(simplify ,loc ,input, #f #f) `(,prev))
-     (define proof (get-proof input
+      (define proof (get-proof input
                               (location-get loc (alt-program prev))
                               (location-get loc prog)))
-     (cond
+      (cond
        [proof
         ;; Proofs are actually on subexpressions,
         ;; we need to construct the proof for the full expression
