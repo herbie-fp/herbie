95708ac68839c19b2070fd4e31af12b4f610f4d8
Varun Pradeep <varun10p@cs.washington.edu>
2023-05-04 16:32:13 -0700

    Fixing for merge

--- src/core/matcher.rkt
+++ src/core/matcher.rkt
@@ -150,11 +150,10 @@
 
 ; NOTE : Copy-pasted from simplify
 (define (get-rr-proof rules input-exprs iter-limit start end)
-  (with-egraph
-    (λ (egg-graph)
-      (define node-ids (map (curry egraph-add-expr egg-graph) input-exprs)) ; TODO : Need to have all exprs from original rr run -> REMOVE (LIST START END)
-      (define iter-data (egraph-run-rules egg-graph #:limit iter-limit (*node-limit*) rules node-ids #t)) ; TODO : limit should have value
-      (define proof (egraph-get-proof egg-graph start end))
-      (when (null? proof)
-        (error (format "Failed to produce proof for ~a to ~a" start end)))
-      proof)))
+  (define egg-graph (make-egraph))
+    (define node-ids (map (curry egraph-add-expr egg-graph) input-exprs)) ; TODO : Need to have all exprs from original rr run -> REMOVE (LIST START END)
+    (define iter-data (egraph-run-rules egg-graph #:limit iter-limit (*node-limit*) rules node-ids #t)) ; TODO : limit should have value
+    (define proof (egraph-get-proof egg-graph start end))
+    (when (null? proof)
+      (error (format "Failed to produce proof for ~a to ~a" start end)))
+    proof)
