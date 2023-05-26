d25657f6ad01e385da12202d6b8503101ed4caf6
zane <39070793+zaneenders@users.noreply.github.com>
2023-05-25 18:42:39 -0600

    ugly but should be passing

--- src/core/localize.rkt
+++ src/core/localize.rkt
@@ -5,54 +5,6 @@
 
 (provide localize-error local-error-as-tree)
 
-(define (all-subexpressions expr repr)
-  (remove-duplicates
-   (reap [sow]
-         (let loop ([expr expr] [repr repr])
-           (sow (cons expr repr))
-           (match expr
-             [(? number?) (void)]
-             [(? variable?) (void)]
-             [`(if ,c ,t ,f)
-              (loop c (get-representation 'bool))
-              (loop t repr)
-              (loop f repr)]
-             [(list op args ...)
-              (define atypes (operator-info op 'itype))
-              (for ([arg args] [atype atypes])
-                (loop arg atype))])))))
-
-; Compute local error or each sampled point at each node in `prog`.
-; Uses math/bigfloat rather than rival for speed.
-(define (compute-local-errors-fast prog ctx)
-  (define expr (program-body prog))
-  (define subexprs (all-subexpressions expr (context-repr ctx)))
-  (define subprogs
-    (for/list ([sexpr (in-list subexprs)])
-      `(λ ,(program-variables prog) ,(first sexpr))))
-  (define exact-fn (batch-eval-progs subprogs 'bf ctx))
-  (define errs (make-hash (map (curryr cons '()) subexprs)))
-  (for ([(pt ex) (in-pcontext (*pcontext*))])
-    (define bf-values (apply exact-fn pt))
-    (define bfhash (make-hash (map cons subexprs bf-values)))
-    (for ([expr (in-list subexprs)])
-      (define err
-        (match expr
-          [(? number?) 1]
-          [(? variable?) 1]
-          [`(if ,c ,ift ,iff) 1]
-          [(list f args ...)
-           (define repr (operator-info f 'otype))
-           (define <-bf (representation-bf->repr repr))
-           (define argapprox
-             (for/list ([arg (in-list args)] [repr (in-list (operator-info f 'itype))])
-               ((representation-bf->repr repr) (hash-ref bfhash arg))))
-           (ulp-difference (<-bf (hash-ref bfhash expr))
-                           (apply (operator-info f 'fl) argapprox) repr)]))
-      (hash-update! errs expr (curry cons err))))
-
-  errs)
-
 ;; Returns a list of expressions sorted by increasing local error
 (define (localize-error prog ctx)
   (define errs (compute-local-errors prog ctx))
@@ -63,67 +15,74 @@
               (sow (cons err expr)))))
     > #:key (compose errors-score car)))
 
-; expr = (posit16->binary32 1)
-; ctx = () () -> binary32
+(define (all-subexpressions expr repr)
+  (remove-duplicates
+  (reap [sow]
+        (let loop ([expr expr] [repr repr])
+          (sow (cons expr repr))
+          (match expr
+            [(? number?) (void)]
+            [(? variable?) (void)]
+            [`(if ,c ,t ,f)
+              (loop c (get-representation 'bool))
+              (loop t repr)
+              (loop f repr)]
+            [(list op args ...)
+              (define atypes (operator-info op 'itype))
+              (for ([arg args] [atype atypes])
+                (loop arg atype))])))))
 
-; subexpr = (posit16->binary32 1)
-; ctx = () () -> binary32
 
-; subexpr = 1 repr = posit16
-
+; Compute local error or each sampled point at each node in `prog`.
 (define (compute-local-errors prog ctx)
-  (define expr (program-body prog))
-  (define subexprs (all-subexpressions expr (context-repr ctx)))
+  (define expression (program-body prog))
+  (define sub-expressions (all-subexpressions expression (context-repr ctx)))
   (define prog-list 
-    (for/list ([sexpr (in-list subexprs)])
-      `(λ ,(context-vars ctx) ,(car sexpr))))
+    (for/list ([subexpr (in-list sub-expressions)])
+      `(λ ,(context-vars ctx) ,(car subexpr))))
   (define ctx-list 
-    (for/list ([sexpr (in-list subexprs)])
-      (struct-copy context ctx [repr (cdr sexpr)])))
-  
+    (for/list ([subexpr (in-list sub-expressions)])
+      (struct-copy context ctx [repr (cdr subexpr)])))
+
   (define subexprs-fn (eval-prog-list-real prog-list ctx-list))
 
+  ; Mutable error hack, this is bad
   (define temp-errs
-    (for/hash ([sexpr (in-list subexprs)])
-      (values (car sexpr) '())))
-
-  ; Mutable error hack this is bad
+    (for/hash ([subexpr (in-list sub-expressions)])
+      (values (car subexpr) '())))
   (define errs (make-hash))
   (for ([(k v) temp-errs]) 
     (hash-set! errs k v))
-
+      
   (for ([(pt ex) (in-pcontext (*pcontext*))])
     (define exacts (apply subexprs-fn pt))
-    (define exacts-hash
-      (for/hash (
-        [expr (in-list subexprs)] 
-        [ex (in-list exacts)])
-        (values (car expr) ex)))
-    (for ([expr (in-list subexprs)])
-      (define err
-        (match (car expr)
-          [(? number?) 1]
-          [(? variable?) 1]
-          [`(if ,c ,ift ,iff) 1]
-          [(list f args ...)
-            (define repr (operator-info f 'otype))
-            (define argapprox
-              (for/list (
-                [arg (in-list args)] 
-                [repr (in-list (operator-info f 'itype))])
-                (hash-ref exacts-hash arg)
-              )
-            )
-            (ulp-difference
-              (hash-ref exacts-hash (car expr))
-              (apply (operator-info f 'fl) argapprox) repr)
-          ]))
-      (hash-update! errs (car expr) (curry cons err))
-    )
+    (define exacts-hash 
+      (make-immutable-hash (map cons sub-expressions exacts)))
+    (for ([expr (in-list sub-expressions)])
+       (define err
+         (match (car expr)
+           [(? number?) 1]
+           [(? variable?) 1]
+           [`(if ,c ,ift ,iff) 1]
+           [(list f args ...)
+             (define repr (operator-info f 'otype))
+             (define argapprox
+               (for/list (
+                 [arg (in-list args)] 
+                 [repr (in-list (operator-info f 'itype))])
+                 (hash-ref exacts-hash 
+                  (cons arg repr))
+               )
+             )
+             (ulp-difference
+               (hash-ref exacts-hash expr)
+               (apply (operator-info f 'fl) argapprox) repr)
+           ]))
+       (hash-update! errs (car expr) (curry cons err))
+     )
   )
-  ; return errs
   errs
-)
+ )
 
 ;; Compute the local error of every subexpression of `prog`
 ;; and returns the error information as an S-expr in the
