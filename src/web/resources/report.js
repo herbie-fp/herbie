49fbf1bc40ad22d22dc69a8dfa2986edf6d1faa4
Pavel Panchekha <me@pavpanchekha.com>
2023-06-07 19:34:44 -0600

    Major refactor to ClientGraph

--- src/web/resources/report.js
+++ src/web/resources/report.js
@@ -117,164 +117,149 @@ var TryIt = new Component("#try-it", {
 });
 
 const ClientGraph = new Component('#graphs', {
-    setup: async () => {
-        const points_json = await (async () => {
-            const get_points_store = {}
-            
-            const get_points_memo = async () => {
-                if (get_points_store.value) { return get_points_store.value }
-                const ps = await get_json('points.json')
-                get_points_store.value = ps
-                return get_points_store.value
-            }
-            const get_json = url => fetch(url, {
-                // body: `_body_`,
+    setup: async function() {
+        const points = await fetch("points.json", {
                 headers: {"content-type": "text/plain"},
                 method: "GET",
                 mode: 'cors'
-                }).then(async response => {
-                //await new Promise(r => setTimeout(() => r(), 200) )  // model network delay
-                return await response.json()
-            })
-            return get_points_memo()
-        })()
-        
-        const plot = async (varName, function_names) => {
-            const functions = [
-                { name: 'start', line: { stroke: '#aa3333ff' }, area: { fill: "#c001"}, dot: { stroke: '#ff000035'} },
-                { name: 'end', line: { stroke: '#0000ffff' }, area: { fill: "#00c1"}, dot: { stroke: '#0000ff35'} },
-                { name: 'target', line: { stroke: 'green' }, dot: { stroke: '#00ff0035'}}
-            ].filter(o => function_names.includes(o.name))
-            const index = all_vars.indexOf(varName)
-            // NOTE ticks and splitpoints include all vars, so we must index
-            const { bits, points, error, ticks_by_varidx, splitpoints_by_varidx } = points_json
-            const ticks = ticks_by_varidx[index]
-            if (!ticks) {
-                return Element("div", "The function could not be plotted on the given range for this input.")
-            }
-            const tick_strings = ticks.map(t => t[0])
-            const tick_ordinals = ticks.map(t => t[1])
-            const tick_0_index = tick_strings.indexOf("0")
-            const splitpoints = splitpoints_by_varidx[index]
-            const grouped_data = points.map((p, i) => ({
-                input: p,
-                error: Object.fromEntries(function_names.map(name => ([name, error[name][i]])))
-            }))
-            const domain = [Math.min(...tick_ordinals), Math.max(...tick_ordinals)]
+        });
+        this.points_json = await points.json();
+        this.all_vars = this.points_json.vars;
 
-            async function extra_axes_and_ticks() {
-                return [
-                    ...splitpoints.map(p => Plot.ruleX([p], { stroke: "lightgray", strokeWidth: 4 })),
-                    ...(tick_0_index > -1 ? [Plot.ruleX([tick_ordinals[tick_0_index]])] : []),
-                ]
-            }
+        await this.render(this.all_vars[0], ['start', 'end']);
+    },
 
+    plot: async function(varName, function_names) {
+        const functions = [
+            { name: 'start', line: { stroke: '#aa3333ff' }, area: { fill: "#c001"}, dot: { stroke: '#ff000035'} },
+            { name: 'end', line: { stroke: '#0000ffff' }, area: { fill: "#00c1"}, dot: { stroke: '#0000ff35'} },
+            { name: 'target', line: { stroke: 'green' }, dot: { stroke: '#00ff0035'}}
+        ].filter(o => function_names.includes(o.name))
+        const index = this.all_vars.indexOf(varName)
+        // NOTE ticks and splitpoints include all vars, so we must index
+        const { bits, points, error, ticks_by_varidx, splitpoints_by_varidx } = this.points_json
+        const ticks = ticks_by_varidx[index]
+        if (!ticks) {
+            return Element("div", "The function could not be plotted on the given range for this input.")
+        }
+        const tick_strings = ticks.map(t => t[0])
+        const tick_ordinals = ticks.map(t => t[1])
+        const tick_0_index = tick_strings.indexOf("0")
+        const splitpoints = splitpoints_by_varidx[index]
+        const grouped_data = points.map((p, i) => ({
+            input: p,
+            error: Object.fromEntries(function_names.map(name => ([name, error[name][i]])))
+        }))
+        const domain = [Math.min(...tick_ordinals), Math.max(...tick_ordinals)]
 
-            async function line_and_dot_graphs({ name, fn, line, dot, area }) {
-                const key_fn = fn => (a, b) => fn(a) - fn(b)
-                const index = all_vars.indexOf(varName)
-                const data = grouped_data.map(({ input, error }) => ({
-                        x: input[index],
-                        y: error[name]
-                })).sort(key_fn(d => d.x))
-                    .map(({ x, y }, i) => ({ x, y, i }))
-                const sliding_window = (A, size) => {
-                    const half = Math.floor(size / 2)
-                    const running_sum = A.reduce((acc, v) => (acc.length > 0 ? acc.push(v.y + acc[acc.length - 1]) : acc.push(v.y), acc), [])
-                    const xs = 
-                    console.log('running', running_sum)
-                    return running_sum.reduce((acc, v, i) => {
+        let extra_axes_and_ticks = [
+            ...splitpoints.map(p => Plot.ruleX([p], { stroke: "lightgray", strokeWidth: 4 })),
+            ...(tick_0_index > -1 ? [Plot.ruleX([tick_ordinals[tick_0_index]])] : []),
+        ]
+
+        let marks = [];
+        for (let { name, fn, line, dot, area } of functions) {
+            const key_fn = fn => (a, b) => fn(a) - fn(b)
+            const index = this.all_vars.indexOf(varName)
+            const data = grouped_data.map(({ input, error }) => ({
+                x: input[index],
+                y: error[name]
+            })).sort(key_fn(d => d.x))
+                  .map(({ x, y }, i) => ({ x, y, i }))
+            const sliding_window = (A, size) => {
+                const half = Math.floor(size / 2)
+                const running_sum = A.reduce((acc, v) => (acc.length > 0 ? acc.push(v.y + acc[acc.length - 1]) : acc.push(v.y), acc), [])
+                const xs = 
+                      console.log('running', running_sum)
+                return running_sum.reduce((acc, v, i) => {
                     const length = 
-                        (i - half) < 0 ? half + i
-                        : (i + half) >= running_sum.length ? (running_sum.length - (i - half))
-                        : size
+                          (i - half) < 0 ? half + i
+                          : (i + half) >= running_sum.length ? (running_sum.length - (i - half))
+                          : size
                     const top =
-                        (i + half) >= running_sum.length ? running_sum[running_sum.length - 1]
-                        : running_sum[i + half]
+                          (i + half) >= running_sum.length ? running_sum[running_sum.length - 1]
+                          : running_sum[i + half]
                     const bottom =
-                        (i - half) < 0 ? 0
-                        : running_sum[i - half]
+                          (i - half) < 0 ? 0
+                          : running_sum[i - half]
                     acc.push({average: (top - bottom) / length, x: A[i].x, length})
                     return acc
-                    }, [])
-                }
-                const compress = (L, out_len, chunk_compressor = points => points[0]) => L.reduce((acc, pt, i) => i % Math.floor(L.length / out_len) == 0 ? (acc.push(chunk_compressor(L.slice(i, i + Math.floor(L.length / out_len)))), acc) : acc, [])
-                const bin_size = 128
-                const sliding_window_data = compress(
-                    sliding_window(data, bin_size), 800, points => ({
-                        average: points.reduce((acc, e) => e.average + acc, 0) / points.length,
-                        x: points.reduce((acc, e) => e.x + acc, 0) / points.length
-                    }))
-                return [
-                    Plot.line(sliding_window_data, {
-                        x: "x",
-                        y: "average",
-                        strokeWidth: 2, ...line,
-                    }),
-                    Plot.dot(compress(data, 800), {x: "x", y: "y", r: 1.3,
-                        title: d => `x: ${d.x} \n i: ${d.i} \n bits of error: ${d.y}`,
-                        ...dot
-                    }),
-                ]
+                }, [])
             }
-            const out = Plot.plot({
-                width: '800',
-                height: '400',                
-                    x: {
-                        tickFormat: d => tick_strings[tick_ordinals.indexOf(d)],
-                        ticks: tick_ordinals, label: varName,
-                        labelAnchor: 'center', /*labelOffset: [200, 20], tickRotate: 70, */
-                        domain,
-                        grid: true
-                    },
-                    y: {
-                        label: "Bits of error", domain: [0, bits],
-                        ticks: new Array(bits / 4 + 1).fill(0).map((_, i) => i * 4),
-                        tickFormat: d => d % 8 != 0 ? '' : d
-                    },
-                    marks: await Promise.all([...await extra_axes_and_ticks(),
-                        ...functions.map(async config =>
-                                        await line_and_dot_graphs(config)).flat()])
-            })
-            out.setAttribute('viewBox', '0 0 800 430')
-            return out
-        }
-        const all_vars = points_json.vars
-        async function render(selected_var_name, selected_functions) {
-            const all_fns = ['start', 'end', 'target'].filter(name => points_json.error[name] != false)
-            const fn_description = {
-                start: "Original expression",
-                end: "Herbie's result",
-                target: "Target expression"
-            }
-            const options_view = Element("div", {id: "plot_options"}, [
-                Element("div", {id: "variables"}, [
-                    "Bits of error vs. ",
-                    all_vars.map(v =>
-                        Element("span", {
-                            className: "variable " + (selected_var_name == v ? "selected" : "")
-                        }, v)),
-                ]),
-                Element("div", {id: "functions"}, [
-                    all_fns.map(fn => Element("div", [
-                        Element("div", {
-                            id: "function_"+fn,
-                            className: "function " + (selected_functions.includes(fn) ? "selected" : ""),
-                        }, []),
-                        Element("span", { className: "functionDescription"}, fn_description[fn]),
-                    ])),
-                ]),
+            const compress = (L, out_len, chunk_compressor = points => points[0]) => L.reduce((acc, pt, i) => i % Math.floor(L.length / out_len) == 0 ? (acc.push(chunk_compressor(L.slice(i, i + Math.floor(L.length / out_len)))), acc) : acc, [])
+            const bin_size = 128
+            const sliding_window_data = compress(
+                sliding_window(data, bin_size), 800, points => ({
+                    average: points.reduce((acc, e) => e.average + acc, 0) / points.length,
+                    x: points.reduce((acc, e) => e.x + acc, 0) / points.length
+                }))
+            marks = marks.concat([
+                Plot.line(sliding_window_data, {
+                    x: "x",
+                    y: "average",
+                    strokeWidth: 2, ...line,
+                }),
+                Plot.dot(compress(data, 800), {x: "x", y: "y", r: 1.3,
+                                               title: d => `x: ${d.x} \n i: ${d.i} \n bits of error: ${d.y}`,
+                                               ...dot
+                                              }),
             ]);
-            const toggle = (option, options) => options.includes(option) ? options.filter(o => o != option) : [...options, option]
-            options_view.querySelectorAll('.variable').forEach(e => e.onclick = () => {
-                render(e.textContent, selected_functions)
-            })
-            options_view.querySelectorAll('.function').forEach(e => e.onclick = () => {
-                render(selected_var_name, toggle(e.id.split('_').slice(1).join('_'), selected_functions))
-            })
-            document.querySelector('#graphs-content').replaceChildren(await plot(selected_var_name, selected_functions), options_view)
         }
-        render(all_vars[0], ['start', 'end'])
+        const out = Plot.plot({
+            width: '800',
+            height: '400',                
+            x: {
+                tickFormat: d => tick_strings[tick_ordinals.indexOf(d)],
+                ticks: tick_ordinals, label: varName,
+                labelAnchor: 'center', /*labelOffset: [200, 20], tickRotate: 70, */
+                domain,
+                grid: true
+            },
+            y: {
+                label: "Bits of error", domain: [0, bits],
+                ticks: new Array(bits / 4 + 1).fill(0).map((_, i) => i * 4),
+                tickFormat: d => d % 8 != 0 ? '' : d
+            },
+            marks: await Promise.all([...extra_axes_and_ticks,
+                                      ...marks])
+        });
+        out.setAttribute('viewBox', '0 0 800 430')
+        return out
+    },
+
+    render: async function(selected_var_name, selected_functions) {
+        const all_fns = ['start', 'end', 'target'].filter(name => this.points_json.error[name] != false)
+        const fn_description = {
+            start: "Original expression",
+            end: "Herbie's result",
+            target: "Target expression"
+        }
+        const options_view = Element("div", {id: "plot_options"}, [
+            Element("div", {id: "variables"}, [
+                "Bits of error vs. ",
+                this.all_vars.map(v =>
+                    Element("span", {
+                        className: "variable " + (selected_var_name == v ? "selected" : "")
+                    }, v)),
+            ]),
+            Element("div", {id: "functions"}, [
+                all_fns.map(fn => Element("div", [
+                    Element("div", {
+                        id: "function_"+fn,
+                        className: "function " + (selected_functions.includes(fn) ? "selected" : ""),
+                    }, []),
+                    Element("span", { className: "functionDescription"}, fn_description[fn]),
+                ])),
+            ]),
+        ]);
+        const toggle = (option, options) => options.includes(option) ? options.filter(o => o != option) : [...options, option]
+        options_view.querySelectorAll('.variable').forEach(e => e.onclick = () => {
+            render(e.textContent, selected_functions)
+        })
+        options_view.querySelectorAll('.function').forEach(e => e.onclick = () => {
+            render(selected_var_name, toggle(e.id.split('_').slice(1).join('_'), selected_functions))
+        })
+        document.querySelector('#graphs-content').replaceChildren(await this.plot(selected_var_name, selected_functions), options_view)
     }
 })
 
