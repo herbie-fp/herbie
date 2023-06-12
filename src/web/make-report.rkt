d982c12dea855648ffbb63458574a1da6de6aca8
zane <39070793+zaneenders@users.noreply.github.com>
2023-06-12 11:54:09 -0600

    fixed formatting

--- src/web/make-report.rkt
+++ src/web/make-report.rkt
@@ -46,9 +46,10 @@
 
   (define table-labels
     '("Test" "Start" "Result" "Target" "Time"))
+
   (define help-text
     #hash(("Result" . "Color key:\nGreen: improved accuracy\nLight green: no initial error\nOrange: no accuracy change\nRed: accuracy worsened\nGray: timeout\nDark Gray: error")
-      ("Target" . "Color key:\nDark green: better than target\nGreen: matched target\nOrange: improved but did not match target\nYellow: no accuracy change\n")))
+        ("Target" . "Color key:\nDark green: better than target\nGreen: matched target\nOrange: improved but did not match target\nYellow: no accuracy change\n")))
 
   (define total-time (apply + (map table-row-time tests)))
   (define total-tests (length tests))
