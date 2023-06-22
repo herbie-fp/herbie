2b2d3af644f07419c04f1c210220b1499a5db941
Jackson Brough <jackson.brough@utah.edu>
2023-06-21 19:31:10 -0600

    Representation specific neg and fabs

--- src/preprocess.rkt
+++ src/preprocess.rkt
@@ -1,25 +1,28 @@
 #lang racket
 
 (require "core/egg-herbie.rkt" "core/simplify.rkt" "syntax/rules.rkt"
-         "syntax/types.rkt" "common.rkt" "programs.rkt" "points.rkt" "float.rkt")
+         "syntax/syntax.rkt" "syntax/types.rkt" "common.rkt" "programs.rkt"
+         "points.rkt" "float.rkt")
 
 (provide find-preprocessing preprocess-pcontext)
 
 ;; See https://pavpanchekha.com/blog/symmetric-expressions.html
-;; TODO: tests
 (define (find-preprocessing expression context rules)
   ;; Here `*` means a test identity that *may* be equal to `expression`, and
   ;; `~` means the simplest form of an expression.
   (define variables (context-vars context))
+  (define variable-representations (context-var-reprs context))
   (define pairs (combinations variables 2))
   (define swaps*
     (for/list ([pair (in-list pairs)])
       (match-define (list a b) pair)
       (replace-vars (list (cons a b) (cons b a)) expression)))
   (define evens*
-    (for/list ([variable (in-list variables)])
-      ;; TODO: Use negate for vars repr here
-      (replace-vars (list (cons variable `(fabs.f64 ,variable))) expression)))
+    (for/list ([variable (in-list variables)]
+               [representation (in-list variable-representations)])
+      ;; TODO: Handle case where neg isn't supported for this representation
+      (define negate (get-parametric-operator 'neg representation))
+      (replace-vars (list (cons variable (list negate variable))) expression)))
   ;; TODO: If egraph detects unsoundness, abort preprocessing. Would need
   ;; explicit access to egraph constructed by run-egg though
   (define query (make-egg-query (cons expression (append swaps* evens*)) rules))
@@ -38,6 +41,17 @@
                #:when (> (length component) 1))
       (cons 'sort component)))
   (append abs-instructions sort-instructions))
+
+(define (connected-components variables swaps)
+  (define components (disjoint-set (length variables)))
+  (for ([swap (in-list swaps)])
+    (match-define (list a b) swap)
+    (disjoint-set-union! components
+                         (disjoint-set-find! components (index-of variables a))
+                         (disjoint-set-find! components (index-of variables b))))
+  (group-by
+   (compose (curry disjoint-set-find! components) (curry index-of variables))
+   variables))
 
 (define (preprocess-pcontext context pcontext preprocessing)
   (define preprocess
@@ -73,16 +87,8 @@
          (list-set* points indices sorted)))]
     [(list 'abs variable)
      (define index (index-of variables variable))
-     ;; TODO: Make abs an actually correct operation
-     (lambda (points) (list-update points index abs))]))
-
-(define (connected-components variables swaps)
-  (define components (disjoint-set (length variables)))
-  (for ([swap (in-list swaps)])
-    (match-define (list a b) swap)
-    (disjoint-set-union! components
-                         (disjoint-set-find! components (index-of variables a))
-                         (disjoint-set-find! components (index-of variables b))))
-  (group-by
-   (compose (curry disjoint-set-find! components) (curry index-of variables))
-   variables))
+     (define abs (operator-info (get-parametric-operator
+                                 'fabs
+                                 (list-ref (context-var-reprs context) index))
+                                'fl))
+     (curryr list-update index abs)]))
