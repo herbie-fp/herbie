9048b857229256cd4e0b86c1a0d5129feb583ea0
Jackson Brough <jacksontbrough@gmail.com>
2023-07-20 19:24:21 -0600

    Try fixing the preprocessing formatting

--- src/web/common.rkt
+++ src/web/common.rkt
@@ -8,7 +8,7 @@
                           *expr-cse-able?*))
 
 (require "../common.rkt" "../syntax/read.rkt" "../programs.rkt"
-         "../syntax/types.rkt" "../syntax/sugar.rkt")
+         "../preprocess.rkt" "../syntax/types.rkt" "../syntax/sugar.rkt")
 
 (provide render-menu render-warnings render-large render-comparison render-program
          render-bogosity render-help
@@ -86,59 +86,59 @@
 
 (define (preprocess->c preprocess)
   (match preprocess
-    [(list 'sort variables ...)
+    [(preprocessing-instruction 'sort variables)
      (format "assert(~a);" (format-less-than-condition variables))]
-    [(list 'abs x)
+    [(preprocessing-instruction 'abs x)
      (format "~a = abs(~a);" x x)]))
 
 (define (preprocess->java preprocess)
   (match preprocess
-    [(list 'sort variables ...)
+    [(preprocessing-instruction 'sort variables)
      (format "assert ~a;" (format-less-than-condition variables))]
-    [(list 'abs x)
+    [(preprocessing-instruction 'abs x)
      (format "~a = Math.abs(~a);" x x)]))
 
 (define (preprocess->python preprocess)
   (match preprocess
-    [(list 'sort variables ...)
+    [(preprocessing-instruction 'sort variables)
      (define comma-joined (string-join (map ~a variables) ", "))
      (format "[~a] = sort([~a])" comma-joined comma-joined)]
-    [(list 'abs x)
+    [(preprocessing-instruction 'abs x)
      (format "~a = abs(~a)" x x)]))
 
 (define (preprocess->julia preprocess)
   (match preprocess
-    [(list 'sort variables ...)
+    [(preprocessing-instruction 'sort variables)
       (define comma-joined (string-join (map ~a variables) ", "))
       (format "~a = sort([~a])" comma-joined comma-joined)]
-    [(list 'abs x)
+    [(preprocessing-instruction 'abs x)
      (format "~a = abs(~a)" x x)]))
 
 (define (preprocess->matlab preprocess)
   (match preprocess
-    [(list 'sort variables ...)
+    [(preprocessing-instruction 'sort variables)
      (define comma-joined (string-join (map ~a variables) ", "))
      (format "~a = num2cell(sort([~a])){:}" comma-joined comma-joined)]
-    [(list 'abs x)
+    [(preprocessing-instruction 'abs x)
      (format "~a = abs(~a)" x x)]))
 
 (define (preprocess->tex preprocess)
   (match preprocess
-    [(list 'sort variables ...)
+    [(preprocessing-instruction 'sort variables)
      (define comma-joined (string-join (map ~a variables) ", "))
      (format "[~a] = \\mathsf{sort}([~a])\\\\" comma-joined comma-joined)]
-    [(list 'abs x)
+    [(preprocessing-instruction 'abs x)
      (format "~a = |~a|\\\\" x x)]))
 
 (define (preprocess->default preprocess)
   (match preprocess
-    [(list 'sort a b)
+    [(preprocessing-instruction 'sort (list a b))
      (format sort-note (format "~a and ~a" a b))]
-    [(list 'sort variables ...)
+    [(preprocessing-instruction 'sort variables)
      (format
       sort-note
       (string-join (map ~a variables) ", " #:before-last ", and "))]
-    [(list 'abs x)
+    [(preprocessing-instruction 'abs x)
      (format abs-note x)]))
 
 (define sort-note
