8cf0a25e86ad24058b0667240da5896936b19dab
Brett Saiki <bksaiki@gmail.com>
2023-10-09 21:32:28 -0700

    better 'default-platform' macro

--- src/platform.rkt
+++ src/platform.rkt
@@ -1,6 +1,8 @@
 #lang racket
 
+(require (for-syntax racket/match))
 (require "syntax/types.rkt" "syntax/syntax.rkt")
+
 (provide *active-platform* activate-platform!)
 
 (module+ internals
@@ -55,17 +57,102 @@
   (define pform (platform reprs ops))
   (hash-set! platforms name pform))
 
+;; Macro version of `register-platform!`
+;; 
+;; Example usage:
+;; ```
+;; (define-platform default
+;;   [bool ()                   ; no conversions
+;;    #:const [TRUE]            ; keyword declaration
+;;    [FALSE]                   ; non-keyword declaration
+;;    #:1ary [not]              ; keyword declaration (e.g., not : bool -> bool)
+;;    #:2ary [and or]           ; keyword declaration (e.g., and : bool x bool -> bool)
+;;    #:2ary binary64 [== > <]  ; keyword declaration (e.g., == : binary64 x binary64 -> bool)
+;;    [>= binary64 binary64]    ; non-keyword declaration
+;;    [<= binary64 binary64]    ; non-keyword declaration
+;;   ]
+;;   ...
+;; )
+;; ```
+(define-syntax (define-platform stx)
+  (syntax-case stx ()
+    [(_ name e1 es ...)
+     (let loop ([entries (syntax->list #'(e1 es ...))]
+                [repr-names '()]
+                [convs/repr '()]
+                [ops/repr '()])
+       (match entries
+         [(list)
+          (with-syntax ([(repr-names ...) repr-names]
+                        [(convs/repr ...) convs/repr]
+                        [(ops/repr ...) ops/repr])
+          #'(begin
+              (define repr-data
+                (for/list ([repr '(repr-names ...)]
+                           [convs-to '(convs/repr ...)]
+                           [op-data '(ops/repr ...)])
+                  (define conversions
+                    (for/list ([conv-to convs-to])
+                      (list 'cast conv-to)))
+                  (cons repr (append conversions op-data))))
+              (register-platform! 'name repr-data)))]
+         [(list entry rest ...)
+          (syntax-case entry ()
+            [(name (conv-targets ...) op-clauses ...)
+             (begin
+               (define targets (syntax->list #'(conv-targets ...)))
+               (define ops
+                 (let loop ([clauses #'(op-clauses ...)] [done '()])
+                   (syntax-case clauses ()
+                     [() done]
+                     [(#:const [ops ...] rest ...)
+                      (loop #'(rest ...) (append (syntax->list #'((ops) ...)) done))]
+                     [(#:1ary [ops ...] rest ...)
+                      (loop #'(#:1ary name [ops ...] rest ...) done)]
+                     [(#:2ary [ops ...] rest ...)
+                      (loop #'(#:2ary name [ops ...] rest ...) done)]
+                     [(#:3ary [ops ...] rest ...)
+                      (loop #'(#:3ary name [ops ...] rest ...) done)]
+                     [(#:4ary [ops ...] rest ...)
+                      (loop #'(#:4ary name [ops ...] rest ...) done)]
+                     [(#:1ary itype [ops ...] rest ...)
+                      (with-syntax ([(itypes ...) (build-list 1 (λ (_) #'itype))])
+                        (loop #'(rest ...) (append (syntax->list #'((ops itypes ...) ...)) done)))]
+                     [(#:2ary itype [ops ...] rest ...)
+                      (with-syntax ([(itypes ...) (build-list 2 (λ (_) #'itype))])
+                        (loop #'(rest ...) (append (syntax->list #'((ops itypes ...) ...)) done)))]
+                     [(#:3ary itype [ops ...] rest ...)
+                      (with-syntax ([(itypes ...) (build-list 3 (λ (_) #'itype))])
+                        (loop #'(rest ...) (append (syntax->list #'((ops itypes ...) ...)) done)))]
+                     [(#:4ary itype [ops ...] rest ...)
+                      (with-syntax ([(itypes ...) (build-list 4 (λ (_) #'itype))])
+                        (loop #'(rest ...) (append (syntax->list #'((ops itypes ...) ...)) done)))]
+                     [([op itypes ...] rest ...)
+                      (loop #'(rest ...) (cons #'(op itypes ...) done))]
+                     [_
+                      (raise-syntax-error 'define-platform "malformed operator" stx clauses)])))
+               (loop rest
+                     (cons (syntax->datum #'name) repr-names)
+                     (cons targets convs/repr)
+                     (cons ops ops/repr)))]
+            [_
+             (raise-syntax-error 'define-platform "malformed entry" stx entry)])]))]
+    [(_ _)
+     (raise-syntax-error 'define-platform "empty platform" stx)]))
+
+;; This is a simpler implementation of `define-platform` that
+;; does not support keywords
 ;; Macro version of `register-platform!`.
-(define-syntax define-platform
-  (syntax-rules ()
-    [(_ name [repr-name (conv-targets ...) [op itypes ...] ...] ...)
-     (begin
-       (define repr-data
-         (for/list ([repr '(repr-name ...)]
-                    [convs-to '((conv-targets ...) ...)]
-                    [op-data '(((op itypes ...) ...) ...)])
-           (define conversions
-             (for/list ([conv-to convs-to])
-               (list 'cast conv-to)))
-           (cons repr (append conversions op-data))))
-       (register-platform! 'name repr-data))]))
+; (define-syntax define-platform
+;   (syntax-rules ()
+;     [(_ name [repr-name (conv-targets ...) [op itypes ...] ...] ...)
+;      (begin
+;        (define repr-data
+;          (for/list ([repr '(repr-name ...)]
+;                     [convs-to '((conv-targets ...) ...)]
+;                     [op-data '(((op itypes ...) ...) ...)])
+;            (define conversions
+;              (for/list ([conv-to convs-to])
+;                (list 'cast conv-to)))
+;            (cons repr (append conversions op-data))))
+;        (register-platform! 'name repr-data))]))
