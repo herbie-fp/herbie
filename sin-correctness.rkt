128d937ee92d2d11edae1c5234e3f991d3afe22f
AYadrov <kowalski228@mail.ru>
2023-11-01 14:10:34 -0600

    ival-sin is changed to an exponent-reduction version in a local copy of rival

--- sin-correctness.rkt
+++ sin-correctness.rkt
@@ -19,41 +19,43 @@
     [(zero? n) '()]
     [else
      (define flo-num-lo (bit-field->flonum (random-bits 64)))
-     (define flo-num-hi (flstep flo-num-lo (random-integer 1 1000)))
+     (define flo-num-hi (flstep flo-num-lo (random-integer 1 100000000)))
+     
      (cons (ival (bf flo-num-lo) (bf flo-num-hi)) (interval-generator (- n 1)))]))
 
 (define (sin-correctness)
   ; Random number in double-precision
-  (define flo-num-lo (bit-field->flonum (random-bits 64)))
-  (define flo-num-hi (flstep flo-num-lo (random-integer 1 1000)))
+  ;(define flo-num-lo (bit-field->flonum (random-bits 64)))
+  ;(define flo-num-hi (flstep flo-num-lo (random-integer 1 1000)))
   
   ; Make an ival out of these floats
-  (define iv (ival (bf flo-num-lo) (bf flo-num-hi)))
+  (define iv (ival (bf* (bf- (bfrandom) (bf 0.5)) (bf 200)) (bf* (bf- (bfrandom) (bf 0.5)) (bf 200))))
   
-  (match-define (list reduced-lo reduced-hi) (ival->list-flo (ival-sin-reduced iv)))
-  (match-define (list reg-lo reg-hi) (ival->list-flo (ival-sin iv)))
+  (match-define (list reduced-lo reduced-hi) (ival->list-flo (ival-sin iv)))
+  (match-define (list reg-lo reg-hi) (ival->list-flo (ival-sin-default iv)))
   (cond
     [(not (ival-fl-cmp reg-lo reg-hi reduced-lo reduced-hi))
+     ;(printf "lo ~a hi ~a\n" flo-num-lo flo-num-hi)
      (printf "interval that caused rounding errors ~a\n" iv)
-     (printf "regular sin in double precision: ~a\n" (ival->list-flo (ival-sin iv)))
-     (printf "mixed sin in double precision  : ~a\n\n" (ival->list-flo (ival-sin-reduced iv)))])
+     (printf "regular sin in double precision: ~a\n" (ival->list-flo (ival-sin-default iv)))
+     (printf "reduced exponent sin in double precision  : ~a\n\n" (ival->list-flo (ival-sin iv)))])
   (void))
   
 
 (bf-precision 2048)
 ;(sin-correctness)
-;(for/list ([i (in-range 10000)]) (sin-correctness))
+(for/list ([i (in-range 100000)]) (sin-correctness))
 
-(define intervals (interval-generator 10000))
+;(define intervals (interval-generator 10000))
 
-(parameterize ([bf-precision 128])
+#;(parameterize ([bf-precision 2048])
   (time (map
          (lambda x
-           (ival-sin-reduced (first x))
+           (ival-sin-exponent (first x))
            (void))
          intervals)))
 
-(parameterize ([bf-precision 2048])
+#;(parameterize ([bf-precision 2048])
   (time (map
          (lambda x
            (ival-sin (first x))
