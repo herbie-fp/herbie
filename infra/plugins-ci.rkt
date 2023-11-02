dd0f8ce315f23f60e545c8b3ef8c87e511d3e62c
Brett Saiki <bksaiki@gmail.com>
2023-11-01 17:17:50 -0700

    fix

--- infra/plugins-ci.rkt
+++ infra/plugins-ci.rkt
@@ -11,29 +11,27 @@
   (platform-union
     ; posit operators
     (platform-product
-      #:type real [posit8 posit16 posit32]
-      #:type bool [bool]
-      #:operators [neg + - * / sqrt]
-      #:operators [== != > < >= <=])
-    ; quire operators
+      [real (posit8 posit16 posit32)]
+      [bool (bool)]
+      (operator-set
+        [(real real) (neg sqrt)]
+        [(real real real) (+ - * /)]
+        [(real real bool) (== != > < >= <=)]))
+    ; quire operations
     (platform
       #:conversions ([posit8 quire8]
                      [posit16 quire16]
                      [posit32 quire32])
-     (posit8)
-     (posit16)
-     (posit32)
-     (quire8
-       [fdp quire8 posit8 posit8 quire8]
-       [fdm quire8 posit8 posit8 quire8])
-     (quire16
-       [fdp quire16 posit16 posit16 quire16]
-       [fdm quire16 posit16 posit16 quire16])
-     (quire32
-       [fdp quire32 posit32 posit32 quire32]
-       [fdm quire32 posit32 posit32 quire32]))))
+                    ;  [posit8 binary64]
+                    ;  [posit16 binary64]
+                    ;  [posit32 binary64])
+      [(quire8 posit8 posit8 quire8) (fdp fdm)]
+      [(quire16 posit16 posit16 quire16) (fdp fdm)]
+      [(quire32 posit32 posit32 quire32) (fdp fdm)])))
 
-(register-platform! 'softposit (platform-union default-platform softposit-platform))
+(register-platform! 'softposit
+  (platform-union default-platform
+                  softposit-platform))
 
 ;; Do not run this file during testing
 (module test racket/base)
