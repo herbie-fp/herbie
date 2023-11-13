fb4879d22ec1e13aeb92e8b0765051b21d0f31e6
Brett Saiki <bksaiki@gmail.com>
2023-11-13 11:35:07 -0800

    fix bad merge

--- src/preprocess.rkt
+++ src/preprocess.rkt
@@ -141,9 +141,9 @@
     [(list 'negabs variable)
      (define index (index-of variables variable))
      (define negate-variable
-       (operator-info (get-parametric-operator 'neg (list-ref (context-var-reprs context) index)) 'fl))
+       (impl-info (get-parametric-operator 'neg (list-ref (context-var-reprs context) index)) 'fl))
      (define negate-expression
-       (operator-info (get-parametric-operator 'neg (context-repr context)) 'fl))
+       (impl-info (get-parametric-operator 'neg (context-repr context)) 'fl))
      (lambda (x y)
        ;; Negation is involutive, i.e. it is its own inverse, so t^1(y') = -y'
        (if (negative? (list-ref x index))
