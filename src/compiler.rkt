885fd495855d55667b185cc1f99195bc1968409e
Brett Saiki <bksaiki@gmail.com>
2023-11-13 11:23:06 -0800

    cleanup 'make-compiler'

--- src/compiler.rkt
+++ src/compiler.rkt
@@ -28,8 +28,14 @@
     (for/list ([root (in-list roots)])
       (vector-ref vregs root))))
 
-(define (make-compiler arg->precision operator-proc operator-itypes
-                       if-proc cond-type interpreter-name)
+;; Translates a Herbie IR into an interpretable IR.
+;; Requires some hooks to complete the translation.
+(define (make-compiler name
+                       #:input->value input->value
+                       #:op->procedure op->proc
+                       #:op->itypes op->itypes
+                       #:if-procedure if-proc
+                       #:cond-type cond-type)
   (lambda (exprs vars type)
     ;; Instruction cache
     (define icache '())
@@ -48,12 +54,12 @@
       (set! size (+ 1 size))
       (define expr
         (match prog
-          [(? number?) (list (const (arg->precision prog type)))]
+          [(? number?) (list (const (input->value prog type)))]
           [(? variable?) prog]
           [`(if ,c ,t ,f)
            (list if-proc (munge c cond-type) (munge t type) (munge f type))]
           [(list op args ...)
-           (cons (operator-proc op) (map munge args (operator-itypes op)))]
+           (cons (op->proc op) (map munge args (op->itypes op)))]
           ;; (cons (operator-info op 'ival) (map munge args))]
           [_ (raise-argument-error 'compile-specs "Not a valid expression!" prog)]))
       (hash-ref! exprhash expr
@@ -66,32 +72,38 @@
     (timeline-push! 'compiler (+ varc size) (+ exprc varc))
     (define ivec (list->vector (reverse icache)))
     (define interpret (make-progs-interpreter vars ivec names))
-    (procedure-rename interpret (sym-append 'eval-prog '- interpreter-name))))
-
-(define (compile-spec spec vars)
-  (compose first (compile-specs (list spec) vars)))
-
-(define (compile-prog expr ctx)
-  (compose first (compile-progs (list expr) ctx)))
+    (procedure-rename interpret (sym-append 'eval-prog '- name))))
 
 ;; Compiles a program of operators into a procedure
 ;; that evaluates the program on a single input of intervals
 ;; returning intervals.
 (define (compile-specs specs vars)
-  ((make-compiler
-    (lambda (prog _) (ival (bf prog)))
-    (curryr operator-info 'ival) (curryr operator-info 'itype)
-    ival-if 'bool
-    'ival)
-   specs vars 'real))
+  (define compile
+    (make-compiler 'ival
+                   #:input->value (lambda (prog _) (ival (bf prog)))
+                   #:op->procedure (lambda (op) (operator-info op 'ival))
+                   #:op->itypes (lambda (op) (operator-info op 'itype))
+                   #:if-procedure ival-if
+                   #:cond-type 'bool))
+  (compile specs vars 'real))
 
 ;; Compiles a program of operator implementations into a procedure
 ;; that evaluates the program on a single input of representation values
 ;; returning representation values.
 (define (compile-progs exprs ctx)
-  ((make-compiler
-    real->repr
-    (curryr impl-info 'fl) (curryr impl-info 'itype)
-    (λ (c ift iff) (if c ift iff)) (get-representation 'bool)
-    'fl)
-   exprs (context-vars ctx) (context-repr ctx)))
+  (define compile
+    (make-compiler 'fl
+                   #:input->value real->repr
+                   #:op->procedure (lambda (op) (impl-info op 'fl))
+                   #:op->itypes (lambda (op) (impl-info op 'itype))
+                   #:if-procedure (λ (c ift iff) (if c ift iff))
+                   #:cond-type (get-representation 'bool)))
+  (compile exprs (context-vars ctx) (context-repr ctx)))
+
+;; Like `compile-specs`, but for a single spec.
+(define (compile-spec spec vars)
+  (compose first (compile-specs (list spec) vars)))
+
+;; Like `compile-progs`, but a single prog.
+(define (compile-prog expr ctx)
+  (compose first (compile-progs (list expr) ctx)))
