a92f49b433707197bf4764f158b9d3bc81dccd27
Brett Saiki <bksaiki@gmail.com>
2023-11-28 10:47:36 -0800

    platform-product takes cost maps

--- src/platforms/default.rkt
+++ src/platforms/default.rkt
@@ -18,41 +18,39 @@
 ; machine floating-point operations (without conversions)
 (define machine-platform
   (with-terminal-cost ([binary64 64] [binary32 32])
-    (platform-union
-      (platform
-        [(binary64) (PI E INFINITY NAN) 64]
-        [(binary64 binary64) neg 64]
-        [(binary64 binary64 binary64) (+ - * /) 64]
-        [(binary64 binary64 bool) (== != > < >= <=) (* 3 64)])
-      (platform
-        [(binary32) (PI E INFINITY NAN) 32]
-        [(binary32 binary32) neg 32]
-        [(binary32 binary32 binary32) (+ - * /) 32]
-        [(binary32 binary32 bool) (== != > < >= <=) (* 3 32)]))))
+    (platform-product
+      [([real binary64] [bool bool])
+       (cost-map
+         [(PI E INFINITY NAN) 64]
+         [(neg + - * /) 64]
+         [(== != > < >= <=) (* 3 64)])]
+      [([real binary32] [bool bool])
+       (cost-map
+         [(PI E INFINITY NAN) 32]
+         [(neg + - * /) 32]
+         [(== != > < >= <=) (* 3 32)])]
+      (operator-set
+        #:default-cost 1
+        [(real) (PI E INFINITY NAN)]
+        [(real real) neg]
+        [(real real real) (+ - * /)]
+        [(real real bool) (== != > < >= <=)]))))
 
 ; libm operations
 (define libm-platform
   (with-terminal-cost ([binary64 64] [binary32 32])
-    (platform-union
-      (platform
-        #:default-cost 6400
-        [(binary64 binary64)
+    (platform-product
+      [([real binary64]) (cost-map #:default-cost 6400)]
+      [([real binary32]) (cost-map #:default-cost 3200)]
+      (operator-set
+        #:default-cost 1
+        [(real real)
          (acos acosh asin asinh atan atanh cbrt ceil cos cosh erf erfc
           exp exp2 expm1 fabs floor lgamma log log10 log2 log1p logb
           rint round sin sinh sqrt tan tanh tgamma trunc)]
-        [(binary64 binary64 binary64)
+        [(real real real)
          (atan2 copysign fdim fmax fmin fmod hypot pow remainder)]
-        [(binary64 binary64 binary64 binary64)
-         (fma)])
-      (platform
-        #:default-cost 3200
-        [(binary32 binary32)
-         (acos acosh asin asinh atan atanh cbrt ceil cos cosh erf erfc
-          exp exp2 expm1 fabs floor lgamma log log10 log2 log1p logb
-          rint round sin sinh sqrt tan tanh tgamma trunc)]
-        [(binary32 binary32 binary32)
-         (atan2 copysign fdim fmax fmin fmod hypot pow remainder)]
-        [(binary32 binary32 binary32 binary32)
+        [(real real real real)
          (fma)]))))
 
 ; compose platforms
