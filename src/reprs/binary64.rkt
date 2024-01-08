7ad2d8d5a013e7520f1780bb8edbd52792907249
Bhargav Kulkarni <bhargav.kulkarni1@gmail.com>
2024-01-08 11:18:19 -0700

    Revert "cleaner libm interface"
    This reverts commit a7be0b35b1d853d30ed7ad93a636e82064c0df7e.

--- src/reprs/binary64.rkt
+++ src/reprs/binary64.rkt
@@ -3,8 +3,7 @@
 ;; Builtin double-precision plugin (:precision binary64)
 
 (require math/flonum math/bigfloat)
-(require "runtime/utils.rkt" "runtime/libm.rkt")
-(require "bool.rkt") ; for raco test
+(require "runtime/utils.rkt" "bool.rkt")
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;; representation ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
@@ -26,17 +25,32 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;; operators ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(define-syntax (define-libm-impl/binary64 stx)
+(require ffi/unsafe)
+(define-syntax (define-libm-operator stx)
   (syntax-case stx (real)
-    [(_ op (itype ...) otype [key value] ...)
-     (with-syntax ([impl (string->symbol (format "~a.f64" (syntax->datum #'op)))])
-       #'(define-libm-impl op (op impl itype ...) otype [key value] ...))]))
+    [(_ (op real ...) [key value] ...)
+     (let* ([num-args (length (cdr (syntax-e (cadr (syntax-e stx)))))]
+            [sym2-append (λ (x y) (string->symbol (string-append (symbol->string x) (symbol->string y))))]
+            [name (sym2-append (syntax-e (car (syntax-e (cadr (syntax-e stx))))) '.f64)])
+       #`(begin
+          (define fl-proc
+            (get-ffi-obj 'op #f (_fun #,@(build-list num-args (λ (_) #'_double)) -> _double)
+                          (λ () #f)))
+           (when fl-proc
+            (define-operator-impl (op #,name #,@(build-list num-args (λ (_) #'binary64))) binary64
+              [fl fl-proc] [key value] ...))))]))
 
-(define-syntax-rule (define-libm-impls/binary64* (itype ... otype) name ...)
-  (begin (define-libm-impl/binary64 name (itype ...) otype) ...))
+(define-syntax-rule (define-1ary-libm-operator op)
+  (define-libm-operator (op real)))
 
-(define-syntax-rule (define-libm-impls/binary64 [(itype ... otype) (name ...)] ...)
-  (begin (define-libm-impls/binary64* (itype ... otype) name ...) ...))
+(define-syntax-rule (define-2ary-libm-operator op)
+  (define-libm-operator (op real real)))
+
+(define-syntax-rule (define-1ary-libm-operators op ...)
+  (begin (define-1ary-libm-operator op) ...))
+
+(define-syntax-rule (define-2ary-libm-operators op ...)
+  (begin (define-2ary-libm-operator op) ...))
 
 (define-operator-impl (neg neg.f64 binary64) binary64 [fl -])
 (define-operator-impl (+ +.f64 binary64 binary64) binary64 [fl +])
@@ -44,15 +58,52 @@
 (define-operator-impl (* *.f64 binary64 binary64) binary64 [fl *])
 (define-operator-impl (/ /.f64 binary64 binary64) binary64 [fl /])
 
-(define-libm-impls/binary64
-  [(binary64 binary64)
-   (acos acosh asin asinh atan atanh cbrt ceil cos cosh erf erfc
-    exp exp2 expm1 fabs floor lgamma log log10 log1p log2 logb
-    rint round sin sinh sqrt tan tanh tgamma trunc)]
-  [(binary64 binary64 binary64)
-   (atan2 copysign fdim fmax fmin fmod hypot pow remainder)]
-  [(binary64 binary64 binary64 binary64)
-   (fma)])
+(define-1ary-libm-operators
+ acos
+ acosh
+ asin
+ asinh
+ atan
+ atanh
+ cbrt
+ ceil
+ cos
+ cosh
+ erf
+ erfc
+ exp
+ exp2
+ expm1
+ fabs
+ floor
+ lgamma
+ log
+ log10
+ log1p
+ log2
+ logb
+ rint
+ round
+ sin
+ sinh
+ sqrt
+ tan
+ tanh
+ tgamma
+ trunc)
+
+(define-2ary-libm-operators
+ atan2
+ copysign
+ fdim
+ fmax
+ fmin
+ fmod
+ hypot
+ pow
+ remainder)
+
+(define-libm-operator (fma real real real))
 
 (define-comparator-impls binary64
   [== ==.f64 =]
