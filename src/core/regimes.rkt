60c1f5286e873e189ffcacb3b09bc8abb2703160
zane <39070793+zaneenders@users.noreply.github.com>
2024-02-03 15:43:31 -0700

    fixed contract

--- src/core/regimes.rkt
+++ src/core/regimes.rkt
@@ -122,7 +122,7 @@
   (define can-split? (append (list #f)
                              (for/list ([val (cdr splitvals*)] [prev splitvals*])
                                (</total prev val repr))))
-  (define split-indices (reverse (vector->list (err-lsts->split-indices bit-err-lsts* can-split?))))
+  (define split-indices (err-lsts->split-indices bit-err-lsts* can-split?))
   (define out (option split-indices alts pts* expr (pick-errors split-indices pts* err-lsts* repr)))
   (timeline-stop!)
   (timeline-push! 'branch (~a expr) (errors-score (option-errors out)) (length split-indices) (~a (representation-name repr)))
@@ -174,7 +174,8 @@
    (= (si-pidx (last split-indices)) (length can-split?))))
 
 (define/contract (err-lsts->split-indices err-lsts can-split)
-  (->i ([e (listof list)] [cs (listof boolean?)]) (vectorof (vectorof si?)))
+  (->i ([e (listof list)] [cs (listof boolean?)]) 
+       [result (cs) (curry valid-splitindices? cs)])
   (define can-split-vec (list->vector can-split))
   (define err-lsts-vec (list->vector err-lsts))
   ;; We have num-candidates candidates, each of whom has error lists of length num-points.
@@ -237,15 +238,8 @@
             next
             (loop next)))))
 
-  (define build-output
-    (let ([vec-out (vector-ref final (- num-points 1))])
-      (cond 
-        [(cand? (cand-prev vec-out))      
-          (let ([prev (cand-prev vec-out)])
-            (eprintf "~a\n" (si (cand-idx prev) (cand-point-idx prev)))
-            (vector (si (cand-idx vec-out) (cand-point-idx vec-out)) 
-                    (si (cand-idx prev) (cand-point-idx prev))))]
-        [else (vector (si (cand-idx vec-out) (cand-point-idx vec-out)))])))
+        
+  (define o (vector-ref final (- num-points 1)))
 
-  build-output)
+  (list (si (cand-idx o) (cand-point-idx o))))
 
