da8d1e1adc3778700b1b1f78a04d16db535eef03
Pavel Panchekha <me@pavpanchekha.com>
2024-02-02 17:15:10 -0700

    Clean up egg-herbie code

--- egg-herbie/src/math.rs
+++ egg-herbie/src/math.rs
@@ -257,3 +257,14 @@ impl Analysis<Math> for ConstantFold {
         }
     }
 }
+
+pub fn mk_rules(tuples: &[(&str, &str, &str)]) -> Vec<Rewrite> {
+    tuples
+        .iter()
+        .map(|(name, left, right)| {
+            let left = Pattern::from_str(left).unwrap();
+            let right = Pattern::from_str(right).unwrap();
+            Rewrite::new(*name, left, right).unwrap()
+        })
+        .collect()
+}
