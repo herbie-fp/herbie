0cea63c53bef8d230449d33b9d380425268f9f29
Pavel Panchekha <me@pavpanchekha.com>
2024-02-08 12:09:57 -0700

    Simplify using all sound rules!

--- src/syntax/rules.rkt
+++ src/syntax/rules.rkt
@@ -97,7 +97,7 @@
     (for ([(_ ruleset) (in-dict (*rulesets*))])
       (match-define (list rules groups _) ruleset)
       (when (and (ormap (curry flag-set? 'rules) groups)
-                 (set-member? groups 'simplify)
+                 (set-member? groups 'sound)
                  (filter rule-ops-supported? rules))
         (for ([rule (in-list rules)])
           (sow rule))))))
