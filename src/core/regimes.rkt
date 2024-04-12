cf7689074ed699905fca7bedc4909f4b87194a14
zane <39070793+zaneenders@users.noreply.github.com>
2024-04-12 14:45:11 -0600

    acost removed correctly

--- src/core/regimes.rkt
+++ src/core/regimes.rkt
@@ -208,7 +208,6 @@
      (define a-prev-idx (vector-ref v-pidx point-idx))
      ;; We take the CSE corresponding to the best choice of previous split point.
      ;; The default, not making a new split-point, gets a bonus of min-weight
-     (let ([acost (fl- a-cost min-weight)])
       (define best (make-vector point-idx #f))
       (define bcost (make-vector point-idx #f))
       (for ([cidx (in-naturals)] [psum (in-vector flvec-psums)])
@@ -225,15 +224,14 @@
        (when (vector-ref can-split-vec (+ prev-split-idx 1))
         (define t (fl+ (flvector-ref v-alt-cost prev-split-idx)
                        (vector-ref bcost prev-split-idx)))
-        (when (fl< t acost)
-         (set! acost t)
-         ;; give benefit to new best alt
-         (set! a-cost (fl- acost min-weight))
+        ;; give benefit to previous best alt
+        (when (fl< t (fl- a-cost min-weight))
+         (set! a-cost t)
          (set! a-best (vector-ref best prev-split-idx))
          (set! a-prev-idx prev-split-idx))))
        (flvector-set! vec-alt-cost point-idx a-cost)
        (vector-set! vec-cidx point-idx a-best)
-       (vector-set! vec-pidx point-idx a-prev-idx)))
+       (vector-set! vec-pidx point-idx a-prev-idx))
   (values vec-alt-cost vec-cidx vec-pidx))
 
   ;; We get the initial set of cse's by, at every point-index,
