cd6438064c38148729d6bfaac6b55fb60bdbaf91
varun10p <varun10p@cs.washington.edu>
2024-05-20 14:50:25 -0700

    Added error checking and absolute paths.

--- infra/baseline.sh
+++ infra/baseline.sh
@@ -1,21 +1,51 @@
 #!/bin/bash
 
+# exit immediately upon first error
+set -e -x
+
+src="${BASH_SOURCE[0]}"
+while [ -L "$src" ]; do
+  dir="$(cd -P "$(dirname "$src")" && pwd)"
+  src="$(readlink "$src")"
+  [[ $src != /* ]] && src="$dir/$src"
+done
+
+INFRA_DIR="$(cd -P "$(dirname "$src")" && pwd)"
+BENCH_DIR="$INFRA_DIR"/../bench
+
+# check arguments
+if [ -z "$1" ]; then
+  echo "Usage: $0 <output_dir>"
+  exit 1
+fi
+
+INPUT_FILE="$BENCH_DIR/$1"
+# check arguments
+if [ -z "$2" ]; then
+  echo "Usage: $0 <output_dir>"
+  exit 1
+else
+  OUT_DIR="$2";
+fi
+
+mkdir -p $OUT_DIR/
+
 echo "Cloning Baseline (Herbie 2.0)..."
 
 git clone --depth 1 --branch v2.0 git@github.com:herbie-fp/herbie.git
 
-mkdir -p $2/
+echo "Building Baseline (Herbie 2.0)..."
 
 cd herbie
 
-echo "Building Baseline (Herbie 2.0)..."
 make install
-
-echo "Running Baseline (Herbie 2.0) on benchmarks..."
-herbie report bench/$1 tmp
 
 cd ..
 
-mv herbie/tmp/results.json $2/
+echo "Running Baseline (Herbie 2.0) on benchmarks..."
+herbie report $INPUT_FILE tmp
 
+mv tmp/results.json $OUT_DIR/
+
+rm -rf tmp
 rm -rf herbie
