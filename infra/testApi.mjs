c65bd352f5dbd46a152cfe99d7d54f1652682f5c
zane <39070793+zaneenders@users.noreply.github.com>
2024-06-11 11:11:32 -0600

    Add call Herbie helper function

--- infra/testApi.mjs
+++ infra/testApi.mjs
@@ -24,15 +24,12 @@ const startResponse = await callHerbie(`/improve-start`, {
   },
   body: URIencodedBody
 })
-const testResult = (startResponse.status == 201) ||
-  (startResponse.status == 202)
+const testResult = startResponse.status == (201 || 202)
 assert.equal(testResult, true)
 const path = startResponse.headers.get("location")
 
 // Check status endpoint
-const checkStatus = await fetch(
-  `http://127.0.0.1:8000${path}`,
-  { method: 'GET' })
+const checkStatus = await callHerbie(path, { method: 'GET' })
 assert.equal(checkStatus.status, 201)
 assert.equal(checkStatus.statusText, 'Job complete')
 
@@ -141,7 +138,7 @@ for (const e in expectedExpressions) {
 // Results.json endpoint
 const jsonResults = await callHerbie("/results.json", { method: 'GET' })
 
-// Basic test that checks that there are the two results after the above test.
+// Basic test that checks that there are the one result after the above test.
 // TODO add a way to reset the results.json file?
 assert.equal(jsonResults.tests.length, 2)
 
