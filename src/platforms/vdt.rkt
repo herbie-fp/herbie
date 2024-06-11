2bae6ce73d304a3cfecca6c2136e0ee758d4eea1
Jonas Regehr <jo.sc.re.04@gmail.com>
2024-06-11 14:31:19 -0600

    tuning and improve working

--- src/platforms/vdt.rkt
+++ src/platforms/vdt.rkt
@@ -1,8 +1,41 @@
 #lang racket
 
-(require "../plugin.rkt")
+(require "../plugin.rkt" vdt-herbie)
 
-(define move-cost 0.02333600000000001)
+(define-accelerator (fast-exp real) real (lambda (x) (exp x)))
+(define-accelerator (fast-sin real) real (lambda (x) (sin x)))
+(define-accelerator (fast-cos real) real (lambda (x) (cos x)))
+(define-accelerator (fast-tan real) real (lambda (x) (tan x)))
+(define-accelerator (fast-tanh real) real (lambda (x) (tanh x)))
+(define-accelerator (fast-log real) real (lambda (x) (log x)))
+(define-accelerator (fast-asin real) real (lambda (x) (asin x)))
+(define-accelerator (fast-acos real) real (lambda (x) (acos x)))
+(define-accelerator (fast-atan real) real (lambda (x) (atan x)))
+(define-accelerator (fast-isqrt real) real (lambda (x) (/ 1 (sqrt x))))
+
+(define-accelerator-impl fast-exp fast-exp.f64 (binary64) binary64 (lambda (x) (vdt-exp x)))
+(define-accelerator-impl fast-sin fast-sin.f64 (binary64) binary64 (lambda (x) (vdt-sin x)))
+(define-accelerator-impl fast-cos fast-cos.f64 (binary64) binary64 (lambda (x) (vdt-cos x)))
+(define-accelerator-impl fast-tan fast-tan.f64 (binary64) binary64 (lambda (x) (vdt-tan x)))
+(define-accelerator-impl fast-tanh fast-tanh.f64 (binary64) binary64 (lambda (x) (vdt-tanh x)))
+(define-accelerator-impl fast-log fast-log.f64 (binary64) binary64 (lambda (x) (vdt-log x)))
+(define-accelerator-impl fast-asin fast-asin.f64 (binary64) binary64 (lambda (x) (vdt-asin x)))
+(define-accelerator-impl fast-acos fast-acos.f64 (binary64) binary64 (lambda (x) (vdt-acos x)))
+(define-accelerator-impl fast-atan fast-atan.f64 (binary64) binary64 (lambda (x) (vdt-atan x)))
+(define-accelerator-impl fast-isqrt fast-isqrt.f64 (binary64) binary64 (lambda (x) (vdt-isqrt x)))
+
+(define-accelerator-impl fast-exp fast-exp.f32 (binary32) binary32 (lambda (x) (vdt-expf x)))
+(define-accelerator-impl fast-sin fast-sin.f32 (binary32) binary32 (lambda (x) (vdt-sinf x)))
+(define-accelerator-impl fast-cos fast-cos.f32 (binary32) binary32 (lambda (x) (vdt-cosf x)))
+(define-accelerator-impl fast-tan fast-tan.f32 (binary32) binary32 (lambda (x) (vdt-tanf x)))
+(define-accelerator-impl fast-tanh fast-tanh.f32 (binary32) binary32 (lambda (x) (vdt-tanhf x)))
+(define-accelerator-impl fast-log fast-log.f32 (binary32) binary32 (lambda (x) (vdt-logf x)))
+(define-accelerator-impl fast-asin fast-asin.f32 (binary32) binary32 (lambda (x) (vdt-asinf x)))
+(define-accelerator-impl fast-acos fast-acos.f32 (binary32) binary32 (lambda (x) (vdt-acosf x)))
+(define-accelerator-impl fast-atan fast-atan.f64 (binary64) binary64 (lambda (x) (vdt-atanf x)))
+(define-accelerator-impl fast-isqrt fast-isqrt.f64 (binary64) binary64 (lambda (x) (vdt-isqrtf x)))
+
+(define move-cost  0.0026300000000000047)
 (define fl-move-cost (* move-cost 4))
 
 (define boolean-platform
@@ -25,74 +58,64 @@
 
 (define cost-model
   (cost-map
-    [* 0.20874800000000002]
-    [+ 0.164604]
-    [- 0.15163999999999997]
-    [/ 0.26615199999999994]
-    [acos 0.357748]
-    [acosh 0.6594719999999998]
-    [asin 0.389788]
-    [asinh 0.8350280000000001]
-    [atan 0.8375199999999997]
-    [atan2 1.4928039999999998]
-    [atanh 0.3623799999999999]
-    [cbrt 1.5651760000000001]
-    [ceil 0.4729999999999999]
-    [copysign 0.20045199999999996]
-    [cos 3.3228800000000005]
-    [cosh 0.9538959999999999]
-    [erf 0.8064360000000002]
-    [erfc 0.816512]
-    [exp 1.0806]
-    [exp2 0.825484]
-    [expm1 0.8374999999999999]
-    [fabs 0.10161999999999997]
-    [fdim 0.592576]
-    [floor 0.46856799999999993]
-    [fma 0.3366239999999999]
-    [fmax 0.3106]
-    [fmin 0.289256]
-    [fmod 94.277144]
-    [hypot 1.175432]
-    [lgamma 1.568012]
-    [log 0.5057240000000001]
-    [log10 0.868856]
-    [log1p 0.768932]
-    [log2 0.6812760000000003]
-    [logb 0.22065599999999996]
-    [neg 0.09659199999999998]
-    [pow 1.52482]
-    [remainder 16.165012]
-    [rint 0.12186400000000001]
-    [round 0.6585639999999999]
-    [sin 3.3181279999999997]
-    [sinh 1.2095399999999996]
-    [sqrt 0.19187200000000001]
-    [tan 3.7109039999999998]
-    [tanh 0.8240159999999999]
-    [tgamma 1.8825760000000002]
-    [trunc 0.46364400000000006]
-
-
-    ;; [vdt-exp 1]
-    ;; [fast-sin 1]
-    ;; [fast-cos 1]
-    ;; [fast-tan 1]
-    ;; [fast-tanh 1]
-    ;; [fast-log 1]
-    ;; [fast-asin 1]
-    ;; [fast-acos 1]
-    ;; [fast-atan 1]
-    [fast-exp 1]
-    [fast-sin 1]
-    [fast-cos 1]
-    [fast-tan 1]
-    [fast-tanh 1]
-    [fast-log 1]
-    [fast-asin 1]
-    [fast-acos 1]
-    [fast-atan 1]
-    ))
+    [* 0.019779999999999985]
+    [+ 0.014449999999999996]
+    [- 0.014110000000000006]
+    [/ 0.027380000000000026]
+    [acos 0.32046]
+    [acosh 0.08731000000000001]
+    [asin 0.05579000000000002]
+    [asinh 0.12150999999999998]
+    [atan 0.2467]
+    [atan2 0.19123999999999997]
+    [atanh 0.04122999999999999]
+    [cbrt 0.19143999999999997]
+    [ceil 0.0262]
+    [copysign 0.015479999999999989]
+    [cos 0.5782700000000001]
+    [cosh 0.13248000000000001]
+    [erf 0.11446]
+    [erfc 0.08851999999999999]
+    [exp 0.14546]
+    [exp2 0.15739]
+    [expm1 0.09623]
+    [fabs 0.009410000000000007]
+    [fast-acos 0.17734]
+    [fast-asin 0.17116999999999996]
+    [fast-atan 0.20786000000000002]
+    [fast-cos 0.17641000000000004]
+    [fast-exp 0.1162]
+    [fast-isqrt 0.04412999999999999]
+    [fast-log 0.12131999999999998]
+    [fast-sin 0.18503999999999995]
+    [fast-tan 0.17883]
+    [fast-tanh 0.11866999999999997]
+    [fdim 0.07086999999999999]
+    [floor 0.025419999999999998]
+    [fma 0.08671999999999999]
+    [fmax 0.017819999999999996]
+    [fmin 0.019980000000000032]
+    [fmod 0.17450000000000002]
+    [hypot 0.14526999999999998]
+    [lgamma 0.37424]
+    [log 0.05956]
+    [log10 0.12073999999999999]
+    [log1p 0.12961]
+    [log2 0.10091000000000001]
+    [logb 0.02410000000000001]
+    [neg 0.010460000000000002]
+    [pow 0.17928000000000002]
+    [remainder 0.07102]
+    [rint 0.02481999999999999]
+    [round 0.07981]
+    [sin 0.41594999999999993]
+    [sinh 0.16324999999999998]
+    [sqrt 0.02347999999999999]
+    [tan 0.53495]
+    [tanh 0.11344000000000001]
+    [tgamma 0.26131999999999994]
+    [trunc 0.025449999999999966]
+  ))
 
 (define tunable
   (with-terminal-cost ([binary64 fl-move-cost])
@@ -103,11 +126,9 @@
         [(real real)
          (neg acos acosh asin asinh atan atanh cbrt ceil cos cosh
           erf erfc exp exp2 expm1 fabs floor lgamma log log10 log2
-          log1p logb rint round sin sinh sqrt tan tanh tgamma trunc
-          
-          ;;vdt-exp
-          ;; fast-sin fast-cos fast-tan fast-tanh fast-log fast-asin
-          ;; fast-acos fast-atan
+          log1p logb rint round sin sinh sqrt tan tanh tgamma trunc      
+          fast-exp fast-sin fast-cos fast-tan fast-tanh fast-log fast-asin
+          fast-acos fast-atan fast-isqrt
           )]
         [(real real real)
          (+ - * / atan2 copysign fdim fmax fmin fmod hypot pow remainder)]
--- src/reprs/binary64.rkt
+++ src/reprs/binary64.rkt
@@ -92,25 +92,25 @@
 (define-accelerator (recip real) real (λ (x) (/ 1 x)))
 (define-accelerator (rsqrt real) real (λ (x) (/ 1 (sqrt x))))
       
-;; (define-accelerator (fmsub real real real) real (lambda (x y z) (- (* x y) z)))
-;; (define-accelerator (fnmadd real real real) real (lambda (x y z) (+ (neg (* x y)) z)))
-;; (define-accelerator (fnmsub real real real) real (lambda (x y z) (- (neg (* x y)) z)))
+(define-accelerator (fmsub real real real) real (lambda (x y z) (- (* x y) z)))
+(define-accelerator (fnmadd real real real) real (lambda (x y z) (+ (neg (* x y)) z)))
+(define-accelerator (fnmsub real real real) real (lambda (x y z) (- (neg (* x y)) z)))
 
-;; (define-accelerator-impl fmsub fmsub.f64 (binary64 binary64 binary64) binary64)
-;; (define-accelerator-impl fnmadd fnmadd.f64 (binary64 binary64 binary64) binary64)
-;; (define-accelerator-impl fnmsub fnmsub.f64 (binary64 binary64 binary64) binary64)
+(define-accelerator-impl fmsub fmsub.f64 (binary64 binary64 binary64) binary64)
+(define-accelerator-impl fnmadd fnmadd.f64 (binary64 binary64 binary64) binary64)
+(define-accelerator-impl fnmsub fnmsub.f64 (binary64 binary64 binary64) binary64)
 
-;; (define-accelerator (vdt-exp real) real (lambda (x) (exp x)))
+(define-accelerator (vdt-exp real) real (lambda (x) (exp x)))
 
-;; (define-operator-impl (recip recip.f64 binary64) binary64
-  ;; [fl (λ (x)
-        ;; (parameterize ([bf-precision 12])
-          ;; (bigfloat->flonum (bf/ 1.bf (bf x)))))])
-;; 
-;; (define-operator-impl (rsqrt rsqrt.f64 binary64) binary64
-  ;; [fl (λ (x)
-        ;; (parameterize ([bf-precision 12])
-          ;; (bigfloat->flonum (bf/ 1.bf (bfsqrt (bf x))))))])
+(define-operator-impl (recip recip.f64 binary64) binary64
+  [fl (λ (x)
+        (parameterize ([bf-precision 12])
+          (bigfloat->flonum (bf/ 1.bf (bf x)))))])
+
+(define-operator-impl (rsqrt rsqrt.f64 binary64) binary64
+  [fl (λ (x)
+        (parameterize ([bf-precision 12])
+          (bigfloat->flonum (bf/ 1.bf (bfsqrt (bf x))))))])
 
 (define-ruleset* reciprocal (arithmetic simplify)
   #:type ([a real])
@@ -118,58 +118,3 @@
   [remove-recip  (recip a)      (/ 1 a)]
   [add-rsqrt     (/ 1 (sqrt a)) (rsqrt a)]
   [remove-rsqrt  (rsqrt a)      (/ 1 (sqrt a))])
-
-;; (define-accelerator (fast-exp real) real (lambda (x) (exp x)))
-;; (define-accelerator (fast-sin real) real (lambda (x) (sin x)))
-;; (define-accelerator (fast-cos real) real (lambda (x) (cos x)))
-;; (define-accelerator (fast-tan real) real (lambda (x) (tan x)))
-;; (define-accelerator (fast-tanh real) real (lambda (x) (tanh x)))
-;; (define-accelerator (fast-log real) real (lambda (x) (log x)))
-;; (define-accelerator (fast-asin real) real (lambda (x) (asin x)))
-;; (define-accelerator (fast-acos real) real (lambda (x) (acos x)))
-;; (define-accelerator (fast-atan real) real (lambda (x) (atan x)))
-;; (define-accelerator (fast-isqrt real) real (lambda (x) (/ 1 (sqrt x))))
-
-
-;; (define libvdt "libvdt")
-
-;; (set-c-header!
-;;  (lambda (previous)
-;;    (string-join (previous) "#include <vdtMath.h>\n")))
-
-;; (set-unknown->c!
-;;  (lambda (previous)
-;;    (lambda (context operation arguments)
-;;      (previous
-;;       context
-;;       (match operation
-;;         ['fast-exp 'exp]
-;;         ['fast-sin 'sin]
-;;         ['fast-cos 'cos]
-;;         ['fast-tan 'tan]
-;;         ['fast-tanh 'tanh]
-;;         ['fast-log 'log]
-;;         ['fast-asin 'asin]
-;;         ['fast-acos 'acos]
-;;         ['fast-atan 'atan])
-;;       arguments))))
-
-;; (define-accelerator-impl fast-exp fast-exp.f64 (binary64) binary64 (get-ffi-obj "exp" libvdt (_fun _double -> _double)))
-;; (define-accelerator-impl fast-sin fast-sin.f64 (binary64) binary64 (get-ffi-obj "sin" libvdt (_fun _double -> _double)))
-;; (define-accelerator-impl fast-cos fast-cos.f64 (binary64) binary64 (get-ffi-obj "cos" libvdt (_fun _double -> _double)))
-;; (define-accelerator-impl fast-tan fast-tan.f64 (binary64) binary64 (get-ffi-obj "tan" libvdt (_fun _double -> _double)))
-;; (define-accelerator-impl fast-tanh fast-tanh.f64 (binary64) binary64 (get-ffi-obj "tanh" libvdt (_fun _double -> _double)))
-;; (define-accelerator-impl fast-log fast-log.f64 (binary64) binary64 (get-ffi-obj "log" libvdt (_fun _double -> _double)))
-;; (define-accelerator-impl fast-asin fast-asin.f64 (binary64) binary64 (get-ffi-obj "asin" libvdt (_fun _double -> _double)))
-;; (define-accelerator-impl fast-acos fast-acos.f64 (binary64) binary64 (get-ffi-obj "acos" libvdt (_fun _double -> _double)))
-;; (define-accelerator-impl fast-atan fast-atan.f64 (binary64) binary64 (get-ffi-obj "atan" libvdt (_fun _double -> _double)))
-
-;; (define-accelerator-impl fast-exp fast-exp.f32 (binary32) binary32 (get-ffi-obj "expf" libvdt (_fun _double -> _double)))
-;; (define-accelerator-impl fast-sin fast-sin.f32 (binary32) binary32 (get-ffi-obj "sinf" libvdt (_fun _double -> _double)))
-;; (define-accelerator-impl fast-cos fast-cos.f32 (binary32) binary32 (get-ffi-obj "cosf" libvdt (_fun _double -> _double)))
-;; (define-accelerator-impl fast-tan fast-tan.f32 (binary32) binary32 (get-ffi-obj "tanf" libvdt (_fun _double -> _double)))
-;; (define-accelerator-impl fast-tanh fast-tanh.f32 (binary32) binary32 (get-ffi-obj "tanhf" libvdt (_fun _double -> _double)))
-;; (define-accelerator-impl fast-log fast-log.f32 (binary32) binary32 (get-ffi-obj "logf" libvdt (_fun _double -> _double)))
-;; (define-accelerator-impl fast-asin fast-asin.f32 (binary32) binary32 (get-ffi-obj "asinf" libvdt (_fun _double -> _double)))
-;; (define-accelerator-impl fast-acos fast-acos.f32 (binary32) binary32 (get-ffi-obj "acosf" libvdt (_fun _double -> _double)))
-;; (define-accelerator-impl fast-atan fast-atan.f32 (binary32) binary32 (get-ffi-obj "atanf" libvdt (_fun _double -> _double)))
