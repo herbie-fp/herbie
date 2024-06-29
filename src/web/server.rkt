e847046643c78ea63e35d80d432a3fab6461a48b
zane <39070793+zaneenders@users.noreply.github.com>
2024-06-29 14:30:31 -0400

    original work-thread indentation.

--- src/web/server.rkt
+++ src/web/server.rkt
@@ -148,20 +148,19 @@
     (symbol->string command) job-id job-str))
 
 (define *worker-thread*
-  (thread
+   (thread
     (λ ()
       (let loop ([seed #f])
         (match (thread-receive)
-          [`(init rand ,vec flags ,flag-table num-iters ,iterations 
-             points ,points timeout ,timeout output-dir ,output reeval ,reeval
-             demo? ,demo?)
-            (set! seed vec)
-            (*flags* flag-table)
-            (*num-iterations* iterations)
-            (*num-points* points)
-            (*timeout* timeout)
-            (*demo-output* output)
-            (*reeval-pts* reeval)
-            (*demo?* demo?)]
+          [`(init rand ,vec flags ,flag-table num-iters ,iterations points ,points
+                  timeout ,timeout output-dir ,output reeval ,reeval demo? ,demo?)
+           (set! seed vec)
+           (*flags* flag-table)
+           (*num-iterations* iterations)
+           (*num-points* points)
+           (*timeout* timeout)
+           (*demo-output* output)
+           (*reeval-pts* reeval)
+           (*demo?* demo?)]
           [job-info (run-job job-info)])
         (loop seed)))))
