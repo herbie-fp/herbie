1c210daa662f6b09d50b5b1ef089f124e51e74ae
varun10p <varun10p@gmail.com>
2024-07-25 14:40:11 -0700

    Fmt

--- infra/plugins-ci.rkt
+++ infra/plugins-ci.rkt
@@ -16,55 +16,55 @@
                  or)
 
 (define-platform softposit-platform
-  #:literal [posit8 1]
-  #:literal [posit16 1]
-  #:literal [posit32 1]
-  #:literal [quire8 1]
-  #:literal [quire16 1]
-  #:literal [quire32 1]
-  #:default-cost 1
-  neg.p32
-  neg.p16
-  neg.p8
-  sqrt.p32
-  sqrt.p16
-  sqrt.p8
-  +.p32
-  +.p16
-  +.p8
-  -.p32
-  -.p16
-  -.p8
-  *.p32
-  *.p16
-  *.p8
-  /.p32
-  /.p16
-  /.p8
-  ==.p32
-  ==.p16
-  ==.p8
-  !=.p32
-  !=.p16
-  !=.p8
-  >.p32
-  >.p16
-  >.p8
-  <.p32
-  <.p16
-  <.p8
-  >=.p32
-  >=.p16
-  >=.p8
-  <=.p32
-  <=.p16
-  <=.p8
-  quire8-mul-add
-  quire16-mul-add
-  quire32-mul-add
-  quire8-mul-sub
-  quire16-mul-sub
-  quire32-mul-sub)
+                 #:literal [posit8 1]
+                 #:literal [posit16 1]
+                 #:literal [posit32 1]
+                 #:literal [quire8 1]
+                 #:literal [quire16 1]
+                 #:literal [quire32 1]
+                 #:default-cost 1
+                 neg.p32
+                 neg.p16
+                 neg.p8
+                 sqrt.p32
+                 sqrt.p16
+                 sqrt.p8
+                 +.p32
+                 +.p16
+                 +.p8
+                 -.p32
+                 -.p16
+                 -.p8
+                 *.p32
+                 *.p16
+                 *.p8
+                 /.p32
+                 /.p16
+                 /.p8
+                 ==.p32
+                 ==.p16
+                 ==.p8
+                 !=.p32
+                 !=.p16
+                 !=.p8
+                 >.p32
+                 >.p16
+                 >.p8
+                 <.p32
+                 <.p16
+                 <.p8
+                 >=.p32
+                 >=.p16
+                 >=.p8
+                 <=.p32
+                 <=.p16
+                 <=.p8
+                 quire8-mul-add
+                 quire16-mul-add
+                 quire32-mul-add
+                 quire8-mul-sub
+                 quire16-mul-sub
+                 quire32-mul-sub)
 
 (register-platform! 'softposit (platform-union boolean-platform softposit-platform))
 
