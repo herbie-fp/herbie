8475ad01dbb625aeff585a16841138e6402549a0
zane <39070793+zaneenders@users.noreply.github.com>
2024-07-26 14:41:20 -0600

    start of a new server

--- src/api/server.rkt
+++ src/api/server.rkt
@@ -4,6 +4,7 @@
 
 (require "sandbox.rkt"
          "../config.rkt"
+         "../syntax/load-plugin.rkt"
          "../syntax/read.rkt")
 (require (submod "../utils/timeline.rkt" debug))
 
@@ -53,10 +54,48 @@
                     #:timeline-disabled? [timeline-disabled? #f])
   (herbie-command command test seed pcontext profile? timeline-disabled?))
 
+(define *job-queue* (list))
+
 ;; Starts a job for a given command object|
 (define (start-job command)
+  (set! *job-queue* (append *job-queue* (list command)))
+  (eprintf "~a added Q:~a\n" command (length *job-queue*))
+  (check-q-for-work)
   (define job-id (compute-job-id command))
   (if (already-computed? job-id) job-id (start-work command)))
+
+(define (check-q-for-work)
+  (if (and (> (length *job-queue*) 0) (> (length *idel-workers*) 0))
+      (assign-work)
+      (eprintf "~a <= 0\n" (length *job-queue*))))
+
+;; To be called when the Q is not empty
+(define (assign-work)
+  (eprintf "assigning work: from ~a jobs\n" (length *job-queue*))
+  (define job (first *job-queue*))
+  (set! *job-queue* (rest *job-queue*))
+  (define job-id (compute-job-id job))
+  (define worker (first *idel-workers*))
+  (set! *idel-workers* (rest *idel-workers*))
+  (place-channel-put worker (list 'apply worker job job-id))
+  (update-worker-queues)
+  (eprintf "works updated\n"))
+
+(define (update-worker-queues)
+  (let loop ([out '()])
+    (with-handlers ([exn:break? (λ (_)
+                                  (eprintf "Terminating after ~a problem~a!\n"
+                                           (length out)
+                                           (if (= (length out) 1) "" "s"))
+                                  out)])
+      (match (apply sync (append *busy-workers* *dead-events*))
+        [(list 'ready self) (set! *idel-workers* (append *idel-workers* (list self)))]
+        [(list 'done self result)
+         (hash-set! *completed-jobs* result)
+         (set! *idel-workers* (append *idel-workers* (list self)))]
+        ; In this case it is a place-dead-event
+        [(? evt?) (error "Thread crashed. Unrecoverable. Terminating immediately.")])))
+      (eprintf "HERE!\n"))
 
 (define (is-job-finished job-id)
   (hash-ref *job-status* job-id #f))
@@ -65,11 +104,76 @@
   (if (already-computed? job-id) (hash-ref *completed-jobs* job-id) (internal-wait-for-job job-id)))
 
 (define (start-job-server config global-demo global-output)
+  (build-worker-pool (processor-count))
+  (check-q-for-work)
   ;; Pass along local global values
   ;; TODO can I pull these out of config or not need ot pass them along.
   (set! *demo?* global-demo)
   (set! *demo-output* global-output)
   (thread-send *worker-thread* config))
+
+(define *busy-workers* (list))
+(define *idel-workers* (list))
+(define *dead-events* (list)) ;; don't fully understand this.
+(define (build-worker-pool number-of-workers)
+  (define workers
+    (for/list ([wid (in-range number-of-workers)])
+      (make-worker wid)))
+  (define dead-workers
+    (for/list ([worker workers])
+      (place-dead-evt worker)))
+  (set! *busy-workers* workers)
+  (set! *dead-events* dead-workers)
+  (for ([worker *busy-workers*])
+    (place-channel-put worker (list 'start worker)))
+  (eprintf "workers ready\n")
+  (update-worker-queues))
+
+(define-syntax (place/context* stx)
+  (syntax-case stx ()
+    [(_ name #:parameters (params ...) body ...)
+     (with-syntax ([(fresh ...) (generate-temporaries #'(params ...))])
+       #'(let ([fresh (params)] ...)
+           (place/context name
+                          (parameterize ([params fresh] ...)
+                            body ...))))]))
+(define (make-worker wid)
+  (place/context* ch
+                  #:parameters (*flags* *num-iterations*
+                                        *num-points*
+                                        *timeout*
+                                        *reeval-pts*
+                                        *node-limit*
+                                        *max-find-range-depth*
+                                        *pareto-mode*
+                                        *platform-name*
+                                        *loose-plugins*)
+                  (parameterize ([current-error-port (open-output-nowhere)]) ; hide output
+                    (load-herbie-plugins))
+                  (for ([_ (in-naturals)])
+                    (match (place-channel-get ch)
+                      [(list 'apply self command id)
+                       (eprintf "[~a] working on [~a].\n" wid id)
+                       ; (define herbie-result (wrapper-run-herbie command id))
+                       ; (match-define (job-result kind test status time _ _ backend) herbie-result)
+                       ; (define out-result
+                       ;   (match kind
+                       ;     ['alternatives (make-alternatives-result herbie-result test id)]
+                       ;     ['evaluate (make-calculate-result herbie-result id)]
+                       ;     ['cost (make-cost-result herbie-result id)]
+                       ;     ['errors (make-error-result herbie-result id)]
+                       ;     ['exacts (make-exacts-result herbie-result id)]
+                       ;     ['improve (make-improve-result herbie-result test id)]
+                       ;     ['local-error (make-local-error-result herbie-result test id)]
+                       ;     ['sample (make-sample-result herbie-result id test)]
+                       ;     [_ (error 'compute-result "unknown command ~a" kind)]))
+                       ; (hash-set! *job-status* id #f)
+                       ; (place-channel-put ch (list 'done out-result))]
+                       (eprintf "~a working on job:~a\n" wid id)
+                       (define result id) ;; finnish work
+                       (place-channel-put self ('done self result))]
+                      [(list 'start self) (place-channel-put self (list 'ready self))]
+                      [(list 'ready self) (place-channel-put self (list 'ready self))]))))
 
 #| End Job Server Public API section |#
 
