c83529b286eb2de9f1eae5e48943e81fb9513bd2
AYadrov <kowalski228@mail.ru>
2024-08-06 12:34:04 -0600

    bug fix

--- src/core/batch.rkt
+++ src/core/batch.rkt
@@ -36,28 +36,28 @@
         [(list 'pow base 2/3)
          #:when taylor
          (cons 'cbrt (munge `(* ,base ,base)))]
-        [(list 'tan args ...)
+        [(list 'tan args)
          #:when taylor
-         (cons '/ (map munge (list `(sin ,args) `(cos ,args))))]
-        [(list 'cosh args ...)
+         (cons '/ (map munge (list `(sin args) `(cos args))))]
+        [(list 'cosh args)
          #:when taylor
          (cons '* (map munge (list 1/2 `(+ (exp ,args) (/ 1 (exp ,args))))))]
-        [(list 'sinh args ...)
+        [(list 'sinh args)
          #:when taylor
          (cons '* (map munge (list 1/2 `(+ (exp ,args) (- (/ 1 (exp ,args)))))))]
-        [(list 'tanh args ...)
+        [(list 'tanh args)
          #:when taylor
          (cons '/
                (map munge
                     (list `(+ (exp ,args) (- (/ 1 (exp ,args))))
                           `(+ (exp ,args) (/ 1 (exp ,args))))))]
-        [(list 'asinh args ...)
+        [(list 'asinh args)
          #:when taylor
          (cons 'log (munge `(+ ,args (sqrt (+ (* ,args ,args) 1)))))]
-        [(list 'acosh args ...)
+        [(list 'acosh args)
          #:when taylor
          (cons 'log (munge `(+ ,args (sqrt (+ (* ,args ,args) -1)))))]
-        [(list 'atanh args ...)
+        [(list 'atanh args)
          #:when taylor
          (cons '* (map munge (list 1/2 `(log (/ (+ 1 ,args) (+ 1 (- ,args)))))))]
         [(list op args ...) (cons op (map munge args))]
