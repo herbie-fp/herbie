573608b6a85f0368d3e777408c06c5121ea5b569
zane <39070793+zaneenders@users.noreply.github.com>
2024-08-14 13:21:38 -0600

    Clean up for PR.

--- src/api/demo.rkt
+++ src/api/demo.rkt
@@ -340,13 +340,6 @@
 
 (define (check-status req job-id)
   (match (get-timeline-for job-id)
-    [#f
-     (response 202
-               #"Job in progress"
-               (current-seconds)
-               #"text/plain"
-               (list (header #"X-Job-Count" (string->bytes/utf-8 (~a (job-count)))))
-               (λ (out) (display "Not done!" out)))]
     [(? box? timeline)
      (response 202
                #"Job in progress"
