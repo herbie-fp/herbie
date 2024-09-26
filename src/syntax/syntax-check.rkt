2cabb9a2116d0a6afbc557e3095e8eda5abaf198
Pavel Panchekha <me@pavpanchekha.com>
2024-09-26 10:43:10 -0600

    Also fix (!) annotations in variable declarations

--- src/syntax/syntax-check.rkt
+++ src/syntax/syntax-check.rkt
@@ -157,14 +157,20 @@
 (define (check-program* stx vars props body error!)
   (unless (list? vars)
     (error! stx "Invalid arguments list ~a; must be a list" stx))
-  (define vars* (filter identifier? vars))
-  (when (list? vars)
-    (for ([var vars]
-          #:unless (identifier? var))
-      (error! stx "Argument ~a is not a variable name" var))
-    (when (check-duplicate-identifier vars*)
-      (error! stx "Duplicate argument name ~a" (check-duplicate-identifier vars*))))
   (define deprecated-ops (mutable-set))
+  (define vars*
+    (reap [sow]
+          (when (list? vars)
+            (for ([var (in-list vars)])
+              (match var
+                [(? identifier? x) (sow var)]
+                [#`(! #,props ... #,name)
+                 (check-properties* props (immutable-bound-id-set '()) error! deprecated-ops)
+                 (cond
+                   [(identifier? name) (sow name)]
+                   [else (error! var "Annotated argument ~a is not a variable name" name)])])))))
+  (when (check-duplicate-identifier vars*)
+    (error! stx "Duplicate argument name ~a" (check-duplicate-identifier vars*)))
   (check-properties* props (immutable-bound-id-set vars*) error! deprecated-ops)
   (check-expression* body (immutable-bound-id-set vars*) error! deprecated-ops)
   (for ([op (in-set deprecated-ops)])
