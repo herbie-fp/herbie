d608f4165a30dfa8fecbc2466ecee5a8e605bb34
zane <39070793+zaneenders@users.noreply.github.com>
2024-10-09 16:15:36 -0600

    Better localize test but found an exact +nan.0.

--- infra/testApi.mjs
+++ infra/testApi.mjs
@@ -8,56 +8,56 @@ const FPCoreFormula = '(FPCore (x) (- (sqrt (+ x 1)) (sqrt x)))'
 const FPCoreFormula2 = '(FPCore (x) (- (sqrt (+ x 1))))'
 const eval_sample = [[[1], -1.4142135623730951]]
 
-// improve endpoint
-const improveResponse = await fetch(makeEndpoint(`/improve?formula=${encodeURIComponent(FPCoreFormula2)}`), { method: 'GET' })
-assert.equal(improveResponse.status, 200)
-let redirect = improveResponse.url.split("/")
-const jobID = redirect[3].split(".")[0]
-// This test is a little flaky as the character count of the response is not consistent.
-// const improveHTML = await improveResponse.text()
-// const improveHTMLexpectedCount = 25871
-// assert.equal(improveHTML.length, improveHTMLexpectedCount, `HTML response character count should be ${improveHTMLexpectedCount} unless HTML changes.`)
+// // improve endpoint
+// const improveResponse = await fetch(makeEndpoint(`/improve?formula=${encodeURIComponent(FPCoreFormula2)}`), { method: 'GET' })
+// assert.equal(improveResponse.status, 200)
+// let redirect = improveResponse.url.split("/")
+// const jobID = redirect[3].split(".")[0]
+// // This test is a little flaky as the character count of the response is not consistent.
+// // const improveHTML = await improveResponse.text()
+// // const improveHTMLexpectedCount = 25871
+// // assert.equal(improveHTML.length, improveHTMLexpectedCount, `HTML response character count should be ${improveHTMLexpectedCount} unless HTML changes.`)
 
-// timeline
-const timelineRSP = await fetch(makeEndpoint(`/timeline/${jobID}`), { method: 'GET' })
-assert.equal(timelineRSP.status, 201)
-const timeline = await timelineRSP.json()
-assert.equal(timeline.length > 0, true)
+// // timeline
+// const timelineRSP = await fetch(makeEndpoint(`/timeline/${jobID}`), { method: 'GET' })
+// assert.equal(timelineRSP.status, 201)
+// const timeline = await timelineRSP.json()
+// assert.equal(timeline.length > 0, true)
 
-// Test with a likely missing job-id
-const badTimelineRSP = await fetch(makeEndpoint("/timeline/42069"), { method: 'GET' })
-assert.equal(badTimelineRSP.status, 404)
+// // Test with a likely missing job-id
+// const badTimelineRSP = await fetch(makeEndpoint("/timeline/42069"), { method: 'GET' })
+// assert.equal(badTimelineRSP.status, 404)
 
-// improve-start endpoint
-const URIencodedBody = "formula=" + encodeURIComponent(FPCoreFormula)
-const startResponse = await fetch(makeEndpoint("/api/start/improve"), {
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/x-www-form-urlencoded',
-  },
-  body: URIencodedBody
-})
-const testResult = (startResponse.status == 201 || startResponse.status == 202)
-assert.equal(testResult, true)
-const improveResultPath = startResponse.headers.get("location")
-let counter = 0
-let cap = 100
-// Check status endpoint
-let checkStatus = await fetch(makeEndpoint(improveResultPath), { method: 'GET' })
-/*
-This is testing if the /api/start/improve test at the beginning has been completed. The cap and counter is a sort of timeout for the test. Ends up being 10 seconds max.
-*/
-while (checkStatus.status != 201 && counter < cap) {
-  counter += 1
-  checkStatus = await fetch(makeEndpoint(improveResultPath), { method: 'GET' })
-  await new Promise(r => setTimeout(r, 100)); // ms
-}
-assert.equal(checkStatus.statusText, 'Job complete')
+// // improve-start endpoint
+// const URIencodedBody = "formula=" + encodeURIComponent(FPCoreFormula)
+// const startResponse = await fetch(makeEndpoint("/api/start/improve"), {
+//   method: 'POST',
+//   headers: {
+//     'Content-Type': 'application/x-www-form-urlencoded',
+//   },
+//   body: URIencodedBody
+// })
+// const testResult = (startResponse.status == 201 || startResponse.status == 202)
+// assert.equal(testResult, true)
+// const improveResultPath = startResponse.headers.get("location")
+// let counter = 0
+// let cap = 100
+// // Check status endpoint
+// let checkStatus = await fetch(makeEndpoint(improveResultPath), { method: 'GET' })
+// /*
+// This is testing if the /api/start/improve test at the beginning has been completed. The cap and counter is a sort of timeout for the test. Ends up being 10 seconds max.
+// */
+// while (checkStatus.status != 201 && counter < cap) {
+//   counter += 1
+//   checkStatus = await fetch(makeEndpoint(improveResultPath), { method: 'GET' })
+//   await new Promise(r => setTimeout(r, 100)); // ms
+// }
+// assert.equal(checkStatus.statusText, 'Job complete')
 
-// up endpoint
-const up = await fetch(makeEndpoint("/up"), { method: 'GET' })
-assert.equal('Up', up.statusText)
-// TODO how do I test down state?
+// // up endpoint
+// const up = await fetch(makeEndpoint("/up"), { method: 'GET' })
+// assert.equal('Up', up.statusText)
+// // TODO how do I test down state?
 
 // Sample endpoint
 const sampleBody = {
@@ -69,31 +69,35 @@ const sampleAsyncResult = await callAsyncAndWaitJSONResult("/api/start/sample", 
 const jid = sampleRSP.headers.get("x-herbie-job-id")
 assert.notEqual(jid, null)
 const sample = await sampleRSP.json()
-assertIdAndPath(sampleAsyncResult)
-assert.ok(sampleAsyncResult.points)
-assert.equal(sampleAsyncResult.points.length, SAMPLE_SIZE)
-assertIdAndPath(sample)
-assert.ok(sample.points)
-assert.equal(sample.points.length, SAMPLE_SIZE, `sample size should be ${SAMPLE_SIZE}`)
+// assertIdAndPath(sampleAsyncResult)
+// assert.ok(sampleAsyncResult.points)
+// assert.equal(sampleAsyncResult.points.length, SAMPLE_SIZE)
+// assertIdAndPath(sample)
+// assert.ok(sample.points)
+// assert.equal(sample.points.length, SAMPLE_SIZE, `sample size should be ${SAMPLE_SIZE}`)
 
-// Make second call to test that results are the same
-const sample2RPS = await fetch(makeEndpoint("/api/sample"), sampleBody)
-const jid2 = sample2RPS.headers.get("x-herbie-job-id")
-assert.notEqual(jid2, null)
-const sample2 = await sample2RPS.json()
-assertIdAndPath(sample2)
-assert.deepEqual(sample.points[1], sample2.points[1])
+// // Make second call to test that results are the same
+// const sample2RPS = await fetch(makeEndpoint("/api/sample"), sampleBody)
+// const jid2 = sample2RPS.headers.get("x-herbie-job-id")
+// assert.notEqual(jid2, null)
+// const sample2 = await sample2RPS.json()
+// assertIdAndPath(sample2)
+// assert.deepEqual(sample.points[1], sample2.points[1])
 
 //Explanations endpoint
 const explainBody = {
   method: 'POST',
   body: JSON.stringify({
-    formula: FPCoreFormula, sample: sample2.points
+    formula: FPCoreFormula, sample: sample.points
   })
 }
+
+console.log("HERE")
 const explain = await (await fetch(makeEndpoint("/api/explanations"), explainBody)).json()
 assertIdAndPath(explain)
 assert.equal(explain.explanation.length > 0, true, 'explanation should not be empty');
+
+console.log("HERE")
 const explainAsyncResult = await callAsyncAndWaitJSONResult("/api/start/explanations", explainBody)
 assertIdAndPath(explainAsyncResult)
 assert.equal(explainAsyncResult.explanation.length > 0, true, 'explanation should not be empty');
@@ -145,6 +149,7 @@ const localErrorBody = {
     formula: FPCoreFormula, sample: sample2.points
   })
 }
+
 const localError = await (await fetch(makeEndpoint("/api/localerror"), localErrorBody)).json()
 assertIdAndPath(localError)
 assert.equal(localError.tree['avg-error'] > 0, true)
@@ -164,7 +169,38 @@ const localError2 = await (await fetch(makeEndpoint("/api/localerror"), {
 })).json()
 // Test that different sample points produce different job ids ensuring that different results are served for these inputs.
 assert.notEqual(localError1.job, localError2.job)
+console.log("HERE")
+// Assert local error works for default example.
+const ignoredValue = 1e+308
+// '(FPCore (1e-100) (- (sqrt (+ x 1)) (sqrt x)))'
+const localError5 = await (await fetch(makeEndpoint("/api/localerror"), {
+  method: 'POST', body: JSON.stringify({
+    formula: FPCoreFormula, sample: [[[1e-100], ignoredValue]], seed: 5
+  })
+})).json()
+const rootMinusNode = localError5.tree
+const leftSQRT = localError5.tree['children'][0]
+const rightSQRT = localError5.tree['children'][1]
+const plusNode = localError5.tree['children'][0]['children'][0]
+const xNode = localError5.tree['children'][0]['children'][0]['children'][0]
+const oneNode = localError5.tree['children'][0]['children'][0]['children'][1]
 
+//        node, name, approx_value, avg_error, exact_value, true_error_value, ulps_error
+assertCheckNode(rootMinusNode, '-', '1.0', '0', '1.0', '-1e-50', 1)
+assertCheckNode(leftSQRT, 'sqrt', '1.0', '0', '1.0', '5e-101', 1)
+assertCheckNode(rightSQRT, 'sqrt', '1e-50', '0', '1e-50', '2.379726195519099e-68', 1)
+assertCheckNode(plusNode, '+', '1.0', '0', '1.0', '1e-100', 1)
+assertCheckNode(xNode, 'x', '1e-100', '0', '1e-100', '0', 1)
+assertCheckNode(oneNode, '1.0', '1.0', '0', '1.0', '0', 1)
+
+function assertCheckNode(node, name, approx, avg_error, exact_value, true_error_value, ulps_error) {
+  assert.equal(node['e'], name)
+  assert.equal(node['approx-value'][0], approx)
+  assert.equal(node['avg-error'], avg_error)
+  assert.equal(node['exact-value'][0], exact_value)
+  assert.equal(node['true-error-value'][0], true_error_value)
+  assert.equal(node['ulps-error'][0], ulps_error)
+}
 // Alternatives endpoint
 const altBody = {
   method: 'POST', body: JSON.stringify({
