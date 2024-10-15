eb2aeebe171df36ccf68d9f4ce1757fd3a6f544d
adityaakhileshwaran <aakhiles@cs.washinton.edu>
2024-10-15 15:35:46 -0700

    aditya-running-egglog Added test code

--- egglog-herbie/main-run-egglog.rkt
+++ egglog-herbie/main-run-egglog.rkt
@@ -1,54 +1,12 @@
 #lang racket
 
-(define program-to-egglog "program-to-egglog.egg")
-
-(define egglog-output "egglog-output")
-
-; prelude, rules, expressions, extractions
+(provide (struct-out egglog-program))
 
 ;; Track the entire Egglog program in one go by "converting" into racket based code
-(define egglog-program 
-  '((datatype M
-     (Num Rational))
-   (datatype MTy
-     (Numf64 Rational))
-    (function typed-id (M String) MTy)
-    (rule ((= e (Num n)))
-          ((let ty "binary64")
-            (let ety (Numf64 n))
-            (union (typed-id e ty) ety)))
-    (let e (Num (rational 1 1)))
-    (run 3)
-    (extract e)))
+;; TODO : prelude, rules, expressions, extractions
+(struct egglog-program (program) #:prefab)
 
-(define (is-simple-expression? expr)
-  (or (symbol? expr) (number? expr) (string? expr) (boolean? expr)))
-
-(define (write-egglog-expression expr out)
-  (cond
-    [(string? expr) (fprintf out "\"~s\"" expr)]  ; Write strings with quotes
-    [(symbol? expr) (fprintf out "~s" expr)]      ; Write symbols as is
-    [(list? expr)    ; If it's a list, recursively write each part
-     (fprintf out "(")
-     (for-each (lambda (sub-expr)
-                 (write-egglog-expression sub-expr out)
-                 (fprintf out " "))  ; Add a space between list elements
-               expr)
-     (fprintf out ")")]
-    [else (fprintf out "~s" expr)]))  ; Fallback for other types
-
-; ;; Function to write the program to a filename
-; (define (write-program-to-egglog program)
-;   (with-output-to-file program-to-egglog
-;     #:exists 'replace
-;     (lambda ()
-;       (for-each (lambda (expr)
-;                   (write-egglog-expression expr (current-output-port))  ; Write each expression
-;                   (newline))  ; Add a newline after each expression for readability
-;                 program))))
-
-    
-;       (for-each write program))))
+(define program-to-egglog "program-to-egglog.egg")
 
 (define (write-program-to-egglog program)
   (with-output-to-file program-to-egglog
@@ -64,7 +22,7 @@
       (display output))))
 
 
-(define (run-egglog egglog-filename)
+(define (process-egglog egglog-filename)
   (define egglog-path (or (find-executable-path "egglog")
                           (error "egglog executable not found in PATH")))
                           
@@ -74,12 +32,6 @@
     (subprocess #f #f #f egglog-path curr-path))
 
   (subprocess-wait sp)
-
-  (displayln (port-closed? out))
-  (displayln (port-closed? err))
-
-  ; (sleep 1)
-  ;; does subprocess-wait close ports
 
   (define stdout-content (port->string out)) 
   (define stderr-content (port->string err))
@@ -93,42 +45,19 @@
   (close-input-port err))
 
 
-
 ;; High-level function that writes the program to a file and then runs it
-(define (run-egglog-program program)
-  (write-program-to-egglog program)
-  (run-egglog program-to-egglog)
-  ; read stdout and stderr
+(define (run-egglog program-struct)
+  (write-program-to-egglog (egglog-program-program program-struct))
+  (process-egglog program-to-egglog)
   
+  ; TODO : read stdout and stderr
   )
 
-(run-egglog-program egglog-program)
-
-;; Can you capture stdout from program
-;;      We have data and we done
-
-;; string ports : open-output-string : use parametrieto override ports
-
-;; Make egglog program a struct -> list of expressions
+;; TODO:
+;; 1. Make egglog program a struct -> list of expressions
 ;;  Embed arbitrary data -> make type egglog-prog
 
-;; Function to run the Egglog file using the system's Egglog engine
-; (define (run-egglog filename)
-;   (with-output-to-string 
-;     (lambda () 
-;         (system (string-append "egglog " filename)))))
 
-
-; boolean
-; f64
-; fn
-; i64
-; macros
-; map
-; set
-; unit -> '()
-; vector
-
-
-; rationals
-; string
+; 2. Types I need
+; - rationals
+; - string
---
+++ egglog-herbie/test-run-egglog.rkt
@@ -0,0 +1,21 @@
+#lang racket
+
+; Define  Egglog program as a list of S-expressions
+(define raw-egglog-program
+  '((datatype M
+     (Num Rational))
+    (datatype MTy
+     (Numf64 Rational))
+    (function typed-id (M String) MTy)
+    (rule ((= e (Num n)))
+          ((let ty "binary64")
+           (let ety (Numf64 n))
+           (union (typed-id e ty) ety)))
+    (let e (Num (rational 1 1)))
+    (run 3)
+    (extract e)))
+
+; Create an instance of the egglog-program struct
+(define my-egglog-program (egglog-program raw-egglog-program))
+
+(run-egglog my-egglog-program)
