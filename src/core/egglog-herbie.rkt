9062bd8bc6b5aac5ba3f217ca71c854fc520653c
adityaakhileshwaran <aakhiles@cs.washinton.edu>
2024-10-17 21:00:42 -0700

    aditya-running-egglog Running formatter

--- src/core/egglog-herbie.rkt
+++ src/core/egglog-herbie.rkt
@@ -15,32 +15,26 @@
 ; - rationals
 ; - string
 (define (write-program-to-egglog program)
-  (with-output-to-file program-to-egglog
-    #:exists 'replace
-    (lambda ()
-      (for-each writeln program))))
-
+  (with-output-to-file program-to-egglog #:exists 'replace (lambda () (for-each writeln program))))
 
 (define (process-egglog egglog-filename)
-  (define egglog-path (or (find-executable-path "egglog")
-                          (error "egglog executable not found in PATH")))
-                          
+  (define egglog-path
+    (or (find-executable-path "egglog") (error "egglog executable not found in PATH")))
+
   (define curr-path (build-path (current-directory) egglog-filename))
 
-  (define-values (sp out in err)
-    (subprocess #f #f #f egglog-path curr-path))
+  (define-values (sp out in err) (subprocess #f #f #f egglog-path curr-path))
 
   (subprocess-wait sp)
 
-  (define stdout-content (port->string out)) 
+  (define stdout-content (port->string out))
   (define stderr-content (port->string err))
 
   (close-input-port out)
   (close-output-port in)
   (close-input-port err)
-  
-  (cons stdout-content stderr-content))
 
+  (cons stdout-content stderr-content))
 
 ;; High-level function that writes the program to a file, runs it then returns output
 ;;; TODO : Faster way to read/write from/to files
