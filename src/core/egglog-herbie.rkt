e3ddbe0235147a92cb1e04daabd5e38b1a9bafd4
adityaakhileshwaran <aakhiles@cs.washinton.edu>
2024-10-18 02:01:41 -0700

    Re-initial commit

--- src/core/egglog-herbie.rkt
+++ src/core/egglog-herbie.rkt
@@ -42,3 +42,104 @@
   (write-program-to-egglog (egglog-program-program program-struct))
 
   (process-egglog program-to-egglog))
+
+;; TODO:
+;; 1. Add make-egglog-runner
+;; Constructs an egg runner.
+;;
+;; The schedule is a list of pairs specifying
+;;  - a list of rules
+;;  - scheduling parameters:
+;;     - node limit: `(node . <number>)`
+;;     - iteration limit: `(iteration . <number>)`
+;;     - constant fold: `(const-fold? . <boolean>)` [default: #t]
+;;     - scheduler: `(scheduler . <name>)` [default: backoff]
+;;        - `simple`: run all rules without banning
+;;        - `backoff`: ban rules if the fire too much
+(define (make-egg-runner batch roots reprs schedule #:context [ctx (*context*)])
+  (define (oops! fmt . args)
+    (apply error 'verify-schedule! fmt args))
+  ; verify the schedule
+  (for ([instr (in-list schedule)])
+    (match instr
+      [(cons rules params)
+       ;; `run` instruction
+       (unless (and (list? rules) (andmap rule? rules))
+         (oops! "expected list of rules: `~a`" rules))
+       (for ([param (in-list params)])
+         (match param
+           [(cons 'node (? nonnegative-integer?)) (void)]
+           [(cons 'iteration (? nonnegative-integer?)) (void)]
+           [(cons 'const-fold? (? boolean?)) (void)]
+           [(cons 'scheduler mode)
+            (unless (set-member? '(simple backoff) mode)
+              (oops! "in instruction `~a`, unknown scheduler `~a`" instr mode))]
+           [_ (oops! "in instruction `~a`, unknown parameter `~a`" instr param)]))]
+      [_ (oops! "expected `(<rules> . <params>)`, got `~a`" instr)]))
+  ; make the runner
+  (egg-runner batch roots reprs schedule ctx))
+
+;; 2. Add run-egglog
+;; Runs egg using an egg runner.
+;;
+;; Argument `cmd` specifies what to get from the e-graph:
+;;  - single extraction: `(single . <extractor>)`
+;;  - multi extraction: `(multi . <extractor>)`
+;;  - proofs: `(proofs . ((<start> . <end>) ...))`
+(define (run-egg runner cmd)
+  ;; Run egg using runner
+  (define ctx (egg-runner-ctx runner))
+  (define-values (root-ids egg-graph)
+    (egraph-run-schedule (egg-runner-batch runner)
+                         (egg-runner-roots runner)
+                         (egg-runner-schedule runner)
+                         ctx))
+  ; Perform extraction
+  (match cmd
+    [`(single . ,extractor) ; single expression extraction
+     (define regraph (make-regraph egg-graph))
+     (define reprs (egg-runner-reprs runner))
+     (when (flag-set? 'dump 'egg)
+       (regraph-dump regraph root-ids reprs))
+     (define extract-id (extractor regraph))
+     (define finalize-batch (last extract-id))
+     ; (Listof (Listof batchref))
+     (define out
+       (for/list ([id (in-list root-ids)]
+                  [repr (in-list reprs)])
+         (regraph-extract-best regraph extract-id id repr)))
+     ; commit changes to the batch
+     (finalize-batch)
+     out]
+    [`(multi . ,extractor) ; multi expression extraction
+     (define regraph (make-regraph egg-graph))
+     (define reprs (egg-runner-reprs runner))
+     (when (flag-set? 'dump 'egg)
+       (regraph-dump regraph root-ids reprs))
+     (define extract-id (extractor regraph))
+     (define finalize-batch (last extract-id))
+     ; (Listof (Listof batchref))
+     (define out
+       (for/list ([id (in-list root-ids)]
+                  [repr (in-list reprs)])
+         (regraph-extract-variants regraph extract-id id repr)))
+     ; commit changes to the batch
+     (finalize-batch)
+     out]
+    [`(proofs . ((,start-exprs . ,end-exprs) ...)) ; proof extraction
+     (for/list ([start (in-list start-exprs)]
+                [end (in-list end-exprs)])
+       (unless (egraph-expr-equal? egg-graph start end ctx)
+         (error 'run-egg
+                "cannot find proof; start and end are not equal.\n start: ~a \n end: ~a"
+                start
+                end))
+       (define proof (egraph-get-proof egg-graph start end ctx))
+       (when (null? proof)
+         (error 'run-egg "proof extraction failed between`~a` and `~a`" start end))
+       proof)]
+    [`(equal? . ((,start-exprs . ,end-exprs) ...)) ; term equality?
+     (for/list ([start (in-list start-exprs)]
+                [end (in-list end-exprs)])
+       (egraph-expr-equal? egg-graph start end ctx))]
+    [_ (error 'run-egg "unknown command `~a`\n" cmd)]))
