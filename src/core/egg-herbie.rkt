19a22e70babf7e91a412c51c3ad34f1a9c58d2fd
Pavel Panchekha <me@pavpanchekha.com>
2024-11-19 15:59:05 -0700

    Just disable the expansive rules handling, for now

--- src/core/egg-herbie.rkt
+++ src/core/egg-herbie.rkt
@@ -479,7 +479,7 @@
 (define (rule->egg-rules ru)
   (define input (rule-input ru))
   (cond
-    [(symbol? input)
+    [false
      ; expansive rules
      (define itype (dict-ref (rule-itypes ru) input))
      (for/list ([op (all-operators)]
