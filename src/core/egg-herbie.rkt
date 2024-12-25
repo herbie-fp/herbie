69371ba32960ea5e0cf4954e0fa5b2b29f64f707
Rupanshu Soi <f20180294@hyderabad.bits-pilani.ac.in>
2024-12-25 11:57:51 -0800

    Fix egraph_copy

--- src/core/egg-herbie.rkt
+++ src/core/egg-herbie.rkt
@@ -1147,8 +1147,7 @@
 
   ;; run the rules
   (let loop ([iter-limit iter-limit])
-    ;; (define egg-graph (egraph-copy egg-graph0))
-    (define egg-graph egg-graph0)
+    (define egg-graph (egraph-copy egg-graph0))
     (define iteration-data
       (egraph-run egg-graph ffi-rules node-limit iter-limit scheduler const-folding?))
 
