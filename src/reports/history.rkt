f7978270005a32c5a12176013905e6b5d1ea3072
Pavel Panchekha <me@pavpanchekha.com>
2025-01-18 17:07:36 -0700

    Rewrite expr->fpcore to work better

--- src/reports/history.rkt
+++ src/reports/history.rkt
@@ -73,16 +73,16 @@
   (values (format-accuracy err repr-bits #:unit "%")
           (format "~a on training set" (format-accuracy err2 repr-bits #:unit "%"))))
 
-(define (remove-literals expr)
-  (match expr
-    [(? symbol?) expr]
-    [(? number?) expr]
-    [(? literal?) (literal-value expr)]
-    [(approx spec impl) (approx (remove-literals spec) (remove-literals impl))]
-    [(list op args ...) (cons op (map remove-literals args))]))
-
 (define (expr->fpcore expr ctx #:ident [ident #f])
-  (list 'FPCore (context-vars ctx) (remove-literals expr)))
+  (list 'FPCore (context-vars ctx)
+        (let loop ([expr expr])
+          (match expr
+            [(? symbol?) expr]
+            [(? number?) expr]
+            [(? literal?) (literal-value expr)]
+            [(approx spec impl) (loop impl)]
+            [(hole precision spec) (loop spec)]
+            [(list op args ...) (cons op (map loop args))]))))
 
 (define (mixed->fpcore expr ctx)
   (define expr*
