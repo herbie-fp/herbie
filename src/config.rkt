3e242348dfba811cbe17adb16ef65662d66cc0be
AYadrov <kowalski228@mail.ru>
2025-03-03 14:09:10 -0700

    default platform set to be c

--- src/config.rkt
+++ src/config.rkt
@@ -150,7 +150,7 @@
 (define *default-precision* (make-parameter 'binary64))
 
 ;; The platform that Herbie will evaluate with.
-(define *platform-name* (make-parameter 'herbie20))
+(define *platform-name* (make-parameter 'c))
 
 ;; True iff using the old cost function
 (define *egraph-platform-cost* (make-parameter #f))
