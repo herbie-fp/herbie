53abd3f24cdb87a1b6677724aaf69ecc9d4df0fe
AYadrov <kowalski228@mail.ru>
2025-03-07 12:50:54 -0700

    removed ormap bit

--- src/core/alt-table.rkt
+++ src/core/alt-table.rkt
@@ -184,26 +184,17 @@
 (define (atab-add-altn atab altn errs cost)
   (match-define (alt-table point-idx->alts alt->point-idxs alt->done? alt->cost pcontext _) atab)
 
-  (define max-alts-per-point 0)
+  (define max-alts-per-pareto-point 0)
   (define point-idx->alts*
     (for/vector #:length (vector-length point-idx->alts)
                 ([pcurve (in-vector point-idx->alts)]
                  [err (in-list errs)])
       (define ppt (pareto-point cost err (list altn)))
-      (define pt*
-        (pareto-union (list ppt)
-                      pcurve
-                      #:combine (lambda (alts1 alts2)
-                                  ; dedup by program
-                                  ; optimization: combining means that `alts1` corresponds to
-                                  ; the new pareto point
-                                  (match-define (list altn) alts1)
-                                  (if (ormap (lambda (a) (alt-equal? a altn)) alts2)
-                                      alts2
-                                      (cons altn alts2)))))
-      (set! max-alts-per-point
-            (max max-alts-per-point (apply max (map (compose length pareto-point-data) pt*))))
-      pt*))
+      (define pcurve* (pareto-union (list ppt) pcurve))
+      (set! max-alts-per-pareto-point
+            (max max-alts-per-pareto-point
+                 (apply max (map (compose length pareto-point-data) pcurve*))))
+      pcurve*))
 
   (define atab*
     (alt-table point-idx->alts*
@@ -213,7 +204,7 @@
                pcontext
                #f))
 
-  (if (> max-alts-per-point (* (*pareto-pick-limit*) 5))
+  (if (> max-alts-per-pareto-point (* (*pareto-pick-limit*) 5))
       (atab-prune (struct-copy alt-table
                                atab*
                                [alt->point-idxs (invert-index (alt-table-point-idx->alts atab*))]))
