bdc09e5679b785a2decb384562c240c43860e6a9
Pavel Panchekha <me@pavpanchekha.com>
2025-03-27 16:56:11 -0600

    derivations.rkt is now typed

--- src/core/derivations.rkt
+++ src/core/derivations.rkt
@@ -1,11 +1,35 @@
-#lang racket
+#lang typed/racket
 
-(require "../utils/alternative.rkt"
-         "programs.rkt"
-         "egg-herbie.rkt")
+(require/typed "../utils/alternative.rkt"
+  [#:struct alt
+   ([expr : Program]
+    [event : (U Symbol (Listof Any))]
+    [prevs : (Listof alt)]
+    [preprocessing : (Listof (Listof Symbol))])]
+  [alt-map (-> (-> alt alt) alt alt)])
+
+(require/typed "programs.rkt"
+  [location-do (-> Loc Program (-> Program Program) Program)]
+  [location-get (-> Loc Program Program)])
+
+(require/typed "egg-herbie.rkt"
+  [#:struct egg-runner
+   ([batch : Any]
+    [roots : Any]
+    [reprs : Any]
+    [schedule : Any]
+    [ctx : Any]
+    [new-roots : Any]
+    [egg-graph : Any])]
+  [egraph-prove (-> egg-runner Program Program Proof)])
 
 (provide add-derivations)
 
+(define-type Program Any)
+(define-type Proof (Listof Any))
+(define-type Loc (Listof Natural))
+
+(: canonicalize-proof (-> Program Proof Loc Proof))
 (define (canonicalize-proof prog proof loc)
   (and proof
        ;; Proofs are actually on subexpressions,
@@ -14,21 +38,23 @@
          (location-do loc prog (const step)))))
 
 ;; Adds proof information to alternatives.
+(: add-derivations-to (-> alt alt))
 (define (add-derivations-to altn)
   (match altn
     ; recursive rewrite or simplify, both using egg
     [(alt expr (list (or 'simplify 'rr) loc (? egg-runner? runner) #f) `(,prev) preprocessing)
-     (define start-expr (location-get loc (alt-expr prev)))
-     (define end-expr (location-get loc expr))
+     (define start-expr (location-get (cast loc Loc) (alt-expr prev)))
+     (define end-expr (location-get (cast loc Loc) expr))
      (define proof (egraph-prove runner start-expr end-expr))
-     (define proof* (canonicalize-proof (alt-expr altn) proof loc))
+     (define proof* (canonicalize-proof (alt-expr altn) proof (cast loc Loc)))
      (alt expr `(rr ,loc ,runner ,proof*) `(,prev) preprocessing)]
 
     ; everything else
     [_ altn]))
 
+(: add-derivations (-> (Listof alt) (Listof alt)))
 (define (add-derivations alts)
-  (define cache (make-hash))
+  (define cache : (HashTable alt alt) (make-hash))
   (for/list ([altn (in-list alts)])
     ;; We need to cache this because we'll see the same alt several times
     (alt-map (lambda (altn) (hash-ref! cache altn (lambda () (add-derivations-to altn)))) altn)))
