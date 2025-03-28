6e113645fcc3d307c3deca765cdbdfeb58276161
Pavel Panchekha <me@pavpanchekha.com>
2025-03-28 09:26:08 -0600

    Move define-operator-impl to the untyped world

--- src/syntax/syntax.rkt
+++ src/syntax/syntax.rkt
@@ -29,8 +29,7 @@
          register-function!)
 
 (module+ internals
-  (provide define-operator-impl
-           register-operator-impl!
+  (provide register-operator-impl!
            define-operator
            register-operator!
            variable?))
@@ -360,65 +359,6 @@
     [(? variable?) #t]
     [`(,impl ,args ...) (andmap well-formed? args)]
     [_ #f]))
-
-(define-syntax (define-operator-impl stx)
-  (define (oops! why [sub-stx #f])
-    (raise-syntax-error 'define-operator-impl why stx sub-stx))
-  (syntax-case stx (:)
-    [(_ (id [var : repr] ...) rtype fields ...)
-     (let ([id #'id]
-           [vars (syntax->list #'(var ...))]
-           [fields #'(fields ...)])
-       (unless (identifier? id)
-         (oops! "expected identifier" id))
-       (for ([var (in-list vars)]
-             #:unless (identifier? var))
-         (oops! "expected identifier" var))
-       (define spec #f)
-       (define core #f)
-       (define fl-expr #f)
-
-       (let loop ([fields fields])
-         (syntax-case fields ()
-           [()
-            (unless spec
-              (oops! "missing `#:spec` keyword"))
-            (with-syntax ([id id]
-                          [spec spec]
-                          [core core]
-                          [fl-expr fl-expr])
-              #'(register-operator-impl! 'id
-                                         (context '(var ...)
-                                                  (get-representation 'rtype)
-                                                  (list (get-representation 'repr) ...))
-                                         'spec
-                                         #:fl fl-expr
-                                         #:fpcore 'core))]
-           [(#:spec expr rest ...)
-            (cond
-              [spec (oops! "multiple #:spec clauses" stx)]
-              [else
-               (set! spec #'expr)
-               (loop #'(rest ...))])]
-           [(#:spec) (oops! "expected value after keyword `#:spec`" stx)]
-           [(#:fpcore expr rest ...)
-            (cond
-              [core (oops! "multiple #:fpcore clauses" stx)]
-              [else
-               (set! core #'expr)
-               (loop #'(rest ...))])]
-           [(#:fpcore) (oops! "expected value after keyword `#:fpcore`" stx)]
-           [(#:fl expr rest ...)
-            (cond
-              [fl-expr (oops! "multiple #:fl clauses" stx)]
-              [else
-               (set! fl-expr #'expr)
-               (loop #'(rest ...))])]
-           [(#:fl) (oops! "expected value after keyword `#:fl`" stx)]
-
-           ; bad
-           [_ (oops! "bad syntax" fields)])))]
-    [_ (oops! "bad syntax")]))
 
 ;; Expression predicates ;;
 
