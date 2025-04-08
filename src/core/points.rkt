cfc7bb9c9b6201c31c78c83de78ded3be579e5ec
Pavel Panchekha <me@pavpanchekha.com>
2025-04-08 14:02:23 -0600

    Fix json->pcontext

--- src/core/points.rkt
+++ src/core/points.rkt
@@ -95,7 +95,7 @@
                (match-define (list pt ex) entry)
                (unless (and (list? pt) (= (length pt) (length var-reprs)))
                  (error 'json->pcontext "Invalid point ~a" pt))
-               (values (map json->value pt var-reprs) (json->value ex output-repr))))
+               (values (list->vector (map json->value pt var-reprs)) (json->value ex output-repr))))
   (mk-pcontext pts exs))
 
 (define (pcontext->json pcontext repr)
