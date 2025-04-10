d5c90430e9c3730074d3bce3db669bd00e432e6b
adityaakhileshwaran <aakhiles@cs.washinton.edu>
2025-04-10 15:30:06 -0700

    Added normalization of costs for egglog

--- src/core/egglog-herbie.rkt
+++ src/core/egglog-herbie.rkt
@@ -277,6 +277,9 @@
       (mutable-batch-push! out term)))
   (batchref input-batch idx))
 
+(define (normalize-cost c min-cost)
+  (exact-round (* (/ c min-cost) 100)))
+
 (define (prelude curr-program #:mixed-egraph? [mixed-egraph? #t])
   (load-herbie-builtins)
   (define pform (*active-platform*))
@@ -290,19 +293,41 @@
 
   (egglog-program-add! spec-egraph curr-program)
 
+  ;;; To add support for floating point cost (which egglog does not support), compute
+  ;;; the minimum by accumulating all raw costs and normalize them
+  (define raw-costs '())
+
+  ;; Add raw num-typed-nodes and var-typed-nodes costs
+  (for ([repr (in-list (all-repr-names))])
+    (set! raw-costs (cons (platform-repr-cost pform (get-representation repr)) raw-costs)))
+
+  ;; Add raw if-cost
+  (set! raw-costs
+        (cons (match (platform-impl-cost pform 'if)
+                [`(max ,n) n] ; Not quite right (copied from egg-herbie.rkt)
+                [`(sum ,n) n])
+              raw-costs))
+
+  ;; Add raw platform-impl-nodes
+  (for ([impl (in-list (platform-impls pform))])
+    (set! raw-costs (cons (platform-impl-cost pform impl) raw-costs)))
+
+  (define min-cost (apply min raw-costs))
+
   (define typed-graph
     `(datatype MTy
-               ,@(num-typed-nodes pform)
-               ,@(var-typed-nodes pform)
+               ,@(num-typed-nodes pform min-cost)
+               ,@(var-typed-nodes pform min-cost)
                (IfTy MTy
                      MTy
                      MTy
                      :cost
-                     ,(match (platform-impl-cost pform 'if)
-                        [`(max ,n) n] ; Not quite right (copied from egg-herbie.rkt)
-                        [`(sum ,n) n]))
-               (Approx M MTy)
-               ,@(platform-impl-nodes pform)))
+                     ,(normalize-cost (match (platform-impl-cost pform 'if)
+                                        [`(max ,n) n] ; Not quite right (copied from egg-herbie.rkt)
+                                        [`(sum ,n) n])
+                                      min-cost))
+                     (Approx M MTy)
+                     ,@(platform-impl-nodes pform min-cost)))
 
   (egglog-program-add! typed-graph curr-program)
 
@@ -382,7 +407,7 @@
                          :cost
                          4294967295)))
 
-(define (platform-impl-nodes pform)
+(define (platform-impl-nodes pform min-cost)
   (for/list ([impl (in-list (platform-impls pform))])
     (define arity (length (impl-info impl 'itype)))
     (define typed-name (string->symbol (string-append (symbol->string (serialize-impl impl)) "Ty")))
@@ -391,7 +416,7 @@
     `(,typed-name ,@(for/list ([i (in-range arity)])
                       'MTy)
                   :cost
-                  ,(platform-impl-cost pform impl))))
+                  ,(normalize-cost (platform-impl-cost pform impl) min-cost))))
 
 (define (typed-num-id repr-name)
   (string->symbol (string-append "Num" (symbol->string repr-name))))
@@ -399,14 +424,20 @@
 (define (typed-var-id repr-name)
   (string->symbol (string-append "Var" (symbol->string repr-name))))
 
-(define (num-typed-nodes pform)
+(define (num-typed-nodes pform min-cost)
   (for/list ([repr (in-list (all-repr-names))]
              #:when (not (eq? repr 'bool)))
-    `(,(typed-num-id repr) BigRat :cost ,(platform-repr-cost pform (get-representation repr)))))
+    `(,(typed-num-id repr) BigRat
+                           :cost
+                           ,(normalize-cost (platform-repr-cost pform (get-representation repr))
+                                            min-cost))))
 
-(define (var-typed-nodes pform)
+(define (var-typed-nodes pform min-cost)
   (for/list ([repr (in-list (all-repr-names))])
-    `(,(typed-var-id repr) String :cost ,(platform-repr-cost pform (get-representation repr)))))
+    `(,(typed-var-id repr) String
+                           :cost
+                           ,(normalize-cost (platform-repr-cost pform (get-representation repr))
+                                            min-cost))))
 
 (define (num-lowering-rules)
   (for/list ([repr (in-list (all-repr-names))]
