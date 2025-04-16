64126e832fc7b039c68b87f5af11247d24804b4a
Paul Zhang <paul.zy.zhang@gmail.com>
2025-04-16 16:27:57 -0700

    Disable pruning

--- egg-herbie/src/lib.rs
+++ egg-herbie/src/lib.rs
@@ -199,11 +199,11 @@ pub unsafe extern "C" fn egraph_run(
     // Prune all e-nodes with children where its e-class has a leaf node (with no children). Pruning
     // safely improves performance because pruning occurs right before extraction and leaf e-nodes
     // always have a lower cost.
-    context.runner.egraph.classes_mut().for_each(|eclass| {
-        if eclass.nodes.iter().any(|n| n.is_leaf()) {
-            eclass.nodes.retain(|n| n.is_leaf());
-        }
-    });
+    // context.runner.egraph.classes_mut().for_each(|eclass| {
+    //     if eclass.nodes.iter().any(|n| n.is_leaf()) {
+    //         eclass.nodes.retain(|n| n.is_leaf());
+    //     }
+    // });
 
     let iterations = context
         .runner
