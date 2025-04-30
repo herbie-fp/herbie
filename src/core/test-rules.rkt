6bddedad94e09f83b9955f36c2b9cf3ddb329c82
AYadrov <kowalski228@mail.ru>
2025-04-30 15:55:06 -0600

    no condition for asinh-2

--- src/core/test-rules.rkt
+++ src/core/test-rules.rkt
@@ -15,8 +15,7 @@
 (define num-test-points (make-parameter 100))
 
 (define *conditions*
-  (list '[asinh-2 . (>= x 0)]
-        '[pow-unpow . (>= a 0)]
+  (list '[pow-unpow . (>= a 0)]
         '[pow-pow . (>= a 0)]
         '[sqrt-pow1 . (>= x 0)]
         '[asin-sin-s . (<= (fabs x) (/ (PI) 2))]
