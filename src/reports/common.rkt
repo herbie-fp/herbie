c143cf917a256a86bfff632c00be1255a6d6cd04
Pavel Panchekha <me@pavpanchekha.com>
2025-06-12 14:38:15 -0600

    fmt

--- src/reports/common.rkt
+++ src/reports/common.rkt
@@ -398,15 +398,15 @@
       "?"))
 
 (define js-tex-include
-   '((link ([rel "stylesheet"]
-            [href "https://cdn.jsdelivr.net/npm/katex@0.16.22/dist/katex.min.css"]
-            [integrity "sha384-5TcZemv2l/9On385z///+d7MSYlvIEw9FuZTIdZ14vJLqWphw7e7ZPuOiCHJcFCP"]
-            [crossorigin "anonymous"]))
-     (script ([defer ""]
-              [src "https://cdn.jsdelivr.net/npm/katex@0.16.22/dist/katex.min.js"]
-              [integrity "sha384-cMkvdD8LoxVzGF/RPUKAcvmm49FQ0oxwDF3BGKtDXcEc+T1b2N+teh/OJfpU0jr6"]
-              [crossorigin "anonymous"]))
-     (script ([defer ""]
-              [src "https://cdn.jsdelivr.net/npm/katex@0.16.22/dist/contrib/auto-render.min.js"]
-              [integrity "sha384-hCXGrW6PitJEwbkoStFjeJxv+fSOOQKOPbJxSfM6G5sWZjAyWhXiTIIAmQqnlLlh"]
-              [crossorigin "anonymous"]))))
+  '((link ([rel "stylesheet"]
+           [href "https://cdn.jsdelivr.net/npm/katex@0.16.22/dist/katex.min.css"]
+           [integrity "sha384-5TcZemv2l/9On385z///+d7MSYlvIEw9FuZTIdZ14vJLqWphw7e7ZPuOiCHJcFCP"]
+           [crossorigin "anonymous"]))
+    (script ([defer ""] [src "https://cdn.jsdelivr.net/npm/katex@0.16.22/dist/katex.min.js"]
+                        [integrity
+                         "sha384-cMkvdD8LoxVzGF/RPUKAcvmm49FQ0oxwDF3BGKtDXcEc+T1b2N+teh/OJfpU0jr6"]
+                        [crossorigin "anonymous"]))
+    (script ([defer ""]
+             [src "https://cdn.jsdelivr.net/npm/katex@0.16.22/dist/contrib/auto-render.min.js"]
+             [integrity "sha384-hCXGrW6PitJEwbkoStFjeJxv+fSOOQKOPbJxSfM6G5sWZjAyWhXiTIIAmQqnlLlh"]
+             [crossorigin "anonymous"]))))
