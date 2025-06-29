30ba6440ab9d688103577a37f5a70390d2b8a8a0
Pavel Panchekha <me@pavpanchekha.com>
2025-06-29 14:09:15 +0800

    Woops

--- src/platforms/rival.rkt
+++ src/platforms/rival.rkt
@@ -5,7 +5,6 @@
 
 (require math/bigfloat
          math/flonum
-         "runtime/float32.rkt" ; float representation helper functions
          "../utils/float.rkt"  ; for shift/unshift
          "../syntax/platform.rkt")
 (provide platform)
