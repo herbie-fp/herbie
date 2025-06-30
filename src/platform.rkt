0a7bc31e4e5e56a07ee7711143ee1e008bc2078e
Pavel Panchekha <me@pavpanchekha.com>
2025-06-30 11:47:53 +0800

    fmt

--- src/platform.rkt
+++ src/platform.rkt
@@ -7,12 +7,11 @@
          (except-out (all-from-out racket) #%module-begin)
          (all-from-out "syntax/platform.rkt")
          (all-from-out "syntax/types.rkt"))
-  
+
 (define-syntax (platform-module-begin stx)
   (with-syntax ([local-platform (datum->syntax stx 'platform)])
     (syntax-case stx ()
       [(_ content ...)
-       #'(#%module-begin
-          (define local-platform (make-empty-platform 'platform))
-          content ...
-          (provide local-platform))])))
+       #'(#%module-begin (define local-platform (make-empty-platform 'platform))
+                         content ...
+                         (provide local-platform))])))
