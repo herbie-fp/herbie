42f584ec34e5fee3adc85afc0b093b04edd70aa9
Pavel Panchekha <me@pavpanchekha.com>
2025-07-31 10:44:17 -0600

    if is now a more normal operator

--- src/core/egglog-herbie.rkt
+++ src/core/egglog-herbie.rkt
@@ -428,7 +428,7 @@
     (rewrite (Round (Num x)) (Num (round x)) :ruleset const-fold)))
 
 (define (platform-spec-nodes)
-  (append* (for/list ([op (in-list (cons 'if (all-operators)))])
+  (append* (for/list ([op (in-list (all-operators))])
              (hash-set! (id->e1) op (serialize-op op))
              (hash-set! (e1->id) (serialize-op op) op)
 
@@ -437,10 +437,7 @@
              (hash-set! (id->e1) unsound-op (serialize-op unsound-op))
              (hash-set! (e1->id) (serialize-op unsound-op) unsound-op)
 
-             (define arity
-               (match op
-                 ['if 3]
-                 [_ (length (operator-info op 'itype))]))
+             (define arity (length (operator-info op 'itype)))
              (list `(,(serialize-op op) ,@(for/list ([i (in-range arity)])
                                             'M)
                                         :cost
@@ -1028,10 +1025,6 @@
       [`(,(? egglog-num? num) (bigrat (from-string ,n) (from-string ,d)))
        (/ (string->number n) (string->number d))]
       [`(,(? egglog-var? var) ,v) (string->symbol v)]
-      [`(If ,cond ,ift ,iff)
-       `(if ,(loop cond)
-            ,(loop ift)
-            ,(loop iff))]
       [`(,op ,args ...) `(,(hash-ref (e1->id) op) ,@(map loop args))])))
 
 (define (e2->expr expr)
