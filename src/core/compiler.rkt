bfe3ab7594ea880ca24c4d4c799a6af784dc1335
AYadrov <kowalski228@mail.ru>
2025-08-24 22:43:17 -0600

    compiler using batch maps

--- src/core/compiler.rkt
+++ src/core/compiler.rkt
@@ -11,54 +11,32 @@
          compile-batch
          compile-prog)
 
-;; Interpreter taking a narrow IR
-;; ```
-;; <prog> ::= #(<instr> ..+)
-;; <instr> ::= '(<op-procedure> <index> ...)
-;; ```
-;; where <index> refers to a previous virtual register.
-;; Must also provide the input variables for the program(s)
-;; as well as the indices of the roots to extract.
-(define (make-progs-interpreter vars ivec rootvec)
-  (define rootlen (vector-length rootvec))
-  (define iveclen (vector-length ivec))
-  (define varc (length vars))
-  (define vregs (make-vector (+ varc iveclen)))
-  (define (compiled-prog args)
-    (vector-copy! vregs 0 args)
-    (for ([instr (in-vector ivec)]
-          [n (in-naturals varc)])
-      (vector-set! vregs n (apply-instruction instr vregs)))
-    (for/vector #:length rootlen
-                ([root (in-vector rootvec)])
-      (vector-ref vregs root)))
-  compiled-prog)
+(define (drop-spec! batch)
+  (batch-apply! batch
+                (lambda (node)
+                  (match node
+                    [(approx spec impl) impl]
+                    [node node]))))
 
-(define (apply-instruction instr regs)
-  ;; By special-casing the 0-3 instruction case,
-  ;; we avoid any allocation in the common case.
-  ;; We could add more cases if we want wider instructions.
-  ;; At some extreme, vector->values plus call-with-values
-  ;; becomes the fastest option.
-  (match instr
-    [(list op) (op)]
-    [(list op a) (op (vector-ref regs a))]
-    [(list op a b) (op (vector-ref regs a) (vector-ref regs b))]
-    [(list op a b c) (op (vector-ref regs a) (vector-ref regs b) (vector-ref regs c))]
-    [(list op args ...) (apply op (map (curry vector-ref regs) args))]))
+(define (rewrite! batch)
+  (batch-apply!
+   batch
+   (lambda (node)
+     (match node
+       [(literal value (app get-representation repr)) (list (const (real->repr value repr)))]
+       [(list op args ...) (cons (impl-info op 'fl) args)]
+       [_ node]))))
 
-; This functions needs to preserve vars
-(define (batch-for-compiler batch brfs vars)
-  (define-values (batch* brfs*)
-    (batch-apply batch
-                 brfs
-                 (lambda (node)
-                   (match node
-                     [(approx spec impl) impl]
-                     [node node]))))
-  (define-values (batch** _) (progs->batch vars))
-  (define brfs** (map (batch-copy-only! batch** batch*) brfs*))
-  (values batch** brfs**))
+(define (evaluate batch args)
+  (batch-map batch
+             (lambda (evaluate-child node)
+               (match node
+                 [(list op) (op)]
+                 [(list op a) (op (evaluate-child a))]
+                 [(list op a b) (op (evaluate-child a) (evaluate-child b))]
+                 [(list op a b c) (op (evaluate-child a) (evaluate-child b) (evaluate-child c))]
+                 [(list op args ...) (apply op (map evaluate-child args))]
+                 [(? symbol?) (hash-ref args node)]))))
 
 ;; Compiles a program of operator implementations into a procedure
 ;; that evaluates the program on a single input of representation values
@@ -72,21 +50,16 @@
 
 (define (compile-batch batch brfs ctx)
   (define vars (context-vars ctx))
-  (define num-vars (length vars))
+  (define args (make-hash))
+  ;; Modifying batch
+  (define drop-spec-f (drop-spec! batch))
+  (define rewrite-f (rewrite! batch))
+  (define brfs* (map (compose rewrite-f drop-spec-f) brfs))
 
-  ; Here we need to keep vars even though no roots refer to the vars
-  (define-values (batch* brfs*) (batch-for-compiler batch brfs vars))
-  (timeline-push! 'compiler (batch-tree-size batch* brfs*) (batch-length batch*))
-
-  (define instructions
-    (for/vector #:length (- (batch-length batch*) num-vars)
-                ([node (in-batch batch* num-vars)])
-      (match node
-        [(literal value (app get-representation repr)) (list (const (real->repr value repr)))]
-        [(list op args ...) (cons (impl-info op 'fl) args)])))
-  (define rootvec (list->vector (map batchref-idx brfs*)))
-
-  (make-progs-interpreter vars instructions rootvec))
+  (define (fn pt)
+    (for-each (curry hash-set! args) vars (vector->list pt))
+    (list->vector (map (evaluate batch args) brfs*)))
+  fn)
 
 ;; Like `compile-progs`, but a single prog.
 (define (compile-prog expr ctx)
