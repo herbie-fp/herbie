7c776612f211d6e6aab136897c905ea4e9377a45
Aditya Kumar <obround.dev@gmail.com>
2025-08-25 04:05:25 -0700

    Added 0 term to taylor expansions

--- src/core/patch.rkt
+++ src/core/patch.rkt
@@ -49,6 +49,10 @@
                 [altn (in-list altns)]
                 [fv (in-list free-vars)]
                 #:when (set-member? fv var)) ; check whether var exists in expr at all
+            (define zero-poly 0)
+            (define gen0 (approx spec-brf (hole (representation-name repr) zero-poly)))
+            (define brf0 (batch-add! global-batch gen0))
+            (sow (alt brf0 `(taylor ,name ,var) (list altn)))
             (for ([i (in-range (*taylor-order-limit*))])
               (define gen (approx spec-brf (hole (representation-name repr) (genexpr))))
               (define brf (batch-add! global-batch gen)) ; Munge gen
