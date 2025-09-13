fa8b9319e1147039907c7ff74fede2772b32053f
Jonas Regehr <jo.sc.re.04@gmail.com>
2025-09-12 21:30:57 -0600

    Add scratch.rkt

---
+++ growlibm/scratch.rkt
@@ -0,0 +1,32 @@
+#lang racket
+
+(require
+  "../src/api/sandbox.rkt"
+  "../src/syntax/types.rkt"
+  "../src/core/points.rkt"
+  "../src/core/rules.rkt"
+  "../src/config.rkt"
+  "../src/core/batch.rkt"
+  "../src/core/egg-herbie.rkt"
+  "../src/syntax/read.rkt"
+  "../src/syntax/load-platform.rkt"
+  "../src/syntax/types.rkt"
+  "../src/core/points.rkt"
+  "../src/core/rules.rkt"
+  "../src/config.rkt"
+  "../src/core/batch.rkt"
+  "../src/core/egg-herbie.rkt"
+  "../src/syntax/read.rkt"
+  "../src/syntax/load-platform.rkt"
+  "../src/syntax/platform.rkt"
+  "../src/syntax/sugar.rkt"
+  "../src/core/programs.rkt"
+  "../src/syntax/syntax.rkt"
+  "../src/reports/common.rkt"
+  "../src/syntax/platform-language.rkt")
+
+(activate-platform! "grow")
+
+(define expr '(*.f64 (cos.f64 (*.f64 #s(literal -6.2831854820251465 binary64) z0)) (sqrt.f64 z1)))
+(define cost-proc (platform-cost-proc (*active-platform*)))
+(displayln (cost-proc expr (get-representation 'binary64)))
