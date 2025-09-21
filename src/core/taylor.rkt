5eaeb7ae9f63e0a7f6ec0cdc92caac6089b36e84
Aditya Kumar <obround.dev@gmail.com>
2025-09-21 03:50:56 -0700

    Series - emit only final candidates to global batch

--- src/core/taylor.rkt
+++ src/core/taylor.rkt
@@ -11,16 +11,17 @@
          taylor-coefficients)
 
 (define (taylor-coefficients batch brfs vars transforms-to-try)
-  (define expand (expand-taylor! batch))
-  (define exprs (batch-exprs batch))
-  (define munge (λ (x) (batch-add! batch x)))
+  (define-values (scratch-batch scratch-brfs) (batch-copy-only batch brfs))
+  (define expand (expand-taylor! scratch-batch))
+  (define exprs (batch-exprs scratch-batch))
+  (define munge (λ (x) (batch-add! scratch-batch x)))
   (define taylor-coeffs
     (for*/list ([var (in-list vars)]
-                #:do [(define taylor* (taylor var batch))]
+                #:do [(define taylor* (taylor var scratch-batch))]
                 [transform-type transforms-to-try])
       (match-define (list name f finv) transform-type)
-      (define replace (batch-replace-expression! batch var (f var)))
-      (for/list ([brf (in-list brfs)])
+      (define replace (batch-replace-expression! scratch-batch var (f var)))
+      (for/list ([brf (in-list scratch-brfs)])
         (taylor* (expand (munge (reduce (exprs (replace brf)))))))))
   taylor-coeffs)
 
