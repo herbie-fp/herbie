59b140935464a3e7c91a90c1a45f866fd758e89c
Jonas Regehr <jo.sc.re.04@gmail.com>
2025-10-27 01:11:00 -0600

    Run on helmholtz

--- infra/nightly.sh
+++ infra/nightly.sh
@@ -8,7 +8,7 @@ export PATH="$PATH:$HOME/.cargo/bin/"
 
 # Seed is fixed for the whole day; this way two branches run the same seed
 SEED=$(date "+%Y%j")
-BENCHDIR="bench/"
+BENCHDIR="bench/growlibm-helmholtz.fpcore"
 REPORTDIR="reports"
 NUMITERS=10
 
