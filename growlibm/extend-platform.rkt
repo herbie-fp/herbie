810e84267cfa94192c0fd118588e9329d0527c26
Jonas Regehr <jo.sc.re.04@gmail.com>
2025-11-09 18:10:32 -0700

    delete unused imports

--- growlibm/extend-platform.rkt
+++ growlibm/extend-platform.rkt
@@ -1,30 +1,11 @@
 #lang racket
 (require json)
 (require
-  "../src/api/sandbox.rkt"
-  "../src/syntax/types.rkt"
-  "../src/core/points.rkt"
-  "../src/core/rules.rkt"
-  "../src/config.rkt"
-  "../src/core/batch.rkt"
-  "../src/core/egg-herbie.rkt"
-  "../src/syntax/read.rkt"
   "../src/syntax/load-platform.rkt"
-  "../src/syntax/types.rkt"
-  "../src/core/points.rkt"
-  "../src/core/rules.rkt"
-  "../src/config.rkt"
-  "../src/core/batch.rkt"
-  "../src/core/egg-herbie.rkt"
-  "../src/syntax/read.rkt"
   "../src/syntax/load-platform.rkt"
-  "../src/syntax/platform.rkt"
   "../src/syntax/sugar.rkt"
   "../src/core/programs.rkt"
-  "../src/syntax/syntax.rkt"
-  "../src/reports/common.rkt"
   "../src/syntax/platform-language.rkt")
-
 
 (activate-platform! "grow")
 
