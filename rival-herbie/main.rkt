c259d4abd4899fa447b23d4d1e2a08c2463fecad
Aditya Kumar <obround.dev@gmail.com>
2025-11-28 16:10:06 -0800

    finish merge????

--- rival-herbie/main.rkt
+++ rival-herbie/main.rkt
@@ -91,10 +91,9 @@
 
 ;; Apply discretization-convert to all outputs (used by both rival-apply and rival-apply-batch)
 (define (apply-discretizations outputs discs)
-  (list->vector
-   (for/list ([output (in-vector outputs)]
-              [disc (in-list discs)])
-     ((discretization-convert disc) output))))
+  (list->vector (for/list ([output (in-vector outputs)]
+                           [disc (in-list discs)])
+                  ((discretization-convert disc) output))))
 
 (define (rival-apply machine pt [hints #f] [max-iterations (*rival-max-iterations*)])
   (define n-args (vector-length pt))
@@ -125,14 +124,12 @@
                  max-iterations))
 
   (match res-code
-    [(== STATUS-SUCCESS)
-     (apply-discretizations outs (machine-wrapper-discs machine))]
+    [(== STATUS-SUCCESS) (apply-discretizations outs (machine-wrapper-discs machine))]
     [(== STATUS-INVALID-INPUT)
      (raise (exn:rival:invalid "Invalid input" (current-continuation-marks) pt))]
     [(== STATUS-UNSAMPLABLE)
      (raise (exn:rival:unsamplable "Unsamplable input" (current-continuation-marks) pt))]
-    [(== STATUS-PANIC)
-     (error 'rival-apply "Rival panic")]
+    [(== STATUS-PANIC) (error 'rival-apply "Rival panic")]
     [else (error 'rival-apply "Unknown result code: ~a" res-code)]))
 
 (define (rival-apply-batch machine pts hints-list [max-iterations (*rival-max-iterations*)])
@@ -156,12 +153,16 @@
   ;; Prepare hints (flattened into single bytes buffer)
   (define hints-bytes-list
     (for/list ([h (in-list hints-list)])
-      (if h (serialize-hints-binary h) #"")))
+      (if h
+          (serialize-hints-binary h)
+          #"")))
   (define total-hints-len (apply + (map bytes-length hints-bytes-list)))
   (define hints-data (make-bytes total-hints-len))
   (define hints-offsets (make-vector batch-size))
   (define hints-lens (make-vector batch-size))
-  (let loop ([i 0] [offset 0] [hbs hints-bytes-list])
+  (let loop ([i 0]
+             [offset 0]
+             [hbs hints-bytes-list])
     (unless (null? hbs)
       (define hb (car hbs))
       (define len (bytes-length hb))
@@ -204,10 +205,10 @@
            (define disc (list-ref discs j))
            ;; Apply discretization convert
            (cond
-             [(= (discretization-type disc) DISC-TYPE-BOOL) ; Bool: convert f64 to boolean
-              (not (zero? raw-val))]
-             [else ; F32/F64: return f64 value as-is
-              raw-val])))
+             ; Bool: convert f64 to boolean
+             [(= (discretization-type disc) DISC-TYPE-BOOL) (not (zero? raw-val))]
+             ; F32/F64: return f64 value as-is
+             [else raw-val])))
        (cons 'valid outs)]
       [(== STATUS-INVALID-INPUT) (cons 'invalid #f)]
       [(== STATUS-UNSAMPLABLE) (cons 'exit #f)]
@@ -315,139 +316,3 @@
                              (execution name number precision time memory iteration)]))
                         res))]
     [_ res]))
-
-(define (rival-apply machine pt [hints #f] [max-iterations (*rival-max-iterations*)])
-  (define n-args (vector-length pt))
-  (define arg-ptrs (machine-wrapper-arg-buf machine))
-  (for ([i (in-range n-args)]
-        [bf (in-vector pt)])
-    (ptr-set! arg-ptrs _pointer i bf))
-
-  (define n-outs (length (machine-wrapper-discs machine)))
-  (define outs (build-vector n-outs (lambda (_) (bf 0.0))))
-  (define out-ptrs (machine-wrapper-out-buf machine))
-  (for ([i (in-range n-outs)]
-        [bf (in-vector outs)])
-    (ptr-set! out-ptrs _pointer i bf))
-
-  (define hints-bytes
-    (if hints
-        (serialize-hints-binary hints)
-        #""))
-  (define res-code
-    (rival_apply (machine-wrapper-ptr machine)
-                 arg-ptrs
-                 n-args
-                 out-ptrs
-                 n-outs
-                 hints-bytes
-                 (bytes-length hints-bytes)
-                 max-iterations))
-
-  (match res-code
-    [0
-     (define discs (machine-wrapper-discs machine))
-     (list->vector (for/list ([bf (in-vector outs)]
-                              [disc (in-list discs)])
-                     ((discretization-convert disc) bf)))]
-    [-1 (raise (exn:rival:invalid "Invalid input" (current-continuation-marks) pt))]
-    [-2 (raise (exn:rival:unsamplable "Unsamplable input" (current-continuation-marks) pt))]
-    [-99 (error 'rival-apply "Rival panic")]
-    [else (error 'rival-apply "Unknown result code: ~a" res-code)]))
-
-(define (rival-apply-batch machine pts [hints #f] [max-iterations (*rival-max-iterations*)])
-  (define n-points (length pts))
-  (if (zero? n-points)
-      '()
-      (let ()
-        (define pt0 (first pts))
-        (define n-args (vector-length pt0))
-        (define n-outs (length (machine-wrapper-discs machine)))
-
-        (define args-ptrs (malloc _pointer (* n-points n-args) 'raw))
-        (define out-ptrs (malloc _pointer (* n-points n-outs) 'raw))
-        (define hints-ptrs (malloc _pointer n-points 'raw))
-        (define hints-lens (malloc _size n-points 'raw))
-        (define statuses (malloc _int32 n-points 'raw))
-
-        (for ([i (in-naturals)]
-              [pt (in-list pts)])
-          (for ([j (in-naturals)]
-                [bf (in-vector pt)])
-            (ptr-set! args-ptrs _pointer (+ (* i n-args) j) bf)))
-
-        (define all-outs
-          (for/list ([i (in-range n-points)])
-            (build-vector n-outs (lambda (_) (bf 0.0)))))
-
-        (for ([i (in-naturals)]
-              [outs (in-list all-outs)])
-          (for ([j (in-naturals)]
-                [bf (in-vector outs)])
-            (ptr-set! out-ptrs _pointer (+ (* i n-outs) j) bf)))
-
-        (define hints-raw-buf #f)
-
-        (if hints
-            (let ()
-               (define hbs (for/list ([h (in-list hints)]) (if h (serialize-hints-binary h) #"")))
-               (define total-len (apply + (map bytes-length hbs)))
-               (when (> total-len 0)
-                 (set! hints-raw-buf (malloc _byte total-len 'raw)))
-               
-               (let loop ([offset 0] [hbs hbs] [i 0])
-                 (unless (null? hbs)
-                   (define hb (car hbs))
-                   (define len (bytes-length hb))
-                   (if (or (zero? len) (not hints-raw-buf))
-                       (begin
-                         (ptr-set! hints-ptrs _pointer i #f)
-                         (ptr-set! hints-lens _size i 0)
-                         (loop offset (cdr hbs) (add1 i)))
-                       (let ()
-                         (define ptr (ptr-add hints-raw-buf offset))
-                         (memcpy ptr hb len)
-                         (ptr-set! hints-ptrs _pointer i ptr)
-                         (ptr-set! hints-lens _size i len)
-                         (loop (+ offset len) (cdr hbs) (add1 i)))))))
-            (for ([i (in-range n-points)])
-              (ptr-set! hints-ptrs _pointer i #f)
-              (ptr-set! hints-lens _size i 0)))
-
-        (rival_apply_batch (machine-wrapper-ptr machine)
-                           args-ptrs
-                           n-points
-                           n-args
-                           out-ptrs
-                           n-outs
-                           hints-ptrs
-                           hints-lens
-                           statuses
-                           max-iterations)
-
-        (define results
-          (for/list ([i (in-range n-points)]
-                     [outs (in-list all-outs)])
-            (define status (ptr-ref statuses _int32 i))
-            (match status
-              [0
-               (define discs (machine-wrapper-discs machine))
-               (list->vector (for/list ([bf (in-vector outs)]
-                                        [disc (in-list discs)])
-                               ((discretization-convert disc) bf)))]
-              [-1 (exn:rival:invalid "Invalid input" (current-continuation-marks) (list-ref pts i))]
-              [-2
-               (exn:rival:unsamplable "Unsamplable input"
-                                      (current-continuation-marks)
-                                      (list-ref pts i))]
-              [-99 (error 'rival-apply-batch "Rival panic")]
-              [else (error 'rival-apply-batch "Unknown result code: ~a" status)])))
-
-        (when hints-raw-buf (free hints-raw-buf))
-        (free args-ptrs)
-        (free out-ptrs)
-        (free hints-ptrs)
-        (free hints-lens)
-        (free statuses)
-
-        results)))
