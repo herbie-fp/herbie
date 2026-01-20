2582405d64902baf412cf380c63a8b1ffe5e66ed
Jonas Regehr <jo.sc.re.04@gmail.com>
2026-01-20 15:27:41 -0700

    helmholtz

--- infra/nightly.sh
+++ infra/nightly.sh
@@ -11,7 +11,7 @@ make install
 
 # Seed is fixed for the whole day; this way two branches run the same seed
 SEED=$(date "+%Y%j")
-BENCHDIR="bench/proj/"
+BENCHDIR="bench/helmholtz.fpcore"
 REPORTDIR="reports"
 NUMITERS=10
 
