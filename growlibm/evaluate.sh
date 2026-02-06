8be753bcd8f8e88daa9d59d2e7f9748e824e7eda
Jonas Regehr <jo.sc.re.04@gmail.com>
2026-02-06 16:32:20 -0700

    proj

--- growlibm/evaluate.sh
+++ growlibm/evaluate.sh
@@ -11,7 +11,7 @@ make install
 
 # Seed is fixed for the whole day; this way two branches run the same seed
 SEED=$(date "+%Y%j")
-BENCHDIR="bench/keplerian.fpcore"
+BENCHDIR="bench/PROJ/"
 REPORTDIR="reports"
 
 mkdir -p "$REPORTDIR"
