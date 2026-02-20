0e3729054b9e390ddf1368ea0929a47376b5496e
Jonas Regehr <jo.sc.re.04@gmail.com>
2026-02-20 16:39:39 -0700

    Sample with seed

--- growlibm/timing/sample.rkt
+++ growlibm/timing/sample.rkt
@@ -1,6 +1,7 @@
 #lang racket
 
 (require "../../src/api/sandbox.rkt"
+         "../../src/config.rkt"
          "../../src/syntax/read.rkt"
          "../../src/syntax/types.rkt"
          "../../src/core/points.rkt"
@@ -12,7 +13,12 @@
 
 (activate-platform! "growlibm")
 
-(define test (load-tests (open-input-string (vector-ref (current-command-line-arguments) 0))))
+(define args (current-command-line-arguments))
+(define test (load-tests (open-input-string (vector-ref args 0))))
+(when (> (vector-length args) 1)
+  (define seed (string->number (vector-ref args 1)))
+  (when seed
+    (set-seed! seed)))
 
 (define (sample-test t)
   (*context* (test-context t))
