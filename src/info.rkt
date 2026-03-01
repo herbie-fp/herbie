7b79d6ddd60ab51936ca26a44a1670698b360d40
Aditya Kumar <obround.dev@gmail.com>
2026-03-01 02:10:31 -0800

    Merge conflict mess up

--- src/info.rkt
+++ src/info.rkt
@@ -22,7 +22,7 @@
                              "rackunit-lib"
                              "web-server-lib"
                              ("egg-herbie" #:version "2.2")
-                             ("rival-herbie" #:version "2.3")
+                             ("rival-herbie" #:version "1.0")
                              ("fpbench" #:version "2.0.3")
                              "fmt"))
 
