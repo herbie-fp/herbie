fac9b677dec6638a2f4c1d66e5e1d030e1ede86b
Pavel Panchekha <me@pavpanchekha.com>
2026-04-20 10:51:59 -0600

    Add sincos FFI ops to the C platform

--- src/syntax/test-syntax.rkt
+++ src/syntax/test-syntax.rkt
@@ -15,6 +15,9 @@
   (define sin-proc (impl-info 'sin.f64 'fl))
   (check-equal? (sin-proc 0.0) 0.0 "sin(0) = 0")
 
+  (define sincos-proc (impl-info 'sincos.f64 'fl))
+  (check-equal? (sincos-proc 0.0) #(0.0 1.0) "sincos(0) = [0, 1]")
+
   ; get-fpcore-impl
   (define (get-impl op props itypes)
     (get-fpcore-impl op props itypes))
@@ -23,5 +26,6 @@
   (check-equal? (get-impl '+ '((:precision . binary64) (:description . "test")) (list f64 f64))
                 '+.f64)
   (check-equal? (get-impl 'sin '((:precision . binary64)) (list f64)) 'sin.f64)
+  (check-equal? (get-impl 'sincos '((:precision . binary64)) (list f64)) 'sincos.f64)
 
   (void))
