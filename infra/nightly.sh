e4853f579af2950df843120653cc8e45253a5b6c
Jonas Regehr <jo.sc.re.04@gmail.com>
2026-04-22 12:13:08 -0600

    shrink diff

---
+++ infra/nightly.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# exit immediately upon first error, log every command executed
+set -e -x
+
+# Ensure egglog is in the path
+export PATH="$HOME/.cargo/bin/:$PATH"
+rustup update
+
+# Keep nightly installs isolated and consistent across install/run steps.
+export PLTADDONDIR="${PLTADDONDIR:-pltlibs}"
+make install
+
+# Seed is fixed for the whole day; this way two branches run the same seed
+SEED=$(date "+%Y%j")
+BRANCH=$(git rev-parse --abbrev-ref HEAD)
+
+BENCHDIR="$1"; shift
+REPORTDIR="$1"; shift
+
+if [[ "$BRANCH" == egglog-* ]]; then
+  set -- "$@" --enable generate:egglog
+fi
+
+if [[ "$BRANCH" == rival2-* || "$BRANCH" == "rival2" ]]; then
+  set -- "$@" --enable setup:rival2
+fi
+
+mkdir -p "$REPORTDIR"
+rm -rf "reports"/* || echo "nothing to delete"
+
+# run
+dirs=""
+for bench in "$BENCHDIR"/*; do
+  name=$(basename "$bench" .fpcore)
+  rm -rf "$REPORTDIR"/"$name"
+
+  racket -y "src/main.rkt" report \
+         --seed "$SEED" \
+         "$@" \
+         "$bench" "$REPORTDIR"/"$name"
+  
+  dirs="$dirs $name";
+done
+
+# merge reports
+racket -y infra/merge.rkt "$REPORTDIR" $dirs
