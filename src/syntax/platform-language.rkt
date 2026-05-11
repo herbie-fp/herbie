0d84754012a5b1a694cc21cdaf5026ad08ad85b3
Jonas Regehr <jo.sc.re.04@gmail.com>
2026-05-11 13:17:32 -0600

    shrink diff

--- src/syntax/platform-language.rkt
+++ src/syntax/platform-language.rkt
@@ -16,8 +16,6 @@
          fpcore-context
          if-impl
          if-cost
-         create-operator-impl!
-         platform-register-implementation!
          (rename-out [platform-module-begin #%module-begin])
          (except-out (all-from-out racket) #%module-begin)
          (all-from-out "platform.rkt")
