d80d11cc98cb171460936925540195e424852249
Pavel Panchekha <me@pavpanchekha.com>
2026-05-11 16:13:05 -0600

    More signature

--- src/core/alt-table.rkt
+++ src/core/alt-table.rkt
@@ -17,7 +17,7 @@
           (atab-active-alts (alt-table? . -> . (listof alt?)))
           (atab-all-alts (alt-table? . -> . (listof alt?)))
           (atab-not-done-alts (alt-table? . -> . (listof alt?)))
-          (atab-eval-altns (alt-table? batch? (listof alt?) context? . -> . (values any/c any/c)))
+          (atab-eval-altns (alt-table? batch? (listof alt?) . -> . (values any/c any/c)))
           (atab-add-altns (alt-table? (listof alt?) any/c any/c . -> . alt-table?))
           (atab-set-picked (alt-table? (listof alt?) . -> . alt-table?))
           (atab-completed? (alt-table? . -> . boolean?))
