504af8d3f78dca703843e59900f67a96466a4395
Brant Qian <siyuanq4@illinois.edu>
2026-08-02 03:29:33 -0500

    Add a structural tuple representation
    Type identity is the ordered sequence of slot representations, so slots
    may have different types; contrast array-representation, whose identity
    is (element type, length). Like array types, tuple types are synthesized
    on demand by get-representation rather than registered by a platform, so
    their names round-trip.
    
    The two representations stay separate: tuples get their own struct, their
    own constructor and their own clause in repr->prop, beside the array ones
    rather than merged with them.

--- src/syntax/types.rkt
+++ src/syntax/types.rkt
@@ -8,6 +8,7 @@
 
 (provide (struct-out representation)
          (struct-out array-representation)
+         (struct-out tuple-representation)
          repr->prop
          array-representation-base
          array-representation-shape
@@ -22,7 +23,8 @@
          context-lookup
          contexts-union
          make-representation
-         make-array-representation)
+         make-array-representation
+         make-tuple-representation)
 
 ;; Representations
 
@@ -34,6 +36,8 @@
      (fprintf port "#<representation ~a>" (representation-name repr)))])
 
 (struct array-representation representation (elem len) #:transparent)
+
+(struct tuple-representation representation (slots) #:transparent)
 
 (define (array-representation-base repr)
   (if (array-representation? repr)
@@ -50,6 +54,7 @@
 (define (repr->prop repr)
   (match repr
     [(? array-representation?) (repr->prop (array-representation-elem repr))]
+    [(? tuple-representation?) (repr->prop (first (tuple-representation-slots repr)))]
     [(? representation?)
      (match (representation-type repr)
        ['bool '()]
@@ -73,6 +78,15 @@
   ;; These should not be called for arrays; we'll clean up the hierarchy later.
   (define total-bits (* len (representation-total-bits elem-repr)))
   (array-representation name array-ty void void void void total-bits void elem-repr len))
+
+(define (make-tuple-representation #:slots slots)
+  (unless (and (list? slots) (pair? slots) (andmap representation? slots))
+    (raise-herbie-error "Tuples require a non-empty list of representations, got ~a" slots))
+  (define tuple-ty `(tuple ,@(map representation-type slots)))
+  (define name `(tuple ,@(map representation-name slots)))
+  ;; TODO: like arrays, tuples inherit unused scalar conversion slots.
+  (define total-bits (apply + (map representation-total-bits slots)))
+  (tuple-representation name tuple-ty void void void void total-bits void slots))
 
 (module hairy racket/base
   (require (only-in math/private/bigfloat/mpfr get-mpfr-fun _mpfr-pointer _rnd_t bf-rounding-mode))
