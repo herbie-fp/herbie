97d0543359c85960b60b9a6682ff55308f9b6f28
Pavel Panchekha <me@pavpanchekha.com>
2026-08-06 22:10:49 -0700

    Preserve approximation batch references during spec conversion

--- src/syntax/platform.rkt
+++ src/syntax/platform.rkt
@@ -110,12 +110,6 @@
      (pattern-substitute spec env)]))
 
 (define (batch-to-spec! in-batch out-batch brfs)
-  (define copy-spec-to-output (batch-copy-only! out-batch (if (empty? brfs) out-batch in-batch)))
-  (define (output-spec spec)
-    (cond
-      [(equal? (batchref-batch spec) out-batch) spec]
-      [(equal? (batchref-batch spec) in-batch) (copy-spec-to-output spec)]
-      [else (error 'batch-to-spec! "spec reference does not belong to the input or output batch")]))
   (define lower
     (batch-recurse
      in-batch
@@ -125,7 +119,7 @@
          [(? literal?) (batch-add! out-batch (literal-value node))]
          [(? number?) (error 'batch-to-spec! "unexpected spec node in input batch: ~a" node)]
          [(? symbol?) (batch-add! out-batch node)]
-         [(approx spec _) (output-spec spec)]
+         [(approx spec _) spec]
          [(list (? impl-exists? impl) args ...)
           (define vars (impl-info impl 'vars))
           (define spec (impl-info impl 'spec))
